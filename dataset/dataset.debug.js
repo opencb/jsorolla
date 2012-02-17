508a2cabee2e1ea0e04456d278968dcfab35f337
rsanchez <rsanchez@cipf.es>
2012-02-17 19:30:40 +0100

    changes in minimized files

--- dataset/dataset.debug.js
+++ dataset/dataset.debug.js
@@ -1506,18 +1506,18 @@ GraphItem.prototype.setName = function(name){
 
 
 
-function GeneFeatureDataSet(){
-	FeatureDataAdapter.prototype.constructor.call(this);
-};
-
-GeneFeatureDataSet.prototype = 			    FeatureDataAdapter;
-GeneFeatureDataSet.prototype.constructor=   FeatureDataAdapter;
-GeneFeatureDataSet.prototype.loadFromJSON = FeatureDataAdapter.prototype.loadFromJSON;
-GeneFeatureDataSet.prototype.loadFromFile = FeatureDataAdapter.prototype.loadFromFile;
-GeneFeatureDataSet.prototype.loadFromURL  = FeatureDataAdapter.prototype.loadFromURL;
-GeneFeatureDataSet.prototype.toJSON  = 	    FeatureDataAdapter.prototype.toJSON;
-GeneFeatureDataSet.prototype.validate  =    FeatureDataAdapter.prototype.validate;
-
+//function GeneFeatureDataSet(){
+//	FeatureDataAdapter.prototype.constructor.call(this);
+//};
+//
+//GeneFeatureDataSet.prototype = 			    FeatureDataAdapter;
+//GeneFeatureDataSet.prototype.constructor=   FeatureDataAdapter;
+//GeneFeatureDataSet.prototype.loadFromJSON = FeatureDataAdapter.prototype.loadFromJSON;
+//GeneFeatureDataSet.prototype.loadFromFile = FeatureDataAdapter.prototype.loadFromFile;
+//GeneFeatureDataSet.prototype.loadFromURL  = FeatureDataAdapter.prototype.loadFromURL;
+//GeneFeatureDataSet.prototype.toJSON  = 	    FeatureDataAdapter.prototype.toJSON;
+//GeneFeatureDataSet.prototype.validate  =    FeatureDataAdapter.prototype.validate;
+//
 
 function ExpressionMatrixDataSet(){
 	DataSet.prototype.constructor.call(this);
