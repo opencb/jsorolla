4fd568a593d7ef9f2108139d9841ed8df73950fe
Francisco Salavert Torres <frasator@gmail.com>
2012-04-26 15:27:43 +0200

    cloud genomic

--- dqs/dqs-data-adapter.js
+++ dqs/dqs-data-adapter.js
@@ -36,51 +36,75 @@ DqsDataAdapter.prototype._getHashMapKey = function(chromosome, start, end){
 	return chromosome + "-" + chunkNum;
 };
 DqsDataAdapter.prototype.isRegionAvalaible = function(chromosome, start, end){
-	var chunkStart = start/this.chunkSize;
-	var chunkEnd = parseInt(end/this.chunkSize);
-	for ( var i = chunkStart; i <= chunkEnd; i++) {
-		var key = chromosome+"-"+i;
-		this.datasets[key]= new Object();
-		this.datasets[key]["data"]= new Array();
-	}
-	
 	return this.datasets[this._getHashMapKey(chromosome, start, end)] != null;
 };
 
 
-DqsDataAdapter.prototype.fill = function(chromosome, start, end, resource){
+//DqsDataAdapter.prototype.fill = function(chromosome, start, end, resource){
+//	var _this = this;
+//	if ((chromosome == null)|| (start == null) || (end == null) || (resource == null)){
+//		throw "Missing value in a not optional parameter: chromosome, start or end";
+//	}
+//	
+//	var chunkStartId = Math.floor(start/this.chunkSize);
+//	var chunkEndId = Math.floor(end/this.chunkSize);
+//	var chunkStart = chunkStartId*this.chunkSize;
+//	var chunkEnd = (Math.ceil(end/this.chunkSize)*this.chunkSize)-1;
+//	
+//	
+//	
+////	console.log("primer fill "+ chunkStart +"--"+ chunkEnd)
+//	
+////	var startWs=chunkStart;
+////	var endWs=chunkEnd;
+////	for ( var i = chunkStartId; i <= chunkEndId; i++) {
+////		var key = chromosome+"-"+i;
+////		if(this.datasets[key]!=null){
+////			//si lo tiene
+////			startWs = startWs+this.chunkSize();
+////		}
+////	}
+//	this.dqsManager = new DqsManager();
+//	this.dqsManager.onBamRegion.addEventListener(function (evt, data){
+//			_this.getFinished(data, chromosome, chunkStart, chunkEnd);
+//	});
+//	var key = chromosome+"-"+chunkStart;
+//	if(this.datasets[key]==null){
+//		this.dqsManager.bamRegion(resource, chromosome+":"+chunkStart+"-"+chunkEnd);
+//	}
+//};
+
+
+DqsDataAdapter.prototype.fill = function(chromosome, start, end, resource, callbackFunction){
 	var _this = this;
-	if ((chromosome == null)|| (start == null) || (end == null) || (resource == null)){
+	this.resource = resource;
+	if ((chromosome == null)|| (start == null) || (end == null)){
 		throw "Missing value in a not optional parameter: chromosome, start or end";
 	}
-	
 	var chunkStartId = Math.floor(start/this.chunkSize);
 	var chunkEndId = Math.floor(end/this.chunkSize);
 	var chunkStart = chunkStartId*this.chunkSize;
 	var chunkEnd = (Math.ceil(end/this.chunkSize)*this.chunkSize)-1;
 	
-	
-	
-//	console.log("primer fill "+ chunkStart +"--"+ chunkEnd)
-	
-//	var startWs=chunkStart;
-//	var endWs=chunkEnd;
-//	for ( var i = chunkStartId; i <= chunkEndId; i++) {
-//		var key = chromosome+"-"+i;
-//		if(this.datasets[key]!=null){
-//			//si lo tiene
-//			startWs = startWs+this.chunkSize();
-//		}
-//	}
-	
-	
-	
-	this.dqsManager = new DqsManager();
-	this.dqsManager.onBamRegion.addEventListener(function (evt, data){
-			_this.getFinished(data, chromosome, chunkStart, chunkEnd);
-	});
-	this.dqsManager.bamRegion(resource, chromosome+":"+chunkStart+"-"+chunkEnd);
+		if (!this.isRegionAvalaible(chromosome, chunkStart, chunkEnd)){
+
+				this.dqsManager = new DqsManager();
+				this.dqsManager.onBamRegion.addEventListener(function (evt, data){
+					if (!_this.lockSuccessEventNotify){
+						_this.getFinished(data, chromosome, chunkStart, chunkEnd);
+					}
+					else{
+						_this.anticipateRegionRetrieved(data, chromosome, chunkStart, chunkEnd);
+					}
+				});
+				this.dqsManager.bamRegion(resource, chromosome+":"+chunkStart+"-"+chunkEnd);
+		}else{
+//				console.log("No need to go to the server: " + chromosome + ":" + start + "-" + end);
+				this.lockSuccessEventNotify = false;
+//				return this.datasets[this._getHashMapKey(chromosome, start, end)];
+		}
 };
+
 
 DqsDataAdapter.prototype.getFinished = function(data, chromosome, start, end){
 	da = this.datasets;
@@ -95,16 +119,20 @@ DqsDataAdapter.prototype.getFinished = function(data, chromosome, start, end){
 			this.datasets[key]["data"] = new Array();
 		}
 	}
-	
 	for ( var i = 0; i < data.length; i++) {
 		var item = data[i];
 		var key = chromosome+"-"+Math.floor(item.start/this.chunkSize);
-			this.datasets[key]["data"].push(item);
+		if(this.datasets[key] == null){
+			this.datasets[key] = new Object();
+		}
+		if(this.datasets[key]["data"]==null){
+			this.datasets[key]["data"] = new Array();
+		}
+		this.datasets[key]["data"].push(item);
 	}
 	
 	this.dataset.loadFromJSON(data);
 //	this.datasets[this._getHashMapKey(chromosome, start, end)] = this.dataset;
-	console.log(this.datasets);
 	this.successed.notify(data);
 };
 
@@ -124,7 +152,13 @@ DqsDataAdapter.prototype.anticipateRegionRetrieved = function(data, chromosome, 
 	for ( var i = 0; i < data.length; i++) {
 		var item = data[i];
 		var key = chromosome+"-"+Math.floor(item.start/this.chunkSize);
-			this.datasets[key]["data"].push(item);
+		if(this.datasets[key] == null){
+			this.datasets[key] = new Object();
+		}
+		if(this.datasets[key]["data"]==null){
+			this.datasets[key]["data"] = new Array();
+		}
+		this.datasets[key]["data"].push(item);
 	}
 	
 	this.dataset.loadFromJSON(data);
@@ -155,30 +189,75 @@ DqsDataAdapter.prototype.getFeaturesByPosition = function(position){
 };
 
 
-DqsDataAdapter.prototype.setIntervalView = function(chromosome,  middleView){
-		
-//	var actualChunk = Math.floor(middleView/this.chunkSize);
-////	var prevChunk = actualChunk-1;
-////	var nextChunk = actualChunk+1;
-//
-//	var start = prevChunk*this.chunkSize;
-//	var end = (nextChunk+1)*this.chunkSize-1;
-//
-//	
-//	if(this.datasets[chromosome+"-"+prevChunk]!=null){
-//		var start = prevChunk*this.chunkSize;
-//	}
-//	
-//	if(this.datasets[chromosome+"-"+actualChunk]){
-//		
-//	}
-//	
-//	this.fill(chromosome, start, end, this.resource);
-//
-//	console.log(start+" - "+end);
+//DqsDataAdapter.prototype.setIntervalView = function(chromosome,  middleView, obj){
+//		console.log(obj.width);
+//		console.log(obj.pixelRatio);
+//		var windowSize = obj.width/obj.pixelRatio;
+//		var data_start = Math.ceil(middleView - windowSize);
+//		var data_end = Math.ceil(middleView +   windowSize);
+////		this.fill(chromosome, data_start, data_end, this.resource);
+//		console.log(data_start+"-"+data_end);
+//};
 
+
+DqsDataAdapter.prototype.setIntervalView = function(chromosome,  middleView, obj){
+	var windowSize = obj.width/obj.pixelRatio;
+	var data_start = Math.ceil(middleView - windowSize);
+	var data_end = Math.ceil(middleView + windowSize);
+	
+	var chunkStartId = Math.floor(data_start/this.chunkSize);
+	var chunkEndId = Math.floor(data_end/this.chunkSize);
+	var chunkStart = chunkStartId*this.chunkSize;
+	var chunkEnd = (Math.ceil(data_end/this.chunkSize)*this.chunkSize)-1;
+	
+	chunkActualId = Math.floor(middleView/this.chunkSize);
+	
+	
+	//XXX no va
+	for ( var i = chunkStartId; i <= chunkEndId; i++) {
+		var key = chromosome+"-"+i;
+		if(this.datasets[key]==null){
+			this.lockSuccessEventNotify = true;
+			this.fill(chromosome, end, newEnd , this.resource);
+			//si lo tiene
+		}
+	}
+	
+	console.log(data_start+"-"+data_end);
+	
+	
+	if (this.autoFill && !this.lockSuccessEventNotify){
+		for ( var iterable_element in this.datasets) {
+			var id = (iterable_element.split("-"));
+			var chromosome = id[0];
+			var start = id[1];
+			var end = id[2];
+			
+			if ((start < middleView) && (middleView < end)){
+				var tier = (end - start)/3;
+				
+				if (end - middleView <= tier){
+					this.lockSuccessEventNotify = true;
+					var newEnd = parseInt(end) + parseInt(end -start);
+					this.fill(chromosome, end, newEnd , this.resource);
+					return;
+				}
+				
+				if(middleView - start <= tier){
+					this.lockSuccessEventNotify = true;
+					var newstart = parseInt(start) - parseInt(end -start);
+					if (newstart < 0){ newstart = 1;}
+					this.fill(chromosome, newstart, start, this.resource);
+					return;
+					
+				}
+				
+			}
+			
+			
+		}
+	}
 };
-
 
 //DqsDataAdapter.prototype.arrayToString = function(array, separator){
 //	var str = new StringBuffer();
