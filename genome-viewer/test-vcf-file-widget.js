4d3e2976ef23451e0159041054a436978b50dfd8
rsanchez <rsanchez@cipf.es>
2012-05-30 13:40:20 +0200

    changes in data adapters

--- genome-viewer/test-vcf-file-widget.js
+++ genome-viewer/test-vcf-file-widget.js
@@ -19,7 +19,6 @@ function TestVCFFileWidget(args){
 TestVCFFileWidget.prototype.getChartItems = function(){
 	return [this.chartWidgetByChromosome.getChart(["features","chromosome"]),
 	        this.chartWidgetQuality.getChart(["features","quality"])];
-//	return [];
 };
 
 
@@ -27,9 +26,9 @@ TestVCFFileWidget.prototype.getChartItems = function(){
 TestVCFFileWidget.prototype.loadFileFromLocal = function(file){
 	console.log(file);
 	var _this = this;
-//	var vcfAdapter = new VCFFileDataAdapter();
 
 	var vcfAdapter = new VCFDataAdapter(new FileDataSource());
+//	var vcfAdapter = new VCFDataAdapter(new UrlDataSource());
 	
 	vcfAdapter.completed.addEventListener(function(sender, data){
 		console.log(data);
@@ -37,69 +36,5 @@ TestVCFFileWidget.prototype.loadFileFromLocal = function(file){
 	});
 	
 	vcfAdapter.getData(file);
-	
-	
-//	this._fileLoad(vcfAdapter);
-	
-//	vcfAdapter.onRead.addEventListener(function(sender, fileReader) {
-//		_this.dataAdapter = new VCFLocalRegionDataAdapter();
-//		_this.dataAdapter.loadFromFileReader(fileReader);
-//		
-//		
-//		var datastore = new Array();
-//	 	for ( var chromosome in _this.dataAdapter.featuresByChromosome) {
-//			datastore.push({ features: _this.dataAdapter.featuresByChromosome[chromosome].length, chromosome: chromosome });
-//		}
-//		
-//		var qualityStore = new Array();
-//		for ( var range in _this.dataAdapter.qualitycontrol) {
-//		
-//			qualityStore.push({features: _this.dataAdapter.qualitycontrol[range],  quality:range });
-//		}
-//		
-//	 	_this.chartWidgetByChromosome.getStore().loadData(datastore);
-//	 	_this.chartWidgetQuality.getStore().loadData(qualityStore);
-//		
-//		
-//	 	_this.panel.setLoading(false);
-//	 	_this.featureCountLabel.setText("Features count: " + _this.dataAdapter.features.length, false);
-//	 	_this.btnOk.enable();
-//	 	console.log("VCFLocalRegion "+(TIME2-TIME1)/1000+" seconds");
-//	});
-	
-//	vcfAdapter.loadFromFile(file);
-};
-
-TestVCFFileWidget.prototype._fileLoad = function(vcfAdapter){
-	var _this = this;
-	this.panel.setLoading();
-	vcfAdapter.onRead.addEventListener(function(sender, id) {
-		_this.dataAdapter = new VCFLocalRegionDataAdapter();
-		_this.dataAdapter.loadFromFileDataAdapter(sender);
-		
-		var datastore = new Array();
-	 	for ( var chromosome in _this.dataAdapter.featuresByChromosome) {
-			datastore.push({ features: _this.dataAdapter.featuresByChromosome[chromosome].length, chromosome: chromosome });
-		}
-		
-		var qualityStore = new Array();
-		for ( var range in _this.dataAdapter.qualitycontrol) {
-		
-			qualityStore.push({features: _this.dataAdapter.qualitycontrol[range],  quality:range });
-		}
-		
-	 	_this.chartWidgetByChromosome.getStore().loadData(datastore);
-	 	_this.chartWidgetQuality.getStore().loadData(qualityStore);
-	 	
-	 	
-	 	_this.panel.setLoading(false);
-	 	_this.featureCountLabel.setText("Features count: " + _this.dataAdapter.features.length, false);
-	 	_this.btnOk.enable();
-	});
-};
-
-TestVCFFileWidget.prototype.loadFileFromServer = function(data){
-	var vcfAdapter = new VCFFileDataAdapter();
-	this._fileLoad(vcfAdapter);
-	vcfAdapter.loadFromContent(data.data);
+//	vcfAdapter.getData("http://rsanchez/example.vcf");
 };
--- genome-viewer/url-data-source.js
+++ genome-viewer/url-data-source.js
@@ -17,24 +17,12 @@ UrlDataSource.prototype.fetch = function(url){
 			url : url,
 			async : true,
 			success : function(data, textStatus, jqXHR) {
-//				try{
-					if(data==""){console.log("data is empty");data="[]";}
-					var jsonResponse = JSON.parse(data);
-					_this.success.notify(jsonResponse);
-//				}
-//				catch(e){
-//					console.log("UrlDataSource: data returned was not json: "+url+" :");
-//					console.log(data+" END");
-//				}
-				
+				_this.success.notify(data);
 			},
-//			complete : function() {
-//				
-//			},
 			error : function(jqXHR, textStatus, errorThrown) {
 				console.log("URL Data source: Ajax call returned : "+errorThrown+'\t'+textStatus+'\t'+jqXHR.statusText+" END");
 				_this.error.notify();
-				
+
 			}
 		});
 	}
