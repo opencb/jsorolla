a02c3809f71b3386ba6ec30ef93f7234226f3984
rsanchez <rsanchez@cipf.es>
2012-07-06 11:53:31 +0200

    fixes

--- genome-viewer/track-svg-layout.js
+++ genome-viewer/track-svg-layout.js
@@ -359,7 +359,6 @@ TrackSvgLayout.prototype.addTrack = function(trackData, args){
 		
 		cleanSvgFeatures();
 		setCallRegion();
-		
 		// check if track is visible in this zoom
 		if(_this.zoom >= visibleRange.start && _this.zoom <= visibleRange.end){
 			trackData.retrieveData({chromosome:_this.chromosome,start:virtualStart,end:vitualEnd, histogram:trackSvg.histogram, interval:trackSvg.interval, transcript:trackSvg.transcript});
