6b27add4c4b5d71a49ff183e2c792af35f651745
frasator <frasator@work>
2012-07-10 10:39:13 +0200

    exon codig region fix

---
+++ genome-viewer/gff2-data-adapter.js
@@ -0,0 +1,64 @@
+GFF2DataAdapter.prototype.getData = FeatureDataAdapter.prototype.getData;
+
+function GFF2DataAdapter(dataSource, args){
+	FeatureDataAdapter.prototype.constructor.call(this, dataSource, args);
+	var _this = this;
+	
+	this.async = true;
+
+	if (args != null){
+		if(args.async != null){
+			this.async = args.async;
+		}
+	}
+	
+	if(this.async){
+		this.dataSource.success.addEventListener(function(sender,data){
+			_this.parse(data);
+			_this.onLoad.notify();
+		});
+		this.dataSource.fetch(this.async);
+	}else{
+		var data = this.dataSource.fetch(this.async);
+		this.parse(data);
+	}
+	
+	//stat atributes
+	this.featuresCount = 0;
+	this.featuresByChromosome = {};
+};
+
+GFF2DataAdapter.prototype.parse = function(data){
+	var _this = this;
+	var dataType = "data";
+	var lines = data.split("\n");
+//	console.log("creating objects");
+	for (var i = 0; i < lines.length; i++){
+		var line = lines[i].replace(/^\s+|\s+$/g,"");
+		if ((line != null)&&(line.length > 0)){
+			var fields = line.split("\t");
+			var chromosome = fields[0].replace("chr", "");
+
+			//NAME  SOURCE  TYPE  START  END  SCORE  STRAND  FRAME  GROUP
+			var feature = {
+					"chromosome": chromosome, 
+					"label": fields[2], 
+					"start": parseInt(fields[3]), 
+					"end": parseInt(fields[4]), 
+					"score": parseFloat(fields[5]),
+					"strand": fields[6], 
+					"frame": fields[7],
+					"group": fields[8],
+					"featureType":	"gff2"
+			} ;
+
+			this.featureCache.putFeatures(feature, dataType);
+			
+			if (this.featuresByChromosome[fields[0]] == null){
+				this.featuresByChromosome[fields[0]] = 0;
+			}
+			this.featuresByChromosome[fields[0]]++;
+			this.featuresCount++;
+		}
+	}
+};
---
+++ genome-viewer/gtf-data-adapter.js
@@ -0,0 +1,67 @@
+GTFDataAdapter.prototype.getData = FeatureDataAdapter.prototype.getData;
+
+function GTFDataAdapter(dataSource, args){
+	FeatureDataAdapter.prototype.constructor.call(this, dataSource, args);
+	var _this = this;
+	
+	this.async = true;
+
+	if (args != null){
+		if(args.async != null){
+			this.async = args.async;
+		}
+	}
+	
+	if(this.async){
+		this.dataSource.success.addEventListener(function(sender,data){
+			_this.parse(data);
+			_this.onLoad.notify();
+		});
+		this.dataSource.fetch(this.async);
+	}else{
+		var data = this.dataSource.fetch(this.async);
+		this.parse(data);
+	}
+};
+
+GTFDataAdapter.prototype.parse = function(data){
+	var _this = this;
+	
+	//parse attributes column
+	var getAttr = function(column){
+		var arr = column.split(";");
+		var obj = {};
+		for (var i = 0, li = arr.length; i<li ; i++){
+			var item = arr[i].split("=");
+			obj[item[0]] = item[1];
+		}
+		return obj;
+	};
+	
+	var dataType = "data";
+	var lines = data.split("\n");
+//	console.log("creating objects");
+	for (var i = 0; i < lines.length; i++){
+		var line = lines[i].replace(/^\s+|\s+$/g,"");
+		if ((line != null)&&(line.length > 0)){
+			var fields = line.split("\t");
+			var chromosome = fields[0].replace("chr", "");
+
+			
+			//NAME  SOURCE  TYPE  START  END  SCORE  STRAND  FRAME  GROUP
+			var feature = {
+					"chromosome": chromosome, 
+					"label": fields[2], 
+					"start": parseInt(fields[3]), 
+					"end": parseInt(fields[4]), 
+					"score": parseFloat(fields[5]),
+					"strand": fields[6], 
+					"frame": fields[7],
+					"attributes": getAttr(fields[8]),
+					"featureType":	"gtf"
+			} ;
+
+			this.featureCache.putFeatures(feature, dataType);
+		}
+	}
+};
