b28a39f3891925076caa1f623f04169482e6aa57
Francisco Salavert Torres <fsalavert@cipf.es>
2013-04-17 17:56:19 +0200

    result widget2 pathiways fix

--- opencga/ui-widgets/result2-widget.js
+++ opencga/ui-widgets/result2-widget.js
@@ -89,7 +89,7 @@ ResultWidget.prototype = {
                             case 'pathiways':
                                 var arr = item.commandLine.split(/ --/g);
                                 var str = arr[2].replace(/ /g,': ')+'<br>';
-                                str +=  arr[5].replace(/ /g,': ').replace('/httpd/bioinfo/opencga/analysis/pathiways/examples/','')+'<br>';
+                                str +=  arr[5].replace(/ /g,': ').replace('/httpd/bioinfo/opencga/analysis/pathiways/examples/','').replace('/httpd/bioinfo/opencga/accounts/','')+'<br>';
                                 str +=  arr[1].replace(/ /g,': ').replace('/httpd/bioinfo/opencga/analysis/pathiways/examples/','')+'<br>';
                                 str +=  arr[3].replace(/ /g,': ')+'<br>';
                                 str +=  arr[7].replace(/ /g,': ')+'<br>';
