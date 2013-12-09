9e532d0263c1d7aeb49ef9803b6d3029111e0abd
frasator <frasator@gmail.com>
2013-12-09 10:29:50 +0100

    GV: fix. Removed _checkRegion and added again setMinRegion from setRegion api method

--- src/genome-viewer/genome-viewer.js
+++ src/genome-viewer/genome-viewer.js
@@ -725,8 +725,8 @@ GenomeViewer.prototype = {
         this.region.load(region);
     },
     setRegion: function (region) {
-        var region = this._checkRegion(region);
         this.region.load(region);
+        this.setMinRegion(this.region, this.getSVGCanvasWidth());
         this.trigger('region:change', {region: this.region, sender: this});
     },
     _checkRegion: function (newRegion) {
