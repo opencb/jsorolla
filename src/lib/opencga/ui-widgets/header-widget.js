f4075d38e8a26c9ea08c293f6971041b380a02cf
Alejandro Alemán <aaleman@cipf.es>
2014-02-18 12:47:23 +0100

    Changed upload-manger style

--- src/lib/opencga/ui-widgets/header-widget.js
+++ src/lib/opencga/ui-widgets/header-widget.js
@@ -41,6 +41,7 @@ function HeaderWidget(args) {
     this.width;
     this.height;
     this.chunkedUpload = false;
+    this.enableTextModeUW = true; // Enable Text Mode in the Upload Widget
 
     //set instantiation args, must be last
     _.extend(this, args);
@@ -120,8 +121,8 @@ HeaderWidget.prototype = {
     _createLoginWidget: function () {
         var _this = this;
         var loginWidget = new LoginWidget({
-            suiteId:this.suiteId,
-            autoRender:true,
+            suiteId: this.suiteId,
+            autoRender: true,
             handlers: {
                 'session:initiated': function () {
                     _this.sessionInitiated();
@@ -134,7 +135,7 @@ HeaderWidget.prototype = {
     },
     _createProfileWidget: function () {
         var profileWidget = new ProfileWidget({
-            autoRender:true
+            autoRender: true
         });
         profileWidget.draw();
         return profileWidget;
@@ -144,7 +145,8 @@ HeaderWidget.prototype = {
         var opencgaBrowserWidget = new OpencgaBrowserWidget({
             suiteId: this.suiteId,
             chunkedUpload: this.chunkedUpload,
-            autoRender:true,
+            enableTextModeUW: this.enableTextModeUW,
+            autoRender: true,
             handlers: {
                 'need:refresh': function () {
                     _this.getAccountInfo();
@@ -213,7 +215,7 @@ HeaderWidget.prototype = {
                 this.helpLink = "https://github.com/babelomics/bierapp/wiki";
                 this.tutorialLink = "https://github.com/babelomics/bierapp/wiki/Tutorial";
                 this.aboutText = 'BiERApp is an interactive tool that allows finding genes affected by deleterious variants that segregate along family pedigrees, case-controls or sporadic samples. BiERApp has built with open and free technologies like HTML5 and Javascript.<br><br>' +
-'BierApp project is a joint development of the <b>BiER</b> and the <b>Computational Biology Unit</b>, in the <b>Computational Genomics Department</b> at CIPF in Valencia, Spain.<br><br>' +'For more information please visit our web page  <span class="info"><a target="_blank" href="http://bioinfo.cipf.es">bioinfo.cipf.es</a></span> <br><br>' +
+                    'BierApp project is a joint development of the <b>BiER</b> and the <b>Computational Biology Unit</b>, in the <b>Computational Genomics Department</b> at CIPF in Valencia, Spain.<br><br>' + 'For more information please visit our web page  <span class="info"><a target="_blank" href="http://bioinfo.cipf.es">bioinfo.cipf.es</a></span> <br><br>' +
                     '<img width="25%" src="http://bioinfo.cipf.es/bierwiki/lib/tpl/arctic/images/logobier.jpg">'
                 ;
                 break;
@@ -222,8 +224,8 @@ HeaderWidget.prototype = {
                 this.homeLink = "http://bioinfo.cipf.es/apps/team/";
                 this.helpLink = "https://github.com/babelomics/team/wiki";
                 this.tutorialLink = "https://github.com/babelomics/team/wiki/Tutorial";
-                this.aboutText = 'TEAM (Targeted Enrichment Analysis and Management) is an open web-based tool for the design and management of panels of genes for targeted enrichment and massive sequencing for diagnostic applications. <br> TEAM has been built with open and free technologies like HTML5 and Javascript.<br><br>'+
-                    'TEAM project is a joint development of the <b>BiER</b> and the <b>Computational Biology Unit</b>, in the <b>Computational Genomics Department</b> at CIPF in Valencia, Spain.<br><br>' +'For more information please visit our web page  <span class="info"><a target="_blank" href="http://bioinfo.cipf.es">bioinfo.cipf.es</a></span> <br><br>' +
+                this.aboutText = 'TEAM (Targeted Enrichment Analysis and Management) is an open web-based tool for the design and management of panels of genes for targeted enrichment and massive sequencing for diagnostic applications. <br> TEAM has been built with open and free technologies like HTML5 and Javascript.<br><br>' +
+                    'TEAM project is a joint development of the <b>BiER</b> and the <b>Computational Biology Unit</b>, in the <b>Computational Genomics Department</b> at CIPF in Valencia, Spain.<br><br>' + 'For more information please visit our web page  <span class="info"><a target="_blank" href="http://bioinfo.cipf.es">bioinfo.cipf.es</a></span> <br><br>' +
                     '<img width="25%" src="http://bioinfo.cipf.es/bierwiki/lib/tpl/arctic/images/logobier.jpg">'
                 ;
                 break;
@@ -235,16 +237,16 @@ HeaderWidget.prototype = {
                 this.aboutText = '';
         }
 
-        if(typeof HEADER_HOME_LINK !== 'undefined'){
+        if (typeof HEADER_HOME_LINK !== 'undefined') {
             this.homeLink = HEADER_HOME_LINK;
         }
-        if(typeof HEADER_HELP_LINK !== 'undefined'){
+        if (typeof HEADER_HELP_LINK !== 'undefined') {
             this.helpLink = HEADER_HELP_LINK;
         }
-        if(typeof HEADER_TUTORIAL_LINK !== 'undefined'){
+        if (typeof HEADER_TUTORIAL_LINK !== 'undefined') {
             this.tutorialLink = HEADER_TUTORIAL_LINK;
         }
-        if(typeof HEADER_ABOUT_HTML !== 'undefined'){
+        if (typeof HEADER_ABOUT_HTML !== 'undefined') {
             this.aboutText = HEADER_ABOUT_HTML;
         }
 
@@ -335,7 +337,7 @@ HeaderWidget.prototype = {
                     text: '<span class="emph">Upload & Manage</span>',
                     iconCls: 'icon-project-manager',
                     handler: function () {
-                        _this.opencgaBrowserWidget.show({mode:"manager"});
+                        _this.opencgaBrowserWidget.show({mode: "manager"});
                     }
                 },
                 {
