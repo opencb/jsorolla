6d0dd391a3962d10a887f266c5cbe744bc842462
Alejandro Alemán <aaleman@cipf.es>
2014-07-11 16:02:13 +0100

    Added License to JS files

--- src/lib/widgets/feature/variant/variant-stats-panel.js
+++ src/lib/widgets/feature/variant/variant-stats-panel.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2014 Francisco Salavert (SGL-CIPF)
+ * Copyright (c) 2014 Alejandro Alemán (SGL-CIPF)
+ * Copyright (c) 2014 Ignacio Medina (EBI-EMBL)
+ *
+ * This file is part of JSorolla.
+ *
+ * JSorolla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * JSorolla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JSorolla. If not, see <http://www.gnu.org/licenses/>.
+ */
 function VariantStatsPanel(args) {
     _.extend(this, Backbone.Events);
     this.id = Utils.genId("VariantStatsPanel");
