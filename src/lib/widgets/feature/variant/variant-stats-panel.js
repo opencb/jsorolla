4249feb45fe146956d196c08e5066b99f1fe8af0
kandaj <kandaj@ebi.ac.uk>
2014-10-07 10:29:30 +0100

    stats panel: study name added

--- src/lib/widgets/feature/variant/variant-stats-panel.js
+++ src/lib/widgets/feature/variant/variant-stats-panel.js
@@ -141,9 +141,20 @@ VariantStatsPanel.prototype = {
         delete attributesData['src'];
         delete attributesData['ACC'];
 
+        //TO BE REMOVED
+        var study_title;
+        if(projects){
+            for (var i = 0; i < projects.length; i++) {
+                if (projects[i].studyId === data.studyId) {
+                    study_title = '<a href="?eva-study='+projects[i].studyId+'" target="_blank">'+projects[i].studyName+'</a> ('+ projects[i].studyId +')';
+                }
+            }
+        }else{
+            study_title = '<a href="?eva-study='+data.studyId+'" target="_blank">'+data.studyId+'</a>';
+        }
 
         var studyPanel = Ext.create('Ext.panel.Panel', {
-            title: data.studyId,
+            title: study_title,
             border: false,
             layout: 'vbox',
             overflowX: true,
