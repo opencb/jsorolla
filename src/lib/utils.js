394642eef87ce2bf3bba14ef34ea6f27603cbf1f
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2015-01-27 15:42:44 +0100

    Added Variant Stats tool

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -213,7 +213,7 @@ var Utils = {
     },
     getLink: function (term) {
         var link = "http://www.ebi.ac.uk/QuickGO/GTerm?id=";
-        if (term.indexOf("(")>=0) {
+        if (term.indexOf("(") >= 0) {
             var id = term.split("(");
             if (id.length > 1)
                 id = id[1];
@@ -312,6 +312,46 @@ var Utils = {
         setTimeout(function () {
             $(div).remove();
         }, 2200);
+    },
+    repeat: function (string, count) {
+        'use strict';
+        if (string == null) {
+            throw new TypeError('can\'t convert ' + string + ' to object');
+        }
+        var str = '' + string;
+        count = +count;
+        if (count != count) {
+            count = 0;
+        }
+        if (count < 0) {
+            throw new RangeError('repeat count must be non-negative');
+        }
+        if (count == Infinity) {
+            throw new RangeError('repeat count must be less than infinity');
+        }
+        count = Math.floor(count);
+        if (str.length == 0 || count == 0) {
+            return '';
+        }
+        // Ensuring count is a 31-bit integer allows us to heavily optimize the
+        // main part. But anyway, most current (august 2014) browsers can't handle
+        // strings 1 << 28 chars or longer, so:
+        if (str.length * count >= 1 << 28) {
+            throw new RangeError('repeat count must not overflow maximum string size');
+        }
+        var rpt = '';
+        for (; ;) {
+            if ((count & 1) == 1) {
+                rpt += str;
+            }
+            count >>>= 1;
+            if (count == 0) {
+                break;
+            }
+            str += str;
+        }
+        return rpt;
+
     }
 
 };
