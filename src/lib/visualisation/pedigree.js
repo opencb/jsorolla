765412736f75a31d68255c2e27993f5b57b03836
imedina <igmecas@gmail.com>
2017-06-09 15:11:18 +0100

    oepncga: several fixes and cleanups in variant-clinical-upload. INTEGER and DOUBLE added to upload types

--- src/lib/visualisation/pedigree.js
+++ src/lib/visualisation/pedigree.js
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */long
 
 /**
  * Created by imedina on 16/05/17.
