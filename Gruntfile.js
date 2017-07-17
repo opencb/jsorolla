1000f89cd4c77848588e8e27f7e0f74d64b337ce
imedina <igmecas@gmail.com>
2017-07-17 14:11:13 +0100

    Fix wrong tak name in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,58 +3,55 @@ module.exports = function (grunt) {
     grunt.initConfig({
         hub: {
             lib: {
-                src: ['Gruntfile-lib.js'],
-                tasks: ['default']
+                src: ["Gruntfile-lib.js"],
+                tasks: ["default"]
             },
 
-             'genome-browser': {
-                src: ['Gruntfile-genome-browser.js'],
-                tasks: ['default']
+            "genome-browser": {
+                src: ["Gruntfile-genome-browser.js"],
+                tasks: ["default"]
             },
-
-            'circular-genome-viewer': {
-                src: ['Gruntfile-circular-genome-viewer.js'],
-                tasks: ['default']
+            "circular-genome-viewer": {
+                src: ["Gruntfile-circular-genome-viewer.js"],
+                tasks: ["default"]
             },
-            'circular-genome-viewer-no-dep': {
-                src: ['Gruntfile-circular-genome-viewer.js'],
-                tasks: ['no-dep']
+            "circular-genome-viewer-no-dep": {
+                src: ["Gruntfile-circular-genome-viewer.js"],
+                tasks: ["no-dep"]
             },
-
-            'network-viewer': {
-                src: ['Gruntfile-network-viewer.js'],
-                tasks: ['default']
+            "network-viewer": {
+                src: ["Gruntfile-network-viewer.js"],
+                tasks: ["default"]
             },
-            'network-viewer-no-dep': {
-                src: ['Gruntfile-network-viewer.js'],
-                tasks: ['no-dep']
+            "network-viewer-no-dep": {
+                src: ["Gruntfile-network-viewer.js"],
+                tasks: ["no-dep"]
             },
-
-            'threed-viewer': {
-                src: ['Gruntfile-threed-viewer.js'],
-                tasks: ['default']
+            "threed-viewer": {
+                src: ["Gruntfile-threed-viewer.js"],
+                tasks: ["default"]
             },
-            'threed-viewer-no-dep': {
-                src: ['Gruntfile-threed-viewer.js'],
-                tasks: ['no-dep']
+            "threed-viewer-no-dep": {
+                src: ["Gruntfile-threed-viewer.js"],
+                tasks: ["no-dep"]
             }
         }
-    })
+    });
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-hub');
+    grunt.loadNpmTasks("grunt-hub");
 
-    grunt.registerTask('default', [
-        'hub:lib',
-        'hub:genome-viewer-no-dep',
+    grunt.registerTask("default", [
+        "hub:lib",
+        "hub:genome-browser",
 //        'hub:circular-genome-viewer-no-dep',
-        'hub:network-viewer-no-dep',
-        'hub:threed-viewer-no-dep',
+        "hub:network-viewer-no-dep",
+        "hub:threed-viewer-no-dep",
     ]);
 
-    grunt.registerTask('gv', ['hub:genome-viewer']);
-    grunt.registerTask('gb', ['hub:genome-browser']);
-    grunt.registerTask('cgv', ['hub:circular-genome-viewer']);
-    grunt.registerTask('nv', ['hub:network-viewer']);
-    grunt.registerTask('3dv', ['hub:threed-viewer']);
+    // grunt.registerTask('gv', ['hub:genome-viewer']);
+    grunt.registerTask("gb", ["hub:genome-browser"]);
+    grunt.registerTask("cgv", ["hub:circular-genome-viewer"]);
+    grunt.registerTask("nv", ["hub:network-viewer"]);
+    grunt.registerTask("3dv", ["hub:threed-viewer"]);
 };
