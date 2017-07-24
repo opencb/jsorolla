89046caaf2ae6d254a665a17ba421a3c2d7b594b
imedina <igmecas@gmail.com>
2017-07-25 00:45:29 +0100

    gb: implement two render modes, closes #53

--- src/genome-browser/renderers/variant-renderer.js
+++ src/genome-browser/renderers/variant-renderer.js
@@ -35,80 +35,83 @@ class VariantRenderer extends Renderer {
             this.sampleNames = this.sampleNames.split(",");
         }
 
-        let y = this.sampleTrackY;
-        for (let i = 0; i < this.sampleNames.length; i++) {
-            let sampleSvg = SVG.addChild(this.track.main, "text", {
-                "x": 0,
-                "y": y,
-                "stroke": "black",
-                "stroke-width": 1,
-                "font-size": this.sampleTrackFontSize,
-                "cursor": "pointer"
-            });
-            sampleSvg.textContent = this.sampleNames[i];
+        // FIXME sampleNames should be renderer here but in the variant-track.js
+        if (typeof this.sampleNames !== "undefined" && this.sampleNames !== null) {
+            let y = this.sampleTrackY;
+            for (let i = 0; i < this.sampleNames.length; i++) {
+                let sampleSvg = SVG.addChild(this.track.main, "text", {
+                    "x": 0,
+                    "y": y,
+                    "stroke": "black",
+                    "stroke-width": 1,
+                    "font-size": this.sampleTrackFontSize,
+                    "cursor": "pointer"
+                });
+                sampleSvg.textContent = this.sampleNames[i];
 
-            y += this.sampleTrackHeight;
+                y += this.sampleTrackHeight;
 
-            let _this = this;
-            $(sampleSvg).click(function (event) {
-                // $(this).css({"strokeWidth":"3","stroke":"#ff7200"}).hide(100).show(500).css({"stroke":"#51c000"})});
-                // amarillo FCFC92
-                let label = $(this);
-                //label.css({"stroke":"#ff7200"}).hide(100).show(500).css({"stroke":"#ff7200"});
+                let _this = this;
+                $(sampleSvg).click(function (event) {
+                    // $(this).css({"strokeWidth":"3","stroke":"#ff7200"}).hide(100).show(500).css({"stroke":"#51c000"})});
+                    // amarillo FCFC92
+                    let label = $(this);
+                    //label.css({"stroke":"#ff7200"}).hide(100).show(500).css({"stroke":"#ff7200"});
 
-                //POSIBILIDAD 1
-                //let rect = SVG.addChild(_this.track.main,  "rect",{
-                //    'x': 0,
-                //    'y': label[0].y.baseVal[0].value-7,
-                //    'width': _this.track.width,
-                //    'height': 8,
-                //    'stroke': '#FFFF00',
-                //    'fill': '#FCFC92'
-                //});
-                //$(rect).css({"z-index": -1});
+                    //POSIBILIDAD 1
+                    //let rect = SVG.addChild(_this.track.main,  "rect",{
+                    //    'x': 0,
+                    //    'y': label[0].y.baseVal[0].value-7,
+                    //    'width': _this.track.width,
+                    //    'height': 8,
+                    //    'stroke': '#FFFF00',
+                    //    'fill': '#FCFC92'
+                    //});
+                    //$(rect).css({"z-index": -1});
 
-                //POSIBILIDAD 2
-                let yrect = label[0].y.baseVal[0].value - 7;
-                if (this.getAttribute("stroke") === "black") {
-                    label.css({"stroke": "#ff7200"}).hide(100).show(500).css({"stroke": "#ff7200"});
-                    this.setAttribute("stroke", "#ff7200");
-                    let rect = SVG.create("rect", {
-                        "x": 0,
-                        "y": yrect,
-                        "width": _this.track.width,
-                        "height": 8,
-                        "stroke": "#FFFF00",
-                        "fill": "#F2F5A9",
-                        "opacity": 0.5
-                    });
-                    rect.setAttribute("id", this.innerHTML + "_rect" + yrect);
-                    _this.track.main.insertBefore(rect, this);
-                } else {
-                    let rect = document.getElementById(this.innerHTML + "_rect" + yrect);
-                    rect.parentNode.removeChild(rect);
-                    this.setAttribute("stroke", "black");
-                    label.css({"stroke": "black"});
-                }
+                    //POSIBILIDAD 2
+                    let yrect = label[0].y.baseVal[0].value - 7;
+                    if (this.getAttribute("stroke") === "black") {
+                        label.css({"stroke": "#ff7200"}).hide(100).show(500).css({"stroke": "#ff7200"});
+                        this.setAttribute("stroke", "#ff7200");
+                        let rect = SVG.create("rect", {
+                            "x": 0,
+                            "y": yrect,
+                            "width": _this.track.width,
+                            "height": 8,
+                            "stroke": "#FFFF00",
+                            "fill": "#F2F5A9",
+                            "opacity": 0.5
+                        });
+                        rect.setAttribute("id", this.innerHTML + "_rect" + yrect);
+                        _this.track.main.insertBefore(rect, this);
+                    } else {
+                        let rect = document.getElementById(this.innerHTML + "_rect" + yrect);
+                        rect.parentNode.removeChild(rect);
+                        this.setAttribute("stroke", "black");
+                        label.css({"stroke": "black"});
+                    }
 
-                //let divpadre = _this.track.main.parentNode;
-                //let selBox = $('<div id="' + this.id + 'selBox"></div>')[0];
-                //divpadre.append(selBox);
-                //$(selBox).css({
-                //    'z-index': 0,
-                //    'position': 'absolute',
-                //    'left': 0,
-                //    'top': label[0].y.baseVal[0].value-8,
-                //    'height': 8,
-                //    'width':'100%',
-                //    'border': '2px solid #FFFF00',
-                //    'opacity': 0.5,
-                //    //'visibility': 'hidden',
-                //    'background-color': '#F2F5A9'
-                //});
-                //$(selBox).click(function(event){
-                //    this.parentNode.removeChild(this);
-                //});
-            });
+                    //let divpadre = _this.track.main.parentNode;
+                    //let selBox = $('<div id="' + this.id + 'selBox"></div>')[0];
+                    //divpadre.append(selBox);
+                    //$(selBox).css({
+                    //    'z-index': 0,
+                    //    'position': 'absolute',
+                    //    'left': 0,
+                    //    'top': label[0].y.baseVal[0].value-8,
+                    //    'height': 8,
+                    //    'width':'100%',
+                    //    'border': '2px solid #FFFF00',
+                    //    'opacity': 0.5,
+                    //    //'visibility': 'hidden',
+                    //    'background-color': '#F2F5A9'
+                    //});
+                    //$(selBox).click(function(event){
+                    //    this.parentNode.removeChild(this);
+                    //});
+                });
+            }
         }
     }
 
@@ -119,11 +122,13 @@ class VariantRenderer extends Renderer {
         if (typeof this.sampleNames !== "undefined" && this.sampleNames !== null && this.sampleNames.length > 0) {
             this._renderExtendedGenotypes(features, args);
         } else {
-            this._renderExtendedGenotypes(features, args);
+            this._renderCompactVariants(features, args);
         }
     }
 
     _renderExtendedGenotypes(features, args) {
+        console.time("Variant Extended GT Renderer");
+
         for (let feature of features) {
             // get feature render configuration
             // let color = _.isFunction(this.color) ? this.color(feature) : this.color;
@@ -200,14 +205,14 @@ class VariantRenderer extends Renderer {
             }
 
             let featureGroup = SVG.addChild(args.svgCanvasFeatures, "g", {"feature_id": feature.id});
-            // let dummyRect = SVG.addChild(featureGroup, "rect", {
-            //     "x": xs,
-            //     "y": 1,
-            //     "width": width,
-            //     "height": ys,
-            //     "fill": "transparent",
-            //     "cursor": "pointer"
-            // });
+            let dummyRect = SVG.addChild(featureGroup, "rect", {
+                "x": xs,
+                "y": 1,
+                "width": width,
+                "height": ys,
+                "fill": "transparent",
+                "cursor": "pointer"
+            });
             if (d00 !== "") {
                 let path = SVG.addChild(featureGroup, "path", {
                     "d": d00,
@@ -269,5 +274,141 @@ class VariantRenderer extends Renderer {
                 lastSampleIndex = sampleIndex;
             });
         }
+        console.timeEnd("Variant Extended GT Renderer");
+    }
+
+    _renderCompactVariants(features, args) {
+        console.time("Variant Compact Renderer");
+
+        let svgGroup = SVG.create("g");
+        for (let i = 0; i < features.length; i++) {
+            let feature = features[i];
+
+            if ("featureType" in feature) {
+                Object.assign(this, FEATURE_TYPES[feature.featureType]);
+            }
+            if ("featureClass" in feature) {
+                Object.assign(this, FEATURE_TYPES[feature.featureClass]);
+            }
+
+            //Temporal fix for clinical
+            if (args.featureType === "clinical") {
+                if ("clinvarSet" in feature) {
+                    Object.assign(this, FEATURE_TYPES["Clinvar"]);
+                } else if ("mutationID" in feature) {
+                    Object.assign(this, FEATURE_TYPES["Cosmic"]);
+                } else {
+                    Object.assign(this, FEATURE_TYPES["GWAS"]);
+                }
+            }
+
+            // get feature render configuration
+            let color = _.isFunction(this.color) ? this.color(feature) : this.color;
+            let strokeColor = _.isFunction(this.strokeColor) ? this.color(feature) : this.strokeColor;
+            let label = _.isFunction(this.label) ? this.label(feature) : this.label;
+            let height = _.isFunction(this.height) ? this.height(feature) : this.height;
+            let tooltipTitle = _.isFunction(this.tooltipTitle) ? this.tooltipTitle(feature) : this.tooltipTitle;
+            let tooltipText = _.isFunction(this.tooltipText) ? this.tooltipText(feature) : this.tooltipText;
+            let infoWidgetId = _.isFunction(this.infoWidgetId) ? this.infoWidgetId(feature) : this.infoWidgetId;
+
+            // get feature genomic information
+            let start = feature.start;
+            let end = feature.end;
+            let length = (end - start) + 1;
+
+            // check genomic length
+            length = (length < 0) ? Math.abs(length) : length;
+            length = (length === 0) ? 1 : length;
+
+            // transform to pixel position
+            let width = length * args.pixelBase;
+
+            //        var svgLabelWidth = this.getLabelWidth(label, args);
+            let svgLabelWidth = label.length * 6.4;
+
+            //calculate x to draw svg rect
+            let x = this.getFeatureX(start, args);
+
+            let maxWidth = Math.max(width, 2);
+            let textHeight = 0;
+            if (args.maxLabelRegionSize > args.regionSize) {
+                textHeight = 9;
+                maxWidth = Math.max(width, svgLabelWidth);
+            }
+
+            let rowY = 0;
+            let textY = textHeight + height;
+            let rowHeight = textHeight + height + 2;
+
+            while (true) {
+                if (!(rowY in args.renderedArea)) {
+                    args.renderedArea[rowY] = new FeatureBinarySearchTree();
+                }
+                let foundArea = args.renderedArea[rowY].add({start: x, end: x + maxWidth - 1});
+
+                if (foundArea) {
+                    let featureGroup = SVG.addChild(svgGroup, "g", {"feature_id": feature.id});
+                    let rect = SVG.addChild(featureGroup, "rect", {
+                        "x": x,
+                        "y": rowY,
+                        "width": width,
+                        "height": height,
+                        "stroke": strokeColor,
+                        "stroke-width": 1,
+                        "stroke-opacity": 0.7,
+                        "fill": color,
+                        "cursor": "pointer"
+                    });
+
+                    if (args.maxLabelRegionSize > args.regionSize) {
+                        let text = SVG.addChild(featureGroup, "text", {
+                            "i": i,
+                            "x": x,
+                            "y": textY,
+                            "font-weight": 400,
+                            "opacity": null,
+                            "fill": "black",
+                            "cursor": "pointer",
+                            "class": this.fontClass
+                        });
+                        text.textContent = label;
+                    }
+
+                    if ("tooltipText" in this) {
+                        $(featureGroup).qtip({
+                            content: {text: tooltipText, title: tooltipTitle},
+                            //                        position: {target: "mouse", adjust: {x: 15, y: 0}, effect: false},
+                            position: {viewport: $(window), target: "mouse", adjust: {x: 25, y: 15}},
+                            style: {width: true, classes: this.toolTipfontClass + " ui-tooltip ui-tooltip-shadow"},
+                            show: {delay: 300},
+                            hide: {delay: 300}
+                        });
+                    }
+
+                    $(featureGroup).mouseover(function (event) {
+                        this.trigger("feature:mouseover", {
+                            query: feature[infoWidgetId],
+                            feature: feature,
+                            featureType: feature.featureType,
+                            mouseoverEvent: event
+                        });
+                    });
+
+                    $(featureGroup).click(function (event) {
+                        this.trigger("feature:click", {
+                            query: feature[infoWidgetId],
+                            feature: feature,
+                            featureType: feature.featureType,
+                            clickEvent: event
+                        });
+                    });
+                    break;
+                }
+                rowY += rowHeight;
+                textY += rowHeight;
+            }
+        }
+        args.svgCanvasFeatures.appendChild(svgGroup);
+        console.timeEnd("Variant Compact Renderer");
     }
 }
