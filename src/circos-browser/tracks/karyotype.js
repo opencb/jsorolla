77e59ff49bd88f3796a07a6618c1c07e43c0ae7c
jmjuanes <josemijuanes@gmail.com>
2019-03-19 23:39:47 +0100

    Import chromosomes data from cellbase using the cellbase data adaptor

--- src/circos-browser/tracks/karyotype.js
+++ src/circos-browser/tracks/karyotype.js
@@ -7,24 +7,38 @@ class CircosKaryotypeTrack extends CircosTrack {
         }
         //Check for undefined data adapter
         if (UtilsNew.isUndefinedOrNull(this.dataAdapter)) {
-            this.dataAdapter = null; //??????
+            this.dataAdapter = new CellBaseAdapter(new CellBaseClient(), "genomic", "chromosome", "search", {});
         }
     }
     //Draw this track
     draw(args) {
         let self = this;
+        //Clean the track
         this.renderer.clean();
-        if (UtilsNew.isNotUndefinedOrNull(this.data)) {
-            //Render the karyotype track with the provided data
-            return this.renderer.render({
-                "data": this.data,
+        //Function to render the data provided as an argument
+        let renderData = function (data) {
+            return self.renderer.render({
+                "data": data,
                 "layout": args.layout,
                 "drawingZone": args.drawingZone,
                 "config": args.config
             });
+        };
+        //Check for initial data provided to the track
+        if (UtilsNew.isNotUndefinedOrNull(this.data)) {
+            return renderData(this.data);
         }
-        else {
-            return null; //???????
+        //Check if a query has been provided
+        else if (UtilsNew.isNotUndefinedOrNull(args.query)) {
+            if (UtilsNew.isNotUndefinedOrNull(this.dataAdapter)) {
+                let request = this.dataAdapter.getData(args.query);
+                request.then(function (response) {
+                    return renderData(response.items[0].result[0].chromosomes);
+                });
+                request.catch(function (response) {
+                    console.error(response);
+                });
+            }
         }
     }
 }
