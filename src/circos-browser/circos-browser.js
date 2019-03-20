513b752e0afcf8964f7885f015f24ae01590cbd1
jmjuanes <josemijuanes@gmail.com>
2019-03-20 12:18:15 +0100

    Updated constructor and init methods. Draw the circos when the layout is ready

--- src/circos-browser/circos-browser.js
+++ src/circos-browser/circos-browser.js
@@ -1,12 +1,19 @@
 class CircosBrowser { 
-    constructor(parent, config) {
+    constructor(parent, args, config) {
+        /* 
+         * args = {
+         *    "layout": ,      //Layout configuration
+         *    "tracks": [],    //Tracks list
+         * };
+         */
         this.parent = parent;
-        this.config = Object.assign({}, this._getDefaultConfig(), config);
-        //Initialize the layout
+        //Default props
         this.layout = null;
-        //Tracks
-        this.tracks = {};
-        this.tracksDisplay = null;
+        this.tracks = [];
+        this.tracksDisplay = [];
+        Object.assign(this, args);
+        //Save the configuration
+        this.config = Object.assign({}, this._getDefaultConfig(), config);
         //Init
         this.init();
     }
@@ -20,16 +27,10 @@ class CircosBrowser {
         //Create the SVG object
         this.svg = SVG.addChild(this.parent, "svg", svgAttributes, null); 
         this.mainGroup = SVG.addChild(this.svg, "g", {}, null);
-        //Register all provided tracks in the config
-        if (this.config.tracks !== null) {
-            Object.keys(this.config.tracks).forEach(function (key) {
-                self.addTrack(key, self.config.tracks[key]);
-            });
-        }
-        //Check for predefined layout data
-        if (this.config.layout.data !== null) {
-            this.layout = new CircosLayout(this.config.layout.data, this.config.layout.config);
-            console.log(this.layout);
+        //Check for no layout provided
+        if (this.layout === null) {
+            this.layout = new CircosLayout({}, {});
+            //console.log(this.layout);
         }
         else {
             //Fetch data from cellbase
@@ -44,46 +45,58 @@ class CircosBrowser {
             //    }
             //});
         }
+        //Build layout
+        this.layout.init({
+            "species": this.config.species,
+            "assembly": this.config.assembly
+        });
+        //Create the group for each track
+        this.tracks.forEach(function (track, index) {
+            track.renderer.target = SVG.addChild(self.mainGroup, "g", {}, null);
+        });
     }
     //Add a new track
-    addTrack(id, track) {
-        this.tracks[id] = track;
-        //Create a group where this track should be rendererd
-        this.tracks[id].renderer.target = SVG.addChild(this.mainGroup, "g", {}, null);
-    }
+    //addTrack(id, track) {
+    //    this.tracks[id] = track;
+    //    //Create a group where this track should be rendererd
+    //    this.tracks[id].renderer.target = SVG.addChild(this.mainGroup, "g", {}, null);
+    //}
     //Draw the circos
-    draw() {
+    draw(query) {
         let self = this;
-        if (this.tracksDisplay === null) {
-            this._buildTracksDisplay();
-        }
-        //Render all tracks
-        Object.keys(this.tracks).forEach(function (id) {
-            console.log("Render track " + id);
-            self.tracks[id].draw({
-                "query": null,
-                "layout": self.layout,
-                "drawingZone": {
-                    "innerRadius": self.tracksDisplay[id].innerRadius,
-                    "outerRadius": self.tracksDisplay[id].outerRadius,
-                    "center": {
-                        "x": self.config.width / 2,
-                        "y": self.config.height / 2
-                    }
-                },
-                "config": {}
+        //Render all tracks when the layout is ready
+        this.layout.onReady(function () {
+            self._buildTracksDisplay();
+            self.tracks.forEach(function (track, index) {
+                console.log("Render track " + index);
+                return track.draw({
+                    "query": {
+                        "species": self.config.species,
+                        "assembly": self.config.assembly
+                    },
+                    "layout": self.layout,
+                    "drawingZone": {
+                        "innerRadius": self.tracksDisplay[index].innerRadius,
+                        "outerRadius": self.tracksDisplay[index].outerRadius,
+                        "center": {
+                            "x": self.config.width / 2,
+                            "y": self.config.height / 2
+                        }
+                    },
+                    "config": {}
+                });
             });
         });
     }
     //Build the tracks display
     _buildTracksDisplay() {
         let self = this;
-        let tracksDisplay = Object.keys(this.tracks).map(function (id) {
+        let tracksDisplay = this.tracks.map(function (track, index) {
             //Build an object with the track rank and the track size
             return {
-                "id": id,
-                "rank": self.tracks[id].config.rank,
-                "size": self.tracks[id].config.size
+                "index": index,
+                "rank": track.config.rank,
+                "size": track.config.size
             };
         });
         //Sort the tracks by the track rank
@@ -91,13 +104,13 @@ class CircosBrowser {
             return (left.rank < right.rank) ? -1 : 1;
         });
         //Reset the current tracks display object
-        this.tracksDisplay = {};
+        this.tracksDisplay = [];
         let currentRadius = this.config.trackOffset;
         tracksDisplay.forEach(function (track) {
             let innerRadius = currentRadius;
             let outerRadius = currentRadius + track.size;
             //Save the display for this track
-            self.tracksDisplay[track.id] = {
+            self.tracksDisplay[track.index] = {
                 "innerRadius": innerRadius,
                 "outerRadius": outerRadius
             };
@@ -112,13 +125,8 @@ class CircosBrowser {
             "height": 400,
             "trackGap": 5,
             "trackOffset": 150,
-            "layout": {
-                "data": null,
-                "cellbase": null,
-                "config": {
-                    "gap": 0.04
-                }
-            }
+            "species": "hsapiens",
+            "assembly": "grch37"
         };
     }
 }
