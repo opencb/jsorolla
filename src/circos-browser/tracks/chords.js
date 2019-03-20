9ad153fb1323902df21be25d8247adc18525338c
jmjuanes <josemijuanes@gmail.com>
2019-03-20 12:30:32 +0100

    Updated draw method of chords track: allow importing data using the data-adaptor

--- src/circos-browser/tracks/chords.js
+++ src/circos-browser/tracks/chords.js
@@ -14,17 +14,32 @@ class CircosChordsTrack extends CircosTrack {
     draw(args) {
         let self = this;
         this.renderer.clean();
-        if (UtilsNew.isNotUndefinedOrNull(this.data)) {
-            //Render the chords track with the provided data
-            return this.renderer.render({
-                "data": this.data,
+        //Function to render the data provided as an argument
+        let renderData = function (data) {
+            //console.log("Render karyotypes data");
+            //console.log(data);
+            return self.renderer.render({
+                "data": data,
                 "layout": args.layout,
                 "drawingZone": args.drawingZone,
                 "config": args.config
             });
+        };
+        //Check for initial data provided to the track
+        if (UtilsNew.isNotUndefinedOrNull(this.data)) {
+            return renderData(this.data);
         }
-        else {
-            return null; //???????
+        //Check if a query has been provided
+        else if (UtilsNew.isNotUndefinedOrNull(args.query)) {
+            if (UtilsNew.isNotUndefinedOrNull(this.dataAdapter)) {
+                let request = this.dataAdapter.getData(args.query);
+                request.then(function (response) {
+                    return renderData(response);
+                });
+                request.catch(function (response) {
+                    console.error(response);
+                });
+            }
         }
     }
 }
