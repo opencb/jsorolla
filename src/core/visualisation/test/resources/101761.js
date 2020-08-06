b0375db611116fdb57940e23bdb20d82d663646f
antonioaltamura <antonioaltamura7@gmail.com>
2020-08-07 00:13:59 +0200

    family sample data added

---
+++ src/core/visualisation/test/resources/101761.js
@@ -0,0 +1,77 @@
+export default [{
+    "id": "112000494",
+    "name": "112000494",
+    "father": {"id": "595652867695", "parentalConsanguinity": false},
+    "mother": {"id": "553040200211", "parentalConsanguinity": false},
+    "sex": "MALE",
+    "parentalConsanguinity": false,
+    "attributes": {"familyId": "101761"}
+}, {
+    "id": "112000493",
+    "name": "112000493",
+    "father": {"id": "431578292159", "parentalConsanguinity": false},
+    "mother": {"id": "450011364336", "parentalConsanguinity": false},
+    "sex": "FEMALE",
+    "parentalConsanguinity": false
+}, {
+    "id": "112000492",
+    "name": "112000492",
+    "father": {"id": "112000494", "parentalConsanguinity": false},
+    "mother": {"id": "112000493", "parentalConsanguinity": false},
+    "sex": "MALE",
+    "disorders": [{
+        "id": "Intellectual disability",
+        "name": "Neurology and neurodevelopmental disorders",
+        "source": "GelDisorder",
+        "attributes": {"diseaseSubGroup": "Neurodevelopmental disorders", "ageOfOnset": "-999.0"}
+    }],
+    "parentalConsanguinity": false
+}, {
+    "id": "553040200211",
+    "name": "553040200211",
+    "father": {"release": 0},
+    "mother": {"release": 0},
+    "sex": "FEMALE",
+    "disorders": [],
+    "parentalConsanguinity": false
+}, {
+    "id": "595652867695",
+    "name": "595652867695",
+    "father": {"release": 0},
+    "mother": {"release": 0},
+    "sex": "MALE",
+    "disorders": [],
+    "parentalConsanguinity": false,
+}, {
+    "id": "431578292159",
+    "name": "431578292159",
+    "father": {"release": 0},
+    "mother": {"release": 0},
+    "sex": "MALE",
+    "disorders": [],
+    "parentalConsanguinity": false
+}, {
+    "id": "162805079698",
+    "name": "162805079698",
+    "father": {"id": "112000494", "parentalConsanguinity": false},
+    "mother": {"id": "112000493", "parentalConsanguinity": false},
+    "sex": "MALE",
+    "disorders": [],
+    "parentalConsanguinity": false
+}, {
+    "id": "885409123253",
+    "name": "885409123253",
+    "father": {"id": "112000494", "parentalConsanguinity": false},
+    "mother": {"id": "112000493", "parentalConsanguinity": false},
+    "sex": "MALE",
+    "disorders": [],
+    "parentalConsanguinity": false
+}, {
+    "id": "450011364336",
+    "name": "450011364336",
+    "father": {"release": 0},
+    "mother": {"release": 0},
+    "sex": "FEMALE",
+    "disorders": [],
+    "parentalConsanguinity": false
+}];
