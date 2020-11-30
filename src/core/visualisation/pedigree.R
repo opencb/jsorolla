43b44bfddb20e48c126101bd6b5a5a7ae7c35100
antonioaltamura <antonioaltamura7@gmail.com>
2020-11-30 13:05:00 +0100

    pedigree kinship2 based in progress #123

--- src/core/visualisation/pedigree.R
+++ src/core/visualisation/pedigree.R
@@ -3,7 +3,7 @@ pedAll <- pedigree(id = sample.ped$id, dadid = sample.ped$father, momid = sample
                    sex = sample.ped$sex, famid = sample.ped$ped)
 print(pedAll)
 
-DATA = pedAll[1]
+DATA = pedAll[2]
 WIDTH = 10 # min width
 
 # TABLEDATA = align.pedigree(pedAll[2], packed=FALSE, width=10, align=TRUE, hints=pedAll$hints)$pos
@@ -37,4 +37,5 @@ write.table(align.pedigree(DATA, packed=FALSE, width=WIDTH, align=TRUE, hints=pe
             row.names = TRUE,
             col.names = NA)
 
-plot(DATA)
+plot(DATA, packed = TRUE,width = 100)
+
