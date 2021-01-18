592df7910982135236b1e32109ca30b27a8bdaee
imedina <igmecas@gmail.com>
2021-01-19 00:54:00 +0100

    wc: minor style fixes

--- src/core/webcomponents/variant/variant-grid-formatter.js
+++ src/core/webcomponents/variant/variant-grid-formatter.js
@@ -1042,12 +1042,6 @@ export default class VariantGridFormatter {
                 }
             },
             position: {target: "mouse", adjust: {x: 2, y: 2, mouse: false}},
-            // position : {
-            //     corner: {
-            //         target: 'topLeft',
-            //         tooltip: 'middleLeft'
-            //     }
-            // },
             style: {classes: "qtip-light qtip-rounded qtip-shadow qtip-custom-class"},
             show: {delay: 200},
             hide: {fixed: true, delay: 300}
