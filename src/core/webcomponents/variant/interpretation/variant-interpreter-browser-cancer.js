12299b051fe40d804ed9eb54508ad5b270dbdfe7
imedina <igmecas@gmail.com>
2021-02-26 02:53:12 +0100

    wc: remove default config for pindel caller

--- src/core/webcomponents/variant/interpretation/variant-interpreter-browser-cancer.js
+++ src/core/webcomponents/variant/interpretation/variant-interpreter-browser-cancer.js
@@ -330,15 +330,14 @@ class VariantInterpreterBrowserCancer extends LitElement {
                     lockedFields: [{id: "sample"}]
                 },
                 callers: [
-                    {
-                        id: "caveman",
-                        queryString: "FILTER=PASS;CLPM=0;ASMD>=140"
-                    },
-                    {
-                        id: "pindel",
-                        queryString: "FILTER=PASS;QUAL>=250;REP<=9"
-                        // queryString: "FILTER=PASS;QUAL>=250"
-                    }
+                    // {
+                    //     id: "caveman",
+                    //     queryString: "FILTER=PASS;CLPM=0;ASMD>=140"
+                    // },
+                    // {
+                    //     id: "pindel",
+                    //     queryString: "FILTER=PASS;QUAL>=250;REP<=9"
+                    // }
                 ],
                 sections: [     // sections and subsections, structure and order is respected
                     {
