e7eccfd1dd38317b5607515a9a6a2992a1d3ad6c
imedina <igmecas@gmail.com>
2021-03-25 03:25:05 +0000

    wc: minor improvements in study-dashboard

--- src/core/webcomponents/study/study-dashboard.js
+++ src/core/webcomponents/study/study-dashboard.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { LitElement, html } from "/web_modules/lit-element.js";
+import {html, LitElement} from "/web_modules/lit-element.js";
 import UtilsNew from "./../../utilsNew.js";
 import "../commons/tool-header.js";
 import "./study-editor.js";
@@ -56,24 +56,22 @@ export default class StudyDashboard extends LitElement {
 
     update(changedProperties) {
         if (changedProperties.has("opencgaSession")) {
-            this.usersAndProjects = this.getProjectPerUser();
+            this.users = this.getProjectPerUser();
         }
         super.update(changedProperties);
     }
 
     getProjectPerUser() {
-        // let users = this.opencgaSession.projects.map(project => project.fqn.split('@')[0]); return duplicate users
-        let users = [...new Set(this.opencgaSession.projects.map(project => this._getUserProject(project)))];
-        return users;
+        return [...new Set(this.opencgaSession.projects.map(project => project.fqn.split('@')[0]))];
     }
 
-    _verifyUserAndProject(pro,user){
-        return this._getUserProject(pro) == user;
-    }
+    // _verifyUserAndProject(pro,user){
+    //     return this._getUserProject(pro) == user;
+    // }
 
-    _getUserProject(project){
-        return project.fqn.split('@')[0];
-    }
+    // _getUserProject(project){
+    //     return project.fqn.split('@')[0];
+    // }
 
     getDefaultConfig() {
         return {
@@ -108,81 +106,78 @@ export default class StudyDashboard extends LitElement {
         modalType[type]();
     }
 
-
-
     render() {
         // Check if there is any project available
         console.log(this.opencgaSession)
         if (!this.opencgaSession?.study) {
             return html`
                 <div class="guard-page">
-                <i class="fas fa-lock fa-5x"></i>
+                    <i class="fas fa-lock fa-5x"></i>
                     <h3>No public projects available to browse. Please login to continue</h3>
                 </div>`;
         }
 
         return html`
-            
             <style>
-            .panel.panel-default:hover {
-                background-color: #eee;
-                text-decoration: none;
-            }
+                .panel.panel-default:hover {
+                    background-color: #eee;
+                    text-decoration: none;
+                }
 
-            .panel.panel-default.child {
-                height: 85px
-            }
+                .panel.panel-default.child {
+                    height: 85px
+                }
             </style>
 
-
-<!--            <tool-header title="${this._config.title}"></tool-header>-->
-            <div class="row"> 
-                
-                <!-- Show Project by user-->
-                ${this.usersAndProjects.map(user => {
+            <div class="row">
+                <!-- Show Project by User-->
+                ${this.users.map(user => {
                     return html `
-                        <h3>${user}</h3>
-                        <hr>
-                        ${this.opencgaSession.projects.filter(pro => this._verifyUserAndProject(pro,user)).map(project => { 
-                            return html`
-                                <div class="col-md-4">
-                                    <div class="panel panel-default">
-                                        <div class="panel-body text-center">
-                                            <h4>${project.name}</h4>
+                        <div class="col-md-12">
+                            <h3>${user}</h3>
+                            <hr>
+                            ${this.opencgaSession.projects.filter(proj => proj.fqn.startsWith(user + "@")).map(project => {
+                                return html`
+                                    <div class="col-md-4">
+                                        <div class="panel panel-default">
+                                            <div class="panel-body text-center">
+                                                <h4>${project.name}</h4>
+                                            </div>
                                         </div>
-                                    </div>
-                                    
-                                    <div class="row">
-                                        ${project.studies.map(study => html`    
+
+                                        <div class="row">
+                                            ${project.studies.map(study => html`
+                                                <div class="col-md-4">
+                                                    <div class="panel panel-default child">
+                                                        <div class="panel-body text-center">
+                                                            ${study.name}
+                                                        </div>
+                                                    </div>
+                                                </div>`
+                                            )}
                                             <div class="col-md-4">
-                                                <div class="panel panel-default child">
+                                                <div class="panel panel-default child"  @click="${() => this.showModal('study')}">
                                                     <div class="panel-body text-center">
-                                                        ${study.name}
+                                                        <i class="fas fa-plus"></i>
+                                                        <p>New Study</p>
                                                     </div>
                                                 </div>
-                                            </div>`
-                                        )}
-                                        <div class="col-md-4">
-                                            <div class="panel panel-default child"  @click="${() => this.showModal('study')}">
-                                                <div class="panel-body text-center">
-                                                    <i class="fas fa-plus"></i>
-                                                    <p>New Study</p>
-                                                </div>
-                                            </div>              
-                                        </div>       
+                                            </div>
+                                        </div>
+                                    </div>
+                                `})}
+                            <div class="col-md-4">
+                                <div class="panel panel-default"  @click="${() => this.showModal('project')}">
+                                    <div class="panel-body text-center">
+                                        <i class="fas fa-plus"></i>
+                                        <p>New Project</p>
                                     </div>
                                 </div>
-                            `})}
-                    <div class="col-md-4">
-                        <div class="panel panel-default"  @click="${() => this.showModal('project')}">
-                            <div class="panel-body text-center">
-                                <i class="fas fa-plus"></i>
-                                <p>Project</p>
                             </div>
-                        </div>              
-                    </div>
-                `})} 
+                        </div>
+                    `})}
             </div>
+            
             <!-- TODO: These modals can be a single one, the component will be rendered according to whether you have selected: study or project inside div. modal-body -->
             <!-- Modal New Project -->
             <div id="newProject" class="modal fade"  tabindex="-1" role="dialog">
@@ -194,7 +189,7 @@ export default class StudyDashboard extends LitElement {
                         </div>
                         <div class="modal-body">
                             <project-editor
-                                @hide="${() => this.onHide('project')}">
+                                    @hide="${() => this.onHide('project')}">
                             </project-editor>
                         </div>
                     </div>
@@ -211,7 +206,7 @@ export default class StudyDashboard extends LitElement {
                         </div>
                         <div class="modal-body">
                             <study-editor
-                                @hide="${() => this.onHide('study')}">
+                                    @hide="${() => this.onHide('study')}">
                             </study-editor>
                         </div>
                     </div>
