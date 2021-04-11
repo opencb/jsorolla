d7f253c810f98330270b4db29f12cecbc87d197c
imedina <igmecas@gmail.com>
2021-04-11 04:39:34 +0100

    wc: initial work on study admin sample

--- src/core/webcomponents/sample/sample-form.js
+++ src/core/webcomponents/sample/sample-form.js
@@ -20,6 +20,9 @@ import "../commons/tool-header.js";
 
 export default class SampleForm extends LitElement {
 
+    static CREATE_MODE = "create";
+    static UPDATE_MODE = "update";
+    
     constructor() {
         super();
         this._init();
@@ -31,6 +34,9 @@ export default class SampleForm extends LitElement {
 
     static get properties() {
         return {
+            sample: {
+                type: Object
+            },
             study: {
                 type: Object
             },
@@ -48,29 +54,125 @@ export default class SampleForm extends LitElement {
 
     _init() {
         this._prefix = UtilsNew.randomString(8);
+
+        // We initialise the sample in for CREATE
         this.sample = {}
     }
 
     connectedCallback() {
         super.connectedCallback();
 
-        // this._config = { ...this.getDefaultConfig(), ...this.config };
+        this._config = { ...this.getDefaultConfig(), ...this.config };
     }
 
-    // update(changedProperties) {
-    //     if (changedProperties.has("opencgaSession")) {
-    //         this.usersAndProjects = this.getProjetcsPerUser();
-    //     }
-    //     super.update(changedProperties);
-    // }
+    onFieldChange(e) {
+        switch (e.detail.param) {
+            case "id":
+            case "name":
+            case "description":
+                this.sample[e.detail.param] = e.detail.value;
+                break;
+        }
+    }
 
-    // getDefaultConfig() {
-    //     return {
-    //         title: "Study Dashboard",
-    //         icon: "variant_browser.svg",
-    //         active: false
-    //     };
-    // }
+    dispatchSessionUpdateRequest() {
+        this.dispatchEvent(new CustomEvent("sessionUpdateRequest", {
+            detail: {
+            },
+            bubbles: true,
+            composed: true
+        }));
+    }
+
+    getDefaultConfig() {
+        return {
+            title: "Edit",
+            icon: "fas fa-edit",
+            type: "form",
+            buttons: {
+                show: true,
+                cancelText: "Cancel",
+                okText: this.mode === SampleForm.CREATE_MODE ? "Save" : "Update"
+            },
+            display: {
+                // width: "8",
+                style: "margin: 10px",
+                labelWidth: 3,
+                labelAlign: "right",
+                defaultLayout: "horizontal",
+                defaultValue: "",
+                help: {
+                    mode: "block", // icon
+                }
+            },
+            sections: [
+                {
+                    elements: [
+                        {
+                            name: "Sample ID",
+                            field: "id",
+                            type: "input-text",
+                            required: true,
+                            display: {
+                                placeholder: "Add a short ID...",
+                                disabled: this.mode === SampleForm.UPDATE_MODE,
+                                help: {
+                                    text: "short Sample id for thehis as;lsal"
+                                },
+                                validation: {
+
+                                }
+                            },
+                        },
+                        {
+                            name: "Name",
+                            field: "name",
+                            type: "input-text",
+                            display: {
+                                placeholder: "Sample name...",
+                            }
+                        },
+                        {
+                            name: "Creation Date",
+                            field: "creationDate",
+                            type: "input-text",
+                            display: {
+                                placeholder: "Sample name...",
+                                visible: this.mode === SampleForm.UPDATE_MODE,
+                                disabled: this.mode === SampleForm.UPDATE_MODE
+                            }
+                        },
+                        {
+                            name: "Species",
+                            field: "organism.scientificName",
+                            type: "input-text",
+                            display: {
+                                placeholder: "e.g. Homo sapiens, ...",
+                            }
+                        },
+                        {
+                            name: "Species Assembly",
+                            field: "organism.assembly",
+                            type: "input-text",
+                            display: {
+                                placeholder: "e.g. GRCh38",
+                            }
+                        },
+                        {
+                            name: "Description",
+                            field: "description",
+                            type: "input-text",
+                            display: {
+                                rows: 3,
+                                placeholder: "Sample description...",
+                            }
+                        },
+                    ]
+                }
+            ]
+        }
+    }
+
 
     saveSample() {
         // this.opencgaSession.opencgaClient.projects().create(this.project)
@@ -111,140 +213,26 @@ export default class SampleForm extends LitElement {
         //         params.error(err);
         //     });
     }
-
-    dispatchSessionUpdateRequest() {
-        this.dispatchEvent(new CustomEvent("sessionUpdateRequest", {
-            detail: {
-            },
-            bubbles: true,
-            composed: true
-        }));
-    }
-
-    getStudyFormConfig() {
-        return {
-            title: "Edit",
-            icon: "fas fa-edit",
-            type: "form",
-            buttons: {
-                show: true,
-                cancelText: "Cancel",
-                okText: this.mode === "CREATE" ? "Save" : "Update"
-            },
-            display: {
-                style: "margin: 25px 50px 0px 0px",
-                labelWidth: 4,
-                labelAlign: "right",
-                defaultLayout: "horizontal",
-                defaultValue: "",
-                help: {
-                    mode: "block", // icon
-                }
-            },
-            sections: [
-                {
-                    elements: [
-                        {
-                            name: "Sample ID",
-                            field: "id",
-                            type: "input-text",
-                            required: true,
-                            display: {
-                                placeholder: "Add a short ID...",
-                                disabled: this.mode === "UPDATE",
-                                help: {
-                                    text: "short Sample id for thehis as;lsal"
-                                },
-                                validation: {
-
-                                }
-                            },
-                        },
-                        {
-                            name: "Name",
-                            field: "name",
-                            type: "input-text",
-                            display: {
-                                placeholder: "Sample name...",
-                            }
-                        },
-                        {
-                            name: "Creation Date",
-                            field: "creationDate",
-                            type: "input-text",
-                            display: {
-                                placeholder: "Sample name...",
-                                visible: this.mode === "UPDATE",
-                                disabled: this.mode === "UPDATE"
-                            }
-                        },
-                        {
-                            name: "Species",
-                            field: "organism.scientificName",
-                            type: "input-text",
-                            display: {
-                                placeholder: "e.g. Homo sapiens, ...",
-                            }
-                        },
-                        {
-                            name: "Species Assembly",
-                            field: "organism.assembly",
-                            type: "input-text",
-                            display: {
-                                placeholder: "e.g. GRCh38",
-                            }
-                        },
-                        {
-                            name: "Description",
-                            field: "description",
-                            type: "input-text",
-                            display: {
-                                rows: 3,
-                                placeholder: "Sample description...",
-                            }
-                        },
-                    ]
-                }
-            ]
-        }
-    }
-
-    onFieldChange(e) {
-        switch (e.detail.param) {
-            case "id":
-            case "name":
-            case "description":
-                this.sample[e.detail.param] = e.detail.value;
-                break;
-        }
-    }
-
-    onHide() {
-        this.dispatchEvent(new CustomEvent("hide", {
-            detail: {},
-            bubbles: true,
-            composed: true
-        }));
-    }
-
-    onSave(e) {
-        // TODO: Check it's ok ?
-        if (mode == "CREATE") {
+    
+    onSubmit(e) {
+        if (mode === SampleForm.CREATE_MODE) {
             this.saveSample()
         } else {
             this.updateSample()
         }
     }
 
+    onClear() {
 
+    }
 
     render() {
         return html`
             <data-form  .data=${this.sample}
-                        .config="${this.getStudyFormConfig()}"
+                        .config="${this._config}"
                         @fieldChange="${e => this.onFieldChange(e)}"
-                        @clear="${this.onHide}"
-                        @submit="${this.onSave}">
+                        @clear="${this.onClear}"
+                        @submit="${this.onSubmit}">
             </data-form>
         `;
     }
--- src/core/webcomponents/study/study-admin-sample.js
+++ src/core/webcomponents/study/study-admin-sample.js
@@ -15,9 +15,9 @@
  */
 
 import { html, LitElement } from "/web_modules/lit-element.js";
+import SampleForm from "../sample/sample-form.js"
+import DetailTabs from "../commons/view/detail-tabs.js";
 import UtilsNew from "./../../utilsNew.js";
-import GridCommons from "../commons/grid-commons.js";
-import "../sample/sample-form.js"
 
 export default class StudyAdminSample extends LitElement {
 
@@ -34,13 +34,13 @@ export default class StudyAdminSample extends LitElement {
 
     static get properties() {
         return {
-            opencgaSession: {
-                type: Object
-            },
             studyId: {
                 type: String
             },
             study: {
+                type: Object
+            },
+            opencgaSession: {
                 type: Object
             },
             config: {
@@ -51,298 +51,74 @@ export default class StudyAdminSample extends LitElement {
 
     _init() {
         this._prefix = UtilsNew.randomString(8);
-
-        this.gridId = this._prefix + "SampleBrowserGrid";
     }
 
     connectedCallback() {
         super.connectedCallback();
 
         this._config = { ...this.getDefaultConfig(), ...this.config };
-        this.gridCommons = new GridCommons(this.gridId, this, this._config);
-    }
-
-    // Note: WE NEED this function because we are rendering using JQuery not lit-element API
-    firstUpdated(changedProperties) {
-        if (changedProperties.has("study")) {
-            // this.studyObserver();
-        }
     }
 
     update(changedProperties) {
-        if (changedProperties.has("studyId")) {
-            for (const project of this.opencgaSession.projects) {
-                for (const study of project.studies) {
-                    if (study.id === this.studyId || study.fqn === this.studyId) {
-                        this.study = { ...study };
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (changedProperties.has("study")) {
-            // this.studyObserver();
-        }
-
         super.update(changedProperties);
     }
-
-    studyObserver() {
-        this.renderPermissionGrid();
-    }
-
-    renderRemoteTable() {
-        if (this.opencgaSession.opencgaClient && this.opencgaSession.study) {
-            const filters = { ...this.query };
-            // TODO fix and replicate this in all browsers (the current filter is not "filters", it is actually built in the ajax() function in bootstrapTable)
-            if (UtilsNew.isNotUndefinedOrNull(this.lastFilters) &&
-                JSON.stringify(this.lastFilters) === JSON.stringify(filters)) {
-                // Abort destroying and creating again the grid. The filters have not changed
-                return;
-            }
-
-            this.table = $("#" + this.gridId);
-            this.table.bootstrapTable("destroy");
-            this.table.bootstrapTable({
-                columns: this._getDefaultColumns(),
-                method: "get",
-                sidePagination: "server",
-                uniqueId: "id",
-                // Table properties
-                pagination: this._config.pagination,
-                pageSize: this._config.pageSize,
-                pageList: this._config.pageList,
-                paginationVAlign: "both",
-                formatShowingRows: this.gridCommons.formatShowingRows,
-                showExport: this._config.showExport,
-                detailView: this._config.detailView,
-                detailFormatter: this._config.detailFormatter,
-                gridContext: this,
-                formatLoadingMessage: () => "<div><loading-spinner></loading-spinner></div>",
-                ajax: params => {
-                    const _filters = {
-                        study: this.opencgaSession.study.fqn,
-                        limit: params.data.limit,
-                        skip: params.data.offset || 0,
-                        count: !this.table.bootstrapTable("getOptions").pageNumber || this.table.bootstrapTable("getOptions").pageNumber === 1,
-                        ...filters
-                    };
-                    // Store the current filters
-                    this.lastFilters = { ..._filters };
-                    this.opencgaSession.opencgaClient.studies().searchAudit(_filters)
-                        .then(sampleResponse => {
-                            // Fetch clinical analysis to display the Case ID
-                            const individualIds = sampleResponse.getResults().map(sample => sample.individualId).filter(Boolean).join(",");
-                            if (individualIds) {
-                                this.opencgaSession.opencgaClient.clinical().search(
-                                    {
-                                        member: individualIds,
-                                        study: this.opencgaSession.study.fqn,
-                                        exclude: "proband.samples,family,interpretation,files"
-                                    })
-                                    .then(caseResponse => {
-                                        // We store the Case ID in the individual attribute
-                                        // Note clinical search results are not sorted
-                                        // FIXME at the moment we only search by proband
-                                        const map = {};
-                                        for (const clinicalAnalysis of caseResponse.responses[0].results) {
-                                            if (!map[clinicalAnalysis.proband.id]) {
-                                                map[clinicalAnalysis.proband.id] = [];
-                                            }
-                                            map[clinicalAnalysis.proband.id].push(clinicalAnalysis);
-                                        }
-                                        for (const sample of sampleResponse.responses[0].results) {
-                                            sample.attributes.OPENCGA_CLINICAL_ANALYSIS = map[sample.individualId];
-                                        }
-                                        params.success(sampleResponse);
-                                    })
-                                    .catch(e => {
-                                        console.error(e);
-                                        params.error(e);
-                                    });
-                            } else {
-                                params.success(sampleResponse);
-                            }
-                        })
-                        .catch(e => {
-                            console.error(e);
-                            params.error(e);
-                        });
-                },
-                responseHandler: response => {
-                    const result = this.gridCommons.responseHandler(response, $(this.table).bootstrapTable("getOptions"));
-                    return result.response;
-                },
-                onClickRow: (row, selectedElement, field) => this.gridCommons.onClickRow(row.id, row, selectedElement),
-                onDblClickRow: (row, element, field) => {
-                    // We detail view is active we expand the row automatically.
-                    // FIXME: Note that we use a CSS class way of knowing if the row is expand or collapse, this is not ideal but works.
-                    if (this._config.detailView) {
-                        if (element[0].innerHTML.includes("icon-plus")) {
-                            this.table.bootstrapTable("expandRow", element[0].dataset.index);
-                        } else {
-                            this.table.bootstrapTable("collapseRow", element[0].dataset.index);
-                        }
-                    }
-                },
-                onCheck: (row, $element) => {
-                    this.gridCommons.onCheck(row.id, row);
-                },
-                onCheckAll: rows => {
-                    this.gridCommons.onCheckAll(rows);
-                },
-                onUncheck: (row, $element) => {
-                    this.gridCommons.onUncheck(row.id, row);
-                },
-                onUncheckAll: rows => {
-                    this.gridCommons.onUncheckAll(rows);
-                },
-                onLoadSuccess: data => {
-                    this.gridCommons.onLoadSuccess(data, 1);
-                },
-                onLoadError: (e, restResponse) => this.gridCommons.onLoadError(e, restResponse),
-                onPostBody: data => {
-                    // Add tooltips?
-                }
-            });
-        }
-    }
-
-    renderPermissionGrid() {
-        this.table = $("#" + this.gridId);
-        this.table.bootstrapTable("destroy");
-        this.table.bootstrapTable({
-            columns: this._getDefaultColumns(),
-            data: this.studyPermissions,
-            sidePagination: "local",
-
-            // Set table properties, these are read from config property
-            uniqueId: "id",
-            pagination: this._config.pagination,
-            pageSize: this._config.pageSize,
-            pageList: this._config.pageList,
-            showExport: this._config.showExport,
-            detailView: this._config.detailView,
-            // detailFormatter: this.detailFormatter,
-            formatLoadingMessage: () => "<div><loading-spinner></loading-spinner></div>",
-
-            onClickRow: (row, selectedElement, field) => this.gridCommons.onClickRow(row.id, row, selectedElement),
-            onPostBody: data => {
-                // We call onLoadSuccess to select first row
-                this.gridCommons.onLoadSuccess({ rows: data, total: data.length }, 1);
-            }
-        });
-    }
-
-    _getDefaultColumns() {
-        return [
-            {
-                title: "Audit Record ID",
-                field: "id",
-            },
-            {
-                title: "User ID",
-                field: "userId",
-            },
-            {
-                title: "Study ID",
-                field: "studyId",
-            },
-            {
-                title: "Action",
-                field: "action",
-                formatter: (value, row) => `${row.action}-${row.resource}`
-            },
-            {
-                title: "Resource ID",
-                field: "resourceId",
-            },
-            {
-                title: "Study Permission",
-                field: "id",
-            },
-            {
-                title: "Date",
-                field: "date",
-                formatter: (value) => `${UtilsNew.dateFormatter(value)}`
-            },
-            {
-                title: "Status",
-                field: "status.name",
-            },
-        ];
-    }
-
+    
     getDefaultConfig() {
         return {
-            pagination: true,
-            pageSize: 25,
-            pageList: [25, 50],
-            showExport: false,
-            detailView: false,
-            detailFormatter: null, // function with the detail formatter
-            multiSelection: false,
-            showSelectCheckbox: true,
-            showToolbar: true,
-            showActions: true,
+            items: [
+                {
+                    id: "view-sample",
+                    name: "View Sample",
+                    // icon: "fas fa-notes-medical",
+                    active: true,
+                    render: (study, active, opencgaSession) => {
+                        return html`
+                            <div class="row">
+                                <div class="col-md-6" style="margin: 20px 10px">
+                                    <sample-form
+                                            .opencgaSession="${opencgaSession}"
+                                            .mode="${SampleForm.UPDATE_MODE}">
+                                    </sample-form>
+                                </div>
+                            </div>`;
+                    }
+                },
+                {
+                    id: "create-sample",
+                    name: "Create Sample",
+                    // icon: "fas fa-dna",
+                    // active: false,
+                    render: (study, active, opencgaSession) => {
+                        return html`
+                            <div class="row">
+                                <div class="col-md-6" style="margin: 20px 10px">
+                                    <sample-form
+                                            .opencgaSession="${opencgaSession}"
+                                            .mode="${SampleForm.CREATE_MODE}">
+                                    </sample-form>
+                                </div>
+                            </div>`;
+                    }
+                }
+            ]
         };
-    }
-
-    // TODO: how to refactored those function actionModal & renderModal (soon)
-    actionModal(modalId, action, sample = {}, mode = "CREATE") {
-        // action: show or hide
-        // mode: CREATE or UPDATE
-        this.mode = mode;
-        if (sample && mode === "UPDATE") {
-            this.sample = sample;
-        } else {
-            this.sample = {};
-        }
-        this.requestUpdate();
-        $(`#${modalId}`).modal(action);
-    }
-
-    renderModal(modalId, name) {
-        return html`
-            <div id="${modalId}" class="modal fade" tabindex="-1" role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title">New ${name}</h4>
-                        </div>
-                        <div class="modal-body">
-                            <sample-form
-                                .opencgaSession="${this.opencgaSession}"
-                                .mode=${this.mode}
-                                @hide="${() => this.actionModal(modalId, 'hide')}">
-                            </sample-form>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
     }
 
     render() {
         return html`
-            <div class="pull-left" style="margin: 10px 0px">
-                Sample Component
-            </div>
-
-            <div class="pull-right" style="margin: 10px 0px">
-                <div style="display:inline-block; margin: 0px 20px">
-                    <button class="btn-custom btn btn-primary" 
-                        @click="${() => this.actionModal('newSample', 'show')}">New Sample
-                    </button>
+            <!--
+                <div class="pull-right" style="margin: 0px 10px">
+                    <i class="fa fa-info" aria-hidden="true"></i>
                 </div>
+            -->
+            
+            <div style="margin: 25px 40px">
+                <detail-tabs
+                        .config="${this._config}"
+                        .mode="${DetailTabs.PILLS_MODE}"
+                        .opencgaSession="${this.opencgaSession}">
+                </detail-tabs>
             </div>
-
-            <!-- <div id="${this._prefix}GridTableDiv" class="force-overflow" style="margin: 20px 0px">
-                <table id="${this._prefix}AuditBrowserGrid"></table>
-            </div> -->
-
-            ${this.renderModal('newSample', 'Sample')}
         `;
     }
 }
