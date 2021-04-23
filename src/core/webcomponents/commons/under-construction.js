67427a6ec3ef201f62917b4c14c7648f472e1881
Rodiel Martinez <Rodielmj@gmail.com>
2021-04-23 16:11:46 +0200

    wc: add under-construction template

---
+++ src/core/webcomponents/commons/under-construction.js
@@ -0,0 +1,9 @@
+import { html } from "/web_modules/lit-element.js";
+
+export const construction =
+    html`
+    <div class="guard-page">
+        <i class="fas fa-pencil-ruler fa-5x"></i>
+        <h3>Component under construction</h3>
+        <h3>(Coming Soon)</h3>
+    </div>`
