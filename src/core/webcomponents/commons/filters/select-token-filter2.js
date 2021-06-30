bef19ec2a5fe234464bcec3d986fe43dc5328e07
antonioaltamura <antonioaltamura7@gmail.com>
2021-06-30 15:18:30 +0200

    select-token-filter2.js debug in progress

--- src/core/webcomponents/commons/filters/select-token-filter2.js
+++ src/core/webcomponents/commons/filters/select-token-filter2.js
@@ -19,6 +19,8 @@ import UtilsNew from "../../../utilsNew.js";
 
 /**
  * Select2 version
+ *
+ * TODO FIXME Update from active-filters lead to an inconsistent state.
  */
 
 export default class SelectTokenFilter2 extends LitElement {
@@ -53,12 +55,14 @@ export default class SelectTokenFilter2 extends LitElement {
     }
 
     _init() {
-        this._prefix = "tk-" + UtilsNew.randomString(6) + "_";
+        this._prefix = "select-" + UtilsNew.randomString(6) + "_";
+        this.separator = ",";
     }
 
     connectedCallback() {
         super.connectedCallback();
         this._config = {...this.getDefaultConfig(), ...this.config};
+        this.state = [];
     }
 
     firstUpdated(_changedProperties) {
@@ -90,11 +94,25 @@ export default class SelectTokenFilter2 extends LitElement {
                     };
                 }
             },
-            templateResult: (r) => {
+            templateResult: r => {
                 return $("<span>" + r.id + "<p class='dropdown-item-extra'><label>ID</label>" + r.id + "</p></span>");
             }
 
-        });
+        })
+            .on("select2:select", e => {
+                console.log("adding", e.params.data.id);
+                console.log($("#" + this._prefix).select2("data"));
+                this.state = [...this.state, e.params.data.id];
+                this.value = this.state.join(",");
+                this.filterChange(e);
+            })
+            .on("select2:unselect", e => {
+                console.log("removing", e.params.data.id);
+                console.log($("#" + this._prefix).select2("data"));
+                this.state = this.state.filter(el => el.id === e.params.data.id);
+                this.value = this.state.join(",");
+                this.filterChange(e);
+            });
 
     }
 
@@ -103,13 +121,24 @@ export default class SelectTokenFilter2 extends LitElement {
             this._config = {...this.getDefaultConfig(), ...this.config};
         }
         if (_changedProperties.has("value")) {
-
+            console.log("this.value", this.value)
+            //const selection = this.value ? this.value.split(this.separator) : null;
+            //$("#" + this._prefix).val(selection); // this wont work as options arent actually there since there is an ajax source
+            // $("#" + this._prefix).trigger('change');
+            //this.requestUpdate();
         }
 
     }
 
     filterChange(e) {
-
+        const selection = $("#" + this._prefix).select2("data");
+        console.log("filterChange", selection)
+        const event = new CustomEvent("filterChange", {
+            detail: {
+                value: selection.map(el => el.id).join(this.separator)
+            }
+        });
+        this.dispatchEvent(event);
     }
 
     getDefaultConfig() {
@@ -122,9 +151,10 @@ export default class SelectTokenFilter2 extends LitElement {
 
     render() {
         return html`
-            
+        ${JSON.stringify(this.value)}
         <div class="">
-            <select id="${this._prefix}" style="width: 100%">
+            <select id="${this._prefix}" style="width: 100%" @change="${this.filterChange}">
+                ${this.value?.split(this.separator)?.map(el => html`<option>${el}</option>`)}
             </select>
         </div>
         `;
