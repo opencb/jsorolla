2b09270edc4c1461cf2db4d856c2ef87c0508f46
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2021-08-18 18:11:34 +0200

    Upgrade Webpack to v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require("path");
-const webpack = require("webpack");
+const {webpack, IgnorePlugin} = require("webpack");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
-// const StringReplacePlugin = require("string-replace-webpack-plugin"); webpack 2
-const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
-const PluginProposalExportDefaultFrom = require("@babel/plugin-proposal-export-default-from"); // Allows `export .. from` syntax in the entry point
 const MergeIntoSingleFilePlugin = require("webpack-merge-and-include-globally");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
 const packageJson = require("./package.json");
 const DIST_PATH = path.resolve(__dirname, "build/");
-// const revision = require("./rev-info.js");
 const execSync = require("child_process").execSync;
 const ivaPath = path.resolve(__dirname, "src/sites/iva");
+
+// const StringReplacePlugin = require("string-replace-webpack-plugin"); webpack 2
+// const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
+// const PluginProposalExportDefaultFrom = require("@babel/plugin-proposal-export-default-from"); // Allows `export .. from` syntax in the entry point
+// const TerserPlugin = require("terser-webpack-plugin");
+// const ESLintPlugin = require("eslint-webpack-plugin");
+// const revision = require("./rev-info.js");
+
 const revision = () => {
     try {
         const jsorollaBranch = execSync("git rev-parse --abbrev-ref HEAD").toString();
@@ -46,7 +48,7 @@ module.exports = {
         "iva-app": ivaPath + "/iva-app.js"
     },
     output: {
-        filename: "[name][hash].js",
+        filename: "[name][contenthash].js",
         path: DIST_PATH
     },
     plugins: [
@@ -134,60 +136,61 @@ module.exports = {
                 "assets/css/styles.css": code => require("uglifycss").processString(code)
             }
         }),
-        new CopyWebpackPlugin([
-            {
-                context: ivaPath,
-                from: "LICENSE",
-                to: DIST_PATH
-            },
-            {
-                context: ivaPath,
-                from: "README.md",
-                to: DIST_PATH
-            },
-            {
-                context: ivaPath + "/conf",
-                from: "**/*.js",
-                to: DIST_PATH + "/conf"
-            },
-            {
-                from: ivaPath + "/favicon.ico",
-                to: DIST_PATH
-            },
-            {
-                context: ivaPath + "/img",
-                from: "**/*",
-                to: DIST_PATH + "/img"
-            },
-            {
-                context: "./styles/fonts",
-                from: "**/*",
-                to: DIST_PATH + "/assets/fonts"
-            },
-            {
-                context: "./node_modules/bootstrap/dist/fonts",
-                from: "**/*",
-                to: DIST_PATH + "/assets/fonts"
-            },
-            {
-                context: "./node_modules/@webcomponents/webcomponentsjs",
-                from: "**/*.js",
-                to: DIST_PATH + "/webcomponents"
-            },
-            {
-                context: "./node_modules/@fortawesome/fontawesome-free/webfonts",
-                from: "*",
-                to: DIST_PATH + "/assets/webfonts"
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    context: ivaPath,
+                    from: "LICENSE",
+                    to: DIST_PATH
+                },
+                {
+                    context: ivaPath,
+                    from: "README.md",
+                    to: DIST_PATH
+                },
+                {
+                    context: ivaPath + "/conf",
+                    from: "**/*.js",
+                    to: DIST_PATH + "/conf"
+                },
+                {
+                    from: ivaPath + "/favicon.ico",
+                    to: DIST_PATH
+                },
+                {
+                    context: ivaPath + "/img",
+                    from: "**/*",
+                    to: DIST_PATH + "/img"
+                },
+                {
+                    context: "./styles/fonts",
+                    from: "**/*",
+                    to: DIST_PATH + "/assets/fonts"
+                },
+                {
+                    context: "./node_modules/bootstrap/dist/fonts",
+                    from: "**/*",
+                    to: DIST_PATH + "/assets/fonts"
+                },
+                {
+                    context: "./node_modules/@webcomponents/webcomponentsjs",
+                    from: "**/*.js",
+                    to: DIST_PATH + "/webcomponents"
+                },
+                {
+                    context: "./node_modules/@fortawesome/fontawesome-free/webfonts",
+                    from: "*",
+                    to: DIST_PATH + "/assets/webfonts"
 
-            }
-        ]),
+                }
+            ]}),
         /* new MethodExtractor({options: true, output: DIST_PATH + "/conf", components: [
             "./lib/jsorolla/src/core/webcomponents/opencga/catalog/cohorts/opencga-cohort-browser.js"
             ]})*/
         // ignore is not the best way to externalize a resource, but webpack don't support external ES modules yet.
         // ignore makes sense because iva-app bundle will be an ES module on its own, so import X from "/jsorolla.min.js" won't be a problem if not processed by webpack
         // why do whe need to bundle iva-app in webpack at all then? Because we need to process litElement imports
-        new webpack.IgnorePlugin({
+        new IgnorePlugin({
             checkResource(resource) {
                 // console.log("res", resource)
                 // if (resource === "./conf/opencga-variant-browser.config.js") return true;
@@ -233,7 +236,7 @@ module.exports = {
                 use: "null-loader"
             },*/
             {
-                test: /\.html$/,
+                test: /\.html$/i,
                 use: ["html-loader"] // rewrite html content (replace automatically <img src="img.jpg"/> in require("img.jpg"))
             },
             {
