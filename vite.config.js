55cad63f1f4f869cf4ac59f65529226eae1cc7a0
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2021-08-30 10:40:47 +0200

    minor improve rollup config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import {defineConfig} from "vite";
 import html from "@web/rollup-plugin-html";
 import copy from "rollup-plugin-copy";
 import resolve from "@rollup/plugin-node-resolve";
-import {terser} from "rollup-plugin-terser";
 import minifyHTML from "rollup-plugin-minify-html-literals";
 import summary from "rollup-plugin-summary";
 import path from "path";
 import del from "rollup-plugin-delete";
+import {babel, getBabelOutputPlugin} from "@rollup/plugin-babel";
+
 
 const buildPath = path.resolve(__dirname, "build-vite");
 const ivaPath = path.resolve(__dirname, "src/sites/iva");
+const patternExt = /\.[0-9a-z]+$/i;
+const patternConfig = /(config|settings|constants|tools)/gi;
 
 export default defineConfig({
     mode: "development",
@@ -22,35 +25,51 @@ export default defineConfig({
     },
     build: {
         rollupOptions: {
-            input: `${ivaPath}/iva-index.html`,
+            input: [`${ivaPath}/iva-index.html`, "src/genome-browser/demo/genome-browser.html"],
             preserveEntrySignatures: "strict",
             plugins: [
                 del({targets: "build-vite/*"}),
                 html(),
                 resolve(),
                 minifyHTML(),
-                terser({
-                    ecma: 2020,
-                    module: true,
-                    warnings: true,
-                }),
+                summary(),
                 copy({
                     targets: [
                         {src: `${ivaPath}/img`, dest: `${buildPath}`},
                         {src: `${ivaPath}/LICENSE`, dest: `${buildPath}`},
                         {src: `${ivaPath}/README.md`, dest: `${buildPath}`},
-                        {src: `${ivaPath}/conf/*.js`, dest: `${buildPath}/conf`},
                         {src: `${ivaPath}/favicon.ico`, dest: `${buildPath}`},
                         {src: "./styles/fonts", dest: `${buildPath}/`},
                         {src: "./node_modules/bootstrap/dist/fonts", dest: `${buildPath}/`},
                         {src: "./node_modules/@fortawesome/fontawesome-free/webfonts", dest: `${buildPath}/`},
                     ]
                 }),
-                summary(),
             ],
             output: {
                 dir: "build-vite",
+                assetFileNames: assetInfo => {
+                    if (assetInfo.name.match(patternConfig) !== null) {
+                        return "conf/[name][extname]";
+                    }
+                    return "assets/[name]-[hash][extname]";
+                },
+                plugins: [
+                    getBabelOutputPlugin({
+                        presets: ["@babel/preset-env"],
+                        plugins: [
+                            "@babel/plugin-proposal-export-default-from",
+                            "@babel/plugin-proposal-nullish-coalescing-operator",
+                            "@babel/transform-runtime",
+                            ["@babel/plugin-proposal-class-properties", {"loose": false}]
+                        ]
+                    })
+                ]
             }
+        },
+        terserOptions: {
+            ecma: 2020,
+            module: true,
+            warnings: true
         }
     }
 });
