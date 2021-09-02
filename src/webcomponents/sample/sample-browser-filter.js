50866afd38ff93b837c178ca297705ffae18dd36
antonioaltamura <antonioaltamura7@gmail.com>
2021-09-02 17:30:25 +0200

    new token component included in sample-id-autocomplete-token.js #165

--- src/webcomponents/sample/sample-browser-filter.js
+++ src/webcomponents/sample/sample-browser-filter.js
@@ -27,9 +27,9 @@ import "../commons/filters/select-token-filter.js";
 import "../commons/forms/select-field-filter-autocomplete.js";
 import "../commons/filters/sample-id-autocomplete.js";
 import "../commons/filters/individual-id-autocomplete.js";
-import "../commons/filters/select-token-filter2.js";
+import "../commons/forms/select-token-filter2.js";
 import "../commons/filters/select-token-filter.js";
-
+import "../commons/filters/sample-id-autocomplete-token.js";
 
 export default class SampleBrowserFilter extends LitElement {
 
@@ -157,11 +157,11 @@ export default class SampleBrowserFilter extends LitElement {
         switch (subsection.id) {
             case "id":
                 content = html`
-                    <!--<select-token-filter2 .config="${subsection}" .opencgaSession="${this.opencgaSession}" .value="${this.preparedQuery[subsection.id]}" ></select-token-filter2>
-                    <select-token-filter .config="${subsection}" resource="SAMPLE" .opencgaSession="${this.opencgaSession}" .value="${this.preparedQuery[subsection.id]}" ></select-token-filter>-->
-                    <sample-id-autocomplete .config="${subsection}" .opencgaSession="${this.opencgaSession}" .value="${this.preparedQuery[subsection.id]}"
-                                            @filterChange="${e => this.onFilterChange(subsection.id, e.detail.value)}">
-                    </sample-id-autocomplete>`;
+                    <sample-id-autocomplete-token .config="${subsection}"
+                                                  .opencgaSession="${this.opencgaSession}"
+                                                  .value="${this.preparedQuery[subsection.id]}"
+                                                  @filterChange="${e => this.onFilterChange(subsection.id, e.detail.value)}">
+                    </sample-id-autocomplete-token>`;
                 break;
             case "individualId":
                 content = html`
