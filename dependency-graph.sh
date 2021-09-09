ff16eb38fcca05d7b041b36949cf75d8e7be5757
antonioaltamura <antonioaltamura7@gmail.com>
2021-09-09 16:21:45 +0200

    wc dependency graph generator added #168

---
+++ dependency-graph.sh
@@ -0,0 +1,21 @@
+exclude=\
+"node_modules|"\
+"src/genome-browser|"\
+"src/webcomponents/loading-spinner.js|"\
+"src/webcomponents/NotificationUtils.js|"\
+"src/core/src/core/utils.js|"\
+"src/core/utilsNew.js|"\
+"src/webcomponents/PolymerUtils.js|"\
+"src/core/clients|"\
+"src/core/visualisation|"\
+"src/webcomponents/opencga/clinical/obsolete|"\
+"src/webcomponents/commons/filters/deprecated|"\
+"src/webcomponents/variant/deprecated|"\
+"./deprecated|"\
+"src/webcomponents/Notification.js"
+
+depcruise "src/webcomponents/**/*.js" -x "^($exclude)" --output-type dot | dot -T svg > dependency.svg
+depcruise "src/sites/iva/**/*.js" -x "^($exclude)" --output-type json > dependency.json
+
+# depcruise "lib/jsorolla/src/core/webcomponents/**/*.js" -x "^($exclude)" --output-type dot | dot -Gsplines=ortho -Grankdir=TD -T svg > dependency.svg
+# depcruise "lib/jsorolla/src/core/webcomponents/**/*.js" -x "^($exclude)" --output-type ddot | dot -Gsplines=ortho -T svg > dependency.svg
