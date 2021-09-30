a5e601e469df74076969533755707c5e8f8b5d52
Josemi <josemijuanes@gmail.com>
2021-09-30 22:45:38 +0200

    wc: add custom comparators in number-field-filter

--- src/webcomponents/commons/forms/data-form.js
+++ src/webcomponents/commons/forms/data-form.js
@@ -552,10 +552,16 @@ export default class DataForm extends LitElement {
         // debugger
         return html`
             <div class="">
-                <number-field-filter label="Value" .value="${value ? value : ""}"
-                                     .min=${min} .max=${max} .step="${step}" .placeholder="${element.display?.placeholder || ""}"
-                                     .classes="${element.display?.updated ? "updated" : ""}"
-                                     @filterChange="${e => this.onFilterChange(element.field, e.detail.value)}">
+                <number-field-filter
+                    label="Value"
+                    .value="${value ? value : ""}"
+                    .comparators=${element.comparators || null}
+                    .min=${min}
+                    .max=${max}
+                    .step="${step}"
+                    .placeholder="${element.display?.placeholder || ""}"
+                    .classes="${element.display?.updated ? "updated" : ""}"
+                    @filterChange="${e => this.onFilterChange(element.field, e.detail.value)}">
                 </number-field-filter>
             </div>
         `;
@@ -1056,8 +1062,9 @@ export default class DataForm extends LitElement {
 
         if (this.config.display && this.config.display?.mode?.type === "modal") {
             const buttonClass = this.config.display.mode.buttonClass ? this.config.display.mode.buttonClass : "btn-primary";
+            const isDisabled = this.config.display.mode.disabled === true;
             return html`
-                <button type="button" class="btn ${buttonClass} ${this.config.display.mode.disabled === true ? "disabled" : null}" data-toggle="modal" disabled="${ifDefined(this.config.display.mode.disabled === true ? "disabled" : undefined)}" data-target="#${this._prefix}DataModal">
+                <button type="button" class="btn ${buttonClass} ${isDisabled ? "disabled" : null}" data-toggle="modal" disabled="${isDisabled ? "disabled" : undefined}" data-target="#${this._prefix}DataModal">
                     <i class="${this.config.icon ? this.config.icon : "fas fa-info-circle"} icon-padding" aria-hidden="true"></i> ${this.config.title}
                 </button>
                 <div class="modal fade" id="${this._prefix}DataModal" tabindex="-1" role="dialog" aria-labelledby="${this._prefix}exampleModalLabel"
