80a4cdfabaca2df5d1f3a83b61ee04450e5cf9af
Josemi <josemijuanes@gmail.com>
2021-10-13 15:39:52 +0200

    Switch to custom-sites and fix structure #188

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import {defineConfig} from "vite";
 const env = process.env || {};
 const sites = ["iva"];
 
-const getSiteConfigPath = name => {
-    if (env.npm_config_custom_config) {
-        return `../../../custom-conf/${env.npm_config_custom_config}/${name}`;
+const getCustomSitePath = (name, folder) => {
+    if (env.npm_config_custom_site) {
+        return `../../../custom-sites/${env.npm_config_custom_site}/${name}/${folder}`;
     }
-    return "conf"; // Default path configuration
+    return folder; // Default path configuration
 };
 
 const transformHtmlContent = html => {
     sites.forEach(name => {
         const regex = new RegExp(`{{ ${name.toUpperCase()}_CONFIG_PATH }}`, "g");
-        html = html.replace(regex, getSiteConfigPath(name));
+        html = html.replace(regex, getCustomSitePath(name, "conf"));
     });
     return html;
 };
@@ -24,7 +24,7 @@ export default defineConfig({
     server: {
         open: "/src/sites/iva/index.html",
         port: 3000,
-        watch: ["src", "styles", "custom-conf"]
+        watch: ["src", "styles", "custom-sites"]
     },
     plugins: [
         {
