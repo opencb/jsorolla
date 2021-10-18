21835344fb59de2c03a4dc8239a2eb7d243caf59
Josemi <josemijuanes@gmail.com>
2021-10-18 13:55:03 +0200

    Add more placeholder pages #186

--- src/sites/iva/conf/pages.js
+++ src/sites/iva/conf/pages.js
@@ -3,6 +3,21 @@ const CUSTOM_PAGES = [
     {
         url: "about",
         title: "About",
-        content: "Work in progress...",
-    }
+        content: "Content of the <b>About</b> page.",
+    },
+    {
+        url: "contact",
+        title: "Contact",
+        content: "Content of the <b>Contact</b> page.",
+    },
+    {
+        url: "terms",
+        title: "Terms of service",
+        content: "Content of the <b>Terms of service</b> page.",
+    },
+    {
+        url: "faq",
+        title: "Frequently Asked Questions",
+        content: "Content of the <b>FAQ</b> page.",
+    },
 ];
