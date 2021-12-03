bc65309f5b7e97da200587ba7c89c493b1f8e27b
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2021-12-03 13:53:01 +0100

    :memo: minor improvement data-form model config #278

--- src/webcomponents/commons/types.js
+++ src/webcomponents/commons/types.js
@@ -2,57 +2,122 @@ export default class Types {
 
     /**
     * Buttons type definitions
-    * @typedef {Object} button
-    * @property {string} button.okText - text for the ok buttons
-    * @property {string} button.cancelText - text for the cancel buttons
+    * @typedef {Object} Button
+    * @property {string} okText - text for the ok buttons
+    * @property {string} cancelText - text for the cancel buttons
     **/
 
     /**
     * Help type definitions
-    * @typedef {Object} help
-    * @property {string} help.mode - indicate the mode of the help
+    * @typedef {Object} Help
+    * @property {string} icon - indicate the mode of the help
+    * @property {string} message
+    **/
+
+
+    /**
+     * Layout type definitions
+     * @typedef {Object} Layout
+     * @property {string} id
+     * @property {string} className
+     * @property {string} style
+     */
+
+    /**
+    * Validation type definitions
+    * @typedef {Object} Validation
+    * @property {Function} validate -
+    * @property {string} message -
     **/
 
     /**
     * Display type definitions
-    * @typedef {Object} display
-    * @property {string} display.style - add a style for the display
-    * @property {number} display.labelWidth - add a with for the display
-    * @property {string} display.labelAlign - add a label align for the display
-    * @property {string} display.defaultLayout - vertical or horizontal
-    * @property {string} [display.defaultValue] - add a default value for display
-    * @property {help} display.help - show a help for the display
+    * @typedef {Object} Display
+    * @property {string} className -
+    * @property {string} style - add a style for the display
+    * @property {string} buttonClassName -
+    * @property {string} buttonStyle -
+    * @property {string} mode -
+    * @property {string} [defaultValue] - add a default value for display
+    * @property {string} [defaultWidth] - add a default value for display
+    * @property {string} [defaultLayout] - add a default value for display
+    * @property {Layout} layout -
+    * @property {Help} help - show a help for the display
+    * @property {number} labelWidth - add a with for the display
+    * @property {string} labelAlign - add a label align for the display
+    * @property {string} width -
+    * @property {string} errorIcon -
+    * @property {string} title -
+    * @property {boolean} showtitle -
+    * @property {string} errorMessage -
+    **/
+
+
+    /**
+    * Column type definitions
+    * @typedef {Object} Column
+    * @property {string} className - name of the element
+    * @property {string} style - type of element form
+    * @property {boolean} defaultValue - is it the element required? True or False
+    * @property {Array} field -
+    * @property {display} type - custom display forEl the element
+    * @property {Function} render -
+    * @property {string} format -
     **/
 
     /**
     * Element type definitions
-    * @typedef {Object} element
-    * @property {string} element.name - name of the element
-    * @property {string} element.type - type of element form
-    * @property {boolean} element.required - is it the element required? True or False
-    * @property {display} element.display - custom display for the element
+    * @typedef {Object} Data
+    * @property {string} key
+    * @property {string} value
+    **/
+
+    /**
+    * Element type definitions
+    * @typedef {Object} Element
+    * @property {string} name - name of the element
+    * @property {string} field - type of element form
+    * @property {boolean} required - is it the element required? True or False
+    * @property {boolean} showLabel
+    * @property {Array} allowedValues -
+    * @property {Display} display - custom display forEl the element
+    * @property {Data[]} data
+    * @property {Column[]} columns
+    * @property {Validation} validation -
     **/
 
     /**
     * Section type definitions
-    * @typedef {Object} section
-    * @property {string} section.title - add a title
-    * @property {Array<element>} section.elements - add elements
+    * @typedef {Object} Section
+    * @property {string} id -
+    * @property {string} title - add a title
+    * @property {string} text -
+    * @property {Display} display -
+    * @property {Array<Element>} elements - add elements
     **/
 
     /**
      * Represents a data form config
-     * @param {object} config - define the config for data-form
-     * @param {string} config.type - type to the config form
-     * @param {string} config.name - name of the config
-     * @param {button} config.button - custom buttons for the data-form config
-     * @param {section[]} config.section - add section config
-     * @returns {object} - return config
+     * @typedef {Object} Config - define the config for data-form
+     * @property {string} title -
+     * @property {string} description -
+     * @property {string} icon -
+     * @property {string} type - type to the config form
+     * @property {Button} button - custom buttons for the data-form config
+     * @property {Section[]} section - add section config
+     * @property {Validation} validation -
+     */
+
+
+    /**
+     * Represent a config
+     * @param {Config} config - config object
+     * @returns {Config} - return configs
      */
     static dataFormConfig(config) {
         return config;
     }
-
+    // static dataFormConfig = config => ({...config})
 
 }
 
