46d0acceaa40b9e71ec22d4590daf4091aa48686
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2021-12-13 12:42:19 +0100

    :memo: upgraded data-form  model config #278

--- src/webcomponents/commons/types.js
+++ src/webcomponents/commons/types.js
@@ -41,28 +41,6 @@ export default class Types {
     * @typedef {("card"|"modal"|"normal")} mode
     **/
 
-    /**
-    * Display type definitions
-    * @typedef {Object} Display
-    * @property {string} className -
-    * @property {string} style - add a style for the display
-    * @property {string} buttonClassName -
-    * @property {string} buttonStyle -
-    * @property {mode} mode -
-    * @property {string} [defaultValue] - add a default value for display
-    * @property {string} [defaultWidth] - add a default value for display
-    * @property {string} [defaultLayout] - add a default value for display
-    * @property {Layout} layout -
-    * @property {Help} help - show a help for the display
-    * @property {number} labelWidth - add a with for the display
-    * @property {string} labelAlign - add a label align for the display
-    * @property {string} width -
-    * @property {string} errorIcon -
-    * @property {string} title -
-    * @property {boolean} showtitle -
-    * @property {string} errorMessage -
-    **/
-
 
     /**
     * Column type definitions
@@ -77,24 +55,113 @@ export default class Types {
     **/
 
     /**
-    * Element type definitions
+    * Data type definitions
     * @typedef {Object} Data
-    * @property {string} key
-    * @property {string} value
+    * @property {string} key - name of the element
+    * @property {string} value - type of element form
+    **/
+
+    /**
+    * Display type definitions
+    * @typedef {Object} DisplayForm
+    * @property {string} className -
+    * @property {string} style - add a style for the display
+    * @property {string} buttonClassName -
+    * @property {string} buttonStyle -
+    * @property {string} showButtons -
+    * @property {string} showTopButtons -
+    * @property {mode} mode -
+    * @property {string} [defaultValue] - add a default value for display
+    * @property {string} [defaultWidth] - add a default value for display
+    * @property {string} [defaultLayout] - add a default value for display
+    * @property {Layout} layout -
+    * @property {number} labelWidth - add a with for the display
+    * @property {number} width -
+    * @property {string} errorIcon -
+    * @property {string} errorMessage -
+    * @property {string} helpIcon -
+    * @property {string} titleClassName -
+    * @property {string} titleStyle -
+    * @property {boolean} showtitle -
+    * @property {boolean} modalWidth  -
+    **/
+
+    /**
+    * Display type definitions
+    * @typedef {Object} DisplaySection
+    * @property {number} labelWidth - add a with for the display
+    * @property {string} errorIcon -
+    * @property {string} helpIcon - show a help for the display
+    * @property {string} titleHeader -
+    * @property {string} titleWidth -
+    * @property {string} titleClassName -
+    * @property {string} titleStyle - add a style for the display
+    * @property {string} textClassName -
+    * @property {string} textStyle -
+    * @property {string} className -
+    * @property {string} style -
+    * @property {string} leftColumnWidth -
+    * @property {string} rightColumnWidth -
+    * @property {string} columnSeparatorStyle -
+    * @property {string} defaultLayout -
+    **/
+
+    /**
+    * Display type definitions
+    * @typedef {Object} DisplayElement
+    * @property {string} visible -
+    * @property {string} disabled -
+    * @property {string} rows -
+    * @property {string} errorIcon -
+    * @property {string} helpIcon -
+    * @property {string} helpMessage -
+    * @property {string} helpMode -
+    * @property {string} [defaultValue] - add a default value for display
+    * @property {string} [defaultLayout] - add a default value for display
+    * @property {string} errorMessage -
+    * @property {string} width -
+    * @property {string} [defaultWidth] - add a default value for display
+    * @property {string} labelAlign - add a label align for the display
+    * @property {number} labelWidth - add a with for the display
+    * @property {string} labelClassName -
+    * @property {string} labelStyle - add a style for the display
+    * @property {string} titleAlign -
+    * @property {string} titleWidth -
+    * @property {string} titleClassName -
+    * @property {string} titleStyle -
+    * @property {string} showTitle -
+    * @property {string} textClassName -
+    * @property {string} textStyle -
+    * @property {string} style -
+    * @property {string} placeholder -
+    * @property {Function} render -
+    * @property {string} onText -
+    * @property {string} offText -
+    * @property {string} activeClassName -
+    * @property {string} inactiveClassName -
+    * @property {string} template -
+    * @property {string} contentLayout -
+    * @property {string} separator -
+    * @property {string} errorClasses -
+    * @property {Function} transform -
+    * @property {Column} columns -
+    * @property {Data[]} data -
+    * @property {boolean} sort -
+    * @property {Object} highcharts -
+    * @property {Function} apply -
     **/
 
     /**
     * Element type definitions
     * @typedef {Object} Element Define the element that makes up a section
     * @property {string} name - name of the element
+    * @property {string} title - name of the element
     * @property {string} field - data name of element form
     * @property {elementType} type - type of element form
     * @property {boolean} required - is it the element required? True or False
-    * @property {boolean} showLabel
     * @property {Array} allowedValues -
-    * @property {Display} display - custom display forEl the element
-    * @property {Data[]} data
-    * @property {Column[]} columns
+    * @property {boolean} showLabel
+    * @property {DisplayElement} display - custom display forEl the element
     * @property {Validation} validation -
     **/
 
@@ -104,7 +171,7 @@ export default class Types {
     * @property {string} id - The id of this section, used to identify section.
     * @property {string} title - The title of this section
     * @property {string} text - The content of this section
-    * @property {Display} display - Custom Config for display to the section
+    * @property {DisplaySection} display - Custom Config for display to the section
     * @property {Array<Element>} elements - A list of elements
     **/
 
@@ -115,7 +182,8 @@ export default class Types {
      * @property {string} description - A description for the data-form
      * @property {string} icon - A icon for the data-form
      * @property {string} type - Define the type to the data-form
-     * @property {Button} button - Custom Config for the button to data-form
+     * @property {Button} buttons - Custom Config for the button to data-form
+     * @property {DisplayForm} display - Custom Config for the display to data-form
      * @property {Section[]} section - Define sections to teh config
      * @property {Validation} validation - Define validation to the data-form
      */
