2afc9c2df45d788de32ee471738247c5c53caa3d
Josemi <josemijuanes@gmail.com>
2021-12-22 12:47:27 +0100

    wc: add notification types and dispatch function (#299)

--- src/webcomponents/commons/utils/notification-utils.js
+++ src/webcomponents/commons/utils/notification-utils.js
@@ -2,4 +2,20 @@ import LitUtils from "./lit-utils.js";
 
 export default class NotificationUtils {
 
+    // Notification types
+    static NOTIFY = "notify";
+    static NOTIFY_INFO = "notifyInfo";
+    static NOTIFY_SUCCESS = "notifySuccess";
+    static NOTIFY_WARNING = "notifyWarning";
+    static NOTIFY_ERROR = "notifyError";
+    static NOTIFY_RESPONSE = "notifyResponse";
+
+    // Dispatch a notification event
+    static dispatch(self, type, value) {
+        return LitUtils.dispatchCustomEvent(self, type, null, value, null, {
+            bubbles: true,
+            composed: true,
+        });
+    }
+
 }
