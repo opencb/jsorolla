0133937f9d4875c5786dc184789c79739fae394f
Josemi <josemijuanes@gmail.com>
2022-01-14 17:39:12 +0100

    gb: init Genome Browser constants (#337)

---
+++ src/genome-browser/genome-browser-constants.js
@@ -0,0 +1,8 @@
+// Global constants for GenomeBrowser
+export default class GenomeBrowserConstants {
+
+    // CellBase constants
+    static CELLBASE_HOST = "https://ws.opencb.org/cellbase/";
+    static CELLBASE_VERSION = "v4";
+
+}
