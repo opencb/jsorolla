f6ad19b7ab67e2dd65d70eee5701650b11e7d7b1
antonioaltamura <antonioaltamura7@gmail.com>
2022-01-14 15:34:41 +0100

    wc: bugfix in state update in checkbox-field-filter.js #343

--- src/webcomponents/commons/forms/checkbox-field-filter.js
+++ src/webcomponents/commons/forms/checkbox-field-filter.js
@@ -50,6 +50,7 @@ export default class CheckboxFieldFilter extends LitElement {
 
     update(changedProperties) {
         if (changedProperties.has("value")) {
+            this.state = {};
             if (this.value) {
                 if (Array.isArray(this.value)) {
                     this.value.forEach(v => this.state[v] = true);
@@ -57,8 +58,6 @@ export default class CheckboxFieldFilter extends LitElement {
                     this.value.split(",").forEach(v => this.state[v] = true);
                 }
                 this.state = {...this.state};
-            } else {
-                this.state = {};
             }
         }
         super.update(changedProperties);
