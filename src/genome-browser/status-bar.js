e8301665c72f73c986b2227477b13a9a28516d39
Josemi <josemijuanes@gmail.com>
2022-01-19 13:18:27 +0100

    gb: fix mouse position in status-bar and minor improvements (#337)

--- src/genome-browser/status-bar.js
+++ src/genome-browser/status-bar.js
@@ -1,4 +1,3 @@
-import Utils from "../core/utils.js";
 import UtilsNew from "../core/utilsNew.js";
 import Region from "../core/bioinfo/region.js";
 
@@ -10,7 +9,10 @@ export default class StatusBar {
 
         this.id = UtilsNew.randomString(8);
         this.target = target;
-        this.config = {...StatusBar.getDefaultConfig(), ...config};
+        this.config = {
+            ...StatusBar.getDefaultConfig(),
+            ...config,
+        };
         this.rendered = false;
 
         // Set new region object
@@ -28,36 +30,15 @@ export default class StatusBar {
                     ${this.config.version}
                 </div>
                 <div class="ocb-gv-status-right">
-                    <span id="${this.id}position">
-                        <span id="${this.id}positionBase" style="margin-right:4px"></span>
-                        <span id="${this.id}positionRegion"></span>
-                    </span>
+                    <span id="${this.id}PositionBase" style="margin-right:4px"></span>
+                    <span id="${this.id}PositionRegion"></span>
                 </div>
             </div>
         `);
 
         this.div = template.querySelector(`div#${this.id}`);
-        this.mousePositionBase = this.div.querySelector(`span#${this.id}positionBase`);
-        this.mousePositionRegion = this.div.querySelector(`span#${this.id}positionRegion`);
-
-        // this.div = $('<div id="' + this.id + '" class="ocb-gv-status-bar"></div>')[0];
-
-        // this.rightDiv = $('<div class="ocb-gv-status-right" id="' + this.id + 'position"</div>')[0];
-        // this.leftDiv = $('<div class="ocb-gv-status-left" id="' + this.id + 'position"></div>')[0];
-        // $(this.div).append(this.leftDiv);
-        // $(this.div).append(this.rightDiv);
-
-        // this.mousePositionEl = $('<span id="' + this.id + 'position"></span>')[0];
-        // this.mousePositionBase = document.createElement('span');
-        // this.mousePositionBase.style.marginRight = '5px';
-        // this.mousePositionRegion = document.createElement('span');
-        // this.mousePositionEl.appendChild(this.mousePositionBase);
-        // this.mousePositionEl.appendChild(this.mousePositionRegion);
-
-        // this.versionEl = $('<span id="' + this.id + 'version">' + this.version + '</span>')[0];
-        // $(this.rightDiv).append(this.mousePositionEl);
-        // $(this.leftDiv).append(this.versionEl);
-
+        this.mousePositionBase = this.div.querySelector(`span#${this.id}PositionBase`);
+        this.mousePositionRegion = this.div.querySelector(`span#${this.id}PositionRegion`);
         this.rendered = true;
     }
 
@@ -73,14 +54,14 @@ export default class StatusBar {
     setRegion(event) {
         this.region.load(event.region);
         this.mousePositionBase.textContent = "";
-        this.mousePositionRegion.textContent = `${this.region.chromosome}:${Utils.formatNumber(event.region.center())}`;
+        this.mousePositionRegion.textContent = `${this.region.chromosome}:${event.region.center()}`;
     }
 
     setMousePosition(event) {
         // TODO: move SEQUENCE_COLORS to global constants module
         this.mousePositionBase.style.color = SEQUENCE_COLORS[event.base];
         this.mousePositionBase.textContent = event.base;
-        this.mousePositionRegion.textContent = `${this.region.chromosome}:${Utils.formatNumber(event.region.center())}`;
+        this.mousePositionRegion.textContent = `${this.region.chromosome}:${event.mousePos}`;
     }
 
     // Get default status bar config
