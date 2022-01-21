f32a4c587dd00e46cc021cc0babcfc4d74eda124
Josemi <josemijuanes@gmail.com>
2022-01-21 13:54:47 +0100

    gb: add cytobands colors constant (#337)

--- src/genome-browser/genome-browser-constants.js
+++ src/genome-browser/genome-browser-constants.js
@@ -5,4 +5,19 @@ export default class GenomeBrowserConstants {
     static CELLBASE_HOST = "https://ws.zettagenomics.com/cellbase";
     static CELLBASE_VERSION = "v5";
 
+    // Cytobands
+    static CYTOBANDS_COLORS = {
+        gneg: "white",
+        stalk: "#666666",
+        gvar: "#CCCCCC",
+        gpos25: "silver",
+        gpos33: "lightgrey",
+        gpos50: "gray",
+        gpos66: "dimgray",
+        gpos75: "darkgray",
+        gpos100: "black",
+        gpos: "gray",
+        acen: "blue",
+    };
+
 }
