fd9f28ca39e8382b01cb990699e24fc7be9e6009
imedina <igmecas@gmail.com>
2022-02-04 02:46:11 +0000

    wc: remove default GT in the interpreters and sample-genotype-filter

--- src/webcomponents/variant/interpretation/variant-interpreter-browser-rd.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-browser-rd.js
@@ -157,14 +157,15 @@ class VariantInterpreterBrowserRd extends LitElement {
                 switch (this.clinicalAnalysis.type.toUpperCase()) {
                     case "SINGLE":
                     case "CANCER":
-                        this._sampleQuery = this.sample.id + ":" + ["0/1", "1/1"].join(",");
+                        this._sampleQuery = this.sample.id + ":" + ["0/1", "1/1", "1/2"].join(",");
                         break;
                     case "FAMILY":
-                        const sampleIds = [this.sample.id + ":" + ["0/1", "1/1"].join(",")];
+                        // Add proband genotypes
+                        const sampleIds = [this.sample.id + ":" + ["0/1", "1/1", "1/2"].join(",")];
                         for (const member of this.clinicalAnalysis.family?.members) {
-                            // Proband is already in the array in the first position
+                            // Proband is already in the array in the first position, we add other family members
                             if (member.id !== this.clinicalAnalysis.proband?.id && member.samples?.length > 0) {
-                                sampleIds.push(member.samples[0].id + ":" + ["0/0", "0/1", "1/1"].join(","));
+                                sampleIds.push(member.samples[0].id + ":" + ["0/0", "0/1", "1/1", "1/2"].join(","));
                             }
                         }
                         this._sampleQuery = sampleIds.join(";");
@@ -427,6 +428,9 @@ class VariantInterpreterBrowserRd extends LitElement {
                                         },
                                         {
                                             separator: true
+                                        },
+                                        {
+                                            id: "1/2", name: "BIALLELIC HET (Genotype 1/2)"
                                         },
                                         {
                                             id: "1", name: "HEMI"
