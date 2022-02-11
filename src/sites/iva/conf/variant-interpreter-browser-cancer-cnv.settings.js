d527b729cbd3e27eb19440ae086a2076f1b48cc5
Josemi <josemijuanes@gmail.com>
2022-02-11 20:17:48 +0100

    iva: update cnv browser settings (#381)

--- src/sites/iva/conf/variant-interpreter-browser-cancer-cnv.settings.js
+++ src/sites/iva/conf/variant-interpreter-browser-cancer-cnv.settings.js
@@ -13,29 +13,9 @@ const VARIANT_INTERPRETER_BROWSER_CANCER_CNV_SETTINGS = {
                 filters: ["region", "feature", "biotype", "type"]
             },
             {
-                title: "Clinical",
-                filters: ["diseasePanels", "clinical-annotation"]
-            },
-            {
-                title: "Consequence Type",
-                filters: ["consequence-type"]
-            },
-            {
-                title: "Population Frequency",
-                filters: ["populationFrequency"]
-            },
-            {
                 title: "Phenotype",
                 filters: ["go", "hpo"]
             },
-            {
-                title: "Deleteriousness",
-                filters: ["proteinSubstitutionScore", "cadd"]
-            },
-            {
-                title: "Conservation",
-                filters: ["conservation"]
-            }
         ]
     },
     table: {
