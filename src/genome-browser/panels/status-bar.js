1382957e727b51c7898858fc7c9d630756841f1f
Josemi <josemijuanes@gmail.com>
2022-02-18 12:01:31 +0100

    gb: minor improvements in status bar (#337)

--- src/genome-browser/panels/status-bar.js
+++ src/genome-browser/panels/status-bar.js
@@ -18,7 +18,7 @@ export default class StatusBar {
     }
 
     #init() {
-        this.id = UtilsNew.randomString(8);
+        this.prefix = UtilsNew.randomString(8);
         this.region = new Region(this.config.region);
 
         this.#initDom();
@@ -26,20 +26,20 @@ export default class StatusBar {
 
     #initDom() {
         const template = UtilsNew.renderHTML(`
-            <div id="${this.id}" class="ocb-gb-status-bar">
+            <div id="${this.prefix}" class="ocb-gb-status-bar">
                 <div class="ocb-gv-status-left">
                     ${this.config.version}
                 </div>
                 <div class="ocb-gv-status-right">
-                    <span id="${this.id}PositionBase" style="margin-right:4px"></span>
-                    <span id="${this.id}PositionRegion"></span>
+                    <span id="${this.prefix}PositionBase" style="margin-right:4px"></span>
+                    <span id="${this.prefix}PositionRegion"></span>
                 </div>
             </div>
         `);
 
-        this.div = template.querySelector(`div#${this.id}`);
-        this.mousePositionBase = this.div.querySelector(`span#${this.id}PositionBase`);
-        this.mousePositionRegion = this.div.querySelector(`span#${this.id}PositionRegion`);
+        this.div = template.querySelector(`div#${this.prefix}`);
+        this.mousePositionBase = this.div.querySelector(`span#${this.prefix}PositionBase`);
+        this.mousePositionRegion = this.div.querySelector(`span#${this.prefix}PositionRegion`);
 
         this.target.appendChild(this.div);
     }
@@ -63,7 +63,6 @@ export default class StatusBar {
     // Get default status bar config
     getDefaultConfig() {
         return {
-            autoRender: true,
             region: null,
             version: "",
         };
