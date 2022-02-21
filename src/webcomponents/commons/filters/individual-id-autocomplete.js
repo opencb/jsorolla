2a46fa88a561321834b9e8d9d92e3d5f3718684e
antonioaltamura <antonioaltamura7@gmail.com>
2022-02-21 15:29:54 +0100

    wc: individual-id-autocomplete.js multiple flag as default #417

--- src/webcomponents/commons/filters/individual-id-autocomplete.js
+++ src/webcomponents/commons/filters/individual-id-autocomplete.js
@@ -63,7 +63,6 @@ export default class IndividualIdAutocomplete extends LitElement {
 
     getDefaultConfig() {
         return {
-            multiple: false,
             limit: 10,
             placeholder: "Start typing...",
             fields: item => ({
@@ -89,6 +88,8 @@ export default class IndividualIdAutocomplete extends LitElement {
 
     render() {
         return html`
+
+            ${JSON.stringify(this._config)}
             <select-token-filter
                 .opencgaSession="${this.opencgaSession}"
                 .config="${this._config}"
