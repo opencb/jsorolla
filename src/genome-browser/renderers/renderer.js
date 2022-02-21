7e3ff0608a76d55a62ed82374fb041ddd0fd4094
Josemi <josemijuanes@gmail.com>
2022-02-21 18:50:01 +0100

    gb: refactor base renderer (#337)

--- src/genome-browser/renderers/renderer.js
+++ src/genome-browser/renderers/renderer.js
@@ -1,41 +1,31 @@
 import {SVG} from "../../core/svg.js";
 
 
-//Parent class for all renderers
 export default class Renderer {
 
-    constructor(args) {
+    constructor(config) {
+        // eslint-disable-next-line no-undef
+        Object.assign(this, Backbone.Events);
+
+        this.config = {
+            ...this.getDefaultConfig(),
+            ...config,
+        };
+
         this.fontClass = "ocb-font-roboto ocb-font-size-11";
         this.toolTipfontClass = "ocb-tooltip-font";
     }
 
-    init() {
-
+    // Returns svg feature x value from feature genomic position
+    // TODO: move to utils?
+    getFeatureX(start, args) {
+        const middle = args.width / 2;
+        return args.pixelPosition + middle - ((args.position - start) * args.pixelBase);
     }
 
-    render(items) {
+    // Placeholder getDefaultConfig
+    getDefaultConfig() {
+        return {};
     }
 
-    getFeatureX(start, args) {    // returns svg feature x value from feature genomic position
-        let middle = args.width / 2;
-        let x = args.pixelPosition + middle - ((args.position - start) * args.pixelBase);
-        return x;
-    }
-
-    getDefaultConfig(type) {
-        return FEATURE_TYPES[type];
-    }
-
-    getLabelWidth(label, args) {
-        /* insert in dom to get the label width and then remove it*/
-        let svgLabel = SVG.create("text", {
-            "font-weight": 400,
-            "class":this.fontClass
-        });
-        svgLabel.textContent = label;
-        $(args.svgCanvasFeatures).append(svgLabel);
-        let svgLabelWidth = $(svgLabel).width();
-        $(svgLabel).remove();
-        return svgLabelWidth;
-    }
 }
