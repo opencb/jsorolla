5da91be1281a29d8fc7663c233d490aa61bf6cf0
Josemi <josemijuanes@gmail.com>
2022-02-23 13:07:44 +0100

    gb: simplified sequence renderer (#337)

--- src/genome-browser/renderers/sequence-renderer.js
+++ src/genome-browser/renderers/sequence-renderer.js
@@ -4,48 +4,44 @@ import GenomeBrowserConstants from "../genome-browser-constants.js";
 
 export default class SequenceRenderer extends Renderer {
 
-    render(chunks, args) {
-        for (let i = 0; i < chunks.length; i++) {
-            this.#paintSequenceChunk(chunks[i], args);
-        }
-    }
+    render(chunks, options) {
+        const middle = options.width / 2;
 
-    #paintSequenceChunk(chunk, args) {
-        const middle = args.width / 2;
+        (chunks || []).forEach(chunk => {
+            let start = chunk.start;
+            const seqStart = chunk.start;
+            const seqString = chunk.sequence;
 
-        let start = chunk.start;
-        const seqStart = chunk.start;
-        const seqString = chunk.sequence;
-
-        for (let i = 0; i < seqString.length; i++) {
-            const x = args.pixelPosition + middle - ((args.position - start) * args.pixelBase);
-            const text = SVG.addChild(args.svgCanvasFeatures, "text", {
-                "x": x + 1,
-                "y": 12,
-                "fill": GenomeBrowserConstants.SEQUENCE_COLORS[seqString.charAt(i)],
-                "data-pos": start,
-                "class": this.fontClass,
-            });
-            start++;
-            text.textContent = seqString.charAt(i);
-            $(text).qtip({
-                content: seqString.charAt(i) + " " + (seqStart + i).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"),
-                position: {
-                    target: "mouse",
-                    adjust: {x: 25, y: 15},
-                },
-                style: {
-                    width: true,
-                    classes: this.toolTipfontClass + " qtip-light qtip-shadow",
-                },
-                show: {
-                    delay: 300,
-                },
-                hide: {
-                    delay: 300,
-                },
-            });
-        }
+            for (let i = 0; i < seqString.length; i++) {
+                const x = options.pixelPosition + middle - ((options.position - start) * options.pixelBase);
+                const text = SVG.addChild(options.svgCanvasFeatures, "text", {
+                    "x": x + 1,
+                    "y": 12,
+                    "fill": GenomeBrowserConstants.SEQUENCE_COLORS[seqString.charAt(i)],
+                    "data-pos": start,
+                    "class": this.config.fontClass,
+                });
+                start++;
+                text.textContent = seqString.charAt(i);
+                $(text).qtip({
+                    content: seqString.charAt(i) + " " + (seqStart + i).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"),
+                    position: {
+                        target: "mouse",
+                        adjust: {x: 25, y: 15},
+                    },
+                    style: {
+                        width: true,
+                        classes: this.config.toolTipfontClass + " qtip-light qtip-shadow",
+                    },
+                    show: {
+                        delay: 300,
+                    },
+                    hide: {
+                        delay: 300,
+                    },
+                });
+            }
+        });
     }
 
 }
