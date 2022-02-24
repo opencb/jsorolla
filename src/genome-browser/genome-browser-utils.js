4215df4efb03e73b946edfdc41d311e61bc5d212
Josemi <josemijuanes@gmail.com>
2022-02-24 16:15:52 +0100

    gb: add more utils and formatters for gene, exon and transcripts (#337)

--- src/genome-browser/genome-browser-utils.js
+++ src/genome-browser/genome-browser-utils.js
@@ -1,5 +1,6 @@
 import UtilsNew from "../core/utilsNew.js";
 import {SVG} from "../core/svg.js";
+import GenomeBrowserConstants from "./genome-browser-constants.js";
 
 export default class GenomeBrowserUtils {
 
@@ -11,6 +12,7 @@ export default class GenomeBrowserUtils {
     static getSpeciesFromCellBase = cellBaseClient => {
         return cellBaseClient.getMeta("species").then(response => {
             const taxonomies = response.response[0].result[0];
+
             // Generate one specie for each assembly
             Object.keys(taxonomies).forEach(name => {
                 const newSpecies = [];
@@ -22,8 +24,10 @@ export default class GenomeBrowserUtils {
                         newSpecies.push(clonedSpecies);
                     });
                 });
+
                 taxonomies[name] = newSpecies;
             });
+
             return taxonomies;
         });
     }
@@ -39,6 +43,7 @@ export default class GenomeBrowserUtils {
     }
 
     // TODO: check if we need to insert the element to calculate the width
+    // eslint-disable-next-line no-unused-vars
     static getLabelWidth(label, track) {
         // insert in dom to get the label width and then remove it
         const svgLabel = SVG.create("text", {
@@ -52,6 +57,7 @@ export default class GenomeBrowserUtils {
         return parseFloat(getComputedStyle(svgLabel, null).width.replace("px", "")) || 0;
     }
 
+    // Generic title formatter
     static titleFormatter(str) {
         return str ? str.charAt(0).toUpperCase() + str.slice(1).replace(/_/gi, "") : "";
     }
@@ -70,6 +76,17 @@ export default class GenomeBrowserUtils {
         return `${feature.chromosome}:${feature.start}-${feature.end}`;
     }
 
+    // Feature info formatter
+    static featureInfoFormatter(feature) {
+        const strand = feature.strand || "NA";
+        const length = (feature.end - feature.start + 1).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
+        return `
+            <div>Start - End: <strong>${feature.start}-${feature.end} (${strand})</strong></div>
+            <div>Length: <strong style="color:#005fdb">${length}</strong></div>
+        `;
+    }
+
     // Feature tooltip title formatter
     static featureTooltipTitleFormatter(feature) {
         return [feature.featureType, feature.id, feature.name].filter(f => !!f).join(" - ");
@@ -77,27 +94,22 @@ export default class GenomeBrowserUtils {
 
     // Feature tooltip text formatter
     static featureTooltipTextFormatter(feature) {
-        const strand = feature.strand || "NA";
-        const length = (feature.end - feature.start + 1).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-        const otherFeatureKeys = Object.keys(feature).filter(key => {
-            return !(key === "start" || key === "end" || key === "id" || key === "name" || key === "length");
+        const otherFeatureInfo = Object.keys(feature).map(key => {
+            // Check for common feature keys
+            if (key === "start" || key === "end" || key === "id" || key === "name" || key === "length") {
+                return "";
+            }
+            // Check for studies key --> join studies IDs
+            const value = key === "studies" ? feature[key].map(s => s.studyId || "-").join(", ") : feature[key];
+            return `
+                <div>
+                    <span style="text-transform:capitalize">${key}</span>: <strong>${value}</strong>
+                </div>
+            `;
         });
         return `
-            <div class="">
-                start-end: <strong>${feature.start}-${feature.end} (${strand})</strong>
-            </div>
-            <div class="">
-                length: <strong style="color:#005fdb">${length}</strong>
-            </div>
-            ${otherFeatureKeys.map(key => {
-                // Check for studies key --> join studies IDs
-                const value = key === "studies" ? feature[key].map(s => s.studyId).join(", ") : feature[key];
-                return `
-                    <div class="">
-                        ${key}: <strong>${value}</strong>
-                    </div>
-                `;
-            }).join("")}
+            ${GenomeBrowserUtils.featureInfoFormatter(feature)}
+            ${otherFeatureInfo.join("")}
         `;
     }
 
@@ -118,6 +130,88 @@ export default class GenomeBrowserUtils {
     // Variant tooltip text formatter
     static variantTooltipTextFormatter(feature) {
         return GenomeBrowserUtils.featureTooltipTextFormatter(feature);
+    }
+
+    //
+    // Gene utils
+    //
+
+    // Gene color formatter
+    static geneColorFormatter(gene) {
+        return GenomeBrowserConstants.GENE_BIOTYPE_COLORS[gene.biotype];
+    }
+
+    // Gene label formatter
+    static geneLabelFormatter(gene) {
+        const name = gene.name || gene.id || "NA";
+        const leftStrand = gene.strand && (gene.strand < 0 || gene.strand === "-") ? "<" : "";
+        const rightStrand = gene.strand && (gene.strand > 0 || gene.strand === "+") ? ">" : "";
+        const biotype = gene.biotype ? "[" + gene.biotype + "]" : "";
+
+        return `${leftStrand} ${name} ${rightStrand} ${biotype}`;
+    }
+
+    // Gene tooltip title formatter
+    static geneTooltipTitleFormatter(gene) {
+        return `Gene - <span>${gene.name || gene.id}</span>`;
+    }
+
+    // Gene tooltip text formatter
+    static geneTooltipTextFormatter(gene) {
+        const color = GenomeBrowserUtils.geneColorFormatter(gene);
+
+        return `
+            <div>ID: <span class="ssel">${gene.id || ""}</span></div>
+            <div>Biotype: <span class="emph" style="color:${color};">${gene.biotype}</span></div>
+            ${GenomeBrowserUtils.featureInfoFormatter(gene)}
+            <div>Source: <span class="ssel">${gene.source || ""}</span></div>
+            <div>Description: <span class="emph">${gene.description || ""}</span></div>
+        `;
+    }
+
+    //
+    // Transcript utils
+    //
+
+    // Transcript title formatter
+    static transcriptTooltipTitleFormatter(transcript) {
+        return `Transcript - <span>${transcript.name || transcript.id}</span>`;
+    }
+
+    //
+    // Exon utils
+    //
+
+    // Exon label formatter
+    static exonLabelFormatter(exon) {
+        return exon.name || exon.id;
+    }
+
+    // Exon tooltip title formatter
+    static exonTooltipTitleFormatter(exon) {
+        return `Exon - <span>${exon.name || exon.id}</span>`;
+    }
+
+    // Exon tooltip text formatter
+    static exonTooltipTextFormatter(exon, transcript) {
+        // return FEATURE_TYPES.getTipCommons(e) + FEATURE_TYPES._getSimpleKeys(e);
+        const color = GenomeBrowserConstants.GENE_BIOTYPE_COLORS[transcript.biotype];
+        const exonInfo = GenomeBrowserUtils.featureTooltipTextFormatter(exon);
+        const transcriptInfo = GenomeBrowserUtils.featureInfoFormatter(transcript);
+
+        return `
+            <div>Transcript:</div>
+            <div style="padding-left:12px">
+                <div>ID: <span class="ssel">${transcript.id}</span></div>
+                <div>Biotype: <span class="emph" style="color:${color};">${transcript.biotype}</span></div>
+                </div>Description: <span class="emph">${transcript.description}</span></div>
+                ${transcriptInfo}
+            </div>
+            <div>Exon:</div>
+            <div style="padding-left:12px">
+                ${exonInfo}
+            </div>
+        `;
     }
 
 }
