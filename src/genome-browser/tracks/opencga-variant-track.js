f6add5b72cb13be5a56fd51636f15dc937c76766
Josemi <josemijuanes@gmail.com>
2022-02-25 13:34:19 +0100

    gb: some improvements in variant track (#337)

--- src/genome-browser/tracks/opencga-variant-track.js
+++ src/genome-browser/tracks/opencga-variant-track.js
@@ -1,8 +1,7 @@
-import UtilsNew from "../../core/utilsNew.js";
 import FeatureTrack from "./feature-track.js";
 import HistogramRenderer from "../renderers/histogram-renderer.js";
 import VariantRenderer from "../renderers/variant-renderer.js";
-import OpencgaAdapter from "../../core/data-adapter/opencga-adapter.js";
+// import OpencgaAdapter from "../../core/data-adapter/opencga-adapter.js";
 
 export default class OpenCGAVariantTrack extends FeatureTrack {
 
@@ -10,7 +9,7 @@ export default class OpenCGAVariantTrack extends FeatureTrack {
         super(config);
 
         // Initialize the dataAdapter
-        this.dataAdapter = this.config.dataAdapter || this.#initDataAdapter();
+        // this.dataAdapter = this.config.dataAdapter || this.#initDataAdapter();
 
         // Initialize Rendererers
         this.histogramRenderer = new HistogramRenderer(this.config.histogramRenderer);
@@ -20,74 +19,49 @@ export default class OpenCGAVariantTrack extends FeatureTrack {
         });
     }
 
-    #initDataAdapter() {
-        let dataAdapter = null;
+    // #initDataAdapter() {
+    //     let dataAdapter = null;
 
-        if (this.config.opencgaClient) {
-            const params = {
+    //     if (this.config.opencgaClient) {
+    //         const params = {
+    //             studies: this.config.opencgaStudies,
+    //             exclude: "studies,annotation",
+    //         };
+
+    //         if (this.config.opencgaSamples && this.config.opencgaSamples.length > 0) {
+    //             params.exclude = "studies.files,studies.stats,annotation";
+    //             params.returnedSamples = this.config.opencgaSamples;
+    //         }
+
+    //         if (this.config.opencgaFiles && this.config.opencgaFiles.length > 0) {
+    //             params.exclude = "studies.files,studies.stats,annotation";
+    //             params.file = this.config.opencgaFiles;
+    //         }
+
+    //         dataAdapter = new OpencgaAdapter(this.config.opencgaClient, "analysis/variant", "", "query", params, {
+    //             chunkSize: 100000,
+    //         });
+    //     } else {
+    //         console.error("No opencga object provided");
+    //     }
+
+    //     return dataAdapter;
+    // }
+
+    getData(options) {
+        if (options.dataType === "histogram") {
+            // Fetch aggregation stats for the current region
+            return this.config.opencgaClient.variants().aggregationStats({
                 studies: this.config.opencgaStudies,
+                region: options.region.toString(),
+                field: `start[${options.region.start}..${options.region.end}]:${this.config.histogramInterval}`,
+            });
+        } else {
+            // Fetch variants
+            return this.config.opencgaClient.variants().query({
+                studies: this.config.opencgaStudies,
+                region: options.region.toString(),
                 exclude: "studies,annotation",
-            };
-
-            if (this.config.opencgaSamples && this.config.opencgaSamples.length > 0) {
-                params.exclude = "studies.files,studies.stats,annotation";
-                params.returnedSamples = this.config.opencgaSamples;
-            }
-
-            if (this.config.opencgaFiles && this.config.opencgaFiles.length > 0) {
-                params.exclude = "studies.files,studies.stats,annotation";
-                params.file = this.config.opencgaFiles;
-            }
-
-            dataAdapter = new OpencgaAdapter(this.config.opencgaClient, "analysis/variant", "", "query", params, {
-                chunkSize: 100000,
-            });
-        } else {
-            console.error("No opencga object provided");
-        }
-
-        return dataAdapter;
-    }
-
-    getDataHandler(event) {
-        // Not histogram data type
-        // if (event.dataType !== "histogram" || UtilsNew.isNotUndefinedOrNull(this.renderer.config.sampleNames)) {
-        if (event.dataType !== "histogram") {
-            const features = this.getFeaturesToRenderByChunk(event);
-            this.renderer.render(features, {
-                cacheItems: event.items,
-                svgCanvasFeatures: this.svgCanvasFeatures,
-                featureTypes: this.featureTypes,
-                renderedArea: this.renderedArea,
-                pixelBase: this.pixelBase,
-                position: this.region.center(),
-                regionSize: this.region.length(),
-                maxLabelRegionSize: this.maxLabelRegionSize,
-                width: this.width,
-                pixelPosition: this.pixelPosition,
-                resource: this.dataAdapter.resource,
-                species: this.dataAdapter.species,
-                featureType: this.featureType
-            });
-            this.updateHeight();
-        } else {
-            (event.items || []).forEach(features => {
-                this.histogramRenderer.render(features, {
-                    cacheItems: features,
-                    svgCanvasFeatures: this.svgCanvasFeatures,
-                    featureTypes: this.featureTypes,
-                    renderedArea: this.renderedArea,
-                    pixelBase: this.pixelBase,
-                    position: this.region.center(),
-                    regionSize: this.region.length(),
-                    maxLabelRegionSize: this.maxLabelRegionSize,
-                    width: this.width,
-                    pixelPosition: this.pixelPosition,
-                    resource: this.dataAdapter.resource,
-                    species: this.dataAdapter.species,
-                    featureType: this.featureType
-                });
-                this.updateHeight();
             });
         }
     }
@@ -101,8 +75,9 @@ export default class OpenCGAVariantTrack extends FeatureTrack {
             opencgaStudies: "",
             opencgaSamples: [],
             opencgaFiles: [],
-            // minHistogramRegionSize: 300000000, // Default track value
-            minHistogramRegionSize: 200000,
+            histogramMinRegionSize: 200000, // 300000000,
+            histogramInterval: 10000,
+            labelMaxRegionSize: 10000000,
             renderer: {}, // Renderer configuration
             histogramRenderer: {}, // Histogram renderer configuration
         };
