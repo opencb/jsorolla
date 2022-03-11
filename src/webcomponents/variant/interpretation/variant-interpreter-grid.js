473a873cbf514b42763005d1c567a87216702266
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2022-03-11 11:41:34 +0100

    wc: replace icon-plus to fa-plus (#424)
    Signed-off-by: Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>

--- src/webcomponents/variant/interpretation/variant-interpreter-grid.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-grid.js
@@ -367,7 +367,7 @@ export default class VariantInterpreterGrid extends LitElement {
                     // We detail view is active we expand the row automatically.
                     // FIXME: Note that we use a CSS class way of knowing if the row is expand or collapse, this is not ideal but works.
                     if (this._config.detailView) {
-                        if (element[0].innerHTML.includes("icon-plus")) {
+                        if (element[0].innerHTML.includes("fa-plus")) {
                             $("#" + this.gridId).bootstrapTable("expandRow", element[0].dataset.index);
                         } else {
                             $("#" + this.gridId).bootstrapTable("collapseRow", element[0].dataset.index);
@@ -465,7 +465,7 @@ export default class VariantInterpreterGrid extends LitElement {
                 // We detail view is active we expand the row automatically.
                 // FIXME: Note that we use a CSS class way of knowing if the row is expand or collapse, this is not ideal but works.
                 if (this._config.detailView) {
-                    if (element[0].innerHTML.includes("icon-plus")) {
+                    if (element[0].innerHTML.includes("fa-plus")) {
                         $("#" + this.gridId).bootstrapTable("expandRow", element[0].dataset.index);
                     } else {
                         $("#" + this.gridId).bootstrapTable("collapseRow", element[0].dataset.index);
