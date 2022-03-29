8f8b1071a5fa6bc4a1186b351995b1952104aaa0
imedina <igmecas@gmail.com>
2022-03-29 16:49:40 +0100

    Increment version to 2.2.1-dev

--- src/sites/iva/conf/config.js
+++ src/sites/iva/conf/config.js
@@ -167,7 +167,7 @@ const CATALOG_NAVBAR_MENU = {
 const SUITE = {
     id: "suite",
     name: "OpenCB Suite",
-    version: "v2.2.0",
+    version: "v2.2.1-dev",
     logo: "img/iva-white.svg",
     companyLogo: "",
     logoAlt: "img/iva.svg",
