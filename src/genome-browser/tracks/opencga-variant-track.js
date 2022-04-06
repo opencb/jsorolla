94f371f8cc465771c4bcb74cff7fbd31aaf3dd1f
Josemi <josemijuanes@gmail.com>
2022-04-06 16:05:22 +0200

    gb: improve displayed sample info in variants track #TASK-440

--- src/genome-browser/tracks/opencga-variant-track.js
+++ src/genome-browser/tracks/opencga-variant-track.js
@@ -12,6 +12,7 @@ export default class OpenCGAVariantTrack extends FeatureTrack {
         super(config);
 
         this.sampleNames = null;
+        this.samplesInfo = null;
 
         // Initialize Rendererers
         this.histogramRenderer = new HistogramRenderer(this.config.histogramRenderer);
@@ -44,24 +45,51 @@ export default class OpenCGAVariantTrack extends FeatureTrack {
         }
     }
 
-    #initSamplesDOM() {
+    async #initSamplesDOM() {
+        this.samplesInfo = new Map();
+        const response = await this.config.opencgaClient.samples().search({
+            study: this.config.opencgaStudy,
+            id: this.sampleNames.join(","),
+            includeIndividual: true,
+        });
+
+        response.getResults().forEach(result => {
+            this.samplesInfo.set(result.id, {
+                disorders: result.attributes?.OPENCGA_INDIVIDUAL?.disorders || [],
+                sex: result.attributes?.OPENCGA_INDIVIDUAL?.sex?.id || "UNKNOWN",
+                somatic: !!result.somatic,
+            });
+        });
+
         const template = UtilsNew.renderHTML(`
             <div id="${this.prefix}SampleNames" style="position:absolute;top:0px;">
-                ${this.sampleNames.map(name => `
-                    <div style="font-size:0.75rem;height:${this.config.sampleHeight}px;">
-                        <span style="font-weight:bold;vertical-align:middle;">${name}</span>
-                    </div>
-                `).join("")}
+                ${this.sampleNames.map(name => {
+                    const info = this.samplesInfo.get(name);
+                    const sampleColor = info.disorders.length > 0 ? "#CC0000" : "inherit";
+                    const sampleSexIcon = GenomeBrowserUtils.getIndividualSexIcon(info.sex);
+                    const sampleSexColor = GenomeBrowserUtils.getIndividualSexColor(info.sex);
+                    return `
+                        <div style="height:${this.config.sampleHeight}px;display:flex;flex-direction:column;justify-content:center;">
+                            <div style="font-size:14px;">
+                                <i class="fas ${sampleSexIcon}" style="padding-right:4px;color:${sampleSexColor};"></i>
+                                <b style="color:${sampleColor};">${name}</b>
+                            </div>
+                            <div style="color:#6C757D;font-size:10px;padding-left:12px;">
+                                <b>${info.somatic ? "Somatic" : "Germline"} sample</b>
+                            </div>
+                        </div>
+                    `;
+                }).join("")}
             </div>
             <div id="${this.prefix}SampleHeaderDivider" style="position:absolute;width:100%;"></div>
         `);
 
         // Sample names
         this.sampleNamesDiv = template.querySelector(`div#${this.prefix}SampleNames`);
-        this.sampleNamesDiv.style.backgroundColor = "rgba(255,255,255,0.6)";
-        this.sampleNamesDiv.style.paddingLeft = "8px";
-        this.sampleNamesDiv.style.paddingRight = "8px";
-        this.sampleNamesDiv.style.paddingTop = `${this.config.sampleHeight}px`;
+        this.sampleNamesDiv.style.backgroundColor = "rgba(255,255,255,0.7)";
+        this.sampleNamesDiv.style.paddingLeft = "16px";
+        this.sampleNamesDiv.style.paddingRight = "16px";
+        this.sampleNamesDiv.style.paddingTop = `${this.config.sampleHeaderHeight}px`;
 
         // Sample header divider
         this.sampleHeaderDividerDiv = template.querySelector(`div#${this.prefix}SampleHeaderDivider`);
@@ -82,7 +110,7 @@ export default class OpenCGAVariantTrack extends FeatureTrack {
                 x: "0px",
                 y: `${this.config.sampleHeaderHeight + (index * this.config.sampleHeight)}px`,
                 width: "100%",
-                height: `${this.config.sampleHeaderHeight}px`,
+                height: `${this.config.sampleHeight}px`,
                 fill: index % 2 ? this.config.sampleBackgroundColorEven : this.config.sampleBackgroundColorOdd,
                 style: `opacity:${this.config.sampleBackgroundOpacity}`,
             });
@@ -127,7 +155,7 @@ export default class OpenCGAVariantTrack extends FeatureTrack {
             labelMaxRegionSize: 10000000,
             renderer: {}, // Renderer configuration
             histogramRenderer: {}, // Histogram renderer configuration
-            sampleHeight: 20,
+            sampleHeight: 40,
             sampleBackgroundOpacity: 0.2,
             sampleBackgroundColorOdd: "#ffffff",
             sampleBackgroundColorEven: "#a4abb6",
