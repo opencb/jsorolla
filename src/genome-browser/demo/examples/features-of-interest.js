223a98121e8b1df176ff1d6ac07ecb0a33317e4f
Josemi <josemijuanes@gmail.com>
2022-06-01 15:38:32 +0200

    gb: add features of interest example #TASK-590

---
+++ src/genome-browser/demo/examples/features-of-interest.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        name: "Primary Findings",
+        features: [
+            {
+                id: "12:1212121:A:T",
+                chromosome: "12",
+                start: 1212121,
+                end: 1212122,
+            }
+        ],
+        display: {
+            visible: true,
+            color: "red",
+        }
+    },
+    {
+        name: "My genes of interest",
+        features: [
+            {
+                id: "BRCA1",
+                chromosome: "17",
+                start: 43044295,
+                end: 43170245,
+            },
+            {
+                id: "BRCA2",
+                chromosome: "13",
+                start: 32315086,
+                end: 32400268,
+            },
+            {
+                id: "TP53",
+                chromosome: "17",
+                start: 7661779,
+                end: 7687538,
+            },
+        ],
+        display: {
+            visible: true,
+            color: "blue",
+        },
+    },
+];
