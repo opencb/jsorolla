05456b4ee71877098cacc1da5cce4df671d657d6
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2022-06-09 19:58:59 +0200

    wc: fixed disable case locked & case with other owner #TASK-431

--- src/webcomponents/clinical/clinical-analysis-grid.js
+++ src/webcomponents/clinical/clinical-analysis-grid.js
@@ -599,7 +599,8 @@ export default class ClinicalAnalysisGrid extends LitElement {
 
                     const lockActionIcon = row.locked ? "fa-unlock" : "fa-lock";
                     const lockActionText = row.locked ? "Unlock" : "Lock";
-                    const isLockedDisabled = row.locked ? "disabled" : "";
+
+                    const isOwnOrIsLocked = row.locked || this.opencgaSession?.user?.id !== row.analyst?.id ? "disabled" : "";
 
                     // Generate actions dropdown
                     return `
@@ -633,7 +634,7 @@ export default class ClinicalAnalysisGrid extends LitElement {
                                     </li>
                                     <!-- Delete the case -->
                                     <li>
-                                        <a href="javascript: void 0" class="${isLockedDisabled} btn force-text-left" data-action="delete">
+                                        <a href="javascript: void 0" class="${isOwnOrIsLocked} btn force-text-left" data-action="delete">
                                             <i class="fas fa-trash icon-padding" aria-hidden="true"></i> Delete
                                         </a>
                                     </li>
