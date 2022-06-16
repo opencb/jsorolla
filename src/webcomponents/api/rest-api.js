8fbe72b36ea2ada2f46a796c861823b9cfc33c69
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2022-06-16 12:57:56 +0200

    wc: added rest-list dynamic panel height #TASK-1083

--- src/webcomponents/api/rest-api.js
+++ src/webcomponents/api/rest-api.js
@@ -81,16 +81,24 @@ export default class RestApi extends LitElement {
         }
     }
 
-    onClick(e, endpoint) {
+    async onClick(e, endpoint) {
         this.endpoint = endpoint;
         this.requestUpdate();
+
+        // Experimental dynamic height
+        const endpointPanel = document.querySelector("#rest-endpoint-panel");
+        const endpointList = document.getElementById("rest-list");
+        endpointList.style.maxHeight = endpointPanel?.clientHeight > 800? endpointPanel?.clientHeight + "px": "800px";
+
     }
 
     render() {
+
+
         return html`
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
-                    <div class="col-md-4 pre-scrollable-custom">
+                    <div id="rest-list" class="col-md-4" style="overflow-y:scroll; max-height:700px">
                         <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="false" data-cy="rest-api-endpoints">
                             ${this.api?.map(category => {
                                 const categoryName = category.name.replaceAll(" ", "_");
@@ -138,7 +146,7 @@ export default class RestApi extends LitElement {
                         </div>
                     </div>
 
-                    <div class="col-md-8">
+                    <div id="rest-endpoint-panel" class="col-md-8">
                         <rest-endpoint
                             .endpoint="${this.endpoint}"
                             .opencgaSession="${this.opencgaSession}">
