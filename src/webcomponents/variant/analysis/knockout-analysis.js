b7fbe93e8c42d0f5163ddc561b557aa2c37f7062
Josemi <josemijuanes@gmail.com>
2022-09-26 15:22:14 +0200

    wc: refactor knockout-analysis component

---
+++ src/webcomponents/variant/analysis/knockout-analysis.js
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2015-2016 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {LitElement, html} from "lit";
+import AnalysisUtils from "../../commons/analysis/analysis-utils.js";
+import FormUtils from "../../commons/forms/form-utils.js";
+import UtilsNew from "../../../core/utilsNew.js";
+import "../../commons/forms/data-form.js";
+import "../../commons/filters/catalog-search-autocomplete.js";
+import "../../commons/filters/consequence-type-select-filter.js";
+
+
+export default class KnockoutAnalysis extends LitElement {
+
+    constructor() {
+        super();
+
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            opencgaSession: {
+                type: Object,
+            },
+            toolParams: {
+                type: Object,
+            },
+            title: {
+                type: String,
+            },
+        };
+    }
+
+    #init() {
+        this.ANALYSIS_TOOL = "knockout";
+        this.ANALYSIS_TITLE = "Knockout Analysis";
+        this.ANALYSIS_DESCRIPTION = "Executes a Knockout analysis job";
+
+        this.DEFAULT_TOOLPARAMS = {};
+        // Make a deep copy to avoid modifying default object.
+        this.toolParams = {
+            ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
+        };
+
+        this.config = this.getDefaultConfig();
+    }
+
+    update(changedProperties) {
+        if (changedProperties.has("toolParams")) {
+            this.toolParams = {
+                ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
+                ...this.toolParams,
+            };
+            this.config = this.getDefaultConfig();
+        }
+        super.update(changedProperties);
+    }
+
+    check() {
+        // TODO: check if there are more required params for Knockout analysis
+        return !!this.toolParams.gene;
+    }
+
+    onFieldChange(e, field) {
+        const param = field || e.detail.param;
+        if (param) {
+            this.toolParams = FormUtils.createObject(this.toolParams, param, e.detail.value);
+        }
+        this.config = this.getDefaultConfig();
+        this.requestUpdate();
+    }
+
+    onSubmit() {
+        const toolParams = {
+            sample: this.toolParams.sample?.split(",") || [],
+            gene: this.toolParams.gene?.split(",") || [],
+            panel: this.toolParams.panel?.split(",") || [],
+            biotype: this.toolParams.biotype || "",
+            consequenceType: this.toolParams.consequenceType || "",
+            filter: this.toolParams.filter || "",
+            qual: this.toolParams.qual || "",
+            skipGenesFile: this.toolParams.skipGenesFile || false,
+            index: this.toolParams.index || false,
+        };
+        const params = {
+            study: this.opencgaSession.study.fqn,
+            ...AnalysisUtils.fillJobParams(this.toolParams, this.ANALYSIS_TOOL),
+        };
+        AnalysisUtils.submit(
+            this.ANALYSIS_TITLE,
+            this.opencgaSession.opencgaClient.variants()
+                .runKnockout(toolParams, params),
+            this,
+        );
+    }
+
+    onClear() {
+        this.toolParams = {
+            ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
+        };
+        this.config = this.getDefaultConfig();
+    }
+
+    render() {
+        return html`
+            <data-form
+                .data="${this.toolParams}"
+                .config="${this.config}"
+                @fieldChange="${e => this.onFieldChange(e)}"
+                @clear="${this.onClear}"
+                @submit="${this.onSubmit}">
+            </data-form>
+        `;
+    }
+
+    getDefaultConfig() {
+        const panelsList = (this.opencgaSession?.study?.panels || []).map(panel => ({
+            id: panel.id,
+            name: `${panel.name}
+                ${panel.source ? ` - ${panel.source.author || ""} ${panel.source.project} ${panel.source.version ? "v" + panel.source.version : ""}` : ""}
+                ${panel.stats ? ` (${panel.stats.numberOfGenes} genes, ${panel.stats.numberOfRegions} regions)` : ""}`,
+        }));
+
+        const params = [
+            {
+                title: "Sample Filters",
+                elements: [
+                    {
+                        title: "Sample",
+                        type: "custom",
+                        display: {
+                            render: toolParams => html`
+                                <catalog-search-autocomplete
+                                    .value="${toolParams?.sample}"
+                                    .resource="${"SAMPLE"}"
+                                    .opencgaSession="${this.opencgaSession}"
+                                    .config="${{multiple: true}}"
+                                    @filterChange="${e => this.onFieldChange(e, "sample")}">
+                                </catalog-search-autocomplete>
+                            `,
+                        },
+                    }
+                ],
+            },
+            {
+                title: "Gene Filters",
+                elements: [
+                    // ...AnalysisUtils.getVariantQueryConfiguration("", this.opencgaSession, this.onFieldChange.bind(this)),
+                    {
+                        title: "Gene",
+                        field: "gene",
+                        type: "input-text",
+                    },
+                    {
+                        title: "Type",
+                        field: "biotype",
+                        type: "select",
+                        allowedValues: BIOTYPES,
+                    },
+                    {
+                        title: "Disease Panel",
+                        field: "panel",
+                        type: "select",
+                        allowedValues: panelsList,
+                    },
+                ]
+            },
+            {
+                title: "Variant Filters",
+                elements: [
+                    {
+                        title: "Consequence Type",
+                        type: "custom",
+                        display: {
+                            render: toolParams => html`
+                                <consequence-type-select-filter
+                                    .ct="${toolParams.consequenceType}"
+                                    .config="${CONSEQUENCE_TYPES}"
+                                    @filterChange="${e => this.onFieldChange(e, "consequenceType")}">
+                                </consequence-type-select-filter>
+                            `,
+                        }
+                    },
+
+                    {
+                        title: "Filter",
+                        field: "filter",
+                        type: "input-text",
+                    },
+                    {
+                        title: "Qual",
+                        field: "qual",
+                        type: "input-text",
+                    },
+                ]
+            },
+            {
+                title: "Configuration Parameters",
+                elements: [
+                    {
+                        title: "Skip Genes File",
+                        field: "skipGenesFile",
+                        type: "checkbox",
+                    },
+                    {
+                        title: "index",
+                        field: "index",
+                        type: "checkbox",
+                    },
+                ],
+            }
+        ];
+
+        return AnalysisUtils.getAnalysisConfiguration(
+            this.ANALYSIS_TOOL,
+            this.title ?? this.ANALYSIS_TITLE,
+            this.ANALYSIS_DESCRIPTION,
+            params,
+            this.check()
+        );
+    }
+
+}
+
+customElements.define("knockout-analysis", KnockoutAnalysis);
