0ca7ae371264eaf596de2914f2b9e48565619b93
Josemi <josemijuanes@gmail.com>
2022-10-19 13:27:07 +0200

    core: clean svg manager and minor improvements #TASK-2174

--- src/core/svg.js
+++ src/core/svg.js
@@ -19,91 +19,57 @@
  * along with JS Common Libs. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Element.prototype.addChildSVG = function(elementName, attributes, index){
-//	var el = document.createElementNS('http://www.w3.org/2000/svg', elementName);
-//	for ( var key in attributes){
-//		el.setAttribute(key, attributes[key]);
-//	}
-//
-//	// insert child at requested index, or as last child if index is too high or no index is specified
-//    if ( null == index ) {
-//      this.appendChild( el );
-//    }
-//    else {
-//      var targetIndex = index + 1;
-//      if ( 0 == index ) {
-//        targetIndex = 0;
-//      }
-//      var targetEl = this.childNodes[ targetIndex ];
-//      if ( targetEl ) {
-//        this.insertBefore( el, targetEl );
-//      }
-//      else {
-//        this.appendChild( el );
-//      }
-//    }
-//    return el;
-// };
-// Element.prototype.initSVG = function(attributes){
-//	return this.addChildSVG("svg", attributes);
-// };
-
 export const SVG = {
-
     create(elementName, attributes) {
         const el = document.createElementNS("http://www.w3.org/2000/svg", elementName);
-        for (const key in attributes) {
+        Object.keys(attributes || {}).forEach(key => {
             el.setAttribute(key, attributes[key]);
-        }
+        });
         return el;
     },
 
     addChild(parent, elementName, attributes, index) {
         const el = document.createElementNS("http://www.w3.org/2000/svg", elementName);
-        for (const key in attributes) {
+        Object.keys(attributes || {}).forEach(key => {
             el.setAttribute(key, attributes[key]);
-        }
+        });
         return this._insert(parent, el, index);
     },
 
     addChildImage(parent, attributes, index) {
         const el = document.createElementNS("http://www.w3.org/2000/svg", "image");
-        for (const key in attributes) {
+        Object.keys(attributes || {}).forEach(key => {
             if (key == "xlink:href") {
                 el.setAttributeNS("http://www.w3.org/1999/xlink", "href", attributes[key]);
             } else {
-			    el.setAttribute(key, attributes[key]);
+                el.setAttribute(key, attributes[key]);
             }
-        }
+        });
         return this._insert(parent, el, index);
     },
 
     _insert(parent, el, index) {
-    // insert child at requested index, or as last child if index is too high or no index is specified
-	    if (index == null) {
-	    	parent.appendChild(el);
-	    } else {
-	      let targetIndex = index + 1;
-	      if (index == 0) {
-	        targetIndex = 0;
-	      }
-	      const targetEl = parent.childNodes[targetIndex];
-	      if (targetEl) {
-	    	  parent.insertBefore(el, targetEl);
-	      } else {
-	    	  parent.appendChild(el);
-	      }
-	    }
-	    return el;
+        // insert child at requested index, or as last child if index is too high or no index is specified
+        if (typeof index !== "number") {
+            parent.appendChild(el);
+        } else {
+            const targetIndex = index === 0 ? 0 : index + 1;
+            const targetEl = parent.childNodes[targetIndex];
+            if (targetEl) {
+                parent.insertBefore(el, targetEl);
+            } else {
+                parent.appendChild(el);
+            }
+        }
+        return el;
     },
 
     init(parent, attributes) {
         return this.addChild(parent, "svg", attributes);
     },
 
-
     //
-    /* Functions to generate arcs with PATH element  */
+    // Functions to generate arcs with PATH element
     //
 
     _polarToCartesian(centerX, centerY, radius, angleInDegrees) {
@@ -123,119 +89,8 @@ export const SVG = {
         const d = [
             "M", start.x, start.y,
             "A", radius, radius, 0, arcSweep, 0, end.x, end.y,
-        ].join(" ");
+        ];
 
-        return d;
+        return d.join(" ");
     },
 };
-
-// createSVG = function(elementName, attributes){
-//	var el = document.createElementNS('http://www.w3.org/2000/svg', elementName);
-//	for ( var key in attributes){
-//		el.setAttribute(key, attributes[key]);
-//	}
-//	return el;
-// };
-
-
-// var SVG =
-// {
-//		svgns : 'http://www.w3.org/2000/svg',
-//		xlinkns : "http://www.w3.org/1999/xlink",
-//
-// //	createSVGCanvas: function(parentNode, attributes)
-// //	{
-// ////		attributes['xmlns'] = SVG.svgns;
-// ////		attributes['xmlns:xlink'] = SVG.xlinkns;
-// ////		attributes.push( ['xmlns', SVG.svgns], ['xmlns:xlink', 'http://www.w3.org/1999/xlink']);
-// //		var svg = document.createElementNS(SVG.svgns, "svg");
-// //
-// //		for ( var key in attributes){
-// //			svg.setAttribute(key, attributes[key]);
-// //		}
-// //
-// //		parentNode.appendChild(svg);
-// //		return svg;
-// //	},
-//
-//	//Shape types : rect, circle, ellipse, line, polyline, polygon , path
-//	createElement : function (svgNode, shapeName, attributes) {
-//		try{
-//			if(attributes.width < 0){
-//				console.log("BIOINFO Warn: on SVG.createRectangle: width is negative, will be set to 0");
-//				attributes.width=0;
-//			}
-//			if(attributes.height < 0){
-//				console.log("BIOINFO Warn: on SVG.createRectangle: height is negative, will be set to 0");
-//				attributes.height=0;
-//			}
-//
-//			var shape = document.createElementNS('http://www.w3.org/2000/svg', shapeName);
-//			for ( var key in attributes){
-//				shape.setAttribute(key, attributes[key]);
-//			}
-//			svgNode.appendChild(shape);
-//		}
-//		catch(e){
-//			console.log("-------------------- ");
-//			console.log("Error on drawRectangle " + e);
-//			console.log(attributes);
-//			console.log("-------------------- ");
-//		}
-//		return shape;
-//	}
-// };
-//
-//
-//
-// var CanvasToSVG = {
-//
-//	convert: function(sourceCanvas, targetSVG, x, y, id, attributes) {
-//
-//		var img = this._convert(sourceCanvas, targetSVG, x, y, id);
-//
-//		for (var i=0; i< attributes.length; i++)
-//		{
-//			img.setAttribute(attributes[i][0], attributes[i][1]);
-//		}
-//	},
-//
-//	_convert: function(sourceCanvas, targetSVG, x, y, id) {
-//		var svgNS = "http://www.w3.org/2000/svg";
-//		var xlinkNS = "http://www.w3.org/1999/xlink";
-//		// get base64 encoded png from Canvas
-//		var image = sourceCanvas.toDataURL();
-//
-//		// must be careful with the namespaces
-//		var svgimg = document.createElementNS(svgNS, "image");
-//
-//		svgimg.setAttribute('id', id);
-//
-//		//svgimg.setAttribute('class', class);
-//		//svgimg.setAttribute('xlink:href', image);
-//		svgimg.setAttributeNS(xlinkNS, 'xlink:href', image);
-//
-//
-//		svgimg.setAttribute('x', x ? x : 0);
-//		svgimg.setAttribute('y', y ? y : 0);
-//		svgimg.setAttribute('width', sourceCanvas.width);
-//		svgimg.setAttribute('height', sourceCanvas.height);
-//		//svgimg.setAttribute('cursor', 'pointer');
-//		svgimg.imageData = image;
-//
-//		targetSVG.appendChild(svgimg);
-//		return svgimg;
-//	},
-//
-//	importSVG: function(sourceSVG, targetCanvas) {
-//	    svg_xml = sourceSVG;//(new XMLSerializer()).serializeToString(sourceSVG);
-//	    var ctx = targetCanvas.getContext('2d');
-//
-//	    var img = new Image();
-//	    img.src = "data:image/svg+xml;base64," + btoa(svg_xml);
-// //	    img.onload = function() {
-//	        ctx.drawImage(img, 0, 0);
-// //	    };
-//	}
-//
-// };
