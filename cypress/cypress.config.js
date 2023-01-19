5d9da58c53a3e28bacd7fb58f287628634f1610e
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-01-19 19:01:28 +0100

    wc: updated cypress configuration #TASK-3847

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -11,11 +11,6 @@ module.exports = defineConfig({
         json: true,
     },
     e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-        setupNodeEvents(on, config) {
-            return require("../cypress/plugins/index.js")(on, config);
-        },
         baseUrl: "http://localhost:3000/src/sites/test-app/",
     },
     // component: {
