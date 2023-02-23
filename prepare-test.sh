6073521f387dff2a8ee8a6111b03c7c63eaa63be
imedina <igmecas@gmail.com>
2023-02-23 17:52:28 +0100

    test: rename remote path

--- prepare-test.sh
+++ prepare-test.sh
@@ -18,7 +18,7 @@ FILE_TEST="src/sites/test-app/test-data/${VERSION}"
 if [ ! -d "$FILE_TEST" ]; then
     echo "create directory"
     mkdir -v -p $FILE_TEST
-    wget -r -np -A .json R "index.html" http://reports.test.zettagenomics.com/iva/tests/${VERSION}/ -P "$FILE_TEST"/ -nd
+    wget -r -np -A .json R "index.html" http://reports.test.zettagenomics.com/iva/test-data/${VERSION}/ -P "$FILE_TEST"/ -nd
     exit 0;
 fi
 
