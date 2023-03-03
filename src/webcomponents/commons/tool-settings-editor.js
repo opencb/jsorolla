a973ee821b51fea80bf5f72753e691c54a9d3008
gpveronica <veronica.zettagenomics@gmail.com>
2023-03-03 13:30:50 +0100

    wc - buttons visible and layout in tabs type

---
+++ src/webcomponents/commons/tool-settings-editor.js
@@ -0,0 +1,215 @@
+/**
+ * Copyright 2015-2023 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {html, LitElement} from "lit";
+import UtilsNew from "../../core/utils-new";
+import NotificationUtils from "./utils/notification-utils";
+import LitUtils from "./utils/lit-utils";
+import "./tool-settings-update.js";
+import "./tool-settings-preview.js";
+
+
+export default class ToolSettingsEditor extends LitElement {
+
+    // --- CONSTRUCTOR ---
+    constructor() {
+        super();
+
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    // --- PROPERTIES ---
+    static get properties() {
+        return {
+            toolSettings: {
+                type: Object,
+            },
+            toolName: {
+                type: Object,
+            },
+            study: {
+                type: Object,
+            },
+            opencgaSession: {
+                type: Object,
+            },
+        };
+    }
+
+    // --- PRIVATE METHODS ---
+    #init() {
+        this.isLoading = false;
+    }
+
+    #initOriginalObjects() {
+        // The original settings and study are maintained. A copy is used for previewing the ongoing changes in json
+        this._toolSettings = UtilsNew.objectClone(this.toolSettings);
+        this._config = {
+            ...this.getDefaultConfig(),
+            // ...this.config,
+        };
+    }
+
+    // --- UPDATE ---
+    update(changedProperties) {
+        if (changedProperties.has("toolSettings")) {
+            this.toolSettingsObserver();
+        }
+        super.update(changedProperties);
+    }
+
+    // --- OBSERVERS ---
+    toolSettingsObserver() {
+        if (this.toolSettings) {
+            this.#initOriginalObjects();
+        }
+    }
+
+    // --- EVENTS ---
+    onStudyToolSettingsChange(e, field) {
+        switch (field) {
+            case "json":
+                this.highlightPreview = true;
+                this._toolSettings = UtilsNew.objectClone(e.detail.value?.json);
+                break;
+            case "default":
+                this._toolSettings = UtilsNew.objectClone(this.opencgaSession.ivaDefaultSettings.settings[this.toolName]);
+                break;
+        }
+        LitUtils.dispatchCustomEvent(this, "studyToolSettingsChange", null, {
+            _toolSettings: this._toolSettings
+        });
+
+        this.requestUpdate();
+    }
+
+    // --- RENDER ---
+    #renderStyle() {
+        return html`
+            <style>
+                #tool-settings-editor-card{
+                    display: flex;
+                    flex-direction: column;
+                }
+                #tool-settings-editor-card-header{
+                    display: -ms-flexbox;
+                    display: -webkit-flex;
+                    display: flex;
+                    -webkit-justify-content: space-between;
+                    -ms-flex-pack: justify;
+                    justify-content: space-between;
+                    padding: 0.5em;
+                }
+
+            </style>`;
+    }
+
+    render() {
+        return html `
+            <!-- STYLE -->
+            ${this.#renderStyle()}
+
+            <div class="card" id="tool-settings-editor-card">
+                <!-- Header -->
+                <div class="card-header" id="tool-settings-editor-card-header">
+                    <div class="card-header-title"></div>
+                    <div class="card-header-options">
+                        <button class="btn btn-warning btn-sm" type="button" @click="${e => this.onStudyToolSettingsChange(e, "default")}">
+                            DEFAULT SETTINGS
+                        </button>
+                    </div>
+                </div>
+                <!-- Content -->
+                <div class="card-body" id="">
+                    <detail-tabs
+                        .data="${this._toolSettings}"
+                        .config="${this._config}"
+                        .opencgaSession="${this.opencgaSession}">
+                    </detail-tabs>
+                </div>
+            </div>
+        `;
+    }
+
+    // --- DEFAULT CONFIG ---
+    getDefaultConfig() {
+        return {
+            display: {
+                align: "left",
+            },
+            items: [
+                {
+                    id: "settings-json",
+                    name: "Configure",
+                    active: true,
+                    display: {
+                        align: "",
+                        titleClass: "",
+                        titleStyle: "",
+                        tabTitleClass: "",
+                        tabTitleStyle: "",
+                        contentClass: "",
+                        contentStyle: "",
+                    },
+                    render: () => {
+                        debugger
+                        return html `
+                            <json-editor
+                                .data="${this._toolSettings}"
+                                @fieldChange="${e => this.onStudyToolSettingsChange(e, "json")}">
+                            </json-editor>
+                        `;
+                    }
+                },
+                // 2. Component responsible for previewing the IVA settings
+                {
+                    id: "settings-preview",
+                    name: "Preview",
+                    active: false,
+                    display: {
+                        align: "",
+                        titleClass: "",
+                        titleStyle: "",
+                        tabTitleClass: "",
+                        tabTitleStyle: "",
+                        contentClass: "",
+                        contentStyle: "",
+                    },
+                    render: (data, isActive) => {
+                        if (isActive) {
+                            debugger
+                            return html`
+                                <tool-settings-preview
+                                    .opencgaSession="${this.opencgaSession}"
+                                    .settings="${this._toolSettings}"
+                                    .tool="${this.toolName}"
+                                    .highlightPreview="${this.highlightPreview}">
+                                </tool-settings-preview>
+                            `;
+                        }
+                    },
+                },
+            ],
+        };
+    }
+
+}
+
+customElements.define("tool-settings-editor", ToolSettingsEditor);
