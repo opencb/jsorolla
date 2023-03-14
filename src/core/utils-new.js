d6611ae1db9fd04785b1b66788c4b6eab8de5fdf
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-03-14 01:44:07 +0100

    wc: added function to parse file to base64 #TASK-4079

--- src/core/utils-new.js
+++ src/core/utils-new.js
@@ -1058,4 +1058,13 @@ export default class UtilsNew {
                 .filter(item => !!item);
     }
 
+    static toBase64(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = error => reject(error);
+        });
+    }
+
 }
