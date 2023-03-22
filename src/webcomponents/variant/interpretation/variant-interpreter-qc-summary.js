237e573359fc3fc7eeef96c5bf75cb7785108dba
Josemi <josemijuanes@gmail.com>
2023-03-22 16:28:55 +0100

    wc: fix typo in interpreter-qc-summary

--- src/webcomponents/variant/interpretation/variant-interpreter-qc-summary.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-qc-summary.js
@@ -237,7 +237,7 @@ class VariantInterpreterQcSummary extends LitElement {
                                         display: {
                                             render: data => html`
                                                 <div>${data.avgInsertSize}</div>
-                                            ,`
+                                            `,
                                         }
                                     },
                                     {
