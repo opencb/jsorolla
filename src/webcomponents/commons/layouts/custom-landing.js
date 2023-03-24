535b21d1c15d4b336dc7869dec854ccd73b81f84
Josemi <josemijuanes@gmail.com>
2023-03-24 12:14:27 +0100

    wc: finish prototype of landing page

--- src/webcomponents/commons/layouts/custom-landing.js
+++ src/webcomponents/commons/layouts/custom-landing.js
@@ -15,6 +15,8 @@
  */
 
 import {LitElement, html} from "lit";
+import UtilsNew from "../../../core/utils-new.js";
+import "../../user/user-login.js";
 
 export default class CustomLanding extends LitElement {
 
@@ -45,43 +47,82 @@ export default class CustomLanding extends LitElement {
     render() {
         return html`
             <style>
-                .landing-top {
+                .landing {
                     align-items: center;
                     display: flex;
-                    height: calc(16px * 5);
-                    justify-content: space-between;
+                    flex-direction: column;
+                    justify-content: center;
+                    min-height: calc(100vh - 70px);
                     padding: 16px;
                     width: 100%;
                 }
-                .landing-top-logo img {
-                    height: 40px;
+                .landing-logo {
+                    height: 100px;
+                    margin-bottom: 32px;
                     width: auto;
                 }
+                .landing-title {
+                    font-size: 3.5rem;
+                    font-weight: bold;
+                    margin-bottom: 32px;
+                    max-width: 640px;
+                    text-align: center;
+                    width: 100%;
+                }
                 .landing-content {
-                    align-items: center;
-                    display: flex;
-                    height: calc(100vh - 112px - 70px);
-                    justify-content: center;
-                    padding: 16px;
+                    margin-bottom: 32px;
+                    max-width: 640px;
+                    width: 100%;
+                }
+                .landing-login-sso-helper {
+                    color: #909294;
+                    font-size: 12px;
+                    margin-top: 1rem;
+                    max-width: 240px;
+                    text-align: center;
                     width: 100%;
                 }
             </style>
             <div class="landing">
-                <!-- Landing top section -->
-                <div class="landing-top">
-                    <div class="landing-top-logo">
-                        ${this.config?.landing?.logo ? html`
-                            <img src="${this.config.landing.logo}" />
-                        ` : null}
+                <!-- Landing logo section -->
+                ${this.config?.landingPage?.logo ? html`
+                    <div class="landing-logo ${this.config.landingPage.display?.logoClass}" style="${this.config.landingPage.display?.logoStyle}">
+                        <img height="100%" src="${this.config.landingPage.logo}" />
                     </div>
-                </div>
-                <div class="landing-content">
-                    <!-- Display login buttons -->
-                    <div align="center" style="margin-top:50px;">
-                        <a class="btn btn-primary" href="${this.getSSOUrl()}">
-                            <strong style="color:white;">Login with SSO</strong>
-                        </a>
+                ` : null}
+                <!-- Landing title -->
+                ${this.config?.landingPage?.title ? html`
+                    <div class="landing-title ${this.config.landingPage.display?.titleClass}" style="${this.config.landingPage.display?.titleStyle}">
+                        ${this.config.landingPage.title}
                     </div>
+                ` : null}
+                <!-- Landing description -->
+                ${this.config?.landingPage?.content ? html`
+                    <div align="center" class="landing-content ${this.config.landingPage.display?.contentClass}" style="${this.config.landingPage.display?.contentStyle}">
+                        ${UtilsNew.renderHTML(this.config.landingPage.content)}
+                    </div>
+                ` : null}
+                <!-- Landing login -->
+                <div class="landing-login">
+                    ${this.opencgaSession?.opencgaClient?._config?.sso ? html `
+                        <div align="center">
+                            <a class="btn-group" role="group" href="${this.getSSOUrl()}">
+                                <button type="button" class="btn btn-primary btn-lg" style="background-color: #286090;">
+                                    <i class="fas fa-user"></i>
+                                </button>
+                                <button type="button" class="btn btn-primary btn-lg">
+                                    <strong style="color:white;">Login with SSO</strong>
+                                </button>
+                            </a>
+                        </div>
+                        <div class="landing-login-sso-helper">
+                            By clicking on the <b>Login with SSO</b> button you will be redirected to your SSO login page.
+                        </div>
+                    ` : html`
+                        <user-login
+                            .opencgaSession="${this.opencgaSession}">
+                        </user-login>
+                    `}
                 </div>
             </div>
         `;
