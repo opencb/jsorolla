16c147dbdc8b28cb0e4fc2bbb96b596d2f0a6853
gpveronica <veronica.zettagenomics@gmail.com>
2023-03-28 13:55:32 +0200

    wc - Fix logo img attributes and style
    Signed-off-by: gpveronica <veronica.zettagenomics@gmail.com>

--- src/webcomponents/commons/layouts/custom-landing.js
+++ src/webcomponents/commons/layouts/custom-landing.js
@@ -50,6 +50,7 @@ export default class CustomLanding extends LitElement {
                 .landing-wrapper {
                     display: flex;
                     justify-content: center;
+                    align-items: stretch;
                     height: 100vh;
                 }
 
@@ -57,8 +58,44 @@ export default class CustomLanding extends LitElement {
                     flex: 1;
                 }
 
-                .landing-wrapper > .landing-left {
+                .landing-wrapper > .landing-company {
                     background-color: var(--main-bg-color);
+                    display: flex;
+                    flex-direction: column-reverse;
+                    align-items: center;
+                }
+
+                .landing-wrapper > .landing-company > div {
+                    flex: 1;
+                    display: flex;
+                    justify-content: center;
+                    align-items: flex-end;
+                    margin: 2em;
+                }
+
+                .landing-wrapper > .landing-company > .landing-logo {
+                    margin-top: 0;
+                    align-items: center;
+                }
+
+                .landing-wrapper .landing-title {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    text-align: center;
+                    font-size: 24px;
+                    font-weight: 100;
+                    letter-spacing: 8px;
+                    text-transform: uppercase;
+                }
+
+                .landing-wrapper > .landing-company > .landing-title {
+                    color: #f2f4f6;
+                    margin-bottom: 0;
+                }
+                .landing-wrapper > .landing > .landing-title {
+                    flex:0;
+                    margin: 0;
                 }
 
                 .landing {
@@ -66,31 +103,39 @@ export default class CustomLanding extends LitElement {
                     flex-direction: column;
                     justify-content: center;
                     align-items: center;
-                    padding: 16px;
                 }
 
-                .landing-logo > img{
+                .landing-wrapper > .landing > div {
+                    flex: 1;
+                    display: flex;
+                    justify-content: center;
+                    align-items: flex-start;
+                    margin: 2em;
+                }
+
+                .landing-wrapper > .landing > .landing-logo {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    margin-bottom: 0;
+                }
+
+
+
+                .landing-logo > img {
                     margin: 1em;
                 }
 
                 .landing-logo {
                     height: 100px;
-                    margin-bottom: 32px;
                     width: auto;
                 }
-                .landing-title {
-                    font-size: 3.5rem;
-                    font-weight: bold;
-                    margin-bottom: 32px;
-                    max-width: 860px;
-                    text-align: center;
-                    width: 100%;
-                }
+
                 .landing-content {
-                    margin-bottom: 32px;
                     max-width: 640px;
                     width: 100%;
                 }
+
                 .landing-login-sso-helper {
                     color: #909294;
                     font-size: 12px;
@@ -99,6 +144,7 @@ export default class CustomLanding extends LitElement {
                     text-align: center;
                     width: 100%;
                 }
+
                 .landing-login button.btn.btn-primary.btn-block {
                     background-color: hsl(222, 20%, 45%);
                     border: 0;
@@ -107,13 +153,23 @@ export default class CustomLanding extends LitElement {
 
             </style>
             <div class="landing-wrapper">
-                <div class="landing-left">
+                <div class="landing-company">
                     <!-- Landing logo section -->
-                    ${this.config?.landingPage?.company?.logo ? html`
-                        <div class="landing-logo ${this.config.landingPage?.company?.display?.logoClass}" style="${this.config.landingPage?.company?.display?.logoStyle}">
-                            <img height="100%" src="${this.config.landingPage?.company?.logo}" />
+                    ${this.config?.landingPage?.company?.logo?.img ? html`
+                        <div class="landing-logo ${this.config.landingPage?.company?.display?.logoClass}"
+                             style="${this.config.landingPage?.company?.display?.logoStyle}">
+                            ${this.config?.landingPage?.company?.logo?.link ? html `
+                                <a href="${this.config?.landingPage?.company?.logo?.link}" target="_blank">
+                                    <img height="${this.config?.landingPage?.company?.logo?.height || "30px"}"
+                                         src="${this.config.landingPage?.company?.logo?.img}"/>
+                                </a>
+                            `: html `
+                                <img height="${this.config?.landingPage?.company?.logo?.height || "30px"}"
+                                     src="${this.config.landingPage?.company?.logo?.img}"/>
+                            `}
                         </div>
-                        <div class="landing-title ${this.config.landingPage?.company?.display?.titleClass}" style="${this.config.landingPage?.company?.display?.titleStyle}">
+                        <div class="landing-title ${this.config.landingPage?.company?.display?.titleClass}"
+                             style="${this.config.landingPage?.company?.display?.titleStyle}">
                             ${this.config.landingPage?.company?.title}
                         </div>
                     ` : null}
@@ -121,22 +177,27 @@ export default class CustomLanding extends LitElement {
                 <div class="landing">
                     <!-- Landing title -->
                     ${this.config?.landingPage?.login?.logo || this.config.landingPage?.login?.title ? html`
-                        <div class="landing-logo ${this.config.landingPage?.login?.display?.logoClass}" style="${this.config.landingPage?.login?.display?.logoStyle}">
-                            <img height="100%" src="${this.config.landingPage?.login?.logo}" />
+                        <div class="landing-logo ${this.config.landingPage?.login?.display?.logoClass}"
+                             style="${this.config.landingPage?.login?.display?.logoStyle}">
+                            <img height="${this.config?.landingPage?.login?.logo?.height || "30px"}"
+                                 src="${this.config.landingPage?.login?.logo?.img}"/>
                         </div>
-                        <div class="landing-title ${this.config.landingPage?.login?.display?.titleClass}" style="${this.config.landingPage?.login?.display?.titleStyle}">
+                        <div class="landing-title ${this.config.landingPage?.login?.display?.titleClass}"
+                             style="${this.config.landingPage?.login?.display?.titleStyle}">
                             ${this.config.landingPage?.login?.title}
                         </div>
                     ` : null}
                     <!-- Landing description -->
                     ${this.config?.landingPage?.login?.content ? html`
-                        <div align="center" class="landing-content ${this.config.landingPage?.login?.display?.contentClass}" style="${this.config.landingPage?.login?.display?.contentStyle}">
+                        <div align="center"
+                             class="landing-content ${this.config.landingPage?.login?.display?.contentClass}"
+                             style="${this.config.landingPage?.login?.display?.contentStyle}">
                             ${UtilsNew.renderHTML(this.config.landingPage?.login?.content)}
                         </div>
                     ` : null}
                     <!-- Landing login -->
                     <div class="landing-login">
-                        ${this.opencgaSession?.opencgaClient?._config?.sso ? html `
+                        ${this.opencgaSession?.opencgaClient?._config?.sso ? html`
                             <div>
                                 <a class="btn-group" role="group" href="${this.getSSOUrl()}">
                                     <button type="button" class="btn btn-primary btn-lg" style="">
@@ -148,11 +209,12 @@ export default class CustomLanding extends LitElement {
                                 </a>
                             </div>
                             <div class="landing-login-sso-helper">
-                                By clicking on the <b>Login with SSO</b> button you will be redirected to your SSO login page.
+                                By clicking on the <b>Login with SSO</b> button you will be redirected to your SSO login
+                                page.
                             </div>
                         ` : html`
                             <user-login
-                                .opencgaSession="${this.opencgaSession}">
+                                    .opencgaSession="${this.opencgaSession}">
                             </user-login>
                         `}
                     </div>
