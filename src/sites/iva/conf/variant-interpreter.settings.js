503e989079fd504eba15fb6aaeb9e5831d7b4ed7
Josemi <josemijuanes@gmail.com>
2023-05-06 00:15:10 +0200

    iva: enable pharmacogenomics step in interpreter settings

--- src/sites/iva/conf/variant-interpreter.settings.js
+++ src/sites/iva/conf/variant-interpreter.settings.js
@@ -36,6 +36,9 @@ const VARIANT_INTERPRETER_SETTINGS = {
         },
         {
             id: "report"
-        }
+        },
+        {
+            id: "pharmacogenomics",
+        },
     ]
 };
