303908b225efb3967c12ef236026d1d38d6f9fb2
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-05-06 15:03:11 +0200

    wc: initialize pharmacogenomics detail component

---
+++ src/webcomponents/variant/interpretation/variant-interpreter-pharmacogenomics-detail.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2015-2023 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {LitElement, html} from "lit";
+import "../../commons/view/detail-tabs";
+
+export default class VariantInterpreterPharmacogenomicsDetail extends LitElement {
+
+    constructor() {
+        super();
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            opencgaSession: {
+                type: Object
+            },
+            variantId: {
+                type: String
+            },
+            variant: {
+                type: Object
+            },
+            config: {
+                type: Object
+            }
+        };
+    }
+
+    #init() {
+        this._config = this.getDefaultConfig();
+    }
+
+    update(changedProperties) {
+        if (changedProperties.has("config")) {
+            this._config = {...this.getDefaultConfig(), ...this.config};
+        }
+        super.update(changedProperties);
+    }
+
+    getDefaultConfig() {
+        return {};
+    }
+
+    render() {
+        if (!this.opencgaSession) {
+            return "";
+        }
+
+        return html`
+            <detail-tabs
+                .data="${this.variant}"
+                .config="${this._config}"
+                .opencgaSession="${this.opencgaSession}">
+            </detail-tabs>`;
+    }
+
+}
+
+customElements.define("variant-interpreter-pharmacogenomics-detail", VariantInterpreterPharmacogenomicsDetail);
