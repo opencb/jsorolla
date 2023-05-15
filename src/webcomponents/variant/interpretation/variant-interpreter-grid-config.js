c550c918fdaa55ef4d64b0f77e25c80627a2c645
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-05-15 21:26:38 +0200

    wc: variant interpreter grid config improvements #TASK-4288

--- src/webcomponents/variant/interpretation/variant-interpreter-grid-config.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-grid-config.js
@@ -16,6 +16,8 @@
 
 import {LitElement, html} from "lit";
 import LitUtils from "../../commons/utils/lit-utils.js";
+import OpencgaCatalogUtils from "../../../core/clients/opencga/opencga-catalog-utils.js";
+import NotificationUtils from "../../commons/utils/notification-utils.js";
 import "../../commons/forms/data-form.js";
 
 export default class VariantInterpreterGridConfig extends LitElement {
@@ -37,6 +39,9 @@ export default class VariantInterpreterGridConfig extends LitElement {
             gridColumns: {
                 type: Object
             },
+            toolId: {
+                type: String
+            },
             config: {
                 type: Object
             }
@@ -55,10 +60,10 @@ export default class VariantInterpreterGridConfig extends LitElement {
     }
 
     onConfigObserver() {
-        this._highlights = this.config.highlights
-            .filter(h => h.active)
-            .map(h => h.id)
-            .join(",") || [];
+        this._highlights = this.config?.highlights
+            ?.filter(h => h.active)
+            ?.map(h => h.id)
+            ?.join(",") || [];
 
 
         // Prepare data for the column select
@@ -141,12 +146,34 @@ export default class VariantInterpreterGridConfig extends LitElement {
         LitUtils.dispatchCustomEvent(this, "configChange", this.config);
     }
 
+    async onSubmit() {
+        const newGridConfig = {...this.config};
+
+        // Remove highlights and copies configuration from new config
+        if (newGridConfig._highlights) {
+            delete newGridConfig._highlights;
+        }
+
+        // Update user configuration
+        try {
+            await OpencgaCatalogUtils.updateGridConfig(this.opencgaSession, this.toolId, newGridConfig);
+            LitUtils.dispatchCustomEvent(this, "settingsUpdate");
+
+            NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_SUCCESS, {
+                message: "Configuration saved",
+            });
+        } catch (error) {
+            NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_RESPONSE, error);
+        }
+    }
+
     render() {
         return html`
             <data-form
                 .data="${this.config}"
                 .config="${this.getConfigForm()}"
-                @fieldChange="${e => this.onFieldChange(e)}">
+                @fieldChange="${e => this.onFieldChange(e)}"
+                @submit="${e => this.onSubmit(e)}">
             </data-form>
         `;
     }
@@ -168,7 +195,7 @@ export default class VariantInterpreterGridConfig extends LitElement {
                 titleAlign: "left",
                 titleWidth: 4,
                 defaultLayout: "vertical",
-                buttonsVisible: false
+                buttonsVisible: true
             },
             sections: [
                 {
