3e44ac0f0c1ec6c3e4406829edde3d3d16f99712
Josemi <josemijuanes@gmail.com>
2023-05-15 13:18:29 +0200

    wc: added error notification in pharmacogenomics-report component

--- src/webcomponents/clinical/pharmacogenomics/pharmacogenomics-report.js
+++ src/webcomponents/clinical/pharmacogenomics/pharmacogenomics-report.js
@@ -31,7 +31,8 @@ export default class PharmacogenomicsReport extends LitElement {
 
     #init() {
         this.active = true;
-        this.isLoading = false;
+        this.loading = false;
+        this.error = false;
         this.variants = [];
         this.config = this.getDefaultConfig();
     }
@@ -46,83 +47,98 @@ export default class PharmacogenomicsReport extends LitElement {
     async sampleIdObserver() {
         this.variants = [];
         if (this.opencgaSession && this.sampleId && this.active) {
-            this.isLoading = true;
+            this.error = false;
+            this.loading = true;
             this.requestUpdate();
-            // 0. Initialize Cellbase client instance
-            const cellbaseClient = new CellBaseClient({
-                // host: this.opencgaSession?.project?.cellbase?.url || this.opencgaSession?.project?.internal?.cellbase?.url,
-                host: "https://ws.zettagenomics.com/cellbase",
-                version: "v5.5",
-                species: "hsapiens",
-            });
-
-            // 1. Import all PGx variants from Cellbase
-            const pgxVariantsResponse = await cellbaseClient.get("clinical", "pharmacogenomics", null, "distinct", {
-                field: "variants.location",
-                assembly: "grch38",
-                dataRelease: "5",
-            });
-
-            // 2. Get the list of variants available in OpenCGA
-            const pgxVariants = pgxVariantsResponse.responses[0].results;
-            const chunkSize = 200;
-            const promises = [];
-            for (let i = 0; i < pgxVariants.length; i = i + chunkSize) {
-                const chunk = pgxVariants.slice(i, i + chunkSize);
-                const query = {
-                    region: chunk.join(","),
-                    sample: this.sampleId,
-                    // include: "id",
-                    study: this.opencgaSession.study.fqn,
-                    includeSampleId: true,
-                };
-                promises.push(this.opencgaSession.opencgaClient.clinical().queryVariant(query));
-            }
-            const variantsResponses = await Promise.all(promises);
-
-            // 3. Generate the list of variants
-            const variants = new Map();
-            variantsResponses.forEach(variantResponse => {
-                variantResponse.responses[0].results.forEach(variant => {
-                    variants.set(variant.id, variant);
+            try {
+                // 0. Initialize Cellbase client instance
+                const cellbaseClient = new CellBaseClient({
+                    // host: this.opencgaSession?.project?.cellbase?.url || this.opencgaSession?.project?.internal?.cellbase?.url,
+                    host: "https://ws.zettagenomics.com/cellbase",
+                    version: "v5.5",
+                    species: "hsapiens",
                 });
-            });
 
-            // 4. Import pharmacogenomics annotation from cellbase
-            const ids = Array.from(variants.keys());
-            const pgxAnnotationPromises = [];
-            for (let i = 0; i < ids.length; i = i + 50) {
-                const chunk = ids.slice(i, i + 50);
-                const query = {
+                // 1. Import all PGx variants from Cellbase
+                const pgxVariantsResponse = await cellbaseClient.get("clinical", "pharmacogenomics", null, "distinct", {
+                    field: "variants.location",
                     assembly: "grch38",
                     dataRelease: "5",
-                    include: "pharmacogenomics",
-                };
-                pgxAnnotationPromises.push(cellbaseClient.get("genomic", "variant", chunk.join(","), "annotation", query));
-            }
-            const pgxAnnotationResponses = await Promise.all(pgxAnnotationPromises);
-
-            // 5. Merge pgx annotation with variants
-            pgxAnnotationResponses.forEach(pgxAnnotationResponse => {
-                pgxAnnotationResponse.responses.forEach(response => {
-                    if (variants.has(response.id)) {
-                        variants.get(response.id).annotation.pharmacogenomics = response.results[0].pharmacogenomics;
-                    }
                 });
-            });
 
-            // 5. Save variants and request update
-            this.variants = Array.from(variants.values());
-            this.isLoading = false;
+                // 2. Get the list of variants available in OpenCGA
+                const pgxVariants = pgxVariantsResponse.responses[0].results;
+                const chunkSize = 200;
+                const promises = [];
+                for (let i = 0; i < pgxVariants.length; i = i + chunkSize) {
+                    const chunk = pgxVariants.slice(i, i + chunkSize);
+                    const query = {
+                        region: chunk.join(","),
+                        sample: this.sampleId,
+                        // include: "id",
+                        study: this.opencgaSession.study.fqn,
+                        includeSampleId: true,
+                    };
+                    promises.push(this.opencgaSession.opencgaClient.clinical().queryVariant(query));
+                }
+                const variantsResponses = await Promise.all(promises);
+
+                // 3. Generate the list of variants
+                const variants = new Map();
+                variantsResponses.forEach(variantResponse => {
+                    variantResponse.responses[0].results.forEach(variant => {
+                        variants.set(variant.id, variant);
+                    });
+                });
+
+                // 4. Import pharmacogenomics annotation from cellbase
+                const ids = Array.from(variants.keys());
+                const pgxAnnotationPromises = [];
+                for (let i = 0; i < ids.length; i = i + 50) {
+                    const chunk = ids.slice(i, i + 50);
+                    const query = {
+                        assembly: "grch38",
+                        dataRelease: "5",
+                        include: "pharmacogenomics",
+                    };
+                    pgxAnnotationPromises.push(cellbaseClient.get("genomic", "variant", chunk.join(","), "annotation", query));
+                }
+                const pgxAnnotationResponses = await Promise.all(pgxAnnotationPromises);
+
+                // 5. Merge pgx annotation with variants
+                pgxAnnotationResponses.forEach(pgxAnnotationResponse => {
+                    pgxAnnotationResponse.responses.forEach(response => {
+                        if (variants.has(response.id)) {
+                            variants.get(response.id).annotation.pharmacogenomics = response.results[0].pharmacogenomics;
+                        }
+                    });
+                });
+
+                // 5. Save variants and request update
+                this.variants = Array.from(variants.values());
+            } catch (error) {
+                console.error(error);
+                this.error = true;
+            }
+            this.loading = false;
             this.requestUpdate();
         }
     }
 
     render() {
-        if (this.isLoading) {
+        if (this.loading) {
             return html`
                 <div style="padding-top:32px">
                     <loading-spinner></loading-spinner>
+                </div>
+            `;
+        }
+        if (this.error) {
+            return html`
+                <div class="col-md-10 col-md-offset-1" style="padding:32px 0px;">
+                    <div class="alert alert-danger">
+                        <i class="fas fa-exclamation-triangle icon-padding"></i> Error getting Pharmacogenomics info. Plase try again later.
+                    </div>
                 </div>
             `;
         }
