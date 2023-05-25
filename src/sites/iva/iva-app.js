65beec4edbf7e5c85c18efc820eb1a9eb7637ebe
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-05-26 00:20:46 +0200

    add custom SCSS and bootstrap js in iva-app #TASK-4482

--- src/sites/iva/iva-app.js
+++ src/sites/iva/iva-app.js
@@ -102,6 +102,10 @@ import "../../webcomponents/commons/layouts/custom-landing.js";
 import "../../webcomponents/clinical/rga/rga-browser.js";
 import OpencgaCatalogUtils from "../../core/clients/opencga/opencga-catalog-utils";
 
+import "../../../styles/scss/custom.scss";
+import * as bootstrap from "bootstrap";
+
+
 class IvaApp extends LitElement {
 
     constructor() {
