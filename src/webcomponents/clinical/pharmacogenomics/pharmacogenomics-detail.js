8f3bd692194221b6844c57c1481df189a9c7c3f3
Josemi <josemijuanes@gmail.com>
2023-05-29 17:40:30 +0200

    wc: added pgx overview tab #TASK-4457

--- src/webcomponents/clinical/pharmacogenomics/pharmacogenomics-detail.js
+++ src/webcomponents/clinical/pharmacogenomics/pharmacogenomics-detail.js
@@ -59,6 +59,55 @@ export default class PharmacogenomicsDetail extends LitElement {
             showTitle: true,
             items: [
                 {
+                    id: "overview",
+                    name: "Overview",
+                    active: true,
+                    render: variant => {
+                        const drugsRows = variant.annotation.pharmacogenomics.map(item => {
+                            return item.annotations.map(annotation => {
+                                const allelesContent = annotation.alleles.map(allele => html`
+                                    <div style="">
+                                        <div style="font-weight:bold;">Allele: ${allele.allele}</div>
+                                        <div>${allele.annotation || "-"}</div>
+                                    </div>
+                                `);
+                                return html`
+                                    <tr>
+                                        <td>
+                                            <div style="font-weight:bold;">${item.name}</div>
+                                            <div style="font-size:12px;color:#737373;width:120px;">
+                                                ${(annotation.phenotypes || []).join(", ")}
+                                            </div>
+                                        </td>
+                                        <td>${annotation.phenotypeType || "-"}</td>
+                                        <td>${annotation.geneName || "-"}</td>
+                                        <td>
+                                            <div style="display:flex;flex-direction:column;gap:8px;">
+                                                ${allelesContent || "-"}
+                                            </div>
+                                        </td>
+                                    </tr>
+                                `;
+                            });
+                        });
+                        return html`
+                            <table class="table table-hover table-no-bordered">
+                                <thead>
+                                    <tr>
+                                        <th style="padding:8px;">Drug</th>
+                                        <th style="padding:8px;">Phenotype Type</th>
+                                        <th style="padding:8px;">Gene</th>
+                                        <th style="padding:8px;">Annotation</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    ${drugsRows.flat()}
+                                </tbody>
+                            </table>
+                        `;
+                    },
+                },
+                {
                     id: "json-view",
                     name: "JSON Data",
                     render: (variant, active) => html`
