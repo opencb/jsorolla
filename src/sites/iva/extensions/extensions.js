de0f294c9e7b6ba4004bb3f29e13c03c044380ea
Josemi <josemijuanes@gmail.com>
2023-05-31 13:01:23 +0200

    iva: added new poc for a columns extension configuration #TASK-4294

--- src/sites/iva/extensions/extensions.js
+++ src/sites/iva/extensions/extensions.js
@@ -20,19 +20,78 @@ window.IVA_EXTENSIONS = {
                 </div>
             `,
         },
+        // {
+        //     id: "custom-column",
+        //     name: "New Column",
+        //     description: "Example column",
+        //     type: "column",
+        //     components: [
+        //         "variant-browser-grid",
+        //     ],
+        //     columnTitle: "New Column",
+        //     columnRowspan: 2,
+        //     columnFormatter: (value, row, index) => {
+        //         return `Row ${index}`;
+        //     },
+        // },
         {
-            id: "custom-column",
-            name: "New Column",
-            description: "Example column",
+            id: "custom-columns",
+            name: "Custom columns",
+            description: "Example columns for Variant Browser",
             type: "column",
             components: [
                 "variant-browser-grid",
             ],
-            columnTitle: "New Column",
-            columnRowspan: 2,
-            columnFormatter: (value, row, index) => {
-                return `Row ${index}`;
-            },
+            maintainer: "",
+            version: "",
+            compatibleWith: "",
+            columns: [
+                [
+                    {
+                        id: "new-column-1",
+                        title: "Extra column",
+                        field: "",
+                        rowspan: 2,
+                        colspan: 1,
+                        align: "center",
+                        formatter: (value, row, index) => `Row ${index}`,
+                        _injectPosition: -4,
+                    },
+                    {
+                        id: "new-column-2",
+                        title: "Extra columns",
+                        field: "",
+                        rowspan: 1,
+                        colspan: 2,
+                        align: "center",
+                        _injectPosition: -2,
+                    },
+                ],
+                [
+                    {
+                        id: "new-subcolumn-1",
+                        title: "Column 1",
+                        field: "",
+                        colspan: 1,
+                        rowspan: 1,
+                        formatter: (value, row, index) => `Row ${index}.1`,
+                        align: "center",
+                        // visible: true,
+                        // _injectPosition: -1,
+                    },
+                    {
+                        id: "new-subcolumn-2",
+                        title: "Column 2",
+                        field: "",
+                        colspan: 1,
+                        rowspan: 1,
+                        formatter: (value, row, index) => `Row ${index}.2`,
+                        align: "center",
+                        // visible: true,
+                        // _injectPosition: -1,
+                    },
+                ],
+            ],
         },
     ],
 };
