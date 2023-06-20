76f76a2162379afd0bc9a04196ef231d8dbb00dc
gpveronica <veronica.zettagenomics@gmail.com>
2023-06-20 17:38:52 +0200

    wc - Rest api reestructured into menu and opencga
    Signed-off-by: gpveronica <veronica.zettagenomics@gmail.com>

--- src/webcomponents/api/rest-api.js
+++ src/webcomponents/api/rest-api.js
@@ -17,7 +17,8 @@
 import {html, LitElement} from "lit";
 import UtilsNew from "../../core/utils-new.js";
 import "../commons/forms/data-form.js";
-import "./rest-endpoint.js";
+import "./opencga-rest-endpoint.js";
+import "./rest-menu.js";
 
 export default class RestApi extends LitElement {
 
@@ -43,18 +44,12 @@ export default class RestApi extends LitElement {
     }
 
     _init() {
-        this._prefix = UtilsNew.randomString(8);
-        this.api = [];
-        this.methodOrder = {
-            "GET": 1,
-            "POST": 2,
-            "DELETE": 3
-        };
-        this.methodColor = {
-            "GET": "blue",
-            "POST": "darkorange",
-            "DELETE": "red"
-        };
+        this.listAPI = [
+            {id: "opencga", title: "OpenCGA API REST", description: ""},
+            {id: "cellbase", title: "Cellbase API REST", description: ""}
+        ];
+        this.selectedAPI = this.listAPI[0];
+
     }
 
     update(changedProperties) {
@@ -63,6 +58,8 @@ export default class RestApi extends LitElement {
         }
         super.update(changedProperties);
     }
+
+    //
 
     opencgaSessionObserver() {
         if (this.opencgaSession) {
@@ -87,76 +84,105 @@ export default class RestApi extends LitElement {
         this.api[categoryIndex].endpoints.splice(endpointsIndex, 1);
     }
 
-    async onClick(e, endpoint) {
-        this.endpoint = endpoint;
+    onEndpointSelect(e) {
+        this.endpoint = e.detail.endpoint;
         this.requestUpdate();
-
-        // Experimental dynamic height
-        const endpointPanel = document.querySelector("#rest-endpoint-panel");
-        const endpointList = document.getElementById("rest-list");
-        endpointList.style.maxHeight = endpointPanel?.clientHeight > 1000? endpointPanel?.clientHeight + "px": "1000px";
-
     }
+
+    onAPISelect(e, api) {
+        switch (api.id) {
+            case "opencga":
+                this.selectedAPI = UtilsNew.objectClone(api);
+                this.opencgaSessionObserver();
+                break;
+            case "cellbase":
+                break;
+        }
+    }
+
+    renderStyle() {
+        return html `
+            <style>
+                .rest-api {
+                    display: flex;
+                    align-items: stretch
+                }
+                .rest-options {
+                    display: flex;
+                    align-items: stretch
+                    flex:0;
+                    background-color: lightgrey;
+                }
+                .rest-option {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    width: 4em;
+                    height: 4em;
+                }
+                .rest-option-button{
+                    background-color: red;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .rest-option-button-icon{
+                    color:white;
+                }
+
+                .rest-manager {
+                    display: flex;
+                    flex:1;
+                }
+
+            </style>
+        `;
+    }
+
 
     render() {
 
-
         return html`
+            ${this.renderStyle()}
             <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <div id="rest-list" class="col-md-4" style="overflow-y:scroll; max-height:700px">
-                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="false" data-cy="rest-api-endpoints">
-                            ${this.api?.map(category => {
-                                const categoryName = category.name.replaceAll(" ", "_");
-                                const isFirst = category.name === this.api[0].name;
-                                return html`
-                                    <div class="panel panel-default">
-                                        <div class="panel-body" role="tab" id="${categoryName}HeadingOne">
-                                            <h5 class="panel-title">
-                                                <div class="">
-                                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#${categoryName}CollapseOne" aria-expanded="true" aria-controls="${category.name}CollapseOne">
-                                                        <div style="margin-bottom: 10px">
-                                                            ${category.name} <span class="badge pull-right">${category.endpoints?.length || "NA"}</span>
-                                                        </div>
-                                                        <div style="font-size: 1em;text-transform: none;letter-spacing: 1px">
-                                                            ${category.path}
-                                                        </div>
-                                                    </a>
-                                                </div>
-                                            </h5>
-                                        </div>
-                                        <div id="${categoryName}CollapseOne" class="panel-collapse collapse ${isFirst ? "in" : ""}" role="tabpanel" aria-labelledby="${categoryName}HeadingOne">
-                                            ${category.endpoints
-                                                .sort((a, b) => {
-                                                    return this.methodOrder[a.method] - this.methodOrder[b.method];
-                                                })
-                                                .map(endpoint => html`
-                                                    <div class="panel-body" style="padding: 5px 15px">
-                                                        <button class="btn btn-link" role="link" style="display:flex;white-space:normal;" @click="${e => this.onClick(e, endpoint)}">
-                                                            <span style="margin-right: 10px; font-weight: bold; color:${this.methodColor[endpoint.method]}">
-                                                                ${endpoint.method}
-                                                            </span>
-                                                            <span style="font-weight:bold;word-break:break-word;text-align:left;" data-cy="endpoint-path">
-                                                                ${endpoint.path.replace("/{apiVersion}", "")}
-                                                            </span>
-                                                        </button>
-                                                        <div class="help-block" style="margin: 0 15px">
-                                                            ${endpoint.description}
-                                                        </div>
-                                                    </div>
-                                                `)}
-                                        </div>
-                                    </div>
-                                `;
-                            })}
+                <div class="rest-api">
+                    <!-- LEFT-BAR -->
+                    <div class="rest-options">
+                        <!-- List wrapper -->
+                        <div class="rest-option dropdown">
+                            <a href="#" class="rest-option-button dropdown-toggle study-switcher dropdown-button-wrapper"
+                               data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <div class="rest-option-button-icon">A</div>
+                            </a>
+                            <ul class="dropdown-menu">
+                                ${this.listAPI.map(api => html`
+                                    <li>
+                                        <a href="javascript: void 0" @click="${e => this.onAPISelect(e, api)}">
+                                            ${api.title}
+                                        </a>
+                                    </li>
+                                `)}
+                            </ul>
                         </div>
                     </div>
-
-                    <div id="rest-endpoint-panel" class="col-md-8">
-                        <rest-endpoint
-                            .endpoint="${this.endpoint}"
-                            .opencgaSession="${this.opencgaSession}">
-                        </rest-endpoint>
+                    <!-- MAIN-MANAGER -->
+                    <div class="rest-manager col-md-10">
+                        <!-- SIDE-BAR -->
+                        <div class="col-md-4">
+                            <rest-menu
+                                .apiInfo="${this.selectedAPI}"
+                                .api="${this.api}"
+                                .opencgaSession="${this.opencgaSession}"
+                                @endpointSelect=${this.onEndpointSelect}>
+                            </rest-menu>
+                        </div>
+                        <!-- DASHBOARD -->
+                        <div id="rest-endpoint-panel" class="col-md-8">
+                            <opencga-rest-endpoint
+                                .endpoint="${this.endpoint}"
+                                .opencgaSession="${this.opencgaSession}">
+                            </opencga-rest-endpoint>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/webcomponents/api/rest-menu.js
@@ -0,0 +1,165 @@
+/**
+ * Copyright 2015-2023 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {html, LitElement} from "lit";
+import LitUtils from "../commons/utils/lit-utils";
+
+export default class RestMenu extends LitElement {
+
+    constructor() {
+        super();
+
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            apiInfo: {
+                type: Object,
+            },
+            api: {
+                type: Object,
+            },
+            opencgaSession: {
+                type: Object,
+            },
+        };
+    }
+
+    #init() {
+        this.config = this.getDefaultConfig();
+    }
+
+    update(changedProperties) {
+        super.update(changedProperties);
+    }
+
+    onClick(e, endpoint) {
+        LitUtils.dispatchCustomEvent(this, "endpointSelect", "", {event: e, endpoint: endpoint}, null);
+    }
+
+    #renderList() {
+        return html `
+            <div id="rest-list">
+                <div class="panel-body" id="accordion" role="tablist" aria-multiselectable="false" data-cy="rest-api-endpoints">
+                    ${
+                        this.api?.map(category => {
+                            const categoryName = category.name.replaceAll(" ", "_");
+                            const isFirst = category.name === this.api[0].name;
+                            return html `
+                                <div class="panel panel-default">
+                                    <div class="panel-body" role="tab" id="${categoryName}HeadingOne">
+                                        <h5 class="panel-title">
+                                            <div class="">
+                                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#${categoryName}CollapseOne" aria-expanded="true" aria-controls="${category.name}CollapseOne">
+                                                    <div style="margin-bottom: 10px">
+                                                        ${category.name} <span class="badge pull-right">${category.endpoints?.length || "NA"}</span>
+                                                    </div>
+                                                    <div style="font-size: 1em;text-transform: none;letter-spacing: 1px">
+                                                        ${category.path}
+                                                    </div>
+                                                </a>
+                                            </div>
+                                        </h5>
+                                    </div>
+                                    <div id="${categoryName}CollapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="${categoryName}HeadingOne">
+                                        ${
+                                            category.endpoints.sort((a, b) => {
+                                                return this.config.methodOrder[a.method] - this.config.methodOrder[b.method];
+                                            }).map(endpoint => html `
+                                                <div class="panel-body" style="padding: 5px 15px">
+                                                    <button class="btn btn-link" role="link" style="display:flex;white-space:normal;" @click="${e => this.onClick(e, endpoint)}">
+                                                        <span style="margin-right: 10px; font-weight: bold; color:${this.config.methodColor[endpoint.method]}">
+                                                            ${endpoint.method}
+                                                        </span>
+                                                        <span style="font-weight:bold;word-break:break-word;text-align:left;" data-cy="endpoint-path">
+                                                            ${endpoint.path.replace("/{apiVersion}", "")}
+                                                        </span>
+                                                    </button>
+                                                    <div class="help-block" style="margin: 0 15px">
+                                                        ${endpoint.description}
+                                                    </div>
+                                                </div>
+                                            `)
+                                        }
+                                    </div>
+                                </div>
+                            `;
+                        })
+                    }
+                </div>
+            </div>
+        `;
+    }
+
+    renderStyle() {
+        return html `
+            <style>
+                .rest-menu-wrapper{
+                    display: flex;
+                    flex-direction: column;
+                }
+            </style>
+        `;
+    }
+
+    render() {
+        return html`
+            ${this.renderStyle()}
+            <div class="rest-menu-wrapper panel panel-default">
+                <!-- SIDE-BAR: APIs title-->
+                <div class="panel-heading">
+                    <div>${this.apiInfo.title}</div>
+                </div>
+                <!-- SIDE-BAR FILTER-->
+                <div class="rest-menu-filter-wrapper">
+                    <!-- 1. Search jetbrains-like -->
+                    <div class="rest-fuzzy-search"></div>
+                    <!-- 2. Filters jetbrains-like -->
+                    <div class="rest-filters"></div>
+                </div>
+                <!-- SIDE-BAR LIST-ENDPOINTS -->
+                <div>
+                    ${this.#renderList()}
+                </div>
+            </div>
+        `;
+    }
+
+    getDefaultConfig() {
+        return {
+            methodOrder: {
+                "GET": 1,
+                "POST": 2,
+                "DELETE": 3
+            },
+            methodColor: {
+                "GET": "blue",
+                "POST": "darkorange",
+                "DELETE": "red"
+            },
+        };
+    }
+
+
+}
+
+customElements.define("rest-menu", RestMenu);
+
