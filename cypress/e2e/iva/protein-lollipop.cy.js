bccfee82adbdc3c16a37486f946904a4944021fc
Josemi <josemijuanes@gmail.com>
2023-07-10 16:38:10 +0200

    test: implemented more tests for highlighted variants in protein lollipop viz #TASK-4724

--- cypress/e2e/iva/protein-lollipop.cy.js
+++ cypress/e2e/iva/protein-lollipop.cy.js
@@ -17,12 +17,14 @@
 context("Protein Lollipop Viz", () => {
     const svgSelector = `div[data-test-id="protein-lollipop-container"] svg`;
 
+    const highlightColor = "rgba(253, 152, 67, 0.6)";
+
     beforeEach(() => {
         cy.visit("#protein-lollipop");
         cy.waitUntil(() => cy.get(svgSelector).should("exist"));
     });
 
-    context("Render", () => {
+    context("render", () => {
         it("should render main tracks", () => {
             const mainTracks = [
                 "main:scale",
@@ -43,7 +45,7 @@ context("Protein Lollipop Viz", () => {
         });
     });
 
-    context("Highlight", () => {
+    context("highlight", () => {
         const highlightedVariant = "X:150638967:G:-";
 
         beforeEach(() => {
@@ -52,15 +54,35 @@ context("Protein Lollipop Viz", () => {
             });
         });
 
-        it("should mark as highlighted in the main variants track", () => {
+        it("should mark the variant as highlighted in the main variants track", () => {
             cy.get("@variant").invoke("attr", "data-highlighted").should("eq", "true");
         });
 
-        it("should make the variant line bolder in the main variant track", () => {
+        it("should style the variant in the main variants track", () => {
             cy.get("@variant").within(() => {
-                cy.get(`path[fill="none"]`).invoke("attr", "style").should("contain", "stroke-width: 4px");
+                cy.get(`path[fill="none"]`).as("variantPath");
+                cy.get("circle").as("variantCircle");
+                cy.get("@variantPath").invoke("attr", "style").should("contain", "stroke-width: 4px");
+                cy.get("@variantPath").invoke("attr", "style").should("contain", `stroke: ${highlightColor}`);
+                cy.get("@variantCircle").invoke("attr", "style").should("contain", "stroke-width: 4px");
+                cy.get("@variantCircle").invoke("attr", "style").should("contain", `stroke: ${highlightColor}`);
             });
         });
+
+        it("should mark the variant as highlighted in the clinvar variants track", () => {
+            cy.get(`g[data-track="variants"]`)
+                .eq(0)
+                .find(`g[data-highlighted="true"]`)
+                .should("exist");
+        });
+
+        it("should mark the variant as highlighted in the cosmic variants track", () => {
+            cy.get(`g[data-track="variants"]`)
+                .eq(1)
+                .find(`g[data-highlighted="true"]`)
+                .should("exist");
+        });
+        
     });
 
 });
