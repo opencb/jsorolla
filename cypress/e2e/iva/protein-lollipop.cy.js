83b7d94ff3ad82cdfb732d3755b6f89a5d6f7e85
Josemi <josemijuanes@gmail.com>
2023-07-10 16:44:15 +0200

    test: minor style improvements in lollipop tests #TASK-4724

--- cypress/e2e/iva/protein-lollipop.cy.js
+++ cypress/e2e/iva/protein-lollipop.cy.js
@@ -33,14 +33,16 @@ context("Protein Lollipop Viz", () => {
             ];
             cy.get(svgSelector).within(() => {
                 mainTracks.forEach(trackName => {
-                    cy.get(`g[data-track="${trackName}"]`).should("exist");
+                    cy.get(`g[data-track="${trackName}"]`)
+                        .should("exist");
                 });
             });
         });
 
         it("should render two variants tracks", () => {
             cy.get(svgSelector).within(() => {
-                cy.get(`g[data-track="variants"]`).should("have.length", 2);
+                cy.get(`g[data-track="variants"]`)
+                    .should("have.length", 2);
             });
         });
     });
@@ -49,23 +51,39 @@ context("Protein Lollipop Viz", () => {
         const highlightedVariant = "X:150638967:G:-";
 
         beforeEach(() => {
-            cy.get(svgSelector).find(`g[data-track="main:variants"]`).within(() => {
-                cy.get(`g[data-id="${highlightedVariant}"]`).as("variant");
-            });
+            cy.get(svgSelector)
+                .find(`g[data-track="main:variants"]`)
+                .within(() => {
+                    cy.get(`g[data-id="${highlightedVariant}"]`).as("variant");
+                });
         });
 
         it("should mark the variant as highlighted in the main variants track", () => {
-            cy.get("@variant").invoke("attr", "data-highlighted").should("eq", "true");
+            cy.get("@variant")
+                .invoke("attr", "data-highlighted")
+                .should("eq", "true");
         });
 
         it("should style the variant in the main variants track", () => {
             cy.get("@variant").within(() => {
                 cy.get(`path[fill="none"]`).as("variantPath");
                 cy.get("circle").as("variantCircle");
-                cy.get("@variantPath").invoke("attr", "style").should("contain", "stroke-width: 4px");
-                cy.get("@variantPath").invoke("attr", "style").should("contain", `stroke: ${highlightColor}`);
-                cy.get("@variantCircle").invoke("attr", "style").should("contain", "stroke-width: 4px");
-                cy.get("@variantCircle").invoke("attr", "style").should("contain", `stroke: ${highlightColor}`);
+
+                // Assert on lollipop path element
+                cy.get("@variantPath")
+                    .invoke("attr", "style")
+                    .should("contain", "stroke-width: 4px");
+                cy.get("@variantPath")
+                    .invoke("attr", "style")
+                    .should("contain", `stroke: ${highlightColor}`);
+
+                // Assert on lollipop circle element
+                cy.get("@variantCircle")
+                    .invoke("attr", "style")
+                    .should("contain", "stroke-width: 4px");
+                cy.get("@variantCircle")
+                    .invoke("attr", "style")
+                    .should("contain", `stroke: ${highlightColor}`);
             });
         });
 
