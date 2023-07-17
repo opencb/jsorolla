a823ab3c5d323da0439f96eaf746911d198368ad
Josemi <josemijuanes@gmail.com>
2023-07-17 13:44:13 +0200

    test: initialize cellbase client mock #TASK-4724

---
+++ src/sites/test-app/clients/cellbase-client-mock.js
@@ -0,0 +1,51 @@
+export class CellBaseClientMock {
+
+    constructor(config) {
+        this._config = {
+            ...this.getDefaultConfig(),
+            ...config,
+        };
+    }
+
+    getDefaultConfig() {
+        return {
+            host: "https://ws.zettagenomics.com/cellbase",
+            version: "v5.1",
+            species: "hsapiens",
+            testDataVersion: "",
+        };
+    }
+
+    getMeta(param, options = {}) {
+        return Promise.reject(new Error("Not implemented"));
+    }
+
+    getFiles(folderId, resource, params, options = {}) {
+        return Promise.reject(new Error("Not implemented"));
+    }
+
+    getGeneClient(id, resource, params, options) {
+        return this.get("feature", "gene", id, resource, params, options);
+    }
+
+    getTranscriptClient(id, resource, params, options) {
+        return this.get("feature", "transcript", id, resource, params, options);
+    }
+
+    getProteinClient(id, resource, params, options) {
+        return this.get("feature", "protein", id, resource, params, options);
+    }
+
+    getVariationClient(id, resource, params, options) {
+        return this.get("feature", "variation", id, resource, params, options);
+    }
+
+    getRegulatoryClient(id, resource, params, options) {
+        return this.get("feature", "regulatory", id, resource, params, options);
+    }
+
+    get(category, subcategory, ids, resource, params, options = {}) {
+        return Promise.reject(new Error("Not implemented"));
+    }
+
+}
