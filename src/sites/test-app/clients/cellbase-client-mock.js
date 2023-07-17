170b6252c37fa90621c1413f4bee9f91c7c5130f
Josemi <josemijuanes@gmail.com>
2023-07-17 14:15:40 +0200

    test: minor fixes in cellbase client mock #TASK-4724

--- src/sites/test-app/clients/cellbase-client-mock.js
+++ src/sites/test-app/clients/cellbase-client-mock.js
@@ -18,35 +18,13 @@ export class CellBaseClientMock {
         };
     }
 
-    getMeta(param, options = {}) {
+    // eslint-disable-next-line no-unused-vars
+    getMeta(param, options) {
         return Promise.reject(new Error("Not implemented"));
     }
 
-    getFiles(folderId, resource, params, options = {}) {
-        return Promise.reject(new Error("Not implemented"));
-    }
-
-    getGeneClient(id, resource, params, options) {
-        return this.get("feature", "gene", id, resource, params, options);
-    }
-
-    getTranscriptClient(id, resource, params, options) {
-        return this.get("feature", "transcript", id, resource, params, options);
-    }
-
-    getProteinClient(id, resource, params, options) {
-        return this.get("feature", "protein", id, resource, params, options);
-    }
-
-    getVariationClient(id, resource, params, options) {
-        return this.get("feature", "variation", id, resource, params, options);
-    }
-
-    getRegulatoryClient(id, resource, params, options) {
-        return this.get("feature", "regulatory", id, resource, params, options);
-    }
-
-    get(category, subcategory, ids, resource, params, options = {}) {
+    // eslint-disable-next-line no-unused-vars
+    get(category, subcategory, ids, resource, params, options) {
         if (category === "genomic") {
             // Genome browser: import chromosomes
             if (subcategory === "chromosome" && resource === "search") {
