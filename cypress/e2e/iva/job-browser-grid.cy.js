bb16621dc3f98c0be125afbb2ca69c473edf0365
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-07-21 13:19:56 +0200

    wc: add job browser test case #TASK-4803

---
+++ cypress/e2e/iva/job-browser-grid.cy.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2015-2023 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UtilsTest from "../../support/utils-test.js";
+
+context("Job Browser Grid", () => {
+    const browserGrid = "job-grid";
+
+    beforeEach(() => {
+        cy.visit("#job-browser-grid")
+        cy.waitUntil(() => {
+            return cy.get(browserGrid)
+                .should("be.visible");
+        });
+    });
+
+    context("Grid", () => {
+        it("should be render job-browser-grid", () => {
+            cy.get(browserGrid)
+                .should("be.visible");
+        });
+
+        it("should change page file-browser-grid", () => {
+            UtilsTest.changePage(browserGrid,2);
+            UtilsTest.changePage(browserGrid,3);
+        });
+    });
+
+    context("Row", () => {
+        it("should download job Json", () => {
+            cy.get("tbody tr:first > td")
+                .eq(-2)
+                .within(() => {
+                    cy.get("button")
+                        .click();
+                    cy.get("ul[class='dropdown-menu dropdown-menu-right']")
+                        .contains("a","Download JSON")
+                        .click();
+            });
+        });
+    });
+
+    context("extension", () => {
+        it("should display 'Extra Column' column", () => {
+            cy.get("thead th")
+                .contains("Extra column")
+                .should('be.visible');
+        });
+    });
+});
