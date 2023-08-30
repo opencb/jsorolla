a1581cd67c2cffb4fe9e3e3fd02d9889ea170664
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-08-31 01:03:48 +0200

    test: refactor new test function #TASK-4287

--- cypress/e2e/iva/cohort-browser-grid.cy.js
+++ cypress/e2e/iva/cohort-browser-grid.cy.js
@@ -44,33 +44,33 @@ context("Cohort Browser Grid", () => {
             cy.get("button[data-action='settings']")
                 .click()
 
-            cy.get('cohort-grid opencb-grid-toolbar')
-                .then($element => {
-                const prefix = $element.prop('_prefix');
-                cy.get(`div[id='${prefix}SettingModal']`).as("settingModal");
+            BrowserTest.getElementByComponent({
+                selector: 'cohort-grid opencb-grid-toolbar',
+                tag:'div',
+                elementId: 'SettingModal'
+            }).as("settingModal")
 
-                cy.get("@settingModal")
-                    .then(($modal) => {
-                        const startPosition = $modal.position()
-                        // Drag the modal to a new position using Cypress's drag command
-                        cy.get("@settingModal")
-                            .find('.modal-header')
-                            .trigger('mousedown', { which: 1 }) // Trigger mouse down event
-                            .trigger('mousemove', { clientX: 100, clientY: 100 }) // Move the mouse
-                            .trigger('mouseup') // Release the mouse
+            cy.get("@settingModal")
+                .then(($modal) => {
+                    const startPosition = $modal.position()
+                    // Drag the modal to a new position using Cypress's drag command
+                    cy.get("@settingModal")
+                        .find('.modal-header')
+                        .trigger('mousedown', { which: 1 }) // Trigger mouse down event
+                        .trigger('mousemove', { clientX: 100, clientY: 100 }) // Move the mouse
+                        .trigger('mouseup') // Release the mouse
 
-                        // Get the final position of the modal
-                        cy.get(`@settingModal`)
-                            .find('.modal-header')
-                            .then(($modal) => {
-                                const finalPosition = $modal.position();
-                                // Assert that the modal has moved
-                                expect(finalPosition.left).to.not.equal(startPosition.left);
-                                expect(finalPosition.top).to.not.equal(startPosition.top);
-                    });
+                    // Get the final position of the modal
+                    cy.get(`@settingModal`)
+                        .find('.modal-header')
+                        .then(($modal) => {
+                            const finalPosition = $modal.position();
+                            // Assert that the modal has moved
+                            expect(finalPosition.left).to.not.equal(startPosition.left);
+                            expect(finalPosition.top).to.not.equal(startPosition.top);
                 });
             });
-        })
+        });
     });
 
     context("Row", () => {
