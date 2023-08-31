7637763eec6db82a6c7500b9718781ba8c56f3f8
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-08-31 16:15:19 +0200

    test:  move the modal in the individual grid #TASK-4287

--- cypress/e2e/iva/individual-browser-grid.cy.js
+++ cypress/e2e/iva/individual-browser-grid.cy.js
@@ -148,6 +148,37 @@ context("Individual Browser Grid", () => {
                     .find(`tbody tr`)
                     .should("be.visible");
             });
+            it("should move modal setting", () => {
+                cy.get("button[data-action='settings']")
+                    .click();
+
+                BrowserTest.getElementByComponent({
+                    selector: 'individual-grid opencb-grid-toolbar',
+                    tag:'div',
+                    elementId: 'SettingModal'
+                }).as("settingModal");
+
+                cy.get("@settingModal")
+                    .then(($modal) => {
+                        const startPosition = $modal.position();
+                        // Drag the modal to a new position using Cypress's drag command
+                        cy.get("@settingModal")
+                            .find('.modal-header')
+                            .trigger('mousedown', { which: 1 }) // Trigger mouse down event
+                            .trigger('mousemove', { clientX: 100, clientY: 100 }) // Move the mouse
+                            .trigger('mouseup'); // Release the mouse
+
+                        // Get the final position of the modal
+                        cy.get(`@settingModal`)
+                            .find('.modal-header')
+                            .then(($modal) => {
+                                const finalPosition = $modal.position();
+                                // Assert that the modal has moved
+                                expect(finalPosition.left).to.not.equal(startPosition.left);
+                                expect(finalPosition.top).to.not.equal(startPosition.top);
+                            });
+                    });
+            });
         });
 
         context("data completeness", () => {
