7604990657ffb06df4ad01b5535a1b335d3a08f2
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-08-31 16:14:41 +0200

    test:  move the modal in the disease panel browser grid #TASK-4287

--- cypress/e2e/iva/disease-panel-browser-grid.cy.js
+++ cypress/e2e/iva/disease-panel-browser-grid.cy.js
@@ -154,6 +154,38 @@ context("Disease Panel Browser Grid", () => {
             it("should change page", () => {
                 UtilsTest.changePage(gridComponent,2);
             });
+            it("should move modal setting", () => {
+
+                cy.get("button[data-action='settings']")
+                    .click();
+
+                BrowserTest.getElementByComponent({
+                    selector: 'disease-panel-grid opencb-grid-toolbar',
+                    tag:'div',
+                    elementId: 'SettingModal'
+                }).as("settingModal");
+
+                cy.get("@settingModal")
+                    .then(($modal) => {
+                        const startPosition = $modal.position();
+                        // Drag the modal to a new position using Cypress's drag command
+                        cy.get("@settingModal")
+                            .find('.modal-header')
+                            .trigger('mousedown', { which: 1 }) // Trigger mouse down event
+                            .trigger('mousemove', { clientX: 100, clientY: 100 }) // Move the mouse
+                            .trigger('mouseup'); // Release the mouse
+
+                        // Get the final position of the modal
+                        cy.get(`@settingModal`)
+                            .find('.modal-header')
+                            .then(($modal) => {
+                                const finalPosition = $modal.position();
+                                // Assert that the modal has moved
+                                expect(finalPosition.left).to.not.equal(startPosition.left);
+                                expect(finalPosition.top).to.not.equal(startPosition.top);
+                            });
+                    });
+            });
 
         });
 
