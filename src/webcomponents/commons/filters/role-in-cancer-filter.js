f71d7686fb6a45e4e981e3ee3b4eaafaac000e82
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-11-13 17:52:18 +0100

    wc: replace select role-in-cancer-filter #TASK-5070

--- src/webcomponents/commons/filters/role-in-cancer-filter.js
+++ src/webcomponents/commons/filters/role-in-cancer-filter.js
@@ -62,13 +62,16 @@ export default class RoleInCancerFilter extends LitElement {
 
     render() {
         return html`
-            <select-field-filter
-                ?multiple="${this._config.multiple}"
+            <select-field-filter2
                 .data="${this._config.rolesInCancer}"
                 .value=${this.roleInCancer}
-                .disabled="${this.disabled}"
+                .config="${{
+                    multiple: this._config.multiple,
+                    disabled: this.disabled,
+                    liveSearch: false
+                }}"
                 @filterChange="${this.filterChange}">
-            </select-field-filter>
+            </select-field-filter2>
         `;
     }
 
