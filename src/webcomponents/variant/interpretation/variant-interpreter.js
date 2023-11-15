60dd40e9abacfa5e9ed3cccdfef6fb6f3726122e
Josemi <josemijuanes@gmail.com>
2023-11-15 12:23:46 +0100

    wc: remove unused fields in interpreter tools configuration #TASK-5262

--- src/webcomponents/variant/interpretation/variant-interpreter.js
+++ src/webcomponents/variant/interpretation/variant-interpreter.js
@@ -505,58 +505,49 @@ class VariantInterpreter extends LitElement {
         return {
             title: "Case Interpreter",
             icon: "fas fa-user-md",
-            active: false,
             tools: [
                 {
                     id: "select",
                     title: "Case Info",
-                    acronym: "VB",
                     description: "",
                     icon: "fa fa-folder-open"
                 },
                 {
                     id: "qc",
                     title: "Quality Control",
-                    acronym: "VB",
                     description: "",
                     icon: "fa fa-chart-bar"
                 },
                 {
                     id: "custom-analysis",
                     title: "Custom Analysis",
-                    acronym: "VB",
                     description: "",
                     icon: "fa fa-sync",
                 },
                 {
                     id: "methods",
                     title: "Interpretation Methods",
-                    acronym: "VB",
                     description: "",
                     icon: "fa fa-sync"
                 },
                 {
                     id: "variant-browser",
                     title: "Sample Variant Browser",
-                    acronym: "VB",
                     description: "",
                     icon: "fa fa-search"
                 },
                 {
                     id: "review",
                     title: "Interpretation Review",
-                    acronym: "VB",
                     description: "",
                     icon: "fa fa-edit"
                 },
                 {
                     id: "report",
                     title: "Observations",
-                    acronym: "VB",
                     description: "",
-                    // disabled: true,
                     icon: "fa fa-file-alt"
-                }
+                },
             ]
         };
     }
