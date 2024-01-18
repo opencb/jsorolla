54da1cfeef719b0b91b2361a75e518cda14800f9
Josemi <josemijuanes@gmail.com>
2024-01-18 12:47:44 +0100

    wc: allow importing additional data for extensions in variant browser grid #TASK-5066

--- src/webcomponents/variant/variant-browser-grid.js
+++ src/webcomponents/variant/variant-browser-grid.js
@@ -211,6 +211,8 @@ export default class VariantBrowserGrid extends LitElement {
                         // summary: !this.query.sample && !this.query.family,
                         ...this.query
                     };
+
+                    let variantResponse = null;
                     this.opencgaSession.opencgaClient.variants().query(this.filters)
                         .then(res => {
                             // FIXME A quick temporary fix -> TASK-947
@@ -265,9 +267,15 @@ export default class VariantBrowserGrid extends LitElement {
                                     });
                                 }
                             }
-
-                            params.success(res);
+                            variantResponse = res;
+                            return;
                         })
+                        .then(() => {
+                            // Prepare data for columns extensions
+                            const rows = variantResponse.responses?.[0]?.results || [];
+                            return this.gridCommons.prepareDataForExtensions(this.COMPONENT_ID, this.opencgaSession, this.filters, rows);
+                        })
+                        .then(() => params.success(variantResponse))
                         .catch(e => params.error(e))
                         .finally(() => {
                             LitUtils.dispatchCustomEvent(this, "queryComplete", null);
@@ -324,9 +332,30 @@ export default class VariantBrowserGrid extends LitElement {
     renderFromLocal() {
         $("#" + this.gridId).bootstrapTable("destroy");
         $("#" + this.gridId).bootstrapTable({
-            data: this.variants,
+            // data: this.variants,
             columns: this._getDefaultColumns(),
-            sidePagination: "local",
+            sidePagination: "server",
+
+            // Josemi Note 2024-01-18: we have added the ajax function for local variants also to support executing async calls
+            // when getting additional data from columns extensions.
+            ajax: params => {
+                const tableOptions = $(this.table).bootstrapTable("getOptions");
+                const limit = params.data.limit || tableOptions.pageSize;
+                const skip = params.data.offset || 0;
+                const rows = this.variants.slice(skip, skip + limit);
+
+                // Get data for extensions
+                this.gridCommons.prepareDataForExtensions(this.COMPONENT_ID, this.opencgaSession, null, rows)
+                    .then(() => params.success(rows))
+                    .catch(error => params.error(error));
+            },
+            // Josemi Note 2024-01-18: we use this method to tell bootstrap-table how many rows we have in our data
+            responseHandler: response => {
+                return {
+                    total: this.clinicalVariants.length,
+                    rows: response,
+                };
+            },
 
             // Set table properties, these are read from config property
             uniqueId: "id",
