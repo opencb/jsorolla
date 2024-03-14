d07e3d124cc99e2674eedf5692448f3d470630f5
Josemi <josemijuanes@gmail.com>
2024-03-14 19:40:30 +0100

    wc: add free text box in results section of steiner report #TASK-5692

--- src/webcomponents/variant/custom/steiner-report.js
+++ src/webcomponents/variant/custom/steiner-report.js
@@ -171,6 +171,7 @@ class SteinerReport extends LitElement {
                 deletionAggreationCount: 0,
                 deletionAggregationStats: null,
                 qcPlots: {},
+                results: "",
             };
 
             const allPromises = [
@@ -1013,6 +1014,15 @@ class SteinerReport extends LitElement {
                                         stop position of segment, total copy number, minor copy number).
                                     </div>
                                 `,
+                            },
+                        },
+                        {
+                            title: "Results Interpretation",
+                            type: "input-text",
+                            field: "results",
+                            display: {
+                                rows: 5,
+                                defaultValue: "",
                             },
                         },
                     ]
