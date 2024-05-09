9b97ea727483fbf4e3f77148c814c1d2cb0c27c9
Josemi <josemijuanes@gmail.com>
2024-05-09 16:21:24 +0200

    wc: Allow to provide a defualtFilter via RD browser settings #TASK-6093 #TASK-6184

--- src/webcomponents/variant/interpretation/variant-interpreter-browser-rd.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-browser-rd.js
@@ -217,9 +217,17 @@ class VariantInterpreterBrowserRd extends LitElement {
                 this.files = this.clinicalAnalysis.files?.filter(file => file.format.toUpperCase() === "VCF") || [];
             }
 
-            // 5. Read defaultFilter from study internal configuration
+            // 5.1. Read defaultFilter from study internal configuration
             if (this.opencgaSession.study.internal?.configuration?.clinical?.interpretation?.defaultFilter) {
                 this.query = {...this.query, ...this.opencgaSession.study.internal.configuration.clinical.interpretation.defaultFilter};
+            }
+
+            // 5.2. Read defaultFilter from browser settings
+            if (this.settings?.menu?.defaultFilter) {
+                this.query = {
+                    ...this.query,
+                    ...this.settings.menu.defaultFilter,
+                };
             }
 
             // Create _config again since getDefaultConfig() uses this.files
