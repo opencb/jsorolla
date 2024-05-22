7501f7e316646592aef547737194371c0497326f
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2024-05-22 17:31:51 +0200

    wc: Add note-preview WIP #TASK-6192 #TASK-6140

--- src/webcomponents/note/note-browser.js
+++ src/webcomponents/note/note-browser.js
@@ -21,7 +21,7 @@ import "../commons/opencb-facet-results.js";
 import "../commons/facet-filter.js";
 import "./note-grid.js";
 import "./note-detail.js";
-import {construction} from "../commons/html-utils.js";
+import "./note-preview.js";
 
 export default class NoteBrowser extends LitElement {
 
@@ -214,6 +214,22 @@ export default class NoteBrowser extends LitElement {
                                     .opencgaSession="${opencgaSession}">
                                 </note-view>
                             `,
+                        },
+                        {
+                            id: "note-preview",
+                            name: "Preview",
+                            render: (note, active, opencgaSession) => {
+                                return html`
+                                    <div class="mb-3">
+                                        <h3>Content</h3>
+                                    </div>
+                                    <note-preview
+                                        .active="${active}"
+                                        .note="${note}"
+                                        .opencgaSession="${opencgaSession}">
+                                    </note-preview>
+                                `;
+                            }
                         },
                         {
                             id: "json-view",
