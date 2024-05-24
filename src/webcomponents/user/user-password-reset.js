c722ac561cd8258d5c0600d537199b931799f55a
gpveronica <veronica.zettagenomics@gmail.com>
2024-05-24 16:43:16 +0200

    wc - WiP Reset password action for organization admins/owners, #TASK-5866, #TASK-5809
    Signed-off-by: gpveronica <veronica.zettagenomics@gmail.com>

--- src/webcomponents/user/user-password-reset.js
+++ src/webcomponents/user/user-password-reset.js
@@ -1,6 +1,7 @@
 import {LitElement, html} from "lit";
 import LitUtils from "../commons/utils/lit-utils.js";
 import NotificationUtils from "../commons/utils/notification-utils.js";
+import UtilsNew from "../../core/utils-new.js";
 
 
 export default class UserPasswordReset extends LitElement {
@@ -16,82 +17,162 @@ export default class UserPasswordReset extends LitElement {
 
     static get properties() {
         return {
+            user: {
+                type: Object,
+            },
             opencgaSession: {
+                type: Object
+            },
+            displayConfig: {
                 type: Object
             },
         };
     }
 
     #init() {
-        this.hasEmptyUser = false;
+        this._user = {};
+        this.displayConfigDefault = {
+            style: "margin: 10px",
+            titleWidth: 3,
+            titleStyle: "color: var(--main-bg-color);margin-bottom:16px;font-weight:bold;",
+            defaultLayout: "horizontal",
+            buttonOkText: "Reset password",
+        };
+        this._config = this.getDefaultConfig();
     }
 
-    redirect(to) {
-        LitUtils.dispatchCustomEvent(this, "redirect", null, {hash: to});
+    #setLoading(value) {
+        this.isLoading = value;
+        this.requestUpdate();
     }
 
-    onSubmit(e) {
-        e.preventDefault();
-        const user = (this.querySelector("#user").value || "").trim();
+    #initOriginalObjects() {
+        this._config = this.getDefaultConfig();
+        this.requestUpdate();
+    }
 
-        // Check for empty user ID
-        this.hasEmptyUser = user.length === 0;
-        if (this.hasEmptyUser) {
-            return this.requestUpdate();
+    update(changedProperties) {
+        if (changedProperties.has("displayConfig") || changedProperties.has("user")) {
+            this.displayConfig = {
+                ...this.displayConfigDefault,
+                ...this.displayConfig
+            };
+            this._config = this.getDefaultConfig();
         }
+        super.update(changedProperties);
+    }
 
-        // Reset password mockup
-        // TODO: call openCGA to the correct endpoint
-        Promise.resolve().then(() => {
-            NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_SUCCESS, {
-                "message": "We have just send you an email with the new password.",
+    /*
+        onSubmit(e) {
+            e.preventDefault();
+            const user = (this.querySelector("#user").value || "").trim();
+
+            // Check for empty user ID
+            this.hasEmptyUser = user.length === 0;
+            if (this.hasEmptyUser) {
+                return this.requestUpdate();
+            }
+
+            // Reset password mockup
+            // TODO: call openCGA to the correct endpoint
+            Promise.resolve().then(() => {
+                NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_SUCCESS, {
+                    "message": "We have just send you an email with the new password.",
+                });
+
+                this.querySelector("#user").value = "";
+                this.hasEmptyUser = false;
+                this.requestUpdate();
+            });
+        }
+    */
+
+    onSubmit() {
+        // QUESTION:
+        //  - TASK-1667, includeResult
+        //  - JS client do not have argument for params, only user
+        const data = {
+            includeResult: true,
+        };
+        let error;
+        this.#setLoading(true);
+        //  Reset password
+        // Fixme: waiting for task:
+        //  https://app.clickup.com/t/36631768/TASK-464
+        //  Check endpoint when released
+        this.opencgaSession.opencgaClient.users()
+            .resetPassword(this.user.id)
+            .then(response => {
+                this._user = UtilsNew.objectClone(response.responses[0].results[0]);
+                this.#initOriginalObjects();
+                NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_SUCCESS, {
+                    title: `User Reset Password`,
+                    message: `User ${this.userId} password reset correctly`,
+                });
+            })
+            .catch(reason => {
+                error = reason;
+                NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_RESPONSE, reason);
+            })
+            .finally(() => {
+                LitUtils.dispatchCustomEvent(this, "userUpdate", this._user, {}, error);
+                this.#setLoading(false);
             });
 
-            this.querySelector("#user").value = "";
-            this.hasEmptyUser = false;
-            this.requestUpdate();
-        });
-    }
-
-    // Handle keyup event --> check for enter key to submit the form
-    onKeyUp(e) {
-        if (e.key === "Enter") {
-            return this.onSubmit(e);
-        }
     }
 
     render() {
+        // TODO: check if opencgaSession has been provided
+        debugger
         return html`
-            <div class="container-fluid" style="max-width:380px;">
-                <div class="card" style="margin-top:96px;">
-                    <div class="card-body" style="padding:32px;">
-                        <div align="center">
-                            <h3 style="font-weight:bold;margin-top:0px;">
-                                Reset your password
-                            </h3>
-                        </div>
-                        <div class="paragraph" style="margin-bottom:16px;">
-                            Please enter your user ID and we will send you an email with your password reset link.
-                        </div>
-                        <div class="form-group ${this.hasEmptyUser ? "has-error" : ""}">
-                            <div class="input-group">
-                                <span class="input-group-addon" id="username">
-                                    <i class="fa fa-user fa-lg"></i>
-                                </span>
-                                <input id="user" type="text" class="form-control" placeholder="User ID" @keyup="${e => this.onKeyUp(e)}">
-                            </div>
-                        </div>
-                        <button class="btn btn-primary btn-block" @click="${e => this.onSubmit(e)}">
-                            <strong>Reset Password</strong>
-                        </button>
-                    </div>
-                </div>
-                <div align="center">
-                    <a @click="${() => this.redirect("#login")}" style="cursor:pointer;">Go back to Login</a>
-                </div>
-            </div>
+            <data-form
+                .data="${this.user}"
+                .config="${this._config}"
+                @submit="${() => this.onSubmit()}"
+                @clear="${() => this.onClear()}">
+            </data-form>
         `;
     }
+
+    getDefaultConfig() {
+        return {
+            title: "Reset Password",
+            display: this.displayConfig || this.displayConfigDefault,
+            sections: [
+                {
+                    title: `Do you really want to reset ${this.user?.id}'s password?`,
+                    elements: [
+                        {
+                            type: "notification",
+                            text: `${this.user?.id} will receive an email with a temporary password... [ TO ELABORATE ]`,
+                            display: {
+                                visible: true,
+                                icon: "fas fa-exclamation-triangle",
+                                notificationType: "error",
+                            },
+                        },
+                        {
+                            title: "User ID",
+                            field: "id",
+                            type: "input-text",
+                            display: {
+                                disabled: true,
+                            }
+                        },
+                        {
+                            title: "Email",
+                            field: "email",
+                            type: "input-text",
+                            display: {
+                                disabled: true,
+                            }
+                        },
+                    ],
+                },
+            ],
+        };
+    }
+
 
 }
 
