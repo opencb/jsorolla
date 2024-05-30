f33041c6788a6a63cc2959fae46275db05ac150c
Josemi <josemijuanes@gmail.com>
2024-05-30 11:41:10 +0200

    wc: Hide details section when no row is selected in cohort browser #TASK-4951

--- src/webcomponents/cohort/cohort-browser.js
+++ src/webcomponents/cohort/cohort-browser.js
@@ -15,7 +15,7 @@
  */
 
 
-import {LitElement, html} from "lit";
+import {LitElement, html, nothing} from "lit";
 import UtilsNew from "../../core/utils-new.js";
 import "../commons/opencga-browser.js";
 import "./cohort-grid.js";
@@ -129,11 +129,14 @@ export default class CohortBrowser extends LitElement {
                             @cohortUpdate="${e => params.onComponentUpdate(e, "cohort")}"
                             @settingsUpdate="${() => this.onSettingsUpdate()}">
                         </cohort-grid>
-                        <cohort-detail
-                            .opencgaSession="${params.opencgaSession}"
-                            .config="${params.config.filter.detail}"
-                            .cohortId="${params.detail.cohort?.id}">
-                        </cohort-detail>`
+                        ${params?.detail ? html`
+                            <cohort-detail
+                                .opencgaSession="${params.opencgaSession}"
+                                .config="${params.config.filter.detail}"
+                                .cohortId="${params.detail?.id}">
+                            </cohort-detail>
+                        ` : nothing}
+                    `,
                 },
                 {
                     id: "facet-tab",
