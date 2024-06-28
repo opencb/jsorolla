1c7cd1db9217da16a608b00c19b838d393a6a275
Josemi <josemijuanes@gmail.com>
2024-06-28 09:10:34 +0100

    wc: Remove usage of status.name in cohort-view #TASK-6396 #TASK-5964

--- src/webcomponents/cohort/cohort-view.js
+++ src/webcomponents/cohort/cohort-view.js
@@ -239,7 +239,7 @@ export default class CohortView extends LitElement {
                             title: "Status",
                             type: "complex",
                             display: {
-                                template: "${internal.status.name} (${internal.status.date})",
+                                template: "${internal.status.id} (${internal.status.date})",
                                 format: {
                                     "internal.status.date": date => UtilsNew.dateFormatter(date),
                                 }
