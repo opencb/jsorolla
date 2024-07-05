80cb050d990902ebada7e3a4845978c97f9b2692
gpveronica <veronica.zettagenomics@gmail.com>
2024-07-05 11:58:12 +0100

    wc - WiP Add N users to M groups in organization projects/studies, #TASK-5866, #TASK-5809
    Signed-off-by: gpveronica <veronica.zettagenomics@gmail.com>

--- src/webcomponents/organization/admin/study-users-manage.js
+++ src/webcomponents/organization/admin/study-users-manage.js
@@ -17,7 +17,7 @@
 import {html, LitElement} from "lit";
 import UtilsNew from "../../../core/utils-new.js";
 import NotificationUtils from "../../commons/utils/notification-utils.js";
-import LitUtils from "../../commons/utils/lit-utils.js";
+import FormUtils from "../../commons/forms/form-utils";
 
 export default class StudyUsersManage extends LitElement {
 
@@ -49,6 +49,7 @@ export default class StudyUsersManage extends LitElement {
     }
 
     #init() {
+        debugger
         this.component = {}; // Original object
         this._component = {}; // Updated object
         this.updateParams = {};
@@ -78,22 +79,15 @@ export default class StudyUsersManage extends LitElement {
     }
 
     #initOriginalObjects() {
-        // this._listUsersStudy = UtilsNew.objectClone(
-        //     this.study.groups.filter(group => group.id === "@members"))
-        //     .userIds;
-        this.updatedFields = {};
-        // Read list organization users to prepare the allowed values in the Users select menu
-        this.groupsAllowedValues = this.study.groups;
-        debugger
+        // 1. Dataform variables
         this.component = {
-            users: [],
-            groups: UtilsNew.objectClone(this.study.groups),
+            selectedGroups: "",
+            selectedUsers: [],
         };
         this._component = UtilsNew.objectClone(this.component);
-        // Prepare allowedValues for the select options menu
-        // for (const group of this.study.groups) {
-        //     this.groupsAllowedValues.push({id: group.id});
-        // }
+        // 2. Display variables
+        this._groupAllowedValues = this.study.groups;
+        this._selectedGroups = [];
         this._config = this.getDefaultConfig();
     }
 
@@ -143,7 +137,18 @@ export default class StudyUsersManage extends LitElement {
     }
 
     // --- EVENTS ---
-    onFieldChange(e, field) {
+    onFieldChange(e) {
+        const param = e.detail.param;
+        this.updatedFields = FormUtils.getUpdatedFields(this.variant, this.updatedFields, param, e.detail.value, e.detail.action);
+
+        if (param === "selectedGroups") {
+            // Update the selectedGroups local variable.
+            // This is needed because the select-field-filter.js component is returning comma separated string
+            this._selectedGroups = this._component.selectedGroups
+                .split(",")
+                .map(selectedGroupId => this._groupAllowedValues.find(group => group.id === selectedGroupId));
+        }
+        this._config = this.getDefaultConfig();
         debugger
         this.requestUpdate();
     }
@@ -183,71 +188,129 @@ export default class StudyUsersManage extends LitElement {
     }
 
     getDefaultConfig() {
+        const sections = [
+            {
+                display: {
+                    // titleHeader: "",
+                    // titleStyle: "",
+                    descriptionClassName: "d-block text-secondary",
+                    // descriptionStyle: "",
+                    // visible: () =>
+                },
+                elements: [
+                    {
+                        title: "Select Group(s)",
+                        field: "selectedGroups",
+                        type: "select",
+                        required: true,
+                        multiple: true,
+                        all: false,
+                        allowedValues: this._groupAllowedValues,
+                        display: {
+                            // apply: () => ,
+                            placeholder: "Select group or groups...",
+                        },
+                    },
+                    {
+                        title: "Select User(s)",
+                        field: "selectedUsers",
+                        type: "custom",
+                        required: true,
+                        display: {
+                            visible: this._selectedGroups?.length > 0,
+                            render: (users, dataFormFilterChange, updateParams, data) => {
+                                debugger
+                                const userIds = Array.isArray(users) ?
+                                    users?.map(user => user.id).join(",") :
+                                    users;
+                                const handleUsersFilterChange = e => {
+                                    // We need to convert value from a string with commas to an array of IDs
+                                    // eslint-disable-next-line no-param-reassign
+                                    const userList = e.detail.value
+                                        ?.split(",")
+                                        .filter(userId => userId)
+                                        .map(userId => ({
+                                            id: userId,
+                                            groups: this._selectedGroups.reduce((acc, group) => {
+                                                return {
+                                                    ...acc,
+                                                    [group.id]: group.userIds.includes(userId)
+                                                };
+                                            }, {}),
+                                        }));
+                                    dataFormFilterChange(userList);
+                                };
+                                return html`
+                                        <catalog-search-autocomplete
+                                            .value="${userIds}"
+                                            .resource="${"USERS"}"
+                                            .opencgaSession="${this.opencgaSession}"
+                                            .classes="${updateParams?.users ? "selection-updated" : ""}"
+                                            .config="${{multiple: true}}"
+                                            @filterChange="${e => handleUsersFilterChange(e)}">
+                                        </catalog-search-autocomplete>
+                                    `;
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                display: {
+                    // titleHeader: "",
+                    // titleStyle: "",
+                    descriptionClassName: "d-block text-secondary",
+                    // descriptionStyle: "",
+                    visible: data => data?.selectedGroups !== "" && data?.selectedUsers?.length > 0,
+                },
+                elements: [
+                    {
+                        title: "Table",
+                        field: "selectedUsers",
+                        type: "table",
+                        display: {
+                            width: 12,
+                            columns: [
+                                {
+                                    id: "id",
+                                    title: "User Id",
+                                    field: "id",
+                                },
+                                ...this._selectedGroups?.map(group => ({
+                                    id: group.id,
+                                    title: group.id,
+                                    field: `groups.${group.id}`,
+                                    type: "custom",
+                                    display: {
+                                        helpMessage: "",
+                                        disabled: data => {
+                                            debugger
+                                            // return !data?.panels || data?.panels?.length === 0;
+                                        },
+                                        render: (checked, dataformfilterchange, updateparams, data) => {
+                                            debugger
+                                            const isOwner = false;
+                                            // ${!isOwner ? "disabled" : ""}
+                                            return html`
+                                                <div class="form-check form-switch">
+                                                    <input class="form-check-input" type="checkbox" id="${checked ? "flexSwitchCheckChecked" : "flexSwitchCheckDefault"}" ?checked="${checked}">
+                                                    <label class="form-check-label" for="${checked ? "flexSwitchCheckChecked" : "flexSwitchCheckDefault"}"></label>
+                                                </div>
+                                            `;
+                                        }
+                                    },
+                                })),
+                            ],
+                        },
+                    },
+                ],
+            },
+        ];
+
         return {
             id: "",
             display: this.displayConfig,
-            sections: [
-                {
-                    // title: "Manage users in study groups",
-                    display: {
-                        // titleHeader: "",
-                        // titleStyle: "",
-                        descriptionClassName: "d-block text-secondary",
-                        // descriptionStyle: "",
-                        // visible: () =>
-                    },
-                    elements: [
-                        {
-                            title: "Groups",
-                            field: "groups",
-                            type: "select",
-                            multiple: true,
-                            all: true,
-                            required: true,
-                            allowedValues: this.groupsAllowedValues,
-                            display: {
-                                // apply: () => ,
-                                placeholder: "Select group or groups...",
-                            },
-                        },
-                        {
-                            title: "Select User or users",
-                            field: "users",
-                            type: "custom",
-                            required: true,
-                            display: {
-                                render: (users, dataFormFilterChange, updateParams) => {
-                                    const userIds = Array.isArray(users) ?
-                                        users?.map(user => user.id).join(",") :
-                                        users;
-
-                                    const handleSamplesFilterChange = e => {
-                                        // We need to convert value from a string wth commas to an array of IDs
-                                        // eslint-disable-next-line no-param-reassign
-                                        const userList = e.detail.value
-                                            ?.split(",")
-                                            .filter(userId => userId)
-                                            .map(userId => ({id: userId}));
-                                        dataFormFilterChange(userList);
-                                    };
-
-                                    return html`
-                                        <catalog-search-autocomplete
-                                                .value="${userIds}"
-                                                .resource="${"USERS"}"
-                                                .opencgaSession="${this.opencgaSession}"
-                                                .classes="${updateParams.users ? "selection-updated" : ""}"
-                                                .config="${{multiple: true}}"
-                                                @filterChange="${e => handleSamplesFilterChange(e)}">
-                                        </catalog-search-autocomplete>
-                                    `;
-                                },
-                            },
-                        },
-
-                    ],
-                },
-            ],
+            sections: sections,
         };
     }
 
