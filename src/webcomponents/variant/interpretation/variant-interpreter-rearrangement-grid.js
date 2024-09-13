0dea70fd7c77007330ff4ed8cd288b75e8737e63
Josemi <josemijuanes@gmail.com>
2024-09-13 13:45:43 +0200

    wc: Remove unused config modal in rearrangement grid #TASK-6850

--- src/webcomponents/variant/interpretation/variant-interpreter-rearrangement-grid.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-rearrangement-grid.js
@@ -979,12 +979,6 @@ export default class VariantInterpreterRearrangementGrid extends LitElement {
         }
     }
 
-    onConfigClick(e) {
-        // eslint-disable-next-line no-undef
-        const configModal = new bootstrap.Modal("#" + this._prefix + "ConfigModal");
-        configModal.show();
-    }
-
     onVariantChange(e) {
         this.variantsReview[0] = e.detail.value;
     }
@@ -1073,32 +1067,6 @@ export default class VariantInterpreterRearrangementGrid extends LitElement {
                         <div class="modal-footer">
                             <button type="button" class="btn btn-light" data-bs-dismiss="modal" @click="${() => this.onCancelVariant()}">Cancel</button>
                             <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="${e => this.onSaveVariant(e)}">OK</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="modal fade pt-0" id="${this._prefix}ConfigModal" tabindex="-1"
-                role="dialog" aria-hidden="true" style="overflow-y: visible">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header" style="padding: 5px 15px">
-                            <h3>Settings</h3>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="container-fluid">
-                                <variant-interpreter-grid-config
-                                    .opencgaSession="${this.opencgaSession}"
-                                    .config="${this._config}"
-                                    .gridColumns="${this._columns}"
-                                    @configChange="${this.onGridConfigChange}">
-                                </variant-interpreter-grid-config>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="${e => this.onGridConfigSave(e)}">OK</button>
                         </div>
                     </div>
                 </div>
