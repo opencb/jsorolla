80fb652dcd5767f1264e07867bc0fe075182acaf
Josemi <josemijuanes@gmail.com>
2024-09-20 16:18:37 +0200

    wc: Include the total count of traits with source cosmic in column text #TASK-6942 #TASK-6700

--- src/webcomponents/variant/variant-grid-formatter.js
+++ src/webcomponents/variant/variant-grid-formatter.js
@@ -1192,7 +1192,7 @@ export default class VariantGridFormatter {
 
                     return `
                         <a class="cosmic-tooltip" tooltip-title='Links' tooltip-text='${tooltipText}' tooltip-position-at="left bottom" tooltip-position-my="right top">
-                            <span style="color: green">${cosmicMap.size} ${cosmicMap.size > 1 ? "entries" : "entry" }</span>
+                            <span style="color: green">${cosmicMap.size} ${cosmicMap.size > 1 ? "entries" : "entry"} (${traits.length})</span>
                         </a>`;
                 case "HGMD":
                     // Prepare the tooltip links
