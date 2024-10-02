6c01699e8703afa993783c800499b956377ba13c
gpveronica <veronica.zettagenomics@gmail.com>
2024-10-02 09:18:20 +0200

    wc - Fix issues in variant secondary sample index #TASK-6986
    Signed-off-by: gpveronica <veronica.zettagenomics@gmail.com>

--- src/webcomponents/variant/operation/variant-secondary-sample-index-configure-operation.js
+++ src/webcomponents/variant/operation/variant-secondary-sample-index-configure-operation.js
@@ -16,8 +16,9 @@
 
 import {LitElement, html} from "lit";
 import AnalysisUtils from "../../commons/analysis/analysis-utils.js";
-import FormUtils from "../../commons/forms/form-utils.js";
 import UtilsNew from "../../../core/utils-new.js";
+import NotificationUtils from "../../commons/utils/notification-utils.js";
+import LitUtils from "../../commons/utils/lit-utils.js";
 import "../../commons/forms/data-form.js";
 import "../../commons/filters/catalog-search-autocomplete.js";
 import "../../commons/filters/consequence-type-select-filter.js";
@@ -53,58 +54,61 @@ export default class VariantSecondarySampleIndexConfigureOperation extends LitEl
         this.TITLE = "Variant Secondary Sample Index Configure Operation";
         this.DESCRIPTION = "Executes a variant secondary sample index configure operation job";
 
+        this.studyId = "";
+        this.study = {};
+        this.studyConfiguration = {};
         this.DEFAULT_TOOLPARAMS = {};
-        // Make a deep copy to avoid modifying default object.
-        this.toolParams = {
-            ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-        };
-
-        this.config = this.getDefaultConfig();
-    }
-
-    firstUpdated(changedProperties) {
-        if (changedProperties.has("toolParams")) {
-            this.study = this.toolParams.study || "";
-        }
+        this._toolParams = {};
     }
 
     update(changedProperties) {
-        // CAUTION 20230110 Vero:
-        //  When study changes out of configuration operations menu option:
-        //    1. this.toolParams is updated with the keys studies and body in condition if (changedProperties.has("opencgaSession"))
-        //  When the user click on one of the configuartion operation options:
-        //    1. The property toolParams is overwritten (see study-variant-admin.js render method) removing the body key with the appropriate
-        //    2. Consequently, json-editor is gets renderd with an empty json
-        //  Besides, the contents get rendered twice.
-        //  The following fix displays the json correctly, but needs to be optimized.
         if (changedProperties.has("toolParams") || changedProperties.has("opencgaSession")) {
-            this.toolParams = {
-                ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-                ...this.toolParams,
-                body: JSON.stringify(this.opencgaSession.study?.internal?.configuration?.variantEngine?.sampleIndex, null, 8) || "-",
-            };
-            this.config = this.getDefaultConfig();
+            // Note: 'study' is the id of the study, not the object.
+            // It is named 'study' for consistency with the endpoint query param
+            if (this.toolParams?.study && this.toolParams.study !== this._toolParams?.study) {
+                this.studyId = this.toolParams.study;
+                this.studyIdObserver();
+            } else {
+                this.#initToolParams();
+            }
         }
-        // if (changedProperties.has("toolParams")) {
-        //     this.toolParams = {
-        //         ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-        //         ...this.toolParams,
-        //     };
-        //     this.config = this.getDefaultConfig();
-        // }
-        // if (changedProperties.has("opencgaSession")) {
-        //     this.toolParams = {
-        //         ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-        //         ...this.toolParams,
-        //         body: JSON.stringify(this.opencgaSession.study?.internal?.configuration?.variantEngine.sampleIndex, null, 8) || "-",
-        //     };
-        //     this.config = this.getDefaultConfig();
-        // }
         super.update(changedProperties);
     }
 
+    studyIdObserver() {
+        if (this.studyId && this.opencgaSession) {
+            this.opencgaSession.opencgaClient.studies()
+                .info(this.studyId)
+                .then(response => {
+                    this.study = response.responses[0].results[0];
+                    this.#initToolParams();
+                    this.requestUpdate();
+                })
+                .catch(reason => {
+                    NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_RESPONSE, reason);
+                });
+        }
+    }
+
+    #initToolParams() {
+        this.studyConfiguration = this.study?.internal?.configuration?.variantEngine?.sampleIndex || this.opencgaSession.study?.internal?.configuration?.variantEngine?.sampleIndex || {};
+        this._toolParams = {
+            ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
+            ...this.toolParams,
+            study: this.study?.fqn || this.opencgaSession.study.fqn,
+            skipRebuild: false,
+            body: UtilsNew.objectClone(this.studyConfiguration),
+        };
+        this.config = this.getDefaultConfig();
+    }
+
+    onStudyChange(e) {
+        this.studyId = e.detail.value;
+        this.studyIdObserver();
+    }
+
     check() {
-        if (!this.toolParams.study) {
+        if (!this._toolParams.study) {
             return {
                 message: "Study is a mandatory parameter, please select one."
             };
@@ -112,49 +116,43 @@ export default class VariantSecondarySampleIndexConfigureOperation extends LitEl
         return null;
     }
 
-    onFieldChange(e, field) {
-        const param = field || e.detail.param;
-        if (param) {
-            this.toolParams = FormUtils.createObject(this.toolParams, param, e.detail.value);
-        }
-        this.config = this.getDefaultConfig();
+    onClear() {
+        this.#initToolParams();
         this.requestUpdate();
     }
 
     onSubmit() {
-        const toolParams = {
-            ...JSON.parse(this.toolParams.body)
-        };
         const params = {
-            study: this.toolParams.study || this.opencgaSession.study.fqn,
-            skipRebuild: this.toolParams.skipRebuild || false,
-            ...AnalysisUtils.fillJobParams(this.toolParams, this.TOOL),
+            study: this._toolParams.study,
+            skipRebuild: this._toolParams.skipRebuild || false,
+            ...AnalysisUtils.fillJobParams(this._toolParams, this.TOOL),
         };
         AnalysisUtils.submit(
             this.TITLE,
             this.opencgaSession.opencgaClient.variantOperations()
-                .configureVariantSecondarySampleIndex(toolParams, params),
+                .configureVariantSecondarySampleIndex(this._toolParams.body, params),
             this,
-        );
-    }
-
-    onClear() {
-        this.toolParams = {
-            ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-        };
-        this.config = this.getDefaultConfig();
+        ).then(response => {
+            if (response) {
+                // If the configuration has been updated, dispatch a study update request
+                LitUtils.dispatchCustomEvent(this, "studyUpdateRequest",
+                    UtilsNew.objectClone(this._toolParams.study)
+                );
+            }
+        });
     }
 
     render() {
-        return html`
-            <data-form
-                .data="${this.toolParams}"
-                .config="${this.config}"
-                @fieldChange="${e => this.onFieldChange(e)}"
-                @clear="${this.onClear}"
-                @submit="${this.onSubmit}">
-            </data-form>
-        `;
+        if (this._toolParams.body) {
+            return html`
+                <data-form
+                    .data="${this._toolParams}"
+                    .config="${this.config}"
+                    @clear="${this.onClear}"
+                    @submit="${this.onSubmit}">
+                </data-form>
+            `;
+        }
     }
 
     getDefaultConfig() {
@@ -164,20 +162,21 @@ export default class VariantSecondarySampleIndexConfigureOperation extends LitEl
                 elements: [
                     {
                         title: "Study",
+                        field: "study",
                         type: "custom",
                         required: true,
                         display: {
-                            render: toolParams => html`
+                            render: study => html`
                                 <catalog-search-autocomplete
-                                    .value="${toolParams?.study}"
+                                    .value="${study}"
                                     .resource="${"STUDY"}"
                                     .opencgaSession="${this.opencgaSession}"
                                     .config="${{multiple: false, disabled: !!this.study}}"
-                                    @filterChange="${e => this.onFieldChange(e, "study")}">
+                                    @filterChange="${e => this.onStudyChange(e, "study")}">
                                 </catalog-search-autocomplete>
                             `,
                         },
-                    }
+                    },
                 ],
             },
             {
@@ -196,14 +195,18 @@ export default class VariantSecondarySampleIndexConfigureOperation extends LitEl
                     {
                         title: "Sample Index Configuration",
                         field: "body",
-                        type: "json-editor",
+                        type: "custom",
                         display: {
-                            rows: 25,
-                            defaultLayout: "vertical"
-                        }
+                            render: (body, dataFormFilterChange) => html `
+                                <json-editor
+                                    .data="${body}"
+                                    @fieldChange="${e => dataFormFilterChange(e.detail.value.json)}">
+                                </json-editor>
+                            `,
+                        },
                     },
                 ],
-            }
+            },
         ];
 
         return AnalysisUtils.getAnalysisConfiguration(
@@ -211,7 +214,16 @@ export default class VariantSecondarySampleIndexConfigureOperation extends LitEl
             this.title ?? this.TITLE,
             this.DESCRIPTION,
             params,
-            this.check()
+            this.check(),
+            {
+                display: {
+                    buttonsLayout: "top"
+                },
+                buttons: {
+                    clearText: "Discard Changes",
+                    okText: "Update",
+                },
+            },
         );
     }
 
