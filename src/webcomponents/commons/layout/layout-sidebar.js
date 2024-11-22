316faa9df285e5337a6b6291a36b9dd26d75f462
Josemi <josemijuanes@gmail.com>
2024-11-22 13:31:03 +0100

    wc: Encode current app using the url and remove unused props in sidebar #TASK-7216 #TASK-7100

--- src/webcomponents/commons/layout/layout-sidebar.js
+++ src/webcomponents/commons/layout/layout-sidebar.js
@@ -1,5 +1,4 @@
 import {html, LitElement, nothing} from "lit";
-import LitUtils from "../utils/lit-utils";
 
 export default class LayoutSidebar extends LitElement {
 
@@ -13,11 +12,8 @@ export default class LayoutSidebar extends LitElement {
 
     static get properties() {
         return {
-            opencgaSession: {
-                type: Object
-            },
-            app: {
-                type: Object
+            currentUrl: {
+                type: String,
             },
             config: {
                 type: Object
@@ -25,8 +21,8 @@ export default class LayoutSidebar extends LitElement {
         };
     }
 
-    onChangeApp(app) {
-        LitUtils.dispatchCustomEvent(this, "changeApp", app.id);
+    renderSectionSeparator(text) {
+        return html`<div class="mt-2 text-gray-500 fw-bold fs-7 text-center">${text}</div>`;
     }
 
     renderLink(link) {
@@ -39,28 +35,16 @@ export default class LayoutSidebar extends LitElement {
         `;
     }
 
-    renderSectionSeparator(text) {
-        return html`<div class="mt-2 text-gray-500 fw-bold fs-7 text-center">${text}</div>`;
-    }
-
-    renderLinkButton(item, active = false) {
+    renderAppButton(app) {
+        const active = this.currentUrl.startsWith(`#${app.id}`); // url always start with the app ID
         return html`
-            <a class="d-flex align-items-center flex-column gap-2 p-2 rounded-2 text-decoration-none ${active ? "bg-gray-200" : "hover:bg-gray-200 cursor-pointer"}" href="${item.url}">
-                <i class="fas ${item?.icon || ""} lh-1 fs-4 text-gray-900"></i>
-                <div class="fw-bold lh-1 fs-8 text-center text-gray-600">${item?.name || "-"}</div>
-            </a>
-        `;
-    }
-
-    renderAppButton(item, active = false) {
-        return html`
-            <div
-                class="d-flex align-items-center flex-column gap-2 p-2 rounded-2 ${active ? "bg-gray-200" : "hover:bg-gray-200 cursor-pointer"}"
-                @click="${() => this.onChangeApp(item)}"
+            <a
+                class="text-decoration-none d-flex align-items-center flex-column gap-2 p-2 rounded-2 cursor-pointer ${active ? "bg-gray-200" : "hover:bg-gray-200"}"
+                href="#${app.id}"
             >
-                <i class="fas ${item?.icon || ""} lh-1 fs-4 text-gray-900"></i>
-                <div class="fw-bold lh-1 fs-8 text-center text-gray-600">${item?.name || "-"}</div>
-            </div>
+                <i class="fas ${app?.icon || "fa-screwdriver-wrench"} lh-1 fs-4 text-gray-900"></i>
+                <div class="fw-bold lh-1 fs-8 text-center text-gray-600">${app?.name || "-"}</div>
+            </a>
         `;
     }
 
@@ -72,10 +56,10 @@ export default class LayoutSidebar extends LitElement {
             <div class="d-flex flex-column justify-content-between flex-shrink-0 border-end bg-gray-100 position-relative h-full" style="width:72px">
                 <div class="d-flex flex-column">
                     <div class="d-flex flex-column gap-1 user-select-none p-2">
-                        ${dashboardApp ? this.renderAppButton(dashboardApp, this.app?.id === dashboardApp?.id) : nothing}
+                        ${dashboardApp ? this.renderAppButton(dashboardApp) : nothing}
                         ${otherApps.length > 0 ? html`
                             ${this.renderSectionSeparator("Apps")}
-                            ${otherApps.map(app => this.renderAppButton(app, this.app?.id === app.id))}
+                            ${otherApps.map(app => this.renderAppButton(app))}
                         `: nothing}
                         <!--
                         <div class="mt-2 text-gray-500 fw-bold small">Favourites</div>
