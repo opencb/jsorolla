50b289eef562a494ab300d1f03793946db4cd260
Josemi <josemijuanes@gmail.com>
2024-11-26 13:30:45 +0100

    wc: Open app home when clicking on the app name in the secondary bar #TASK-7216 #TASK-7100

--- src/webcomponents/commons/layout/layout-secondary-bar.js
+++ src/webcomponents/commons/layout/layout-secondary-bar.js
@@ -37,10 +37,10 @@ export default class LayoutSecondaryBar extends LitElement {
     render() {
         return html`
             <div class="d-flex align-items-stretch w-full py-2 px-3">
-                <div class="d-flex align-items-center gap-2 me-5 user-select-none">
+                <a class="d-flex align-items-center gap-2 me-5 user-select-none text-body text-decoration-none" href="#${this.app.id}/home">
                     <i class="fas ${this.app.icon || ""} fs-3"></i>
                     <span class="fs-3 fw-bold">${this.app.name || "-"}</span>
-                </div>
+                </a>
                 <ul class="nav nav-underline">
                     ${(this.app?.menu || []).map(tool => this.renderTool(tool))}
                 </ul>
