2e36a4922bc3badd6544b7f30406b6b0f0cb200b
Josemi <josemijuanes@gmail.com>
2024-12-17 11:37:15 +0100

    wc: Add notes tab in interpreter browser rd #TASK-7267 #TASK-6445

--- src/webcomponents/variant/interpretation/variant-interpreter-browser-rd.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-browser-rd.js
@@ -19,6 +19,7 @@ import UtilsNew from "../../../core/utils-new.js";
 import "./variant-interpreter-browser-template.js";
 import "./exomiser/variant-interpreter-exomiser-view.js";
 import "../variant-samples.js";
+import "../variant-notes.js";
 import "../../visualization/protein-lollipop-variant-view.js";
 import "../annotation/variant-annotation-pharmacogenomics-view.js";
 
@@ -680,6 +681,17 @@ class VariantInterpreterBrowserRd extends LitElement {
                                     .variantId="${variant?.id}"
                                     .active="${active}">
                                 </variant-samples>
+                            `,
+                        },
+                        {
+                            id: "notes",
+                            name: "Notes",
+                            render: (variant, active, opencgaSession) => html`
+                                <variant-notes
+                                    .opencgaSession="${opencgaSession}"
+                                    .variant="${variant}"
+                                    .active="${active}">
+                                </variant-notes>
                             `,
                         },
                         {
