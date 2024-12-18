50a8b45b5f1eaebc64d0c360f7235b8b62befa3a
Josemi <josemijuanes@gmail.com>
2024-12-18 14:20:39 +0100

    wc: Fix border color of secondary bar #TASK-7216 #TASK-7100

--- src/webcomponents/commons/layout/layout-secondary-bar.js
+++ src/webcomponents/commons/layout/layout-secondary-bar.js
@@ -1,5 +1,4 @@
 import {html, LitElement} from "lit";
-import {styleMap} from "lit/directives/style-map.js";
 
 export default class LayoutSecondaryBar extends LitElement {
 
@@ -36,12 +35,8 @@ export default class LayoutSecondaryBar extends LitElement {
     }
 
     render() {
-        const secondaryBarStyle = styleMap({
-            borderColor: (this.app.color || "") + "!important",
-        });
-
         return html`
-            <div class="d-flex align-items-stretch w-full mb-3 px-1 border-bottom" style="${secondaryBarStyle}">
+            <div class="d-flex align-items-stretch w-full mb-3 px-1 border-bottom" style="border-color:${this.app.color || ""}!important;">
                 <a class="d-flex align-items-center gap-2 me-5 user-select-none text-body text-decoration-none" href="#${this.app.id}/home">
                     <i class="fas ${this.app.icon || ""} fs-3"></i>
                     <span class="fs-3 fw-bold">${this.app.title || this.app.name || "-"}</span>
