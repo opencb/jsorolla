902b5115cc0baa4b4b4cc98e0235b44c852ceed2
Josemi <josemijuanes@gmail.com>
2024-12-20 19:24:57 +0100

    wc: Fix condition to highlight a tool as active in secondary bar #TASK-7216 #TASK-7100

--- src/webcomponents/commons/layout/layout-secondary-bar.js
+++ src/webcomponents/commons/layout/layout-secondary-bar.js
@@ -22,7 +22,7 @@ export default class LayoutSecondaryBar extends LitElement {
     }
 
     renderTool(tool) {
-        const active = this.currentUrl.startsWith(`#${this.app.id}/${tool.id}`);
+        const active = this.currentUrl.startsWith(`#${this.app.id}/${tool.id}/`);
         return html`
             <li class="nav-item">
                 <a class="nav-link text-body ${active ? "active" : ""}" style="${active ? `border-color:${this.app.color || ""};border-width: 2px` : ""}" href="#${this.app.id}/${tool.id}">
