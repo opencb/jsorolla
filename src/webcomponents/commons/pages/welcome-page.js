0304b99a3aa78d55403f3938db269f22cf421cdf
Josemi <josemijuanes@gmail.com>
2025-01-07 17:51:13 +0100

    wc: Allow to open apps by clicking on app icon or name #TASK-7216 #TASK-7100

--- src/webcomponents/commons/pages/welcome-page.js
+++ src/webcomponents/commons/pages/welcome-page.js
@@ -33,11 +33,13 @@ export default class WelcomePage extends LitElement {
                     <div class="col-3">
                         <div class="d-block text-decoration-none text-body rounded-3">
                             <div class="d-flex mb-3">
-                                <div class="d-flex align-items-center justify-content-center text-white bg-primary rounded-4" style="width:4rem;height:4rem;">
-                                    <i class="fas ${item.icon} fs-1"></i>
-                                </div>
+                                <a href="#${item.id}/home" class="text-decoration-none d-flex align-items-center justify-content-center bg-primary rounded-4" style="width:4rem;height:4rem;">
+                                    <i class="fas ${item.icon} fs-1 text-white"></i>
+                                </a>
                             </div>
-                            <div class="text-decoration-none fs-3 fw-bold mb-1">${item.title || item.name}</div>
+                            <a href="#${item.id}/home" class="d-block text-decoration-none text-body fs-3 fw-bold mb-1">
+                                <span>${item.title || item.name}</span>
+                            </a>
                             ${item.description ? html`
                                 <div class="fs-5 mb-3 text-gray-700">
                                     ${item.description}
