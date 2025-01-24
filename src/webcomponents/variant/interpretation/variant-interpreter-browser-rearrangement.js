7bb774af023578952ddad3956666e40cec620849
Josemi <josemijuanes@gmail.com>
2025-01-24 12:02:44 +0100

    wc: Add title as a property in all interpreter browsers #TASK-7216 #TASK-7100

--- src/webcomponents/variant/interpretation/variant-interpreter-browser-rearrangement.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-browser-rearrangement.js
@@ -47,6 +47,9 @@ class VariantInterpreterBrowserRearrangement extends LitElement {
             cellbaseClient: {
                 type: Object
             },
+            title: {
+                type: String,
+            },
             active: {
                 type: Boolean,
             },
@@ -80,9 +83,6 @@ class VariantInterpreterBrowserRearrangement extends LitElement {
         super.update(changedProperties);
     }
 
-    /*
-     * Fetch the ClinicalAnalysis object from REST and trigger the observer call.
-     */
     clinicalAnalysisIdObserver() {
         if (this.opencgaSession && this.clinicalAnalysisId) {
             this.opencgaSession.opencgaClient.clinical().info(this.clinicalAnalysisId, {study: this.opencgaSession.study.fqn})
@@ -271,11 +271,11 @@ class VariantInterpreterBrowserRearrangement extends LitElement {
 
     getDefaultConfig() {
         return {
-            title: "Cancer Case Interpreter",
+            title: this.title || "Cancer Case Interpreter",
             icon: "fas fa-search",
             active: false,
             showOtherTools: false,
-            showTitle: false,
+            showTitle: true,
             filter: {
                 title: "Filter",
                 searchButton: true,
