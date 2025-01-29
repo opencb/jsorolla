377ac70de593d4bf9e25cd397a1c95e170652172
Josemi <josemijuanes@gmail.com>
2025-01-29 18:37:47 +0100

    iva: Remove clinical analysis portal settings #TASK-7216 #TASK-7100

--- src/sites/iva/conf/browsers.settings.js
+++ src/sites/iva/conf/browsers.settings.js
@@ -356,44 +356,6 @@ const CATALOG_SETTINGS = {
         // merge criterium: uses this array as filter for internal 1D array.
         details: ["clinical-analysis-view", "json-view"]
     },
-    CLINICAL_ANALYSIS_PORTAL_BROWSER: {
-        title: "Case Portal",
-        icon: "fas fa-window-restore",
-        menu: {
-            // merge criterium: internal sections and filters are used to hydrates the external filters list for each section (which is a list of string). Sections and filter order is respected.
-            sections: [
-                {
-                    filters: ["id", "family", "proband", "sample", "status", "priority", "type", "creationDate", "dueDate"],
-                },
-            ],
-            // merge criterium: full outer join-like. it adds objects presents in internal array only and in external array only. In case of same id, the external value overwrite the internal.
-            examples: [
-                /* {
-                    id: "Intellectual disability2",
-                    active: false,
-                    query: {
-                        disorder: "Intellectual disability"
-                    }
-                }*/
-            ],
-        },
-        table: {
-            // merge criterium: spread operator
-            toolbar: {
-                showSettings: true,
-                showColumns: false,
-                showCreate: true,
-                showExport: true,
-                showDownload: false,
-                // columns list for the dropdown will be added in grid components based on settings.table.columns
-            },
-            // merge criterium: uses this array as filter for internal 1D/2D array. It handles row/col span
-            // It is supported either columns[] or hiddenColumns[].
-            // TODO NOTE this refers to clinical-analysis-grid (same list in clinical-analysis-browser.settings)
-            // columns: ["caseId", "probandId", "familyId", "disorderId", "interpretation", "actions"]
-        },
-        details: ["clinical-analysis-view"],
-    },
     // RGA_BROWSER: {},
     // CLINICAL_ANALYSIS_VIEW: {
     //     /** clinical-analysis-view settings
