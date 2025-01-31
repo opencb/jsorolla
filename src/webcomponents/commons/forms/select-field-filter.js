59cc0bddace349921f8470fa2c71753d2644e640
Josemi <josemijuanes@gmail.com>
2025-01-31 18:13:47 +0100

    wc: Fix scrolling bug when opening selects inside dropdown or offcanvas elements #TASK-7216 #TASK-7100

--- src/webcomponents/commons/forms/select-field-filter.js
+++ src/webcomponents/commons/forms/select-field-filter.js
@@ -157,6 +157,15 @@ export default class SelectFieldFilter extends LitElement {
             this.select.val(null).trigger("change");
         }
 
+        // Disable scrolling block
+        // This is to avoid the select2 dropdown from being blocked by the parent element, for example when
+        // using it inside a modal or a dropdown
+        this.select.on("select2:open", event => {
+            // disable scrolling on parent elements
+            $(event.target).parents().off("scroll.select2");
+            // disable scrolling on window
+            $(window).off("scroll.select2");
+        });
     }
 
     customAdapter() {
