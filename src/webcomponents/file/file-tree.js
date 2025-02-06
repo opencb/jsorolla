aa295bf9b389847b50e864ed94dcbfd7ee79f898
Josemi <josemijuanes@gmail.com>
2025-02-06 16:19:14 +0100

    wc: Fetch subdirectories when expanding a directory in tree #TASK-7216 #TASK-7100

--- src/webcomponents/file/file-tree.js
+++ src/webcomponents/file/file-tree.js
@@ -23,8 +23,8 @@ export default class FileTree extends LitElement {
     }
 
     #init() {
-        this._tree = null;
-        this._treeExpandedNodes = new Set();
+        this._directories = new Map();
+        this._expandedDirectories = new Set();
         this._config = this.getDefaultConfig();
     }
 
@@ -44,64 +44,74 @@ export default class FileTree extends LitElement {
     }
 
     opencgaSessionObserver() {
-        this._tree = null;
-        this._treeExpandedNodes = new Set();
+        this._directories = new Map();
+        this._expandedDirectories = new Set();
 
         if (this.opencgaSession) {
-            this.opencgaSession.opencgaClient.files()
-                .tree(":", {
-                    study: this.opencgaSession.study.fqn,
-                    include: "id,name,path",
-                })
-                .then(response => {
-                    this._tree = response.responses?.[0]?.results?.[0] || null;
-                    this.requestUpdate();
-                })
-                .catch(error => {
-                    console.error(error);
-                });
+            this.fetchDirectory(":");
         }
     }
 
-    onExpandCollapseNode(node) {
-        if (this._treeExpandedNodes.has(node.file.id)) {
-            this._treeExpandedNodes.delete(node.file.id);
+    fetchDirectory(directoryId) {
+        this.opencgaSession.opencgaClient.files()
+            .tree(directoryId, {
+                study: this.opencgaSession.study.fqn,
+                maxDepth: 1,
+                include: "id,name,path",
+            })
+            .then(response => {
+                const subDirectories = (response.responses?.[0]?.results?.[0]?.children || [])
+                    .filter(child => child.file.type.toUpperCase() === "DIRECTORY")
+                    .map(child => child.file);
+                this._directories.set(directoryId, subDirectories);
+                this.requestUpdate();
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    onExpandCollapseDirectory(directory) {
+        if (this._expandedDirectories.has(directory.id)) {
+            this._expandedDirectories.delete(directory.id);
         } else {
-            this._treeExpandedNodes.add(node.file.id);
+            this._expandedDirectories.add(directory.id);
+        }
+        // check if we have to fetch this directory
+        if (!this._directories.has(directory.id)) {
+            this.fetchDirectory(directory.id);
         }
         this.requestUpdate();
     }
 
-    renderTree(children, indent = 0) {
-        return children
-            .filter(child => child.file.type.toUpperCase() === "DIRECTORY")
-            .map(child => {
-                return html`
-                    <div class="d-flex align-items-center hover:bg-gray-200 p-2 rounded-2">
-                        <div class="flex-shrink-0" style="width: ${indent * 10}px"></div>
-                        <div class="flex-shrink-0 d-flex cursor-pointer px-2" @click="${() => this.onExpandCollapseNode(child)}">
-                            <i class="fas ${this._treeExpandedNodes.has(child.file.id) ? "fa-angle-down" : "fa-angle-right"} fs-7"></i>
-                        </div>
-                        <div class="d-flex flex-shrink-1 align-items-center gap-2 cursor-pointer" style="min-width:0;">
-                            <i class="fas fa-folder fs-5"></i>
-                            <span class="lh-1 text-truncate" title="${child.file.name}">
-                                ${child.file.name}
-                            </span>
-                        </div>
+    renderTree(directoryId, indent = 0) {
+        return (this._directories.get(directoryId) || []).map(directory => {
+            return html`
+                <div class="d-flex align-items-center hover:bg-gray-200 p-2 rounded-2">
+                    <div class="flex-shrink-0" style="width: ${indent * 10}px"></div>
+                    <div class="flex-shrink-0 d-flex cursor-pointer px-2" @click="${() => this.onExpandCollapseDirectory(directory)}">
+                        <i class="fas ${this._expandedDirectories.has(directory.id) ? "fa-angle-down" : "fa-angle-right"} fs-7"></i>
                     </div>
-                    ${this._treeExpandedNodes.has(child.file.id) ? this.renderTree(child.children, indent + 1) : nothing}
-                `;
-            });
+                    <div class="d-flex flex-shrink-1 align-items-center gap-2 cursor-pointer" style="min-width:0;">
+                        <i class="fas fa-folder fs-5"></i>
+                        <span class="lh-1 text-truncate" title="${directory.name}">
+                            ${directory.name}
+                        </span>
+                    </div>
+                </div>
+                ${this._expandedDirectories.has(directory.id) ? this.renderTree(directory.id, indent + 1) : nothing}
+            `;
+        });
     }
 
     render() {
-        if (!this.opencgaSession || !this._tree) {
+        if (!this.opencgaSession || !this._directories.has(":")) {
             return nothing;
         }
 
         return html`
             <div class="d-flex flex-column overflow-y-auto" style="${this._config.display.containerStyle}">
-                ${this.renderTree(this._tree.children, 0)}
+                ${this.renderTree(":", 0)}
             </div>
         `;
     }
