ea879643056fe3be0af66ed48e4d77f3ae7126e6
gpveronica <veronica.zettagenomics@gmail.com>
2025-02-09 18:08:40 +0100

    wc - Fix workflow permission name. #TASK-7357, #TASK-7100
    Signed-off-by: gpveronica <veronica.zettagenomics@gmail.com>

--- src/webcomponents/commons/utils/web-utils.js
+++ src/webcomponents/commons/utils/web-utils.js
@@ -55,7 +55,7 @@ export default class WebUtils {
             "STUDY": "STUDIES",
             "USER": "USERS",
             "NOTE": "NOTE",
-            "WORKFLOW": "WORKFLOW",
+            "WORKFLOW": "WORKFLOWS",
         };
         return (resource && mapResourcePermissionId[resource] && mode) ? `${mode.toUpperCase()}_${mapResourcePermissionId[resource]}` : "";
     }
