283d2a2f86ea304b3689423a2a8a06722317372c
Josemi <josemijuanes@gmail.com>
2025-02-11 12:22:30 +0100

    wc: Change order of rest api link in primary bar #TASK-7216 #TASK-7100

--- src/webcomponents/commons/layout/layout-primary-bar.js
+++ src/webcomponents/commons/layout/layout-primary-bar.js
@@ -180,13 +180,13 @@ export default class LayoutPrimaryBar extends LitElement {
                             </a>
                         </div>
                         <div class="d-flex">
-                            <a href="#rest-api" class="d-flex align-items-center btn border border-gray-700 hover:bg-gray-800 text-white">
-                                <i class="fas fa-code lh-1"></i>
+                            <a href="#swagger-ui" class="d-flex align-items-center btn border border-gray-700 hover:bg-gray-800 text-white">
+                                <img src="https://raw.githubusercontent.com/swagger-api/swagger-ui/refs/heads/master/dist/favicon-16x16.png">
                             </a>
                         </div>
                         <div class="d-flex">
-                            <a href="#swagger-ui" class="d-flex align-items-center btn border border-gray-700 hover:bg-gray-800 text-white">
-                                <img src="https://raw.githubusercontent.com/swagger-api/swagger-ui/refs/heads/master/dist/favicon-16x16.png">
+                            <a href="#rest-api" class="d-flex align-items-center btn border border-gray-700 hover:bg-gray-800 text-white">
+                                <i class="fas fa-code lh-1"></i>
                             </a>
                         </div>
 
