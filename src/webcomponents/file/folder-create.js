c0e18ce4fbaf52952d404f9c4c0a9ccb86aa9da4
Josemi <josemijuanes@gmail.com>
2025-02-11 18:32:44 +0100

    wc: Add missing data-form dependency in folder-create #TASK-7216 #TASK-7100

--- src/webcomponents/file/folder-create.js
+++ src/webcomponents/file/folder-create.js
@@ -17,7 +17,7 @@
 import {html, LitElement} from "lit";
 import LitUtils from "../commons/utils/lit-utils.js";
 import NotificationUtils from "../commons/utils/notification-utils.js";
-import "../commons/filters/catalog-search-autocomplete.js";
+import "../commons/forms/data-form.js";
 
 export default class FolderCreate extends LitElement {
 
