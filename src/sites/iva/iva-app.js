801d701388bcd3d746944af9d4b3ab07acf2f618
Josemi <josemijuanes@gmail.com>
2025-02-26 12:47:10 +0100

    iva: Remove confirmation message after updating a study #TASK-7216 #TASK-7100

--- src/sites/iva/iva-app.js
+++ src/sites/iva/iva-app.js
@@ -842,11 +842,6 @@ class IvaApp extends LitElement {
     }
 
     onSessionUpdateRequest() {
-        // NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_SUCCESS, {
-        //     title: "Refresh Session: Session Update Request",
-        //     message: "Session updated correctly",
-        // });
-        console.log("Session updated correctly");
         this._createOpenCGASession();
     }
 
@@ -865,42 +860,37 @@ class IvaApp extends LitElement {
         this.opencgaSession = {...this.opencgaSession};
     }
 
-    onStudyUpdateRequest(e) {
-        if (e.detail.value) {
-            NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_SUCCESS, {
-                title: "Refresh Session",
-                message: "Session updated correctly",
-            });
-            this._createOpenCGASession();
-
-            // this.opencgaSession.opencgaClient.studies()
-            //     .info(e.detail.value)
-            //     .then(res => {
-            //         const updatedStudy = res.responses[0].results[0];
-            //         for (const project of this.opencgaSession.user.projects) {
-            //             if (project.studies?.length > 0) {
-            //                 const studyIndex = project.studies.findIndex(study => study.fqn === e.detail.value);
-            //                 if (studyIndex >= 0) {
-            //                     project.studies[studyIndex] = updatedStudy;
-            //                     break;
-            //                 }
-            //             }
-            //         }
-            //
-            //         // Update opencgaSession.study if the study updated is the active one
-            //         if (this.opencgaSession.study && this.opencgaSession.study.fqn === e.detail.value) {
-            //             this.opencgaSession.study = updatedStudy;
-            //         }
-            //
-            //         this.settings = UtilsNew.objectClone(this.opencgaSession.study.attributes[SETTINGS_NAME].settings);
-            //         this.opencgaSession = {...this.opencgaSession};
-            //         // this.requestUpdate();
-            //     })
-            //     .catch(e => {
-            //         console.error(e);
-            //         // params.error(e);
-            //     });
-        }
+    onStudyUpdateRequest() {
+        // TODO: update only the current study instead of refreshing the whole session
+        this._createOpenCGASession();
+        
+        // this.opencgaSession.opencgaClient.studies()
+        //     .info(e.detail.value)
+        //     .then(res => {
+        //         const updatedStudy = res.responses[0].results[0];
+        //         for (const project of this.opencgaSession.user.projects) {
+        //             if (project.studies?.length > 0) {
+        //                 const studyIndex = project.studies.findIndex(study => study.fqn === e.detail.value);
+        //                 if (studyIndex >= 0) {
+        //                     project.studies[studyIndex] = updatedStudy;
+        //                     break;
+        //                 }
+        //             }
+        //         }
+        //
+        //         // Update opencgaSession.study if the study updated is the active one
+        //         if (this.opencgaSession.study && this.opencgaSession.study.fqn === e.detail.value) {
+        //             this.opencgaSession.study = updatedStudy;
+        //         }
+        //
+        //         this.settings = UtilsNew.objectClone(this.opencgaSession.study.attributes[SETTINGS_NAME].settings);
+        //         this.opencgaSession = {...this.opencgaSession};
+        //         // this.requestUpdate();
+        //     })
+        //     .catch(e => {
+        //         console.error(e);
+        //         // params.error(e);
+        //     });
     }
 
     renderTool() {
