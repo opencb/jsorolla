7b3253187a4e4d79c2ec8c97369761c930c068ef
Josemi <josemijuanes@gmail.com>
2025-03-03 14:12:11 +0100

    wc: Rewrite job-result for using file-tree instead of displaying a list of files generated by the job #TASK-7220 #TASK-7100

--- src/webcomponents/job/job-result.js
+++ src/webcomponents/job/job-result.js
@@ -14,12 +14,9 @@
  * limitations under the License.
  */
 
-import {LitElement, html} from "lit";
-import UtilsNew from "../../core/utils-new.js";
-import LitUtils from "../commons/utils/lit-utils.js";
-import WebUtils from "../commons/utils/web-utils.js";
-import "../commons/forms/data-form.js";
-import "./job-detail-log.js";
+import {LitElement, html, nothing} from "lit";
+import "../file/file-preview.js";
+import "../file/file-tree.js";
 import "../loading-spinner.js";
 
 export default class JobResult extends LitElement {
@@ -42,13 +39,7 @@ export default class JobResult extends LitElement {
             jobId: {
                 type: String,
             },
-            // search: {
-            //     type: Boolean,
-            // },
             opencgaSession: {
-                type: Object,
-            },
-            displayConfig: {
                 type: Object,
             },
             config: {
@@ -58,226 +49,107 @@ export default class JobResult extends LitElement {
     }
 
     #init() {
-        this.job = {};
-        // this.search = false;
-        this.isLoading = false;
-
-        this.displayConfigDefault = {
-            collapsable: true,
-            titleVisible: false,
-            titleWidth: 3,
-            defaultValue: "-",
-            defaultLayout: "horizontal",
-            buttonsVisible: false,
-            pdf: false,
-        };
+        this._loading = false;
+        this._selectedFile = null;
         this._config = this.getDefaultConfig();
     }
 
     #setLoading(value) {
-        this.isLoading = value;
+        this._loading = value;
         this.requestUpdate();
     }
 
     update(changedProperties) {
-        if (changedProperties.has("job")) {
+        if (changedProperties.has("job") || changedProperties.has("opencgaSession")) {
             this.jobObserver();
         }
+
         if (changedProperties.has("jobId")) {
             this.jobIdObserver();
         }
-        if (changedProperties.has("displayConfig")) {
-            this.displayConfig = {
-                ...this.displayConfigDefault,
-                ...this.displayConfig
-            };
-            this._config = this.getDefaultConfig();
-        }
+
         super.update(changedProperties);
     }
 
-    async jobObserver() {
-        const params = {
-            study: this.opencgaSession.study.fqn
-        };
-        await this.opencgaSession.opencgaClient.files()
-            .tree(this.job.outDir.id, params)
-            .then(response => {
-                this.tree = response.responses[0].results[0];
-                // this.#prepareData();
-            })
-            .catch(reason => {
-                console.error(reason);
-            });
-
-        this.createDataFormConfig();
+    jobObserver() {
+        this._selectedFile = null; // clear selected file
     }
 
     jobIdObserver() {
+        this.job = null;
+
         if (this.jobId && this.opencgaSession) {
-            const params = {
-                study: this.opencgaSession.study.fqn
-            };
-            let error;
             this.#setLoading(true);
             this.opencgaSession.opencgaClient.jobs()
-                .info(this.jobId, params)
+                .info(this.jobId, {
+                    study: this.opencgaSession.study.fqn,
+                    include: "outDir",
+                })
                 .then(response => {
                     this.job = response.responses[0].results[0];
-                    // this.#prepareData();
                 })
                 .catch(reason => {
-                    this.job = {};
-                    error = reason;
                     console.error(reason);
                 })
                 .finally(() => {
-                    this._config = this.getDefaultConfig();
-                    LitUtils.dispatchCustomEvent(this, "jobSearch", this.job, {}, error);
                     this.#setLoading(false);
                 });
-        } else {
-            this.job = {};
         }
     }
 
-    createX(files) {
-        const elements = [];
-        for (const f of files) {
-            const file = f.file;
-
-            switch (file.format) {
-
-                case "JSON":
-                    elements.push({
-                        name: file.name,
-                        field: file.id,
-                        type: "file-preview",
-                        display: {
-
-                        }
-                    });
-                    break;
-                case "IMAGE":
-                    elements.push({
-                        name: file.name,
-                        field: file.id,
-                        type: "image",
-                        display: {
-
-                        }
-                    });
-                    break;
-                default:
-                    debugger
-                    elements.push({
-                        name: file.name,
-                        field: file.id,
-                        type: "file-preview",
-                        display: {
-
-                        }
-                    });
-                    break;
-            }
+    onSelectFile(event) {
+        if (event.detail?.type === "FILE") {
+            this._selectedFile = event.detail;
+            this.requestUpdate();
         }
-        return elements;
-    }
-
-    createDataFormConfig() {
-        // 1.
-        const outputMap = {};
-        this.job.output.forEach(file => outputMap[file.id] = file);
-
-
-
-        const dataFormConfig = {
-            title: "Results",
-            display: {
-                mode: "page",
-                type: "PILLS"
-            },
-            sections: [
-            ]
-        };
-
-        const firstLevelDirectories = this.tree.children.filter(f => f.file.type === "DIRECTORY");
-        if (firstLevelDirectories?.length === 0) {
-            // this.job.outDir = this.tree.children.find(f => f.file.type === "DIRECTORY");
-        } else {
-            debugger
-            for (const directory of firstLevelDirectories) {
-
-                const section = {
-                    title: directory.file.name,
-                    elements: []
-                };
-
-                const files = directory.children.filter(f => f.file.type === "FILE");
-                section.elements.push(...this.createX(files));
-
-                const secondLevelDirectories = directory.children.filter(f => f.file.type === "DIRECTORY");
-                for (const subDirectory of secondLevelDirectories) {
-                    const files = subDirectory.children.filter(f => f.file.type === "FILE");
-
-                    if (files.length > 0) {
-                        section.elements.push({
-                            title: subDirectory.file.name,
-                            type: "title",
-                            display: {
-                                titleStyle: "color:red; margin-top: 20px",
-                            }
-                        });
-                        section.elements.push(...this.createX(files));
-                    } else {
-
-                    }
-
-                }
-
-                dataFormConfig.sections.push(section);
-            }
-        }
-
-        this._data = outputMap;
-        this._config = {...dataFormConfig};
-        debugger
-
-        this.requestUpdate();
     }
 
     render() {
-        if (this.isLoading) {
-            return html`<loading-spinner></loading-spinner>`;
-        }
-
-        if (!this.job?.id) {
+        if (this._loading) {
             return html`
-                <div class="alert alert-info">
-                    <i class="fas fa-3x fa-info-circle align-middle" style="padding-right: 10px"></i>
-                    No Job ID found.
-                </div>
+                <loading-spinner></loading-spinner>
             `;
         }
 
+        if (!this.opencgaSession || !this.job?.id) {
+            return nothing;
+        }
+
         return html`
-            <data-form
-                .data="${this._data}"
-                .opencgaSession="${this.opencgaSession}"
-                .config="${this._config}">
-            </data-form>
+            <div class="row">
+                <div class="col-md-2">
+                    <file-tree
+                        .opencgaSession="${this.opencgaSession}"
+                        .rootDirectoryId="${this.job.outDir.id}"
+                        .currentPath="${this._selectedFile?.path || null}"
+                        .config="${{
+                            rootDirectoryName: this.job.outDir.name,
+                            rootDirectoryIcon: "fa-folder",
+                            showFiles: true,
+                        }}"
+                        @pathChange="${event => this.onSelectFile(event)}">
+                    </file-tree>
+                </div>
+                <div class="col-md-10">
+                    ${this._selectedFile ? html`
+                        <file-preview
+                            .fileId="${this._selectedFile.id}"
+                            .active="${true}"
+                            .opencgaSession="${this.opencgaSession}">
+                        </file-preview>
+                    ` : html`
+                        <div class="alert alert-info">
+                            <i class="fas fa-info-circle pe-2"></i>
+                            <span>Please select a file on the tree to display the content here.</span>
+                        </div>
+                    `}
+                </div>
+            </div>
         `;
     }
 
     getDefaultConfig() {
-        return {
-            title: "Results",
-            icon: "",
-            nullData: "",
-            display: this.displayConfig || this.displayConfigDefault,
-            sections: [
-                ]
-        };
+        return {};
     }
 
 }
