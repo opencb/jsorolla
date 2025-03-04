361ec983a1d8647cb16ae6774e4dfe9dba646a47
imedina <igmecas@gmail.com>
2025-03-04 13:57:37 +0000

    wc: rename ALL to all

--- src/webcomponents/variant/operation/variant-secondary-sample-index-operation.js
+++ src/webcomponents/variant/operation/variant-secondary-sample-index-operation.js
@@ -178,7 +178,7 @@ export default class VariantSecondarySampleIndexOperation extends LitElement {
                                     .value="${toolParams?.sample}"
                                     .resource="${"SAMPLE"}"
                                     .opencgaSession="${this.opencgaSession}"
-                                    .config="${{multiple: true, additionalValues: [{id: "ALL"}]}}"
+                                    .config="${{multiple: true, additionalValues: [{id: "all"}]}}"
                                     @filterChange="${e => this.onFieldChange(e, "sample")}">
                                 </catalog-search-autocomplete>
                             `,
