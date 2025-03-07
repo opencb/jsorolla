487ed13c7ed4b1cdf5f47b7f8b53f2b7cea07988
Josemi <josemijuanes@gmail.com>
2025-03-07 12:50:24 +0100

    wc: Add basic empty state component #TASK-7450 #TASK-7100

---
+++ src/webcomponents/commons/empty-state.js
@@ -0,0 +1,76 @@
+import {LitElement, html, nothing} from "lit";
+
+export default class EmptyState extends LitElement {
+
+    constructor() {
+        super();
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            icon: {
+                type: String,
+            },
+            title: {
+                type: String,
+            },
+            description: {
+                type: String,
+            },
+            config: {
+                type: Object,
+            },
+        };
+    }
+
+    #init() {
+        this._config = this.getDefaultConfig();
+    }
+
+    update(changedProperties) {
+        if (changedProperties.has("config")) {
+            this._config = {
+                ...this.getDefaultConfig(),
+                ...this.config
+            };
+        }
+
+        super.update(changedProperties);
+    }
+
+    render() {
+        return html`
+            <div class="card">
+                <div class="card-body d-flex flex-column align-items-center justify-content-between py-5 my-5">
+                    ${this.icon ? html`
+                        <div class="d-flex text-gray-800 mb-2">
+                            <i class="fas ${this.icon} display-1"></i>
+                        </div>
+                    ` : nothing}
+                    ${(this.title || this.description) ? html`
+                        <div class="d-flex flex-column align-items-center gap-3" style="max-width:560px;">
+                            ${this.title ? html`
+                                <h2 class="mb-0 text-center fw-bold">${this.title}</h2> 
+                            ` : nothing}
+                            ${this.description ? html`
+                                <p class="text-center fs-5 text-gray-700">${this.description}</p>
+                            ` : nothing}
+                        </div>
+                    ` : nothing}
+                </div>
+            </div>
+        `;
+    }
+
+    getDefaultConfig() {
+        return {};
+    }
+
+}
+
+customElements.define("empty-state", EmptyState);
