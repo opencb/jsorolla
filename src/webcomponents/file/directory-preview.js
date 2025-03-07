ed9cee006d03ee9f2dd504820198cac72d9a6b33
Josemi <josemijuanes@gmail.com>
2025-03-07 18:14:45 +0100

    wc: Add component to preview the content of a directory #TASK-7450 #TASK-7100

---
+++ src/webcomponents/file/directory-preview.js
@@ -0,0 +1,143 @@
+import {LitElement, html, nothing} from "lit";
+import LitUtils from "../commons/utils/lit-utils.js";
+import "../commons/image-viewer.js";
+
+export default class DirectoryPreview extends LitElement {
+
+    constructor() {
+        super();
+
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            directoryId: {
+                type: String,
+            },
+            opencgaSession: {
+                type: Object,
+            },
+            active: {
+                type: Object,
+            },
+            config: {
+                type: Object,
+            },
+        };
+    }
+
+    #init() {
+        this._directories = [];
+        this._files = [];
+        this._config = this.getDefaultConfig();
+    }
+
+    update(changedProperties) {
+        if (changedProperties.has("directoryId") || changedProperties.has("opencgaSession") || changedProperties.has("active")) {
+            this.directoryIdObserver();
+        }
+
+        if (changedProperties.has("config")) {
+            this._config = {
+                ...this.getDefaultConfig(),
+                ...this.config,
+            };
+        }
+
+        super.update(changedProperties);
+    }
+
+    directoryIdObserver() {
+        this._directories = [];
+        this._files = [];
+
+        if (this.directoryId && this.opencgaSession && this.active) {
+            this.opencgaSession.opencgaClient.files()
+                .tree(this.directoryId, {
+                    study: this.opencgaSession.study.fqn,
+                    maxDepth: 1,
+                    // include: "id,name,path,type,format",
+                })
+                .then(response => {
+                    const content = response.responses?.[0]?.results?.[0]?.children || [];
+                    this._directories = content
+                        .filter(child => child.file.type.toUpperCase() === "DIRECTORY")
+                        .map(child => child.file);
+                    this._files = content
+                        .filter(child => child.file.type.toUpperCase() === "FILE")
+                        .map(child => child.file);
+                    this.requestUpdate();
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }
+
+    onClickFile(file) {
+        LitUtils.dispatchCustomEvent(this, "fileClick", null, file);
+    }
+
+    renderDirectories() {
+        return this._directories.map(directory => {
+            return html`
+                <div class="col-3" @click="${() => this.onClickFile(directory)}">
+                    <div class="d-flex align-items-center p-3 gap-3 bg-white hover:bg-gray-100 cursor-pointer rounded-2 border">
+                        <i class="fas fa-folder fs-3"></i>
+                        <div class="">${directory.name}</div>
+                    </div>
+                </div>
+            `;
+        });
+    }
+
+    renderFiles() {
+        return this._files.map(file => {
+            return html`
+                <div class="col-3" @click="${() => this.onClickFile(file)}">
+                    <div class="d-flex flex-column p-3 bg-white hover:bg-gray-100 cursor-pointer rounded-2 border">
+                        <div class="d-flex justify-content-center align-items-center bg-gray-200 rounded-1" style="height: 160px;">
+                            <i class="fas fa-file-alt fs-2 text-gray-500"></i>
+                        </div>
+                        <div class="mt-2">${file.name}</div>
+                    </div>
+                </div>
+            `;
+        });
+    }
+
+    render() {
+        if (!this.directoryId || !this.opencgaSession || !this.active) {
+            return nothing;
+        }
+
+        return html`
+            <div class="d-flex flex-column">
+                ${this._directories.length > 0 ? html`
+                    <div class="fs-5 fw-bold mb-2">Folders</div>
+                    <div class="row g-3 mb-5">
+                        ${this.renderDirectories()}
+                    </div>    
+                ` : nothing}
+                ${this._files.length > 0 ? html`
+                    <div class="fs-5 fw-bold mb-2">Files</div>
+                    <div class="row g-3">
+                        ${this.renderFiles()}
+                    </div>
+                ` : nothing}
+            </div>
+        `;
+    }
+
+    getDefaultConfig() {
+        return {};
+    }
+
+}
+
+customElements.define("directory-preview", DirectoryPreview);
