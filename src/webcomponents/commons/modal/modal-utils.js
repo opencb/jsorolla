e419f40f05f37ff98a12857735aa02e091035713
Josemi <josemijuanes@gmail.com>
2025-03-20 12:14:21 +0100

    wc: Fix overflow in modal title #TASK-7100

--- src/webcomponents/commons/modal/modal-utils.js
+++ src/webcomponents/commons/modal/modal-utils.js
@@ -22,7 +22,6 @@ export default class ModalUtils {
         const modalStyle = config.display?.modalStyle || "";
         const modalSize = config.display?.modalSize || "";
         const modalTitle = config.display?.modalTitle || "";
-        const modalTitleHeader = config.display?.modalTitleHeader || "h4";
         const modalTitleClassName = config.display?.modalTitleClassName || "";
         const modalTitleStyle = config.display?.modalTitleStyle || "";
         const btnsVisible = config.display?.modalbtnsVisible;
@@ -45,7 +44,9 @@ export default class ModalUtils {
                 <div class="modal-dialog ${modalSize}" style="${modalStyle}">
                     <div class="modal-content">
                         <div class="modal-header">
-                            ${ModalUtils.#getTitleHeader(modalTitleHeader, modalTitle, "modal-title " + modalTitleClassName, modalTitleStyle)}
+                            <h4 class="modal-title text-truncate ${modalTitleClassName}" style="${modalTitleStyle}">
+                                ${modalTitle}
+                            </h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
                                     @click="${e => config?.onCancel ? config.onCancel(e) : LitUtils.dispatchCustomEvent(self, "modalCancel", null, e)}">
                             </button>
@@ -83,23 +84,6 @@ export default class ModalUtils {
                 </div>
             </div>
         `;
-    }
-
-    static #getTitleHeader(header, title, classes, style) {
-        switch (header) {
-            case "h1":
-                return html`<h1 class="${classes}" style="${style}">${title}</h1>`;
-            case "h2":
-                return html`<h2 class="${classes}" style="${style}">${title}</h2>`;
-            case "h3":
-                return html`<h3 class="${classes}" style="${style}">${title}</h3>`;
-            case "h4":
-                return html`<h4 class="${classes}" style="${style}">${title}</h4>`;
-            case "h5":
-                return html`<h5 class="${classes}" style="${style}">${title}</h5>`;
-            case "h6":
-                return html`<h6 class="${classes}" style="${style}">${title}</h6>`;
-        }
     }
 
     static draggableModal(modalElm) {
