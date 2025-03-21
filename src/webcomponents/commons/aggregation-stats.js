bd38becb582c87a02c1934898f1da6ef923bb5a3
Josemi <josemijuanes@gmail.com>
2025-03-21 16:16:03 +0100

    wc: Fix panels endpoint to query aggregation stats #TASK-7100

--- src/webcomponents/commons/aggregation-stats.js
+++ src/webcomponents/commons/aggregation-stats.js
@@ -109,7 +109,7 @@ class AggregationStats extends LitElement {
             case "COHORT":
                 return this.opencgaSession.opencgaClient.cohorts();
             case "DISEASE_PANEL":
-                return this.opencgaSession.opencgaClient.clinical();
+                return this.opencgaSession.opencgaClient.panels();
             case "CLINICAL_ANALYSIS":
                 return this.opencgaSession.opencgaClient.clinical();
             default:
