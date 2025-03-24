cd8b7079ee7ae425fab03924cacb52087271b8fd
Josemi <josemijuanes@gmail.com>
2025-03-24 14:27:00 +0100

    wc: Fix displaying All checkbox in select-field-filter #TASK-7100

--- src/webcomponents/commons/forms/select-field-filter.js
+++ src/webcomponents/commons/forms/select-field-filter.js
@@ -350,8 +350,8 @@ export default class SelectFieldFilter extends LitElement {
     renderShowSelectAll() {
         return html`
             <span class="input-group-text rounded-start-0">
-                <input class="form-check-input mt-0" id="${this._prefix}-all-checkbox" type="checkbox" aria-label="..." @click=${this.selectAll}>
-                <span class="fw-bold ms-1">All</span>
+                <input class="form-check-input mt-0 me-2" id="${this._prefix}-all-checkbox" type="checkbox" aria-label="..." @click=${this.selectAll}>
+                <span class="fw-bold">All</span>
             </span>
         `;
     }
@@ -386,10 +386,10 @@ export default class SelectFieldFilter extends LitElement {
             <div class="input-group select-field-filter">
                 <select
                     class="form-select"
-                    id="${this._prefix}"
+                    id="${this._prefix || ""}"
                     @change="${this.filterChange}">
                 </select>
-                ${this.all ? this.renderShowSelectAll() : nothing}
+                ${this._config?.all ? this.renderShowSelectAll() : nothing}
             </div>
         `;
     }
