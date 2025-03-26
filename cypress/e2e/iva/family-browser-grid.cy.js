73fcad317a1104f290dd4993da38a1034f08b065
Josemi <josemijuanes@gmail.com>
2025-03-26 17:09:13 +0100

    test: Remove detailed tab test in family grid tests #TASK-7489 #TASK-7359

--- cypress/e2e/iva/family-browser-grid.cy.js
+++ cypress/e2e/iva/family-browser-grid.cy.js
@@ -273,15 +273,5 @@ context("Family Browser Grid", () => {
                 .contains("Extra column")
                 .should("be.visible");
         });
-
-        it("should display 'New Catalog Tab' Tab", () => {
-            // eslint-disable-next-line cypress/unsafe-to-chain-command
-            cy.get("detail-tabs > div.detail-tabs > ul")
-                .find("li")
-                .contains("New Catalog Tab")
-                .as("catalogTab")
-                .click()
-                .should("be.visible");
-        });
     });
 });
