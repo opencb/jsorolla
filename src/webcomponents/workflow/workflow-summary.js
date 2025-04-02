db098ea82bc3909ef3d87b5709cf54372c1660aa
Josemi <josemijuanes@gmail.com>
2025-04-02 14:39:22 +0200

    wc: Fix configuration and remove search in workflow-summary #TASK-7489 #TASK-7359

--- src/webcomponents/workflow/workflow-summary.js
+++ src/webcomponents/workflow/workflow-summary.js
@@ -14,13 +14,9 @@
  * limitations under the License.
  */
 
-import {html, LitElement} from "lit";
+import {html, LitElement, nothing} from "lit";
 import UtilsNew from "../../core/utils-new.js";
-import Types from "../commons/types.js";
-import LitUtils from "../commons/utils/lit-utils.js";
 import "../commons/forms/data-form.js";
-import "../commons/filters/catalog-search-autocomplete.js";
-import "../loading-spinner.js";
 
 export default class WorkflowSummary extends LitElement {
 
@@ -42,9 +38,6 @@ export default class WorkflowSummary extends LitElement {
             workflowId: {
                 type: String,
             },
-            search: {
-                type: Boolean,
-            },
             opencgaSession: {
                 type: Object,
             },
@@ -55,146 +48,76 @@ export default class WorkflowSummary extends LitElement {
     }
 
     #init() {
-        this.workflow = {};
-        this.search = false;
-
-        this.isLoading = false;
-        this.displayConfigDefault = {
-            collapsable: true,
-            titleVisible: false,
-            titleWidth: 2,
-            defaultValue: "-",
-            defaultLayout: "horizontal",
-            buttonsVisible: false,
-            pdf: false,
-        };
+        this._workflow = null;
         this._config = this.getDefaultConfig();
     }
 
-    #setLoading(value) {
-        this.isLoading = value;
-        this.requestUpdate();
-    }
-
     update(changedProperties) {
-        // to update disorders if it has more than one
-        // if (changedProperties.has("workflow")) {
-        //     this._config = this.getDefaultConfig();
-        // }
+        if (changedProperties.has("workflow")) {
+            this.workflowObserver();
+        }
+
         if (changedProperties.has("workflowId")) {
             this.workflowIdObserver();
         }
+
         if (changedProperties.has("displayConfig")) {
-            this.displayConfig = {
-                ...this.displayConfigDefault,
-                ...this.displayConfig
-            };
             this._config = this.getDefaultConfig();
         }
+
         super.update(changedProperties);
     }
 
     workflowIdObserver() {
+        this._workflow = null;
         if (this.workflowId && this.opencgaSession) {
-            const params = {
-                study: this.opencgaSession.study.fqn,
-            };
-            let error;
-            this.#setLoading(true);
             this.opencgaSession.opencgaClient.workflows()
-                .info(this.workflowId, params)
+                .info(this.workflowId, {
+                    study: this.opencgaSession.study.fqn,
+                })
                 .then(response => {
-                    this.workflow = response.responses[0].results[0];
+                    this._workflow = response.responses[0].results[0];
+                    this.requestUpdate();
                 })
                 .catch(reason => {
-                    this.workflow = {};
-                    error = reason;
                     console.error(reason);
-                })
-                .finally(() => {
-                    this._config = this.getDefaultConfig();
-                    LitUtils.dispatchCustomEvent(this, "workflowSearch", this.workflow, {}, error);
-                    this.#setLoading(false);
                 });
-        } else {
-            this.workflow = {};
         }
     }
 
-    onFilterChange(e) {
-        this.workflowId = e.detail.value;
+    workflowObserver() {
+        this._workflow = {...this.workflow};
     }
 
     render() {
-        if (this.isLoading) {
-            return html`<loading-spinner></loading-spinner>`;
-        }
-
-        if (!this.workflow?.id && this.search === false) {
-            return html`
-                <div class="alert alert-info">
-                    <i class="fas fa-3x fa-info-circle align-middle" style="padding-right: 10px"></i>
-                    Workflow ID not found.
-                </div>
-            `;
+        if (!this.opencgaSession || !this._workflow) {
+            return nothing;
         }
 
         return html`
             <data-form
-                .data="${this.workflow}"
-                .config="${this._config}">
+                .data="${this._workflow}"
+                .config="${this._config || {}}">
             </data-form>
         `;
     }
 
     getDefaultConfig() {
-        return Types.dataFormConfig({
-            title: "Summary",
-            icon: "",
-            display: this.displayConfig || this.displayConfigDefault,
+        return {
+            display: {
+                titleVisible: false,
+                buttonsVisible: false,
+                ...this.displayConfig,
+            },
             sections: [
                 {
-                    title: "Search",
-                    display: {
-                        visible: workflow => !workflow?.id && this.search === true,
-                    },
-                    elements: [
-                        {
-                            title: "Workflow ID",
-                            // field: "workflowId",
-                            type: "custom",
-                            display: {
-                                render: () => html `
-                                    <catalog-search-autocomplete
-                                        .value="${this.sample?.id}"
-                                        .resource="${"WORKFLOW"}"
-                                        .opencgaSession="${this.opencgaSession}"
-                                        .config="${{multiple: false}}"
-                                        @filterChange="${e => this.onFilterChange(e)}">
-                                    </catalog-search-autocomplete>`,
-                            },
-                        },
-                    ],
-                },
-                {
                     title: "General Information",
-                    collapsed: false,
-                    display: {
-                        visible: workflow => workflow?.id,
-                    },
                     elements: [
                         {
                             title: "Workflow ID",
-                            // type: "custom",
                             type: "complex",
                             display: {
-                                // render: data => `
-                                //     <span style="font-weight: bold">${data.id}</span> (UUID: ${data.uuid})
-                                // `,
                                 template: "${id} (UUID: ${uuid})",
-                                // transform: {
-                                //     id: id => id.toLowerCase(),
-                                // },
                                 style: {
                                     id: {
                                         "font-weight": "bold",
@@ -243,7 +166,6 @@ export default class WorkflowSummary extends LitElement {
                             title: "Status",
                             type: "complex",
                             display: {
-                                // render: field => field ? `${field.name} (${UtilsNew.dateFormatter(field.date)})` : "-"
                                 template: "${internal.status.name} (${internal.status.date})",
                                 format: {
                                     "internal.status.date": date => UtilsNew.dateFormatter(date)
@@ -258,14 +180,12 @@ export default class WorkflowSummary extends LitElement {
                             title: "Creation Date",
                             field: "creationDate",
                             display: {
-                                // render: field => field ? UtilsNew.dateFormatter(field) : "-"
                                 format: date => UtilsNew.dateFormatter(date)
                             },
                         },
                         {
                             title: "Modification Date",
                             field: "modificationDate",
-                            // type: "custom",
                             display: {
                                 format: modificationDate => UtilsNew.dateFormatter(modificationDate),
                             },
@@ -315,34 +235,8 @@ export default class WorkflowSummary extends LitElement {
                         },
                     ],
                 },
-                /*
-                {
-                    title: "Scripts",
-                    elements: [
-                        {
-                            title: "Scripts",
-                            field: "scripts",
-                            type: "list",
-                            display: {
-                                defaultValue: "No scripts are currently available.",
-                                contentLayout: "vertical",
-                                format: script => {
-                                    return `
-                                        <h5 class="card-title">${script.fileName} ${script.main ? `(main)`: ``}</h5>
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <p class="card-text">${script.content}</p>
-                                            </div>
-                                        </div>
-                                    `;
-                                }
-                            },
-                        },
-                    ],
-                },
-                 */
             ],
-        });
+        };
     }
 
 }
