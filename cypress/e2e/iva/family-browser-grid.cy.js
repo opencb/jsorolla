85aa1f1d829e041d7d20c196a55c07dbcbc9e1bb
Josemi <josemijuanes@gmail.com>
2025-04-09 11:35:18 +0200

    test: Fix tests for family-grid #TASK-7582 #TASK-7359

--- cypress/e2e/iva/family-browser-grid.cy.js
+++ cypress/e2e/iva/family-browser-grid.cy.js
@@ -14,264 +14,228 @@
  * limitations under the License.
  */
 
-import BrowserTest from "../../support/browser-test.js";
-import UtilsTest from "../../support/utils-test.js";
-
 context("Family Browser Grid", () => {
-    const browserGrid = "family-grid";
-    const browserDetail = "family-detail";
-
     beforeEach(() => {
         cy.visit("#family-browser-grid");
+        cy.get("family-grid")
+            .as("family-grid");
         cy.waitUntil(() => {
-            return cy.get(browserGrid)
+            return cy.get("@family-grid")
                 .should("be.visible");
         });
     });
 
-    // TOOLBAR
-    context("Family Toolbar", () => {
+    context("toolbar", () => {
         beforeEach(() => {
-            cy.get(browserGrid)
-                .find("div[data-cy='toolbar']")
+            cy.get("@family-grid")
+                .find("opencb-grid-toolbar")
                 .as("toolbar");
         });
 
-        //1. Render the toolbar
-        context("render", () => {
-            // 1.1. It should render a div with the toolbar
-            it("should render toolbar", () => {
-                cy.get(browserGrid)
-                    .find("div[data-cy='toolbar-wrapper']")
-                    .should("be.visible");
-            });
-            // 1.1. If configured, it should render a New button
-            it("should render New button", () => {
-                cy.get(browserGrid)
-                    .find("button[data-action='create']")
-                    .should("be.visible");
-            });
+        it("should be visible", () => {
+            cy.get("@toolbar")
+                .should("be.visible");
+        });
+
+        it("should display the 'Create Family' button", () => {
+            cy.get("@toolbar")
+                .contains("button", "Create Family")
+                .should("be.visible");
+        });
+
+        it("should display the 'Settings' button", () => {
+            cy.get("@toolbar")
+                .contains("button", "Settings")
+                .should("be.visible");
         });
     });
 
-    // MODAL CREATE
-    context("Modal Create", () => {
+    context("create a family", () => {
         beforeEach(() => {
-            cy.get(browserGrid)
-                .find("button[data-action='create']")
+            cy.get("@family-grid")
+                .find("opencb-grid-toolbar")
+                .contains("button", "Create Family")
                 .click();
-            cy.get(browserGrid)
-                .find("div[data-cy='modal-create']")
-                .as("modal-create");
+            cy.get("@family-grid")
+                .find(`div[data-cy="family-create"]`)
+                .as("modal-family-create");
         });
-        // 1. Open modal and render create
-        it("should render create modal", () => {
-            cy.get("@modal-create")
+
+        it("should be visible when clicking on the 'Create Family' button", () => {
+            cy.get("@modal-family-create")
                 .find("div.modal-dialog")
                 .should("be.visible");
         });
-        // 2. Render title
-        it("should render create title", () => {
-            cy.get("@modal-create")
+
+        it("should display the text 'Create Family' as a title", () => {
+            cy.get("@modal-family-create")
                 .find("h4.modal-title")
                 .should("contain.text", "Create Family");
         });
-        // 3. Render button clear
-        it("should render button clear", () => {
-            cy.get("@modal-create")
+
+        it("should display a 'Clear' button", () => {
+            cy.get("@modal-family-create")
                 .contains("button", "Clear")
                 .should("be.visible");
         });
-        // 4. Render button create
-        it("should render button create", () => {
-            cy.get("@modal-create")
+
+        it("should display a 'Create' button", () => {
+            cy.get("@modal-family-create")
                 .contains("button", "Create")
                 .should("be.visible");
         });
-        // 5. Render tabs
-        it("should render form tabs", () => {
-            cy.get("@modal-create")
+
+        it("should display content as tabs", () => {
+            cy.get("@modal-family-create")
                 .find("ul.nav.nav-tabs > li")
                 .should("have.length.at.least", 1);
         });
-        // 6. Render Family ID
-        it("should have form field ID", () => {
-            cy.get("@modal-create")
-                .find("data-form div.form-horizontal div.row div.col-md-3")
+
+        it("should display a field to set the Family ID", () => {
+            cy.get("@modal-family-create")
+                .find("data-form")
                 .should("contain.text", "Family ID");
         });
     });
 
-    // MODAL UPDATE
-    context("Modal Update", () => {
+    context("update a family", () => {
         beforeEach(() => {
-            cy.get(browserGrid)
-                .find("table tbody tr td button.dropdown-toggle")
+            cy.get("@family-grid")
+                .find(`table tbody tr td button[data-cy="actions-button"]`)
                 .first()
                 .click();
-            cy.get(browserGrid)
-                .find("a[data-action='edit']")
+            cy.get("@family-grid")
+                .find(`a[data-action="edit"]`)
                 .first()
                 .click();
-            cy.get("div[data-cy='modal-update']")
-                .as("modal-update");
+            cy.get(`div[data-cy="family-update"]`)
+                .as("modal-family-update");
         });
-        // 1. Open modal and render update
-        it("should render update modal", () => {
-            cy.get("@modal-update")
+        
+        it("should be visible after clicking on the 'Edit' action", () => {
+            cy.get("@modal-family-update")
                 .find("div.modal-dialog")
                 .should("be.visible");
         });
-        // 2. Render title
-        it("should render update title", () => {
-            cy.get("@modal-update")
+
+        it("should display the text 'Update Family' as a title", () => {
+            cy.get("@modal-family-update")
                 .find("h4.modal-title")
                 .should("contain.text", "Update Family");
         });
-        // 3. Render button clear
-        it("should render button clear", () => {
-            cy.get("@modal-update")
+
+        it("should display a 'Discard Changes' button", () => {
+            cy.get("@modal-family-update")
                 .contains("button", "Discard Changes")
                 .should("be.visible");
         });
-        // 4. Render button create
-        it("should render button create", () => {
-            cy.get("@modal-update")
+
+        it("should display a 'Update' button", () => {
+            cy.get("@modal-family-update")
                 .contains("button", "Update")
                 .should("be.visible");
         });
-        // 5. Render tabs
-        it("should render form tabs", () => {
-            cy.get("@modal-update")
+
+        it("should display the content as tabs", () => {
+            cy.get("@modal-family-update")
                 .find("ul.nav.nav-tabs > li")
                 .should("have.length.at.least", 1);
         });
-        // 6. Render Sample ID
-        it("should have form field ID equal to sample selected", () => {
-            cy.get("@modal-update")
-                .find("data-form div.row div.row div.col-md-3")
+
+        it("should display a field with the Family ID", () => {
+            cy.get("@modal-family-update")
+                .find("data-form")
                 .should("contain.text", "Family ID");
         });
     });
 
-    context("Modal Setting", () => {
-
-        it("should move modal setting", () => {
-            cy.get("button[data-action='settings']")
+    context("settings", () => {
+        it("should display the settings modal when clicking on the 'Settings' button", () => {
+            cy.get("@family-grid")
+                .find(`button[data-action="settings"]`)
                 .click();
-
-            BrowserTest.getElementByComponent({
-                selector: `${browserGrid} opencb-grid-toolbar`,
-                tag:"div",
-                elementId: "SettingModal"
-            }).as("settingModal");
-
-            cy.get("@settingModal")
-                .then(($modal) => {
-                    const startPosition = $modal.offset();
-                    cy.log("start Position:", startPosition);
-                    // Drag the modal to a new position using Cypress's drag command
-                    cy.get("@settingModal")
-                        .find(".modal-header")
-                        .as("modalHeader");
-
-                    cy.get("@modalHeader")
-                        .trigger("mousedown", { which: 1 }); // Trigger mouse down event
-                    cy.get("@modalHeader")
-                        .trigger("mousemove", { clientX: 100, clientY: 100 }); // Move the mouse
-                    cy.get("@modalHeader")
-                        .trigger("mouseup"); // Release the mouse
-
-                    // Get the final position of the modal
-                    cy.get("@modalHeader")
-                        .then(($modal) => {
-                            const finalPosition = $modal.offset();
-                            cy.log("final Position:", finalPosition);
-                            // Assert that the modal has moved
-                            expect(finalPosition.left).to.not.equal(startPosition.left);
-                            expect(finalPosition.top).to.not.equal(startPosition.top);
-                        });
-                });
+            cy.get("div.modal-dialog")
+                .should("be.visible");
         });
 
-        it("should hide columns [Case ID,Phenotypes]",() => {
-            const columns = ["Case ID","Phenotypes"];
-            cy.get(`${browserGrid} thead th`)
-                .as("headerColumns");
+        it("should allow to hide columns in the grid", () => {
+            const columns = ["Case ID", "Phenotypes"];
 
-            columns.forEach(col => {
+            cy.get("@family-grid")
+                .find("thead th")
+                .as("headerColumns");
+            columns.forEach(column => {
                 cy.get("@headerColumns")
-                    .contains("div",col)
+                    .contains("div", column)
                     .should("be.visible");
             });
-            cy.get("button[data-action='settings']")
+            cy.get("@family-grid")
+                .find(`button[data-action="settings"]`)
                 .click();
-            UtilsTest.getByDataTest("test-columns", "select-field-filter .select2-container")
+            cy.get("@family-grid")
+                .find(`div[data-testid="test-columns"] select-field-filter`)
+                .as("columnsSelector");
+            cy.get("@columnsSelector")
+                .find(".select2-container")
                 .click();
-            columns.forEach(col => {
-                UtilsTest.getByDataTest("test-columns", "select-field-filter span.select2-results li")
-                    .contains(col)
+            columns.forEach(column => {
+                cy.get("@columnsSelector")
+                    .find("span.select2-results li")
+                    .contains(column)
                     .click();
             });
-            UtilsTest.getByDataTest("test-columns", "select-field-filter .select2-selection")
+            cy.get("@columnsSelector")
+                .find(".select2-selection")
                 .click();
-            BrowserTest.getElementByComponent({
-                selector: `${browserGrid} opencb-grid-toolbar`,
-                tag:"div",
-                elementId: "SettingModal"
-            }).as("settingModal");
-
-            cy.get("@settingModal")
+            cy.get("@family-grid")
+                .find(".modal-body")
                 .contains("button", "OK")
                 .click();
-
             cy.get("@headerColumns")
                 .should($header => {
-                    const _columns = Array.from($header, th => th.textContent?.trim());
+                    const visibleColumns = Array.from($header, th => th.textContent.trim());
                     columns.forEach(col => {
-                        expect(col).not.to.be.oneOf(_columns);
+                        expect(col).not.to.be.oneOf(visibleColumns);
                     });
                 });
         });
     });
 
-    context("Grid", () => {
-        it("should render", () => {
-            cy.get(browserGrid)
-                .should("be.visible");
-        });
-    });
+    context("grid", () => {
+        context("content", () => {
+            it("should display a <table> element", () => {
+                cy.get("@family-grid")
+                    .find("table")
+                    .should("be.visible");
+            });
 
-    context("Row", () => {
-        it("should display row #1 as selected", () => {
-            // eslint-disable-next-line cypress/unsafe-to-chain-command
-            cy.get("tbody tr")
-                .eq(1)
-                .as("rowSelected")
-                .click();
+            it("should display at least one row", () => {
+                cy.get("@family-grid")
+                    .find("tbody tr")
+                    .should("be.visible");
+            });
 
-            cy.get("@rowSelected")
-                .should("have.class","table-success");
-        });
+            it("should display at least one column", () => {
+                cy.get("@family-grid")
+                    .find("thead tr th")
+                    .should("be.visible");
+            });
 
-        it("should download family json", () => {
-            cy.get("tbody tr:first > td")
-                .eq(-2)
-                .within(() => {
-                    cy.get("button")
-                        .click();
-                    cy.get("ul[class*='dropdown-menu']")
-                        .contains("a","Download JSON")
-                        .click();
+            it("should display column titles", () => {
+                cy.get("@family-grid")
+                    .find(`thead tr th div[class="th-inner "]`)
+                    .first()
+                    .should("not.be.empty");
             });
         });
-    });
 
-    context("extension", () => {
-        it("should display 'Extra Column' column", () => {
-            cy.get("thead th")
-                .contains("Extra column")
-                .should("be.visible");
+        context("extensions", () => {
+            it("should display 'Extra Column' column", () => {
+                cy.get("@family-grid")
+                    .find("thead th")
+                    .contains("Extra column")
+                    .should("be.visible");
+            });
         });
     });
 });
