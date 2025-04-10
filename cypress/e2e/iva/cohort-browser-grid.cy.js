76ba1f6c1c733b088506b7676ae335d14ac7a431
Josemi <josemijuanes@gmail.com>
2025-04-10 11:59:24 +0200

    test: Fix cohort-grid tests #TASK-7582 #TASK-7359

--- cypress/e2e/iva/cohort-browser-grid.cy.js
+++ cypress/e2e/iva/cohort-browser-grid.cy.js
@@ -14,264 +14,171 @@
  * limitations under the License.
  */
 
-import UtilsTest from "../../support/utils-test.js";
-import BrowserTest from "../../support/browser-test.js";
-
 context("Cohort Browser Grid", () => {
-    const browserGrid = "cohort-grid";
-
     beforeEach(() => {
         cy.visit("#cohort-browser-grid");
+        cy.get("cohort-grid")
+            .as("cohort-grid")
         cy.waitUntil(() => {
-            return cy.get(browserGrid)
+            return cy.get("@cohort-grid")
                 .should("be.visible");
         });
     });
 
-    // TOOLBAR
-    context("Cohort Toolbar", () => {
+    context("toolbar", () => {
         beforeEach(() => {
-            cy.get(browserGrid)
-                .find(`div[data-cy="toolbar"]`)
+            cy.get("@cohort-grid")
+                .find("opencb-grid-toolbar")
                 .as("toolbar");
         });
 
-        //1. Render the toolbar
-        context("render", () => {
-            // 1.1. It should render a div with the toolbar
-            it("should render toolbar", () => {
-                cy.get(browserGrid)
-                    .find(`div[data-cy="toolbar-wrapper"]`)
-                    .should("be.visible");
-            });
-            // 1.1. If configured, it should render a New button
-            it("should render New button", () => {
-                cy.get(browserGrid)
-                    .find(`button[data-action="create"]`)
-                    .should("be.visible");
-            });
+        it("should be visible", () => {
+            cy.get("@toolbar")
+                .should("be.visible");
+        });
+
+        it("should display the 'Create Cohort' button", () => {
+            cy.get("@toolbar")
+                .contains("button", "Create Cohort")
+                .should("be.visible");
+        });
+
+        it("should display the 'Settings' button", () => {
+            cy.get("@toolbar")
+                .contains("button", "Settings")
+                .should("be.visible");
         });
     });
 
-    // MODAL CREATE
-    context("Modal Create", () => {
+    context("create a cohort", () => {
         beforeEach(() => {
-            cy.get(browserGrid)
-                .find(`button[data-action="create"]`)
+            cy.get("@cohort-grid")
+                .contains("button", "Create Cohort")
                 .click();
-            cy.get(browserGrid)
-                .find(`div[data-cy="modal-create"]`)
-                .as("modal-create");
+            cy.get("@cohort-grid")
+                .find(`div[data-cy="cohort-create"]`)
+                .as("modal-cohort-create");
         });
-        // 1. Open modal and render create
-        it("should render create modal", () => {
-            cy.get("@modal-create")
+
+        it("should display the modal for creating a cohort when clicking the 'Create Cohort' button", () => {
+            cy.get("@modal-cohort-create")
                 .find("div.modal-dialog")
                 .should("be.visible");
         });
-        // 2. Render title
-        it("should render create title", () => {
-            cy.get("@modal-create")
+
+        it("should display the 'Create Cohort' title in the modal", () => {
+            cy.get("@modal-cohort-create")
                 .find("h4.modal-title")
                 .should("contain.text", "Create Cohort");
         });
-        // 3. Render button clear
-        it("should render button clear", () => {
-            cy.get("@modal-create")
-                .contains("button", "Clear")
-                .should("be.visible");
-        });
-        // 4. Render button create
-        it("should render button create", () => {
-            cy.get("@modal-create")
+
+        it("should display the 'Create' button in the modal", () => {
+            cy.get("@modal-cohort-create")
                 .contains("button", "Create")
                 .should("be.visible");
         });
-        // 5. Render tabs
-        it("should render form tabs", () => {
-            cy.get("@modal-create")
-                .find("ul.nav.nav-tabs > li")
-                .should("have.length.at.least", 1);
+
+        it("should display the 'Clear' button in the modal", () => {
+            cy.get("@modal-cohort-create")
+                .contains("button", "Clear")
+                .should("be.visible");
         });
-        // 6. Render Cohort ID
-        it("should have form field ID", () => {
-            cy.get("@modal-create")
-                .find(`data-form div.form-horizontal div.row div.col-md-3`)
+
+        it("should ask for a cohort ID in the form", () => {
+            cy.get("@modal-cohort-create")
+                .find("data-form")
                 .should("contain.text", "Cohort ID");
         });
     });
 
-    // MODAL UPDATE
-    // context("Modal Update", () => {
-    //     beforeEach(() => {
-    //         // eslint-disable-next-line cypress/unsafe-to-chain-command
-    //         cy.get(browserGrid)
-    //             .find(`table tbody tr td button.dropdown-toggle`)
-    //             .first()
-    //             .click();
-    //         cy.get(browserGrid)
-    //             .find(`a[data-action="edit"]`)
-    //             .first()
-    //             .click();
-    //         cy.get(browserGrid)
-    //             .find(`div[data-cy="modal-update"]`)
-    //             .as("modal-update");
-    //     });
-    //     // 1. Open modal and render update
-    //     it("should render update modal", () => {
-    //         // eslint-disable-next-line cypress/unsafe-to-chain-command
-    //         cy.get("@modal-update")
-    //             .find("div.modal-dialog")
-    //             .should("be.visible");
-    //     });
-    //     // 2. Render title
-    //     it("should render update title", () => {
-    //         // eslint-disable-next-line cypress/unsafe-to-chain-command
-    //         cy.get("@modal-update")
-    //             .find("h4.modal-title")
-    //             .should("contain.text", "Cohort Update");
-    //     });
-    //     // 3. Render button clear
-    //     it("should render button clear", () => {
-    //         // eslint-disable-next-line cypress/unsafe-to-chain-command
-    //         cy.get("@modal-update")
-    //             .contains('button', 'Discard Changes')
-    //             .should("be.visible");
-    //     });
-    //     // 4. Render button create
-    //     it("should render button create", () => {
-    //         // eslint-disable-next-line cypress/unsafe-to-chain-command
-    //         cy.get("@modal-update")
-    //             .contains('button', 'Update')
-    //             .should("be.visible");
-    //     });
-    //     // 5. Render tabs
-    //     it("should render form tabs", () => {
-    //         // eslint-disable-next-line cypress/unsafe-to-chain-command
-    //         cy.get("@modal-update")
-    //             .find("ul.nav.nav-tabs > li")
-    //             .should("have.length.greaterThan", 1);
-    //     });
-    //     // 6. Render Sample ID
-    //     it("should have form field ID equal to sample selected", () => {
-    //         // eslint-disable-next-line cypress/unsafe-to-chain-command
-    //         cy.get("@modal-update")
-    //             .find(`data-form div.row div.row.form-group  label.control-label`)
-    //             .should("contain.text", "Cohort ID");
-    //     });
-    // });
-
-    context("Modal Setting", () => {
-
-        it("should move modal setting", () => {
-            cy.get("button[data-action='settings']")
+    context("settings", () => {
+        it("should display the settings modal when clicking on the 'Settings' button", () => {
+            cy.get("@cohort-grid")
+                .find(`button[data-action="settings"]`)
                 .click();
-
-            BrowserTest.getElementByComponent({
-                selector: `${browserGrid} opencb-grid-toolbar`,
-                tag: "div",
-                elementId: "SettingModal"
-            }).as("settingModal");
-
-            cy.get("@settingModal")
-                .then(($modal) => {
-                    // const startPosition = $modal.position()
-                    const startPosition = $modal.offset();
-                    cy.log("start Position:", startPosition);
-                    // Drag the modal to a new position using Cypress's drag command
-                    cy.get("@settingModal")
-                        .find(".modal-header")
-                        .as("modalHeader");
-
-                    cy.get("@modalHeader")
-                        .trigger("mousedown", { which: 1 }); // Trigger mouse down event
-                    cy.get("@modalHeader")
-                        .trigger("mousemove", { clientX: 100, clientY: 100 }); // Move the mouse
-                    cy.get("@modalHeader")
-                        .trigger("mouseup"); // Release the mouse
-
-                    // Get the final position of the modal
-                    cy.get(`@modalHeader`)
-                        .then(($modal) => {
-                            // const finalPosition = $modal.position();
-                            const finalPosition = $modal.offset();
-                            cy.log("final Position:", finalPosition);
-
-                            // Assert that the modal has moved
-                            expect(finalPosition.left).to.not.equal(startPosition.left);
-                            expect(finalPosition.top).to.not.equal(startPosition.top);
-                        });
-                });
+            cy.get("div.modal-dialog")
+                .should("be.visible");
         });
 
-        it("should hide columns [Cohort ID,Creation Date]", () => {
+        it("should allow to hide columns in the grid", () => {
             const columns = ["Cohort ID", "Creation Date"];
-            cy.get(`${browserGrid} thead th`)
-                .as("headerColumns");
 
-            columns.forEach(col => {
+            cy.get("@cohort-grid")
+                .find("thead th")
+                .as("headerColumns");
+            columns.forEach(column => {
                 cy.get("@headerColumns")
-                    .contains("div", col)
+                    .contains("div", column)
                     .should("be.visible");
             });
-            cy.get("button[data-action='settings']")
+            cy.get("@cohort-grid")
+                .find(`button[data-action="settings"]`)
                 .click();
-            UtilsTest.getByDataTest("test-columns", "select-field-filter .select2-container")
+            cy.get("@cohort-grid")
+                .find(`div[data-testid="test-columns"] select-field-filter`)
+                .as("columnsSelector");
+            cy.get("@columnsSelector")
+                .find(".select2-container")
                 .click();
             columns.forEach(col => {
-                UtilsTest.getByDataTest("test-columns", "select-field-filter span.select2-results li")
+                cy.get("@columnsSelector")
+                    .find("span.select2-results li")
                     .contains(col)
                     .click();
             });
-            UtilsTest.getByDataTest("test-columns", "select-field-filter .select2-selection")
+            cy.get("@columnsSelector")
+                .find(".select2-selection")
                 .click();
-            BrowserTest.getElementByComponent({
-                selector: `${browserGrid} opencb-grid-toolbar`,
-                tag: "div",
-                elementId: "SettingModal"
-            }).as("settingModal");
-
-            cy.get("@settingModal")
+            cy.get("@cohort-grid")
+                .find(".modal-body")
                 .contains("button", "OK")
                 .click();
 
             cy.get("@headerColumns")
                 .should($header => {
-                    const _columns = Array.from($header, th => th.textContent.trim());
+                    const visibleColumns = Array.from($header, th => th.textContent.trim());
                     columns.forEach(col => {
-                        expect(col).not.to.be.oneOf(_columns);
+                        expect(col).not.to.be.oneOf(visibleColumns);
                     });
                 });
         });
     });
 
-    context("Grid", () => {
-        it("should render", () => {
-            cy.get(browserGrid)
-                .should("be.visible");
+    context("grid", () => {
+        context("content", () => {
+            it("should render a <table> element", () => {
+                cy.get("@cohort-grid")
+                    .find("table")
+                    .should("be.visible");
+            });
+
+            it("should display at least one row in the table", () => {
+                cy.get("@cohort-grid")
+                    .find("tbody tr")
+                    .should("be.visible");
+            });
+
+            it("should display at least one column in the table", () => {
+                cy.get("@cohort-grid")
+                    .find("thead tr th")
+                    .should("be.visible");
+            });
+
+            it("should display titles in of each column", () => {
+                cy.get("@cohort-grid")
+                    .find(`thead tr th div[class="th-inner "]`)
+                    .first()
+                    .should("not.be.empty");
+            });
         });
 
-        it("should change page", () => {
-            UtilsTest.changePage(browserGrid, 2);
-            UtilsTest.changePage(browserGrid, 3);
-        });
-    });
-
-    context("Row", () => {
-        it("should display row #3 as selected", () => {
-            // eslint-disable-next-line cypress/unsafe-to-chain-command
-            cy.get("tbody tr")
-                .eq(3)
-                .click()
-                .should("have.class", "table-success");
-        });
-    });
-
-    context("extension", () => {
-        it("should display 'Extra Column' column", () => {
-            cy.get("thead th")
-                .contains("Extra column")
-                .should("be.visible");
+        context("extensions", () => {
+            it("should display a 'Extra Column' column", () => {
+                cy.get("@cohort-grid")
+                    .find("thead th")
+                    .contains("Extra column")
+                    .should("be.visible");
+            });
         });
     });
 });
--- src/sites/test-app/webcomponents/cohort-browser-grid-test.js
+++ src/sites/test-app/webcomponents/cohort-browser-grid-test.js
@@ -15,11 +15,9 @@
  * limitations under the License.
  */
 
-import {html, LitElement} from "lit";
+import {html, LitElement, nothing} from "lit";
 import UtilsNew from "../../../core/utils-new.js";
 import "../../../webcomponents/cohort/cohort-grid.js";
-import "../../../webcomponents/cohort/cohort-update.js";
-import "../../../webcomponents/cohort/cohort-create.js";
 
 class CohortBrowserGridTest extends LitElement {
 
@@ -49,7 +47,6 @@ class CohortBrowserGridTest extends LitElement {
             "cohorts-1000G.json",
         ];
         this._data = null;
-        this._selectedRow = null;
         this._config = this.getDefaultConfig();
     }
 
@@ -70,7 +67,6 @@ class CohortBrowserGridTest extends LitElement {
             Promise.all(allPromises)
                 .then(data => {
                     this._data = data[0];
-                    this._selectedRow = this._data[0];
                     this.mutate();
                     this.requestUpdate();
                 })
@@ -92,30 +88,22 @@ class CohortBrowserGridTest extends LitElement {
         this.requestUpdate();
     }
 
-    onSelectRow(e) {
-        this._selectedRow = e.detail.row;
-        this.requestUpdate();
-    }
-
     render() {
         if (!this._data) {
-            return "Loading...";
+            return nothing;
         }
 
         return html`
-            <div data-cy="cohort-browser">
-                <h2 style="font-weight: bold;">
-                    Cohort Browser Grid (${this.FILES[0]})
-                </h2>
-                <cohort-grid
-                    .toolId="${this.COMPONENT_ID}"
-                    .cohorts="${this._data}"
-                    .opencgaSession="${this.opencgaSession}"
-                    .config="${this._config.grid}"
-                    @settingsUpdate="${() => this.onSettingsUpdate()}"
-                    @selectrow="${e => this.onSelectRow(e)}">
-                </cohort-grid>
-            </div>
+            <h2 class="fw-bold">
+                Cohort Browser Grid (${this.FILES[0]})
+            </h2>
+            <cohort-grid
+                .toolId="${this.COMPONENT_ID}"
+                .cohorts="${this._data}"
+                .opencgaSession="${this.opencgaSession}"
+                .config="${this._config.grid}"
+                @settingsUpdate="${() => this.onSettingsUpdate()}">
+            </cohort-grid>
         `;
     }
 
