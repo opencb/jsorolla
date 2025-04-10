f4fa05478ce4f69e3c3f2418f22e639cc789a547
Josemi <josemijuanes@gmail.com>
2025-04-10 14:26:19 +0200

    wc: Remove unused imports in clinical-analysis-grid #TASK-7582 #TASK-7359

--- src/webcomponents/clinical/clinical-analysis-grid.js
+++ src/webcomponents/clinical/clinical-analysis-grid.js
@@ -15,7 +15,6 @@
  */
 
 import {html, LitElement, nothing} from "lit";
-import OpencgaCatalogUtils from "../../core/clients/opencga/opencga-catalog-utils.js";
 import UtilsNew from "../../core/utils-new.js";
 import GridCommons from "../commons/grid-commons.js";
 import CatalogGridFormatter from "../commons/catalog-grid-formatter.js";
