698d80b9fe1d5e8b1e537f8b873f4ae1b5e0272c
Josemi <josemijuanes@gmail.com>
2025-04-24 11:43:16 +0200

    wc: Using new rearrangement view in interpreter browser template #TASK-7489 #TASK-7359

--- src/webcomponents/variant/interpretation/variant-interpreter-browser-template.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-browser-template.js
@@ -24,6 +24,8 @@ import Region from "../../../core/bioinfo/region.js";
 import "./variant-interpreter-browser-toolbar.js";
 import "./variant-interpreter-grid.js";
 import "./variant-interpreter-view.js";
+import "./variant-interpreter-rearrangement-grid.js";
+import "./variant-interpreter-rearrangement-view.js";
 import "../variant-browser-filter.js";
 import "../../commons/tool-header.js";
 import "../../commons/interpreter-ai.js";
@@ -448,14 +450,21 @@ class VariantInterpreterBrowserTemplate extends LitElement {
                     }
                     <!-- Bottom tabs with detailed variant information -->
                     ${this.variant ? html`
-                        <variant-interpreter-view
-                            .opencgaSession="${this.opencgaSession}"
-                            .clinicalAnalysis="${this.clinicalAnalysis}"
-                            .toolId="${this.toolId}"
-                            .variant="${this.variant}"
-                            .cellbaseClient="${this.cellbaseClient}"
-                            .config="${this._config.filter.detail}">
-                        </variant-interpreter-view>
+                        ${!this._config.filter.result.grid.isRearrangement ? html`
+                            <variant-interpreter-view
+                                .opencgaSession="${this.opencgaSession}"
+                                .clinicalAnalysis="${this.clinicalAnalysis}"
+                                .toolId="${this.toolId}"
+                                .variant="${this.variant}">
+                            </variant-interpreter-view>
+                        ` : html`
+                            <variant-interpreter-rearrangement-view
+                                .opencgaSession="${this.opencgaSession}"
+                                .clinicalAnalysis="${this.clinicalAnalysis}"
+                                .toolId="${this.toolId}"
+                                .variants="${this.variant}">
+                            </variant-interpreter-rearrangement-view>
+                        `}
                     ` : nothing}
                 </div>
                 <!-- Genome browser view -->
