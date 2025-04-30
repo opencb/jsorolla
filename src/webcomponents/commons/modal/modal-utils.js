8c94cfa528aeb431c1c6fd81e4224491c3618f5c
Josemi <josemijuanes@gmail.com>
2025-04-30 13:41:56 +0200

    wc: Fix initial position of modal when it is draggable #TASK-7582 #TASK-7359

--- src/webcomponents/commons/modal/modal-utils.js
+++ src/webcomponents/commons/modal/modal-utils.js
@@ -5,10 +5,11 @@ export default class ModalUtils {
 
     static show(id) {
         const modalElm = document.querySelector(`#${id}`);
+        // note: first we need to show the modal before we can set the draggable
+        $(`#${id}`).modal("show");
         if (modalElm.dataset?.draggable === "true") {
             ModalUtils.draggableModal(modalElm);
         }
-        $(`#${id}`).modal("show");
     }
 
     static close(id) {
@@ -39,7 +40,7 @@ export default class ModalUtils {
         };
 
         return html`
-            <div class="modal fade ${modalContainerClass}" id="${id}" tabindex="-1" data-draggable="${modalDraggable}" data-cy="${modalCyName}">
+            <div class="modal ${modalContainerClass}" id="${id}" tabindex="-1" data-draggable="${modalDraggable}" data-cy="${modalCyName}">
                 <div class="modal-dialog ${modalSize}" style="${modalStyle}">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -79,8 +80,9 @@ export default class ModalUtils {
         const modalHeader = modalElm.querySelector(".modal-header");
 
         if (modalDialog) {
+            const modalSize = modalDialog.getBoundingClientRect();
             modalDialog.style.margin = "0";
-            modalDialog.style.left = (window.innerWidth * 0.30) + "px";
+            modalDialog.style.left = ((window.innerWidth - (modalSize?.width || 0)) * 0.50) + "px";
             modalDialog.style.top = (window.innerHeight * 0.05) + "px";
         }
 
