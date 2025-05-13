4945cbc4e60a78a133125f4030214a02f885b539
Josemi <josemijuanes@gmail.com>
2025-05-13 14:18:10 +0200

    iva: Update opencga host #TASK-7664 #TASK-7516

--- src/sites/iva/conf/config.js
+++ src/sites/iva/conf/config.js
@@ -26,7 +26,7 @@ const hosts = [
     },
     {
         id: "reference",
-        url: "https://demo.app.zettagenomics.com/opencga"
+        url: "https://test.app.zettagenomics.com/task-7516/opencga"
     },
 ];
 
