c6586dc4ad58172887a6f8726bf8a794a1db6f18
Josemi <josemijuanes@gmail.com>
2025-05-16 12:26:32 +0200

    wc: Using new custom filename filter in file-browser #TASK-7664 #TASK-7516

--- src/webcomponents/file/file-browser.js
+++ src/webcomponents/file/file-browser.js
@@ -19,6 +19,7 @@ import {LitElement, html, nothing} from "lit";
 import UtilsNew from "../../core/utils-new.js";
 import "../commons/opencga-browser.js";
 import "../commons/aggregation-stats.js";
+import "../commons/filters/filename-filter.js";
 import "./file-grid.js";
 import "./file-tree.js";
 
@@ -204,10 +205,19 @@ export default class FileBrowser extends LitElement {
                         filters: [
                             {
                                 id: "name",
-                                title: "File Name",
+                                title: "File",
                                 type: "string",
                                 placeholder: "accepted_hits.bam, phenotypes.vcf...",
                                 description: "",
+                                render: (onFilterChange, query, opencgaSession) => {
+                                    return html`
+                                        <filename-filter
+                                            .opencgaSession="${opencgaSession}"
+                                            .query="${query}"
+                                            @filterChange="${event => onFilterChange(event.detail.field, event.detail.value)}">
+                                        </filename-filter>
+                                    `;
+                                },
                                 quick: true,
                             },
                             {
