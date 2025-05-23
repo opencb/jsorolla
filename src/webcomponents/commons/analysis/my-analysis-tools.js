954d14ca2cb82eb65ac3622b2c0550a9cf4ea41d
Josemi <josemijuanes@gmail.com>
2025-05-23 14:29:09 +0200

    wc: Initialize my analysis tools component #TASK-7689 #TASK-7610

---
+++ src/webcomponents/commons/analysis/my-analysis-tools.js
@@ -0,0 +1,109 @@
+import {LitElement, html, nothing, render} from "lit";
+import "../tool-header.js";
+import "../view/vertical-menu.js";
+import "../../job/analysis/tool-analysis.js";
+import "../../job/analysis/custom-tool-builder.js";
+import "../../workflow/analysis/workflow-analysis.js";
+
+export default class MyAnalysisTools extends LitElement {
+
+    constructor() {
+        super();
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            opencgaSession: {
+                type: Object,
+            },
+        };
+    }
+
+    #init() {
+        this._config = this.getDefaultConfig();
+    }
+
+    render() {
+        return html`
+            <tool-header .title="${this._config.title}"></tool-header>
+            <vertical-menu
+                .opencgaSession="${this.opencgaSession}"
+                .config="${this._config || {}}">
+            </vertical-menu>
+        `;
+    }
+
+    getDefaultConfig() {
+        return {
+            title: "My Analysis Tools",
+            display: {
+                contentClassName: "mx-auto",
+                contentStyle: "max-width:920px;",
+                menuStyle: "width:240px",
+            },
+            menu: [
+                {
+                    id: "custom-tools",
+                    name: "Custom Tools",
+                    submenu: [
+                        {
+                            id: "tool-analysis",
+                            name: "Tool Executor",
+                            render: opencgaSession => html`
+                                <tool-analysis
+                                    .opencgaSession="${opencgaSession}">
+                                </tool-analysis>
+                            `,
+                        },
+                        {
+                            id: "custom-tool-builder",
+                            name: "Custom Tool Builder",
+                            render: opencgaSession => html`
+                                <custom-tool-builder
+                                    .opencgaSession="${opencgaSession}">
+                                </custom-tool-builder>
+                            `,
+                        },
+                    ],
+                },
+                {
+                    id: "workflows",
+                    name: "Workflows",
+                    submenu: [
+                        {
+                            id: "workflow-analysis",
+                            name: "Workflow Executor",
+                            render: opencgaSession => html`
+                                <workflow-analysis
+                                    .opencgaSession="${opencgaSession}">
+                                </workflow-analysis>
+                            `,
+                        },
+                    ],
+                },
+                {
+                    id: "variant-walker",
+                    name: "Variant Walker",
+                    submenu: [
+                        {
+                            id: "variant-walker",
+                            name: "Custom Variant Walker",
+                            description: "",
+                            render: opencgaSession => html`
+                                <div>Variant Walker tool</div>
+                            `,
+                        },
+                    ],
+                },
+            ],
+        };
+    }
+
+}
+
+customElements.define("my-analysis-tools", MyAnalysisTools);
