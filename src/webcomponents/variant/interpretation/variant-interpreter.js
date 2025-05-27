44c5c943e47b6a002ac86073ead6bb6509c53784
Josemi <josemijuanes@gmail.com>
2025-05-27 17:44:12 +0200

    wc: Using new report component in variant-interpreter #TASK-7707 #TASK-7706

--- src/webcomponents/variant/interpretation/variant-interpreter.js
+++ src/webcomponents/variant/interpretation/variant-interpreter.js
@@ -35,6 +35,7 @@ import "../../download-button.js";
 import "../../loading-spinner.js";
 import "../../clinical/clinical-analysis-review.js";
 import "../../clinical/interpretation/clinical-interpretation-update.js";
+import "../../clinical/report/clinical-analysis-report.js";
 
 class VariantInterpreter extends LitElement {
 
@@ -347,11 +348,17 @@ class VariantInterpreter extends LitElement {
                                 <tool-header
                                     title="Interpretation - ${this.clinicalAnalysis?.interpretation?.id}">
                                 </tool-header>
+                                <!--
                                 <clinical-analysis-review
                                     @clinicalAnalysisUpdate="${e => this.onClinicalAnalysisUpdate(e)}"
                                     .clinicalAnalysis="${this.clinicalAnalysis}"
                                     .opencgaSession="${this.opencgaSession}">
                                 </clinical-analysis-review>
+                                -->
+                                <clinical-analysis-report
+                                    .opencgaSession="${this.opencgaSession}"
+                                    .clinicalAnalysis="${this.clinicalAnalysis}">
+                                </clinical-analysis-report>
                             </div>
                         </div>
                     `;
