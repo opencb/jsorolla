23680084223fcf7f0723172ec0a71462c5d522ae
Josemi <josemijuanes@gmail.com>
2025-05-29 12:14:01 +0200

    clients: Remove unused methods and some fixes #TASK-7710

--- src/core/clients/cellbase/cellbase-client.js
+++ src/core/clients/cellbase/cellbase-client.js
@@ -15,7 +15,6 @@
  */
 
 import RestClient from "../rest-client.js";
-import UtilsNew from "../../utils-new.js";
 
 export class CellBaseClient {
 
@@ -25,104 +24,61 @@ export class CellBaseClient {
         this.check();
     }
 
-    async check() {
-        const globalEvent = (type, value) => {
-            globalThis.dispatchEvent(
-                new CustomEvent(type, {
-                    detail: value
-                }));
+    check() {
+        const dispatchGlobalEvent = (type, value) => {
+            globalThis.dispatchEvent(new CustomEvent(type, {
+                detail: {
+                    host: "cellbase",
+                    value: value,
+                },
+            }));
         };
+
         if (!this._config?.host) {
-            globalEvent("hostInit", {
-                host: "cellbase",
-                value: "NOT DEFINED"
-            });
+            dispatchGlobalEvent("hostInit", "NOT DEFINED");
         } else {
             this.getMeta("about")
                 .then(response => {
                     const result = response?.response?.[0]?.result[0];
                     // Older versions of cellbase are using 'Version: ' as the key instead of 'Version' (Issue #185).
                     // To keep compatibility, we will check for both keys, but in the future only the newest key will be used.
-                    globalEvent("hostInit", {
-                        host: "cellbase",
-                        value: "v" + (result["Version"] || result["Version: "]),
-                    });
+                    dispatchGlobalEvent("hostInit", "v" + (result["Version"] || result["Version: "]));
                 })
-                .catch(e => {
-                    console.error(e);
-                    // globalEvent("signingInError", {value: "Cellbase host not available."});
-                    globalEvent("hostInit", {
-                        host: "cellbase",
-                        value: "NOT AVAILABLE"
-                    });
+                .catch(error => {
+                    console.error(error);
+                    dispatchGlobalEvent("hostInit", "NOT AVAILABLE");
                 });
         }
     }
 
-    /*
-     * This method has been implemented to be backward compatible with old cellbase-manager.js
-     */
-    getOldWay(args) {
-        this._config.species = args.species.id || this._config.species;
-        return this.get(args.category, args.subcategory, args.id, args.resource, args.params, args.options);
-    }
-
     getMeta(param, options = {}) {
-        const host = options.host || this._config.host;
+        let host = options.host || this._config.host;
         const version = options.version || this._config.version;
 
-        let url;
+        // Remove trailing '/' in the host
+        if (host.endsWith("/")) {
+            host = host.slice(0, -1);
+        }
+
+        let url = `${host}/webservices/rest/${version}/meta/${param}`;
+
+        // If the param is 'dataReleases', we need to add the species to the URL
         if (param === "dataReleases") {
-            // We need to add the species
             url = `${host}/webservices/rest/${version}/meta/${this._config.species}/${param}`;
-        } else {
-            url = `${host}/webservices/rest/${version}/meta/${param}`;
         }
 
         // By default, we assume https protocol instead of http
         if (!url.startsWith("http://") && !url.startsWith("https://")) {
-            url = `http://${url}`;
+            url = `https://${url}`;
         }
 
-        // Append search params
+        // append search params to the url
         const searchParams = this._createSearchParams({});
         if (searchParams) {
             url += `?${searchParams}`;
         }
 
-        // options.error = function() {
-        //     if (++count < hosts.length) {
-        //         // we need a new URL
-        //         url = "http://" + hosts[count] + "/webservices/rest/" + version + "/" + "meta" + "/" + param;
-        //         response = this.restClient.call(url, options);
-        //     } else {
-        //         userError(this);
-        //     }
-        // };
-        // response = this.restClient.call(url, options);
-        const k = this.generateKey(param);
-        return this.restClient.call(url, options, k);
-    }
-
-    getFiles(folderId, resource, params, options = {}) {
-        const host = options.host || this._config.host;
-        const version = options.version || this._config.version;
-        const species = options.species || this._config.species;
-        let url = `http://${host}/webservices/rest/${version}/${species}/` + "files";
-
-        if (typeof folderId !== "undefined" && folderId !== null && folderId !== "") {
-            url += `/${folderId}/${resource}`;
-        } else {
-            url += `/${resource}`;
-        }
-
-        // We add the query params formatted in URL
-        const queryParamsUrl = this._createSuffixKey(params, false);
-        if (typeof queryParamsUrl !== "undefined" && queryParamsUrl !== null && queryParamsUrl !== "") {
-            url += `?${queryParamsUrl}`;
-        }
-        const k = this.generateKey(params);
-        return this.restClient.call(url, options, k);
+        return this.restClient.call(url, options);
     }
 
     getGeneClient(id, resource, params, options) {
@@ -156,15 +112,18 @@ export class CellBaseClient {
 
     _createRestUrl(host, version, species, category, subcategory, ids, resource, params) {
         let _host = host;
+
         // Remove trailing '/'
         if (_host.endsWith("/")) {
             _host = _host.slice(0, -1);
         }
 
-        // By default we assume https protocol instead of http
+        // initialize the URL with the host, version, and species
         let url = _host + `/webservices/rest/${version}/${species}/`;
+
+        // By default we assume https protocol instead of http
         if (!url.startsWith("https://") && !url.startsWith("http://")) {
-            url = `https://${_host}/webservices/rest/${version}/${species}/`;
+            url = `https://${url}`;
         }
 
         // Some web services do not need IDs
@@ -182,29 +141,14 @@ export class CellBaseClient {
         return url;
     }
 
-    _createSuffixKey(params, suffix) {
-        // Do not remove the sort! we need to sort the array to ensure that the key of the cache will be correct
-        const keyArray = Object.keys(params || {}).sort();
-        const keyValueArray = [];
-        for (const i in keyArray) {
-            if (Object.prototype.hasOwnProperty.call(keyArray, i)) {
-                keyValueArray.push(`${keyArray[i]}=${encodeURIComponent(params[keyArray[i]])}`);
-            }
-        }
-        let suffixKey = keyValueArray.join("&");
-        // suffixKey is preceded by '_' if suffix is true. Else it is treated as queryParam that needs to be sorted
-        if (suffix && suffixKey !== "") {
-            suffixKey = `_${suffixKey}`;
-        }
-        return suffixKey;
-    }
-
     _createSearchParams(params = {}) {
         const searchParams = new URLSearchParams(params);
+
         // check if apiKey is defined in the configuration
         if (this._config.apiKey) {
             searchParams.set("apiKey", this._config.apiKey);
         }
+
         // generate the search params string
         return searchParams.toString();
     }
@@ -218,12 +162,6 @@ export class CellBaseClient {
             ...this.getDefaultConfig(),
             ...config,
         };
-    }
-
-    generateKey(params) {
-        // params is added to the key to avoid unwanted request abort.
-        // We can do it because we don't have tables in IVA that queries Cellbase.
-        return `${new Error().stack.split("\n    at ").slice(0, 5).join("|") + JSON.stringify(params)}`;
     }
 
     getDefaultConfig() {
