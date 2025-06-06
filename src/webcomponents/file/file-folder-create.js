6e7641fb5362f307fe1681a2f15980596c6ec5d5
Josemi <josemijuanes@gmail.com>
2025-06-06 12:32:30 +0200

    wc: Allow to add a description and tags when creating a folder #TASK-7516

--- src/webcomponents/file/file-folder-create.js
+++ src/webcomponents/file/file-folder-create.js
@@ -18,6 +18,7 @@ import {html, LitElement} from "lit";
 import LitUtils from "../commons/utils/lit-utils.js";
 import NotificationUtils from "../commons/utils/notification-utils.js";
 import "../commons/forms/data-form.js";
+import "../commons/filters/catalog-distinct-autocomplete.js";
 
 export default class FileFolderCreate extends LitElement {
 
@@ -96,6 +97,7 @@ export default class FileFolderCreate extends LitElement {
         const {name, ...otherFileData} = this._folder;
         const data = {
             ...otherFileData,
+            tags: otherFileData.tags ? otherFileData.tags.split(",").map(t => t.trim()) : [],
             path: `${this.path || ""}${name}`,
         };
 
@@ -164,6 +166,35 @@ export default class FileFolderCreate extends LitElement {
                             type: "input-text",
                             display: {
                                 helpMessage: "Name of the folder to be created.",
+                            },
+                        },
+                        {
+                            title: "Tags",
+                            field: "tags",
+                            type: "custom",
+                            display: {
+                                render: (tags, onFilterChange) => html`
+                                    <catalog-distinct-autocomplete
+                                        .opencgaSession="${this.opencgaSession}"
+                                        .resource="${"FILE"}"
+                                        .value="${(tags || []).join(",")}"
+                                        .queryField="${"tags"}"
+                                        .distinctFields="${"tags"}"
+                                        .config="${{
+                                            freeTag: true,
+                                        }}"
+                                        @filterChange="${event => onFilterChange(event.detail.value)}">
+                                    </catalog-distinct-autocomplete>
+                                `,
+                            },
+                        },
+                        {
+                            title: "Description",
+                            field: "description",
+                            type: "input-text",
+                            display: {
+                                rows: 3,
+                                helpMessage: "Description of the folder.",
                             },
                         },
                     ],
