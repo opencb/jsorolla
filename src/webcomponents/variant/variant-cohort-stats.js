c2636e88375e97a0ec1e44a9659a493ce93275a3
imedina <igmecas@gmail.com>
2025-06-06 02:07:02 +0100

    wc: add stats from other Projects

--- src/webcomponents/variant/variant-cohort-stats.js
+++ src/webcomponents/variant/variant-cohort-stats.js
@@ -15,7 +15,6 @@
  */
 
 import {html, LitElement} from "lit";
-import UtilsNew from "../../core/utils-new.js";
 import "./variant-cohort-stats-grid.js";
 
 export default class VariantCohortStats extends LitElement {
@@ -51,30 +50,32 @@ export default class VariantCohortStats extends LitElement {
     }
 
     #init() {
-        this._prefix = UtilsNew.randomString(8);
         this.active = false;
         this.studyNames = {};
+        this.projectToStudyStats = {};
     }
 
     update(changedProperties) {
         if (changedProperties.has("opencgaSession")) {
             this.opencgaSessionObserver();
         }
-
         if (changedProperties.has("variantId") || changedProperties.has("active")) {
             this.variantIdObserver();
         }
-
+        if (changedProperties.has("variant") || changedProperties.has("active")) {
+            this.variantObserver();
+        }
         super.update(changedProperties);
     }
 
     opencgaSessionObserver() {
         this.studyNames = {};
-        if (this.opencgaSession?.project?.studies) {
-            this.opencgaSession.project.studies.forEach(study => {
-                this.studyNames[study.id] = study.name;
-                this.studyNames[study.fqn] = study.name;
-            });
+        if (this.opencgaSession?.projects) {
+            for (const project of this.opencgaSession?.projects) {
+                (project.studies || []).forEach(study => {
+                    this.studyNames[study.fqn] = study.name;
+                });
+            }
         }
     }
 
@@ -90,13 +91,43 @@ export default class VariantCohortStats extends LitElement {
                 })
                 .then(response => {
                     if (response.responses[0].results[0]) {
+                        this.projectToStudyStats[this.opencgaSession.project.name] = response.responses[0].results[0];
                         this.variant = response.responses[0].results[0];
-                        // this.requestUpdate();
                     }
                 })
                 .catch(error => {
                     console.error(error);
                 });
+        }
+    }
+
+    variantObserver() {
+        if (this.variant && this.active) {
+            // Store the stats for the current project
+            this.projectToStudyStats[this.opencgaSession.project.name] = this.variant;
+
+            // Fetch stats for the other projects
+            for (const project of this.opencgaSession.projects) {
+                if (project.id !== this.opencgaSession.project.id && project.studies?.length > 0) {
+                    this.opencgaSession.opencgaClient.variants()
+                        .query({
+                            id: this.variant.id,
+                            study: project.studies[0].fqn,
+                            includeStudy: "all",
+                            exclude: "annotation,studies.files,studies.samples,studies.scores,studies.issues",
+                            useSearchIndex: "no",
+                        })
+                        .then(response => {
+                            if (response.responses[0].results[0]) {
+                                this.projectToStudyStats[project.name] = response.responses[0].results[0];
+                                this.requestUpdate();
+                            }
+                        })
+                        .catch(error => {
+                            console.error(error);
+                        });
+                }
+            }
         }
     }
 
@@ -111,14 +142,22 @@ export default class VariantCohortStats extends LitElement {
         }
 
         return html`
-            ${this.variant.studies.map(study => html`
-                <h3>${this.studyNames[study.studyId] || ""}</h3>
-                <div style="">
-                    <variant-cohort-stats-grid
-                        .stats="${study.stats}">
-                    </variant-cohort-stats-grid>
-                </div>
-            `)}
+            <div>
+                ${Object.keys(this.projectToStudyStats).map(
+                    projectName => html`
+                        <h3 class="pt-2">Project: ${projectName}</h3>
+                        <hr class="my-1">
+                        ${this.projectToStudyStats[projectName].studies.map(study => html`
+                            <div class="p-2">
+                                <h4 class="py-1">${this.studyNames[study.studyId] || ""}</h4>
+                                <variant-cohort-stats-grid
+                                    .stats="${study.stats}">
+                                </variant-cohort-stats-grid>
+                            </div>
+                        `)}
+                    `
+                )}
+            </div>
         `;
     }
 
