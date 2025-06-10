60532504b2ef9bd8a6cab764d80453594c5b6afc
Josemi <josemijuanes@gmail.com>
2025-06-10 12:50:57 +0200

    wc: Add listener for variant select event in new variant-review #TASK-7705 #TASK-7645

--- src/webcomponents/variant/interpretation/variant-interpreter-grid.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-grid.js
@@ -189,6 +189,8 @@ export default class VariantInterpreterGrid extends LitElement {
                         .opencgaSession="${this.opencgaSession}"
                         .clinicalAnalysis="${this.clinicalAnalysis}"
                         .variant="${this._selectedVariant}"
+                        .selected="${this.checkedVariants.has(this._selectedVariant.id)}"
+                        @selectChange="${event => this.onVariantSelectChange(event)}"
                         @variantChange="${event => {
                             // TODO
                         }}">
@@ -1561,6 +1563,34 @@ export default class VariantInterpreterGrid extends LitElement {
     onVariantCurate(event, row) {
         this._selectedVariant = UtilsNew.objectClone(row);
         this.gridCommons.changeActiveModal("review-variant");
+    }
+
+    onVariantSelectChange(event) {
+        console.log(event);
+        const variantId = event.detail.variant.id;
+
+        // NOTE Josemi 20221121: we will check first if this variant is in the primaryFindings list
+        // If not, we will get the variant from the rows list
+        let variant = (this.clinicalAnalysis?.interpretation?.primaryFindings || []).find(item => item.id === variantId);
+        if (!variant) {
+            variant = this._rows.find(row => row.id === variantId);
+        }
+
+        if (event.detail.selected) {
+            // Add current filter executed when variant is checked
+            variant.filters = {...this.filters};
+            this.checkedVariants.set(variantId, variant);
+        } else {
+            this.checkedVariants.delete(variantId);
+        }
+
+        LitUtils.dispatchCustomEvent(this, "checkrow", null, {
+            id: variantId,
+            row: variant,
+            checked: event.detail.selected,
+            rows: Array.from(this.checkedVariants.values())
+        });
+        this.requestUpdate();
     }
 
     renderToolbarLeftContent() {
