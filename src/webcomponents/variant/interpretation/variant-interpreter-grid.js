17147f79ee4a4001f899c082d8fa81bad89adbf0
Josemi <josemijuanes@gmail.com>
2025-06-10 16:48:55 +0200

    wc: Do not close variant review modal when clicking outside the modal #TASK-7705 #TASK-7645

--- src/webcomponents/variant/interpretation/variant-interpreter-grid.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-grid.js
@@ -183,6 +183,7 @@ export default class VariantInterpreterGrid extends LitElement {
                     buttonsVisible: true,
                     buttonCancelText: "Cancel",
                     buttonSaveText: "Save",
+                    backdrop: "static",
                 },
                 render: () => html`
                     <variant-review
