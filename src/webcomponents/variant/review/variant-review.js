6eb61e15c140a24fdb33040bf9acd0ec63e2a6a1
Josemi <josemijuanes@gmail.com>
2025-06-11 15:47:31 +0200

    wc: Restore internal selected state in variant-review #TASK-7705 #TASK-7645

--- src/webcomponents/variant/review/variant-review.js
+++ src/webcomponents/variant/review/variant-review.js
@@ -54,6 +54,7 @@ export default class VariantReview extends LitElement {
             "HIGH",
         ];
         this._variant = null;
+        this._selected = false;
         this._updateParams = {};
         this._config = this.getDefaultConfig();
     }
@@ -65,6 +66,10 @@ export default class VariantReview extends LitElement {
 
         if (changedProperties.has("variant")) {
             this.variantObserver();
+        }
+
+        if (changedProperties.has("selected")) {
+            this._selected = !!this.selected;
         }
 
         if (changedProperties.has("displayConfig") || changedProperties.has("selected")) {
@@ -98,6 +103,7 @@ export default class VariantReview extends LitElement {
 
     dispatchChange() {
         LitUtils.dispatchCustomEvent(this, "variantChange", null, {
+            selected: this._selected,
             variant: {
                 ...this._variant,
                 comments: (this._variant?.comments || []).map(comment => ({
@@ -109,10 +115,10 @@ export default class VariantReview extends LitElement {
     }
 
     onSelectChange() {
-        LitUtils.dispatchCustomEvent(this, "selectChange", null, {
-            selected: !this.selected,
-            variant: this._variant,
-        });
+        this._selected = !this._selected;
+        this._config = this.getDefaultConfig();
+        this.dispatchChange();
+        this.requestUpdate();
     }
 
     onStatusChange(event) {
@@ -148,16 +154,16 @@ export default class VariantReview extends LitElement {
 
     renderVariantSelect() {
         return html`
-            <div class="alert ${this.selected ? "alert-primary" : "alert-light"} d-flex align-items-center justify-content-between gap-2 flex-grow-1">
+            <div class="alert ${this._selected ? "alert-primary" : "alert-light"} d-flex align-items-center justify-content-between gap-2 flex-grow-1">
                 <label class="form-label mb-0 fw-bold">
-                    ${this.selected ? html`
+                    ${this._selected ? html`
                         <span>This Variant is on the <b>Primary Findings</b> of the Interpretation.</span>    
                     ` : html`
                         <span>Select this Variant to add it to the <b>Primary Findings</b> of the Interpretation.</span>
                     `}
                 </label>
-                <button class="btn btn-sm ${this.selected ? "btn-primary" : "btn-light"} rounded-2" @click="${() => this.onSelectChange()}">
-                    <i class="fa fa-check lh-1 ${this.selected ? "opacity-100" : "opacity-25 text-secondary"}"></i>
+                <button class="btn btn-sm ${this._selected ? "btn-primary" : "btn-light"} rounded-2" @click="${() => this.onSelectChange()}">
+                    <i class="fa fa-check lh-1 ${this._selected ? "opacity-100" : "opacity-25 text-secondary"}"></i>
                 </button>
             </div>
         `;
@@ -168,7 +174,7 @@ export default class VariantReview extends LitElement {
             <div class="alert alert-light d-flex align-items-center">
                 <div class="d-flex align-items-center gap-2">
                     <label class="form-label mb-0 fw-bold">Status</label>
-                    <select class="form-select form-select-sm" ?disabled="${!this.selected}" @change="${event => this.onStatusChange(event)}">
+                    <select class="form-select form-select-sm" ?disabled="${!this._selected}" @change="${event => this.onStatusChange(event)}">
                         ${this.STATUS_VALUES.map(status => html`
                             <option value="${status}" ?selected="${this._variant?.status === status}">
                                 ${status}
@@ -178,7 +184,7 @@ export default class VariantReview extends LitElement {
                 </div>
                 <div class="d-flex align-items-center gap-2 ms-3">
                     <label class="form-label mb-0 fw-bold">Confidence</label>
-                    <select class="form-select form-select-sm" ?disabled="${!this.selected}" @change="${event => this.onConfidenceChange(event)}">
+                    <select class="form-select form-select-sm" ?disabled="${!this._selected}" @change="${event => this.onConfidenceChange(event)}">
                         ${this.CONFIDENCE_VALUES.map(confidence => html`
                             <option value="${confidence}" ?selected="${this._variant?.confidence?.value === confidence}">
                                 ${confidence}
@@ -244,7 +250,7 @@ export default class VariantReview extends LitElement {
                             type: "input-text",
                             field: "discussion.text",
                             display: {
-                                disabled: () => !this.selected,
+                                disabled: () => !this._selected,
                                 placeholder: "Add your discussion here...",
                                 rows: 10,
                             },
@@ -261,7 +267,7 @@ export default class VariantReview extends LitElement {
                             field: "comments",
                             type: "object-list",
                             display: {
-                                disabled: () => !this.selected,
+                                disabled: () => !this._selected,
                                 style: "border-left: 2px solid #0c2f4c; padding-left: 12px; margin-bottom:24px",
                                 showAddBatchListButton: false,
                                 showEditItemListButton: false,
