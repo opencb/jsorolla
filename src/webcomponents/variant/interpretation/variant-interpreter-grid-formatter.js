9319d057d725c2cb98221be01e52fa10662ce529
Josemi <josemijuanes@gmail.com>
2025-06-25 16:49:29 +0200

    wc: Fix size of review button in interpreter grid #TASK-7705 #TASK-7645

--- src/webcomponents/variant/interpretation/variant-interpreter-grid-formatter.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-grid-formatter.js
@@ -1032,7 +1032,7 @@ export default class VariantInterpreterGridFormatter {
             <div>
                 ${config?.showEditReview ? `
                     <div class="d-flex justify-content-center">
-                        <button class="d-flex align-items-center btn btn-sm ${checked ? "btn-primary" : "btn-light"} ${disabled ? "disabled" : ""}" data-variant="${variant.id}">
+                        <button class="d-flex align-items-center btn ${checked ? "btn-primary" : "btn-light"} ${disabled ? "disabled" : ""}" data-variant="${variant.id}">
                             <i class="fa fa-edit pe-2"></i>
                             <span>Review</span>
                         </button>
