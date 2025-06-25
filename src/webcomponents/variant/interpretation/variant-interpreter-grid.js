9c7b6ed5e67a51adc781124738d381e28cbcf1fb
Josemi <josemijuanes@gmail.com>
2025-06-25 14:11:08 +0200

    wc: Using new review formatter for the review column in interpreter grid #TASK-7705 #TASK-7645

--- src/webcomponents/variant/interpretation/variant-interpreter-grid.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-grid.js
@@ -1049,9 +1049,7 @@ export default class VariantInterpreterGrid extends LitElement {
                     rowspan: 1,
                     colspan: 1,
                     formatter: (value, row) => {
-                        return `
-                            <button class="btn btn-light btn-sm">Review</button>
-                        `;
+                        return VariantInterpreterGridFormatter.newReviewFormatter(row, this.clinicalAnalysis, this.checkedVariants, this._config);
                     },
                     align: "center",
                     events: {
