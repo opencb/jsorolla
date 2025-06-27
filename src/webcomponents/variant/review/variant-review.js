130036e57b09314fc654b6e5c7eac0748f094841
Josemi <josemijuanes@gmail.com>
2025-06-27 13:22:16 +0200

    wc: Fix align of items in variant-review mini summary #TASK-7705 #TASK-7645

--- src/webcomponents/variant/review/variant-review.js
+++ src/webcomponents/variant/review/variant-review.js
@@ -226,12 +226,12 @@ export default class VariantReview extends LitElement {
         }
 
         return html`
-            <div class="alert alert-light flex-grow-1">
-                <div class="">
-                    <span class="fw-bold lh-1">${this._variant.id}</span>
+            <div class="alert alert-light flex-grow-1 d-flex justify-content-center flex-column">
+                <div class="lh-1">
+                    <span class="fw-bold">${this._variant.id}</span>
                 </div>
                 ${consequenceTypes.length > 0 ? html`
-                    <div class="d-flex align-items-center flex-wrap column-gap-2" style="max-width:900px;">
+                    <div class="mt-1 d-flex align-items-center flex-wrap column-gap-2" style="max-width:900px;">
                         ${consequenceTypes}
                     </div>
                 ` : nothing}
