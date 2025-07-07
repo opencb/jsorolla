6c239e7106787aadf3c155068bbd563019274e5f
Josemi <josemijuanes@gmail.com>
2025-07-07 12:37:46 +0200

    wc: Using new updateVariants method of clinical analysis manager in interpreter browser template #TASK-7705 #TASK-7645

--- src/webcomponents/variant/interpretation/variant-interpreter-browser-template.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-browser-template.js
@@ -310,7 +310,7 @@ class VariantInterpreterBrowserTemplate extends LitElement {
         // We save current query so we can execute the same query after refreshing, check 'clinicaAnalysisObserver'
         this.currentQueryBeforeSaveEvent = this.executedQuery;
 
-        this.clinicalAnalysisManager.updateVariantInPrimaryFindings(event.detail.variant, event.detail.action)
+        this.clinicalAnalysisManager.updateVariants(event.detail.variant, event.detail.primaryFinding, event.detail.action)
             .then(() => {
                 LitUtils.dispatchCustomEvent(this, "clinicalAnalysisUpdate", null, {
                     clinicalAnalysis: this.clinicalAnalysis,
