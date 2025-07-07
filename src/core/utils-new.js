abe30bb876018bd56cda2e605e0f5e287edf7729
Josemi <josemijuanes@gmail.com>
2025-07-07 14:37:15 +0200

    core: Add utility to read and convert a file to base64 #TASK-7705 #TASK-7645

--- src/core/utils-new.js
+++ src/core/utils-new.js
@@ -1114,4 +1114,17 @@ export default class UtilsNew {
         }
     }
 
+    // convert a file object to base64 string
+    // reference: https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
+    static fileToBase64(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.addEventListener("load", () => {
+                resolve(reader.result.split(",")[1]); // extract Base64 part
+            });
+            reader.addEventListener("error", error => reject(error));
+            reader.readAsDataURL(file); // Reads file as a Data URL
+        });
+    }
+
 }
