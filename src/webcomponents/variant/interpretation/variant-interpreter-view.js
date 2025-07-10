dbe753ac01a377eb623c59992fe8233e057428c4
vgarciaperez <vgarciaperez@icr.ac.uk>
2025-07-10 10:48:00 +0200

    wc - Add new summary, #TASK-7765, #TASK-7645

--- src/webcomponents/variant/interpretation/variant-interpreter-view.js
+++ src/webcomponents/variant/interpretation/variant-interpreter-view.js
@@ -21,6 +21,7 @@ import "../../commons/forms/data-form.js";
 import "../../commons/json-viewer.js";
 import "../../visualization/protein-lollipop-variant-view.js";
 import "../annotation/cellbase-variant-annotation-summary.js";
+import "../annotation/variant-summary.js";
 import "../annotation/cellbase-population-frequency-grid.js";
 import "../annotation/variant-consequence-type-view.js";
 import "../annotation/variant-annotation-clinical-view.js";
@@ -59,6 +60,9 @@ export default class VariantInterpreterView extends LitElement {
             },
             variantId: {
                 type: String
+            },
+            settings: {
+                type: Object,
             },
             displayConfig: {
                 type: Object
@@ -139,15 +143,33 @@ export default class VariantInterpreterView extends LitElement {
             sections: [
                 {
                     id: "annotationSummary",
-                    name: "Summary",
-                    render: variant => html`
-                        <cellbase-variant-annotation-summary
-                            .variantAnnotation="${variant?.annotation}"
-                            .consequenceTypes="${CONSEQUENCE_TYPES}"
-                            .proteinSubstitutionScores="${PROTEIN_SUBSTITUTION_SCORE}"
-                            .assembly="${this.opencgaSession.project.organism.assembly}">
-                        </cellbase-variant-annotation-summary>
-                    `,
+                    name: "Summary New",
+                    render: variant => {
+                        return html`
+                            <variant-summary
+                                .variant="${variant}"
+                                .consequenceTypes="${CONSEQUENCE_TYPES}"
+                                .proteinSubstitutionScores="${PROTEIN_SUBSTITUTION_SCORE}"
+                                .settings="${this.settings}"
+                                .opencgaSession="${this.opencgaSession}"
+                                .assembly="${this.opencgaSession?.project?.organism?.assembly}">
+                            </variant-summary>
+                        `;
+                    }
+                },
+                {
+                    id: "annotationSummary",
+                    name: "Summary 2",
+                    render: variant => {
+                        return html`
+                            <cellbase-variant-annotation-summary
+                                .variantAnnotation="${variant?.annotation}"
+                                .consequenceTypes="${CONSEQUENCE_TYPES}"
+                                .proteinSubstitutionScores="${PROTEIN_SUBSTITUTION_SCORE}"
+                                .assembly="${this.opencgaSession.project.organism.assembly}">
+                            </cellbase-variant-annotation-summary>
+                        `;
+                    }
                 },
                 {
                     id: "annotationConsType",
