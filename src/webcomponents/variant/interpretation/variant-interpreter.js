d66e0f1029d8613e02ce38a751a655ded41af574
Josemi <josemijuanes@gmail.com>
2025-07-14 14:30:24 +0200

    wc: Using new clinical-analysis-report component in interpreter #TASK-7806 #TASK-7645

--- src/webcomponents/variant/interpretation/variant-interpreter.js
+++ src/webcomponents/variant/interpretation/variant-interpreter.js
@@ -32,8 +32,8 @@ import "./variant-interpreter-methods.js";
 import "../../commons/opencga-active-filters.js";
 import "../../download-button.js";
 import "../../loading-spinner.js";
-import "../../clinical/clinical-analysis-review.js";
 import "../../clinical/interpretation/clinical-interpretation-update.js";
+import "../../clinical/report/clinical-analysis-report.js";
 
 class VariantInterpreter extends LitElement {
 
@@ -266,11 +266,11 @@ class VariantInterpreter extends LitElement {
                             <tool-header
                                 title="Interpretation - ${this.clinicalAnalysis?.interpretation?.id}">
                             </tool-header>
-                            <clinical-analysis-review
-                                @clinicalAnalysisUpdate="${e => this.onClinicalAnalysisUpdate(e)}"
+                            <clinical-analysis-report
+                                .opencgaSession="${this.opencgaSession}"
                                 .clinicalAnalysis="${this.clinicalAnalysis}"
-                                .opencgaSession="${this.opencgaSession}">
-                            </clinical-analysis-review>
+                                @clinicalAnalysisUpdate="${e => this.onClinicalAnalysisUpdate(e)}">
+                            </clinical-analysis-report>
                         </div>
                     `;
                 default:
