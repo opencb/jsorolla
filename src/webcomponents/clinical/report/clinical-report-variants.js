0fee7ec31c1eb437bfaad92359e703eef1f1c334
Josemi <josemijuanes@gmail.com>
2025-07-17 13:07:29 +0200

    wc: Allow to display vairant view in the reported variants table #TASK-7806 #TASK-7645

--- src/webcomponents/clinical/report/clinical-report-variants.js
+++ src/webcomponents/clinical/report/clinical-report-variants.js
@@ -1,5 +1,7 @@
 import {LitElement, html, nothing} from "lit";
 import LitUtils from "../../commons/utils/lit-utils.js";
+import GridCommons from "../../commons/grid-commons.js";
+import "../../variant/interpretation/variant-interpreter-view.js";
 
 export default class ClinicalReportVariants extends LitElement {
 
@@ -31,6 +33,36 @@ export default class ClinicalReportVariants extends LitElement {
 
     #init() {
         this._config = this.getDefaultConfig();
+        this._selectedVariant = null;
+        this._gridCommons = new GridCommons(null, this, null);
+
+        // initialize available modals
+        this._gridCommons.registerModals({
+            "view-variant": () => ({
+                display: {
+                    scrollable: true,
+                    title: `Variant ${this._selectedVariant.id}`,
+                    size: "modal-3xl",
+                    buttonsVisible: false,
+                },
+                render: () => html`
+                    <variant-interpreter-view
+                        .opencgaSession="${this.opencgaSession}"
+                        .settings="${this._config}"
+                        .clinicalAnalysis="${this.clinicalAnalysis}"
+                        .toolId="${"variant-interpreter-report"}"
+                        .variant="${this._selectedVariant}">
+                    </variant-interpreter-view>
+                `,
+            }),
+        });
+    }
+
+    onViewVariant(variantId) {
+        this._selectedVariant = this.variants.find(variant => variant.id === variantId);
+        if (this._selectedVariant) {
+            this._gridCommons.changeActiveModal("view-variant");
+        }
     }
 
     render() {
@@ -43,6 +75,8 @@ export default class ClinicalReportVariants extends LitElement {
                 .data="${this.variants}"
                 .config="${this._config}">
             </data-form>
+
+            ${this._gridCommons.renderModals()}
         `;
     }
 
@@ -70,6 +104,14 @@ export default class ClinicalReportVariants extends LitElement {
                                     {
                                         title: "ID",
                                         field: "id",
+                                        type: "custom",
+                                        display: {
+                                            render: id => html`
+                                                <a class="link fw-bold" @click="${() => this.onViewVariant(id)}">
+                                                    ${id}
+                                                </>
+                                            `,
+                                        },
                                     },
                                     {
                                         title: "Type",
