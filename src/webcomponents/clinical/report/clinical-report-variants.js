51ba04dafa579a309713ee88fbb8db1777cccefe
Josemi <josemijuanes@gmail.com>
2025-07-17 16:41:08 +0200

    wc: Allow to select a variant by clicing on the table row of report variants #TASK-7806 #TASK-7645

--- src/webcomponents/clinical/report/clinical-report-variants.js
+++ src/webcomponents/clinical/report/clinical-report-variants.js
@@ -25,6 +25,9 @@ export default class ClinicalReportVariants extends LitElement {
             variants: {
                 type: Array,
             },
+            active: {
+                type: Boolean,
+            },
             config: {
                 type: Object
             }
@@ -58,6 +61,58 @@ export default class ClinicalReportVariants extends LitElement {
         });
     }
 
+    update(changedProperties) {
+        if (changedProperties.has("variants")) {
+            this._selectedVariant = null; // reset selected variant when variants change
+            if (this.variants?.length > 0) {
+                this._selectedVariant = this.variants[0]; // select the first variant by default
+            }
+        }
+
+        super.update(changedProperties);
+    }
+
+    updated(changedProperties) {
+        const dataFormElement = this.querySelector(`data-form[data-role="variants"]`);
+
+        // 1. register events listeners when the user clicks on a table row
+        if (changedProperties.has("variants") || changedProperties.has("active")) {
+            if (this.active && this.variants?.length > 0) {
+                dataFormElement.updateComplete.then(() => {
+                    Array.from(dataFormElement.querySelectorAll(`table tbody tr[data-row-index]`)).forEach(row => {
+                        Array.from(row.querySelectorAll("td")).forEach(td => {
+                            td.addEventListener("click", event => {
+                                // console.log("td clicked", event);
+                                this.onSelectVariant(event, this.variants[parseInt(row.dataset.rowIndex)]);
+                            });
+                        });
+                    });
+                });
+            }
+        }
+
+        // 2. change the selected variant in the row
+        // note that this should be executed every update of the component
+        if (this._selectedVariant && this.active) {
+            dataFormElement.updateComplete.then(() => {
+                Array.from(dataFormElement.querySelectorAll(`table tbody tr[data-row-index]`)).forEach(row => {
+                    const variant = this.variants[parseInt(row.dataset.rowIndex)];
+                    if (variant?.id === this._selectedVariant.id) {
+                        row.classList.add("selected");
+                    } else {
+                        row.classList.remove("selected");
+                    }
+                });
+            });
+        }
+    }
+
+    onSelectVariant(event, variant) {
+        event.preventDefault();
+        this._selectedVariant = variant;
+        this.requestUpdate();
+    }
+
     onViewVariant(event, variantId) {
         event.preventDefault();
         event.stopPropagation();
@@ -68,19 +123,20 @@ export default class ClinicalReportVariants extends LitElement {
     }
 
     render() {
-        if (!this.opencgaSession || !this.clinicalAnalysis || !this.variants) {
+        if (!this.opencgaSession || !this.clinicalAnalysis || !this.variants || !this.active) {
             return nothing;
         }
 
         return html`
             <div class="row">
-                <div class="col-6">
+                <div class="col-8">
                     <data-form
+                        data-role="variants"
                         .data="${this.variants}"
                         .config="${this._config}">
                     </data-form>
                 </div>
-                <div class="col-6"></div>
+                <div class="col-4"></div>
             </div>
 
             ${this._gridCommons.renderModals()}
