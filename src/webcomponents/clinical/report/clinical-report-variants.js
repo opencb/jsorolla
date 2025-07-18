03a33622e317ea16a38d681aaf666475b81545ab
Josemi <josemijuanes@gmail.com>
2025-07-18 13:00:40 +0200

    wc: Add references in reported variants #TASK-7806 #TASK-7645

--- src/webcomponents/clinical/report/clinical-report-variants.js
+++ src/webcomponents/clinical/report/clinical-report-variants.js
@@ -244,6 +244,44 @@ export default class ClinicalReportVariants extends LitElement {
                                 return data.selectedVariant?.recommendation || "-";
                             },
                         },
+                        {
+                            title: "References",
+                            field: "selectedVariant.references",
+                            type: "object-list",
+                            display: {
+                                showAddBatchListButton: false,
+                                showEditItemListButton: false,
+                                showDeleteItemListButton: false,
+                                showAddItemListButton: false,
+                                view: reference => {
+                                    return html`
+                                        <div class="mb-2">
+                                            <div class="fw-bold">
+                                                ${reference.title || reference.name || "-"}
+                                            </div>
+                                            <div class="text-secondary">
+                                                ${reference.authors?.join(", ") || "-"}
+                                            </div>
+                                            <div class="text-muted d-flex align-items-center flex-row flex-wrap gap-1 fs-7">
+                                                ${reference.journal ? html`
+                                                    <span>${reference.journal}.</span>
+                                                ` : nothing}
+                                                ${reference.date ? html`
+                                                    <span>${UtilsNew.dateFormatter(reference.date)}.</span>
+                                                ` : nothing}
+                                                ${reference.url ? html`
+                                                    <span class="text-nowrap d-flex align-items-center gap-1 ms-2">
+                                                        <i class="fa fa-link fs-8"></i>
+                                                        <span>${reference.url || "-"}</span>
+                                                    </span>
+                                                ` : nothing}
+                                            </div>
+                                        </div>
+                                    `;
+                                },
+                            },
+                            elements: [],
+                        },
                     ],
                 },
             ],
