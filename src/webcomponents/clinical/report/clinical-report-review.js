b075a355d1aad7bf4cffccf856496865fb44d98f
Josemi <josemijuanes@gmail.com>
2025-07-28 16:56:28 +0200

    wc: Fix report-review to display only reported variants as cards #TASK-7806 #TASK-7645

--- src/webcomponents/clinical/report/clinical-report-review.js
+++ src/webcomponents/clinical/report/clinical-report-review.js
@@ -144,8 +144,7 @@ export default class ClinicalReportReview extends LitElement {
     renderReportedVariants() {
         // get only variants with status "REPORTED"
         const reportedVariants = (this.clinicalAnalysis?.interpretation?.primaryFindings || []).filter(variant => {
-            // return variant.status.id === "REPORTED";
-            return true;
+            return variant.status.id === "REPORTED";
         });
 
         return html`
