7a83cc91089dcb9c9339f8e67eedeaf7d5e93f5a
Josemi <josemijuanes@gmail.com>
2025-08-01 12:26:45 +0200

    wc: Remove card wrapper in comments formatter of clinical variant utils #TASK-7705 #TASK-7645

--- src/webcomponents/clinical/variant/clinical-variant-utils.js
+++ src/webcomponents/clinical/variant/clinical-variant-utils.js
@@ -32,23 +32,19 @@ export default class ClinicalVariantUtils {
 
     static formatComment(comment) {
         return html`
-            <div class="card border border-1 border-gray-200 bg-white">
-                <div class="card-body">
-                    <div class="d-flex align-items-center gap-2 mb-2">
-                        <i class="fas fa-comment text-secondary"></i>
-                        <span class="fw-bold">${comment.author || "Unknown"}</span>
-                        <span class="text-secondary fs-7">${UtilsNew.dateFormatter(comment.date)}</span>
-                    </div>
-                    <div>${comment.message || "-"}</div>
-                    ${comment?.tags?.length > 0 ? html`
-                        <div class="mt-2 d-flex gap-2 flex-wrap">
-                            ${comment.tags.map(tag => html`
-                                <span class="badge bg-secondary">${tag}</span>
-                            `)}
-                        </div>    
-                    ` : nothing}
-                </div>
+            <div class="d-flex align-items-center gap-2 mb-2">
+                <i class="fas fa-comment text-secondary"></i>
+                <span class="fw-bold">${comment.author || "Unknown"}</span>
+                <span class="text-secondary fs-7">${UtilsNew.dateFormatter(comment.date)}</span>
             </div>
+            <div>${comment.message || "-"}</div>
+            ${comment?.tags?.length > 0 ? html`
+                <div class="mt-2 d-flex gap-2 flex-wrap">
+                    ${comment.tags.map(tag => html`
+                        <span class="badge bg-secondary">${tag}</span>
+                    `)}
+                </div>    
+            ` : nothing}
         `;
     }
 
