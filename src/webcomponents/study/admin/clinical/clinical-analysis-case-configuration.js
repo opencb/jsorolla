ce5c17c7251b12311bbce9b478fb88d97745a1c7
Josemi <josemijuanes@gmail.com>
2025-08-07 13:40:19 +0200

    wc: Fix typo displaying priorities preview #TASK-7806 #TASK-7645

--- src/webcomponents/study/admin/clinical/clinical-analysis-case-configuration.js
+++ src/webcomponents/study/admin/clinical/clinical-analysis-case-configuration.js
@@ -173,7 +173,7 @@ export default class ClinicalAnalysisCaseConfiguration extends LitElement {
                                 view: priority => html`
                                     <div class="d-flex flex-row align-items-center gap-2">
                                         <span class="fw-bold">${priority.id}</span>
-                                        ${priotiry?.rank ? html`<span>(rank: ${priority.rank})</span>` : nothing}
+                                        ${priority?.rank ? html`<span>(rank: ${priority.rank})</span>` : nothing}
                                         ${priority?.defaultPriority ? html`<span class="badge bg-primary">DEFAULT</span>` : nothing}
                                     </div>
                                 `,
