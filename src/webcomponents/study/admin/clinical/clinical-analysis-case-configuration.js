d5795404efb57ee0edf4e3e857e19d95c8b77809
Josemi <josemijuanes@gmail.com>
2025-08-08 17:29:42 +0200

    wc: Allow to configure consents in case configuration #TASK-7806 #TASK-7645

--- src/webcomponents/study/admin/clinical/clinical-analysis-case-configuration.js
+++ src/webcomponents/study/admin/clinical/clinical-analysis-case-configuration.js
@@ -297,6 +297,52 @@ export default class ClinicalAnalysisCaseConfiguration extends LitElement {
                                 },
                             ],
                         },
+                        {
+                            title: "Consents",
+                            description: "Configure the consents that can be assigned to clinical cases.",
+                            field: "consents",
+                            type: "object-list",
+                            display: {
+                                collapsedUpdate: false,
+                                itemAddText: "Add Consent",
+                                maxNumItems: 25,
+                                view: consent => html`
+                                    <div class="">
+                                        <span class="fw-bold">${consent.name || consent.id || ""}</span>
+                                    </div>
+                                `,
+                            },
+                            elements: [
+                                {
+                                    title: "Consent ID",
+                                    field: "consents[].id",
+                                    type: "input-text",
+                                    display: {
+                                        placeholder: "E.g. CONSENT_1",
+                                        helpMessage: "Unique identifier for the consent.",
+                                    },
+                                },
+                                {
+                                    title: "Consent Name",
+                                    field: "consents[].name",
+                                    type: "input-text",
+                                    display: {
+                                        placeholder: "E.g. Consent for research",
+                                        helpMessage: "Name of the consent. This will be displayed in the clinical case.",
+                                    },
+                                },
+                                {
+                                    title: "Description",
+                                    field: "consents[].description",
+                                    type: "input-text",
+                                    display: {
+                                        rows: 2,
+                                        placeholder: "Add a description for this consent...",
+                                        helpMessage: "Provide a brief description of the consent. This will help users understand the purpose of this consent in the clinical workflow.",
+                                    },
+                                },
+                            ],
+                        },
                     ],
                 },
             ],
