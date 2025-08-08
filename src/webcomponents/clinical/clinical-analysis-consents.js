44c133e452167f7f6df6eb8ac9b08b6696c95cc6
Josemi <josemijuanes@gmail.com>
2025-08-08 17:52:39 +0200

    wc: Initial working adapting consents editor component #TASK-7806 #TASK-7645

--- src/webcomponents/clinical/clinical-analysis-consents.js
+++ src/webcomponents/clinical/clinical-analysis-consents.js
@@ -32,110 +32,74 @@ class ClinicalAnalysisConsents extends LitElement {
     static get properties() {
         return {
             opencgaSession: {
-                type: Object
-            },
-            clinicalAnalysisId: {
-                type: String
+                type: Object,
             },
             clinicalAnalysis: {
-                type: Object
+                type: Object,
             },
-            config: {
-                type: Object
-            }
+            displayConfig: {
+                type: Object,
+            },
+            active: {
+                type: Boolean,
+            },
         };
     }
 
     #init() {
-        this._prefix = UtilsNew.randomString(8);
-        this.updateParams = {};
+        this._consents = {};
         this._config = this.getDefaultConfig();
     }
 
-    updated(changedProperties) {
-        if (changedProperties.has("opencgaSession")) {
-            this.opencgaSessionObserver();
-        }
-
+    update(changedProperties) {
         if (changedProperties.has("clinicalAnalysis")) {
             this.clinicalAnalysisObserver();
         }
 
-        if (changedProperties.has("clinicalAnalysisId")) {
-            this.clinicalAnalysisIdObserver();
+        if (changedProperties.has("opencgaSession") || changedProperties.has("displayConfig")) {
+            this._config = this.getDefaultConfig();
         }
 
-        if (changedProperties.has("config")) {
-            this._config = {
-                ...this.getDefaultConfig(),
-                ...this.config,
-            };
-        }
-    }
-
-    opencgaSessionObserver() {
-        this._users = [];
-        if (this.opencgaSession && this.opencgaSession.study) {
-            for (let group of this.opencgaSession.study.groups) {
-                if (group.id === "@members") {
-                    this._users.push(...group.userIds.filter(user => user !== "*"));
-                }
-            }
-        }
+        super.update(changedProperties);
     }
 
     clinicalAnalysisObserver() {
-        if (this.opencgaSession && this.clinicalAnalysis) {
-            this._clinicalAnalysis = JSON.parse(JSON.stringify(this.clinicalAnalysis));
-        }
-    }
-
-    clinicalAnalysisIdObserver() {
-        if (this.opencgaSession && this.clinicalAnalysisId) {
-            this.opencgaSession.opencgaClient.clinical().info(this.clinicalAnalysisId, {study: this.opencgaSession.study.fqn})
-                .then(response => {
-                    this.clinicalAnalysis = response.responses[0].results[0];
-                    this.clinicalAnalysisObserver();
-                })
-                .catch(response => {
-                    console.error("An error occurred fetching clinicalAnalysis: ", response);
-                });
-        }
+        // TODO: clone the consents object to avoid modifying the original object
     }
 
     onFieldChange(e) {
-        switch (e.detail.param) {
-            case "consent.primaryFindings":
-            case "consent.secondaryFindings":
-            case "consent.carrierFindings":
-            case "consent.researchFindings":
-                let field = e.detail.param.split(".")[1];
-                this.updateParams.consent = {...this.clinicalAnalysis.consent};
-                if (this._clinicalAnalysis?.consent[field] !== e.detail.value && e.detail.value) {
-                    this.clinicalAnalysis.consent[field] = e.detail.value;
-                    this.updateParams.consent[field] = e.detail.value;
-                } else {
-                    delete this.updateParams.consent[field];
-                }
-                if (UtilsNew.isEmpty(this.updateParams.consent)) {
-                    delete this.updateParams.consent;
-                }
-                break;
-        }
-        this.requestUpdate();
+        // switch (e.detail.param) {
+        //     case "consent.primaryFindings":
+        //     case "consent.secondaryFindings":
+        //     case "consent.carrierFindings":
+        //     case "consent.researchFindings":
+        //         let field = e.detail.param.split(".")[1];
+        //         this.updateParams.consent = {...this.clinicalAnalysis.consent};
+        //         if (this._clinicalAnalysis?.consent[field] !== e.detail.value && e.detail.value) {
+        //             this.clinicalAnalysis.consent[field] = e.detail.value;
+        //             this.updateParams.consent[field] = e.detail.value;
+        //         } else {
+        //             delete this.updateParams.consent[field];
+        //         }
+        //         if (UtilsNew.isEmpty(this.updateParams.consent)) {
+        //             delete this.updateParams.consent;
+        //         }
+        //         break;
+        // }
+        // this.requestUpdate();
     }
 
-    onRun() {
-        if (this.updateParams && UtilsNew.isNotEmpty(this.updateParams)) {
-            this.opencgaSession.opencgaClient.clinical().update(this.clinicalAnalysis.id, this.updateParams, {study: this.opencgaSession.study.fqn})
-                .then(response => {
-                    this._clinicalAnalysis = JSON.parse(JSON.stringify(this.clinicalAnalysis));
-                    this.updateParams = {};
-                })
-                .catch(response => {
-                    console.error("An error occurred updating clinicalAnalysis: ", response);
-                });
-        }
+    onSubmit() {
+        // if (this.updateParams && UtilsNew.isNotEmpty(this.updateParams)) {
+        //     this.opencgaSession.opencgaClient.clinical().update(this.clinicalAnalysis.id, this.updateParams, {study: this.opencgaSession.study.fqn})
+        //         .then(response => {
+        //             this._clinicalAnalysis = JSON.parse(JSON.stringify(this.clinicalAnalysis));
+        //             this.updateParams = {};
+        //         })
+        //         .catch(response => {
+        //             console.error("An error occurred updating clinicalAnalysis: ", response);
+        //         });
+        // }
     }
 
     render() {
@@ -147,35 +111,24 @@ class ClinicalAnalysisConsents extends LitElement {
             <data-form 
                 .data="${this.clinicalAnalysis}"
                 .config="${this._config}"
-                @fieldChange="${e => this.onFieldChange(e)}"
-                @submit="${this.onRun}">
+                @fieldChange="${event => this.onFieldChange(event)}"
+                @submit="${event => this.onSubmit(event)}">
             </data-form>
         `;
     }
 
     getDefaultConfig() {
         return {
-            id: "clinical-analysis-consent",
-            title: "Consent Editor",
-            icon: "fas fa-user-md",
-            type: "form",
-            buttons: {
-                show: true,
-                clearText: "Clear",
-                okText: "Save"
-            },
             display: {
-                width: "6",
                 showTitle: false,
                 infoIcon: "",
                 labelAlign: "left",
                 labelWidth: "3",
                 defaultLayout: "horizontal",
+                ...this.displayConfig,
             },
             sections: [
                 {
-                    id: "consent",
-                    title: "Consent Management",
                     elements: [
                         {
                             name: "Primary Findings",
