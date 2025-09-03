966da3446b6c6d55e70c3626ecb29b58beffae82
Josemi <josemijuanes@gmail.com>
2025-09-03 12:24:58 +0200

    wc: Fix displaying vairant id in variant browser grid #TASK-7880 #TASK-7733

--- src/webcomponents/variant/variant-browser-grid.js
+++ src/webcomponents/variant/variant-browser-grid.js
@@ -539,7 +539,7 @@ export default class VariantBrowserGrid extends LitElement {
                     rowspan: 2,
                     colspan: 1,
                     formatter: (value, row, index) => {
-                        VariantGridFormatter.variantIdFormatter(value, row, index, this.opencgaSession?.project?.organism?.scientificName, this.opencgaSession.project.organism.assembly, this._config);
+                        return VariantGridFormatter.variantIdFormatter(value, row, index, this.opencgaSession?.project?.organism?.scientificName, this.opencgaSession.project.organism.assembly, this._config);
                     },
                     events: {
                         "click a": (event, value, row) => this.onActionClick(event, row)
