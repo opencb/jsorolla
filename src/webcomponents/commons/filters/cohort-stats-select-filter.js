7e97b81e9313a8ae4329cdd4818eed1ce6ffb14f
Josemi <josemijuanes@gmail.com>
2025-09-09 10:39:32 +0200

    wc: Add chevron indicator in cohort stats filter #TASK-7880 #TASK-7733

--- src/webcomponents/commons/filters/cohort-stats-select-filter.js
+++ src/webcomponents/commons/filters/cohort-stats-select-filter.js
@@ -219,7 +219,12 @@ export default class CohortStatsSelectFilter extends LitElement {
         return keyed("cohort:" + study.fqn, html`
             <div class="p-2 border border-gray-200 rounded-3">
                 <div class="cursor-pointer d-flex align-items-center gap-2" @click="${event => this.onStudyToggle(event, study.fqn)}">
-                    <div class="fs-5">Study <b>${study.id}</b> (${(study.cohorts).length})</div>
+                    <div class="flex-shrink-0 d-flex align-items-center justify-content-center" style="width:12px;height:13px;">
+                        <i class="fas fa-chevron-${isExpanded ? "down" : "right"}"></i>
+                    </div>
+                    <div class="fs-5 d-flex align-items-center">
+                        <span>Study <b>${study.id}</b> (${(study.cohorts).length})</span>
+                    </div>
                 </div>
                 <div class="${isExpanded ? "d-block mt-1" : "d-none"}">
                     ${favoriteCohorts.length > 0 ? html`
