8937d07e67d32d7b0c376df7600db2297d54234e
Josemi <josemijuanes@gmail.com>
2025-09-09 17:43:40 +0200

    wc: Force to reset query params for phenotypes and disorders in observer of individual-create #TASK-7880 #TASK-7733

--- src/webcomponents/individual/individual-create.js
+++ src/webcomponents/individual/individual-create.js
@@ -58,6 +58,8 @@ export default class IndividualCreate extends LitElement {
 
     update(changedProperties) {
         if (changedProperties.has("opencgaSession")) {
+            this._disordersQueryParams = {};
+            this._phenotypesQueryParams = {};
             if (this.opencgaSession?.study?.attributes?.IVA_CONFIG?.settings?.INDIVIDUAL_BROWSER?.model?.disorders?.source) {
                 const source = this.opencgaSession.study.attributes.IVA_CONFIG.settings.INDIVIDUAL_BROWSER.model.disorders.source;
                 this._disordersQueryParams = {
