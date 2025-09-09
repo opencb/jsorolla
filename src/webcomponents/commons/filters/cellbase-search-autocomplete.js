5862044729505cac152c95485ff6fa1738b1f55d
Josemi <josemijuanes@gmail.com>
2025-09-09 16:31:16 +0200

    wc: Add new queryParams property in cellbase-search-autocomplete #TASK-7880 #TASK-7733

--- src/webcomponents/commons/filters/cellbase-search-autocomplete.js
+++ src/webcomponents/commons/filters/cellbase-search-autocomplete.js
@@ -51,6 +51,9 @@ export default class CellbaseSearchAutocomplete extends LitElement {
             searchField: {
                 type: String,
             },
+            queryParams: {
+                type: Object,
+            },
         };
     }
 
@@ -242,6 +245,7 @@ export default class CellbaseSearchAutocomplete extends LitElement {
                 const queryParams = {
                     ...this.defaultQueryParams,
                     ...this.RESOURCES[this.resource].queryParams,
+                    ...this.queryParams,
                     skip: (page - 1) * this._config.limit,
                 };
                 if (params?.data?.term) {
