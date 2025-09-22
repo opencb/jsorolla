ee6b5898fd1a0aa874b890f571ebe8edcfe441f6
imedina <igmecas@gmail.com>
2025-09-22 16:36:13 +0100

    wc: add cohort view link to sample-grid

--- src/webcomponents/sample/sample-grid.js
+++ src/webcomponents/sample/sample-grid.js
@@ -22,6 +22,7 @@ import CatalogGridFormatter from "../commons/catalog-grid-formatter.js";
 import NotificationUtils from "../commons/utils/notification-utils.js";
 import "../commons/grid-toolbar.js";
 import "../cohort/cohort-create-samples.js";
+import "../cohort/cohort-view.js";
 import "../individual/individual-view.js";
 import "../file/file-view.js";
 import "../variant/analysis/knockout-analysis.js";
@@ -74,6 +75,7 @@ export default class SampleGrid extends LitElement {
         this.gridId = this._prefix + this.COMPONENT_ID;
         this._selectedSampleId = null;
         this._selectedIndividualId = null;
+        this._selectedCohortId = null;
         this._selectedFileId = null;
         this._config = this.getDefaultConfig();
     }
@@ -220,6 +222,21 @@ export default class SampleGrid extends LitElement {
                         .active="${true}"
                         .opencgaSession="${this.opencgaSession}">
                     </individual-view>
+                `,
+            }),
+            "view-cohort": () => ({
+                display: {
+                    modalTitle: `Cohort ${this._selectedCohortId}`,
+                    modalSize: "modal-2xl",
+                    modalCyDataName: "cohort-view",
+                    modalDraggable: true,
+                },
+                render: () => html`
+                    <cohort-view
+                        .cohortId="${this._selectedCohortId}"
+                        .active="${true}"
+                        .opencgaSession="${this.opencgaSession}">
+                    </cohort-view>
                 `,
             }),
             "view-clinical-analysis": () => ({
@@ -505,9 +522,7 @@ export default class SampleGrid extends LitElement {
                         .filter(cohortId => cohortId !== "ALL")
                         .map(cohortId => {
                             return `
-                                <div>
-                                    <span>${cohortId}</span>
-                                </div>
+                                <a class="link fw-bold" data-action="view-cohort" data-cohort="${cohortId}">${cohortId}</a>
                             `;
                         });
                     return `
@@ -515,6 +530,9 @@ export default class SampleGrid extends LitElement {
                             ${items.length > 0 ? items.join("") : "-"}
                         </div>
                     `;
+                },
+                events: {
+                    "click a": (event, value, row) => this.onActionClick(event, row),
                 },
                 visible: this.gridCommons.isColumnVisible("cohorts")
             },
@@ -676,6 +694,10 @@ export default class SampleGrid extends LitElement {
             case "view-individual":
                 this._selectedIndividualId = event.currentTarget.dataset.individual;
                 this.gridCommons.changeActiveModal("view-individual");
+                break;
+            case "view-cohort":
+                this._selectedCohortId = event.currentTarget.dataset.cohort;
+                this.gridCommons.changeActiveModal("view-cohort");
                 break;
             case "view-clinical-analysis":
                 this._selectedClinicalAnalysisId = event.currentTarget?.dataset?.clinicalAnalysis;
