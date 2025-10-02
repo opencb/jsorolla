6ced1b3bbec37860b749afd1a928a622d924336e
Josemi <josemijuanes@gmail.com>
2025-10-02 18:16:04 +0200

    wc: add missing imports and fix help message in folder create

--- src/webcomponents/file/file-folder-create.js
+++ src/webcomponents/file/file-folder-create.js
@@ -20,6 +20,7 @@ import NotificationUtils from "../commons/utils/notification-utils.js";
 import CatalogUtils from "../../core/clients/opencga/opencga-catalog-utils.js";
 import "../commons/forms/data-form.js";
 import "../commons/filters/catalog-distinct-autocomplete.js";
+import "../commons/filters/catalog-search-autocomplete.js";
 
 export default class FileFolderCreate extends LitElement {
 
@@ -169,7 +170,7 @@ export default class FileFolderCreate extends LitElement {
                                         @filterChange="${e => onFieldChange(e.detail.value)}">
                                     </catalog-search-autocomplete>
                                 `,
-                                helpMessage: "Path where the files will be uploaded.",
+                                helpMessage: "Path where the folder will be created.",
                             },
                         },
                         {
