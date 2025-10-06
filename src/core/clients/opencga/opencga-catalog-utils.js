549cde6859fd13fb9e5b226ac4cf348d04d2c2f1
Josemi <josemijuanes@gmail.com>
2025-10-06 14:46:56 +0200

    core: add alias to get effective study permission in catalog utils #TASK-7645 #TASK-7806

--- src/core/clients/opencga/opencga-catalog-utils.js
+++ src/core/clients/opencga/opencga-catalog-utils.js
@@ -142,6 +142,12 @@ export default class OpencgaCatalogUtils {
         return getEffectivePermission(userPermissionsStudy, groupPermissions) >= permissionLevel[permission];
     }
 
+    // simplified version of getStudyEffectivePermission
+    static hasStudyPermission(opencgaSession, permission) {
+        const simplifyPermissions = opencgaSession?.organization?.configuration?.optimizations?.simplifyPermissions ?? false;
+        return OpencgaCatalogUtils.getStudyEffectivePermission(opencgaSession?.study, opencgaSession?.user?.id, permission, simplifyPermissions);
+    }
+
     // Check if the user has the right the permissions in the study.
     static isAdmin(study, userLogged) {
         if (!study || !userLogged) {
