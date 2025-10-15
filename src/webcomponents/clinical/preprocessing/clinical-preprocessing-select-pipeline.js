1b8ad61c7bf0dc4cc5136bc6d14c670681ba0f76
Josemi <josemijuanes@gmail.com>
2025-10-15 17:53:36 +0200

    wc: initialize component to select an existing pipeline in clinical preprocessing #TASK-7645

--- src/webcomponents/clinical/preprocessing/clinical-preprocessing-select-pipeline.js
+++ src/webcomponents/clinical/preprocessing/clinical-preprocessing-select-pipeline.js
@@ -1,5 +1,4 @@
-import {html, LitElement} from "lit";
-import NotificationUtils from "../../commons/utils/notification-utils.js";
+import {html, LitElement, nothing} from "lit";
 import LitUtils from "../../commons/utils/lit-utils.js";
 import UtilsNew from "../../../core/utils-new.js";
 import "../../commons/forms/data-form.js";
@@ -27,10 +26,15 @@ export default class ClinicalPreprocessingSelectPipeline extends LitElement {
     }
 
     #init() {
+        this._data = {};
         this._config = this.getDefaultConfig();
     }
 
     update(changedProperties) {
+        if (changedProperties.has("opencgaSession")) {
+            this.opencgaSessionObserver();
+        }
+
         if (changedProperties.has("displayConfig")) {
             this._config = this.getDefaultConfig();
         }
@@ -38,23 +42,114 @@ export default class ClinicalPreprocessingSelectPipeline extends LitElement {
         super.update(changedProperties);
     }
 
-    onFieldChange() {
-        // LitUtils.dispatchCustomEvent(this, "paramsChange", null, this._toolParams);
-        // this.requestUpdate();
+    opencgaSessionObserver() {
+        if (this.opencgaSession) {
+            this.opencgaSession.opencgaClient.files()
+                .search({
+                    study: this.opencgaSession.study.fqn,
+                    directory: "RESOURCES/clinical/pipelines",
+                    include: "id,name,path",
+                })
+                .then(response => {
+                    const files = (response.responses?.[0]?.results || []).filter(file => {
+                        return file.name.endsWith(".json");
+                    });
+                    // 2. donwload each file and parse the content
+                    return Promise.all(files.map(file => {
+                        return this.opencgaSession.opencgaClient.files()
+                            .download(file.id, {
+                                study: this.opencgaSession.study.fqn,
+                            })
+                            .then(fileContent => {
+                                return JSON.parse(fileContent);
+                            })
+                            .then(content => {
+                                return {
+                                    name: file.name,
+                                    path: file.path,
+                                    content: content,
+                                };
+                            })
+                            .catch(error => {
+                                console.error(`Error loading pipeline from file ${file.name}:`, error);
+                                return null; // Return null for failed templates
+                            });
+                    }));
+                })
+                .then(pipelines => {
+                    this._data = {
+                        pipelines: pipelines.filter(Boolean) // Filter out null values
+                    };
+                    this.requestUpdate();
+                })
+                .catch(error => {
+                    console.error("Error loading pipelines:", error);
+                });
+        }
     }
 
     render() {
         return html`
             <data-form
-                .data="${this._toolParams}"
-                .config="${this._config}"
-                @fieldChange="${e => this.onFieldChange(e)}">
+                .data="${this._data}"
+                .config="${this._config}">
             </data-form>
         `;
     }
 
     getDefaultConfig() {
-        return {};
+        return {
+            display: {
+                buttonsVisible: false,
+                className: "row",
+                layout: [
+                    {
+                        id: "select",
+                        className: "col-md-6",
+                    },
+                    {
+                        id: "create",
+                        className: "col-md-6",
+                    },
+                ],
+                ...this.displayConfig,
+            },
+            sections: [
+                {
+                    id: "select",
+                    title: "Select Pipeline",
+                    elements: [
+                        {
+                            text: "Select a preprocessing pipeline from the list of available pipelines.",
+                            type: "text",
+                        },
+                        {
+                            type: "list",
+                            field: "pipelines",
+                            display: {
+                                contentLayout: "vertical",
+                                listClassName: "d-flex flex-column gap-2",
+                                listItemClassName: "p-4 rounded-4 bg-white border border-gray-200",
+                                format: pipeline => html`
+                                    <div class="">
+                                        <span class="fw-bold">${pipeline.content.name || pipeline.name}</span>
+                                        ${pipeline.content.version ? html`<span class="badge bg-secondary ms-2">v${pipeline.content.version}</span>` : nothing}
+                                    </div>
+                                    ${pipeline.content.description ? html`
+                                        <div class="text-muted">${pipeline.content.description}</div>
+                                    ` : nothing}
+                                `,
+                            },
+                        },
+                    ],
+                },
+                {
+                    id: "create",
+                    title: "Create Pipeline",
+                    elements: [],
+                },
+            ],
+        };
     }
 
 }
