3061bb31149ad795d2add046cb0db395a1792ee7
Josemi <josemijuanes@gmail.com>
2025-10-17 18:05:48 +0200

    wc: initialize component to fill the information of the pipeline #TASK-7977 #TASK-7645

---
+++ src/webcomponents/clinical/preprocessing/clinical-preprocessing-save-pipeline.js
@@ -0,0 +1,89 @@
+import {html, LitElement, nothing} from "lit";
+import LitUtils from "../../commons/utils/lit-utils.js";
+import "../../commons/forms/data-form.js";
+
+export default class ClinicalPreprocessingSavePipeline extends LitElement {
+
+    constructor() {
+        super();
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            opencgaSession: {
+                type: Object,
+            },
+            displayConfig: {
+                type: Object
+            },
+        };
+    }
+
+    #init() {
+        this._data = {};
+        this._config = this.getDefaultConfig();
+    }
+
+    update(changedProperties) {
+        if (changedProperties.has("displayConfig")) {
+            this._config = this.getDefaultConfig();
+        }
+
+        super.update(changedProperties);
+    }
+
+    onSubmit() {
+        LitUtils.dispatchCustomEvent(this, "pipelineSave", null, this._data);
+    }
+
+    render() {
+        return html`
+            <data-form
+                .data="${this._data}"
+                .config="${this._config}"
+                @submit="${event=> this.onSubmit(event)}">
+            </data-form>
+        `;
+    }
+
+    getDefaultConfig() {
+        return {
+            display: {
+                buttonsVisible: true,
+                buttonClearText: "",
+                buttonOkText: "Save Pipeline",
+                ...this.displayConfig,
+            },
+            sections: [
+                {
+                    title: "",
+                    elements: [
+                        {
+                            type: "input-text",
+                            field: "name",
+                            title: "Name",
+                            required: true,
+                            display: {
+                            },
+                        },
+                        {
+                            type: "input-text",
+                            field: "description",
+                            title: "Description",
+                            display: {
+                            },
+                        },
+                    ],
+                },
+            ],
+        };
+    }
+
+}
+
+customElements.define("clinical-preprocessing-save-pipeline", ClinicalPreprocessingSavePipeline);
