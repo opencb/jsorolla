3a154a10106afb78d6e1638c941a229592aa9077
Josemi <josemijuanes@gmail.com>
2025-10-23 17:54:52 +0200

    wc: initialize component to configure a variant calling tool #TASK-7977 #TASK-7645

---
+++ src/webcomponents/clinical/preprocessing/clinical-preprocessing-variant-calling-tool-configure.js
@@ -0,0 +1,111 @@
+import {html, LitElement, nothing} from "lit";
+import LitUtils from "../../commons/utils/lit-utils.js";
+import "../../commons/forms/data-form.js";
+
+export default class ClinicalPreprocessingVariantCallingToolConfigure extends LitElement {
+
+    constructor() {
+        super();
+        this.#init();
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    static get properties() {
+        return {
+            opencgaSession: {
+                type: Object,
+            },
+            toolName: {
+                type: String,
+            },
+            toolData: {
+                type: Object,
+            },
+            displayConfig: {
+                type: Object,
+            },
+        };
+    }
+
+    #init() {
+        this._data = {};
+        this._config = this.getDefaultConfig();
+    }
+
+    update(changedProperties) {
+        if (changedProperties.has("displayConfig")) {
+            this._config = this.getDefaultConfig();
+        }
+
+        super.update(changedProperties);
+    }
+
+    onFieldChange(event) {
+        this._data = {
+            ...this._data,
+        };
+        this.requestUpdate();
+    }
+
+    onSubmit() {
+        LitUtils.dispatchCustomEvent(this, "toolConfigureSave", null, this._data);
+    }
+
+    render() {
+        return html`
+            <data-form
+                .data="${this._data}"
+                .config="${this._config}"
+                @fieldChange="${event => this.onFieldChange(event)}"
+                @submit="${event=> this.onSubmit(event)}">
+            </data-form>
+        `;
+    }
+
+    getDefaultConfig() {
+        return {
+            display: {
+                buttonsVisible: true,
+                buttonClearText: "",
+                buttonOkText: "Save",
+                ...this.displayConfig,
+            },
+            sections: [
+                {
+                    title: "",
+                    elements: [
+                        {
+                            type: "input-text",
+                            field: "fileName",
+                            title: "File Name",
+                            required: true,
+                            display: {
+                            },
+                        },
+                        {
+                            type: "input-text",
+                            field: "name",
+                            title: "Pipeline Name",
+                            required: true,
+                            display: {
+                            },
+                        },
+                        {
+                            type: "input-text",
+                            field: "description",
+                            title: "Pipeline Description",
+                            display: {
+                            },
+                        },
+                    ],
+                },
+            ],
+        };
+    }
+
+}
+
+customElements.define("clinical-preprocessing-variant-calling-tool-configure", ClinicalPreprocessingVariantCallingToolConfigure);
