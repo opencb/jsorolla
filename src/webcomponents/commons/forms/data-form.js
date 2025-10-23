5afdddf6e867c75e9d0d0e6c2710abd6d87f637f
Josemi <josemijuanes@gmail.com>
2025-10-23 13:46:05 +0200

    wc: add new input-parameters element in data-form #TASK-7977 #TASK-7645

--- src/webcomponents/commons/forms/data-form.js
+++ src/webcomponents/commons/forms/data-form.js
@@ -731,6 +731,10 @@ export default class DataForm extends LitElement {
                 case "object-list":
                     content = this._createObjectListElement(element);
                     break;
+                case "input-parameters":
+                case "parameters-list":
+                    content = this._createInputParametersElement(element);
+                    break;
                 default:
                     throw new Error("Element type not supported:" + element.type);
             }
@@ -1875,6 +1879,72 @@ export default class DataForm extends LitElement {
             contents.push(createHtml);
         }
         return this._createElementTemplate(element, null, contents);
+    }
+
+    _createInputParametersElement(element) {
+        return this._createObjectListElement({
+            title: element.title,
+            description: element.description,
+            field: element.field,
+            display: {
+                itemId: "name",
+                itemAddText: "Add parameter",
+                itemsNotFoundText: "No parameters registered.",
+                view: variable => html`
+                    <div class="">
+                        <b>${variable.name || ""}</b> ${variable.value ? html` = ${variable.value}` : nothing}
+                    </div>
+                `,
+                ...element.display,
+            },
+            elements: [
+                {
+                    title: "Parameter Name",
+                    field: `${element.field}[].name`,
+                    type: "input-text",
+                    display: {
+                        placeholder: "",
+                        helpMessage: "Add parameter name, eg: t, -t, or --threads. Parameters can include hyphen (-) or double hyphen (--) at the beginning.",
+                    }
+                },
+                // {
+                //     title: "Check if the parameter value is a file path.",
+                //     field: `${element.field}[].isFile`,
+                //     type: "checkbox",
+                // },
+                {
+                    title: "Parameter Value",
+                    field: `${element.field}[].value`,
+                    type: "input-text",
+                    // display: {
+                    //     visible: (data, item) => {
+                    //         return !item.isFile;
+                    //     },
+                    // }
+                },
+                // {
+                //     title: "Select File",
+                //     field: `${element.field}[].value`,
+                //     type: "custom",
+                //     display: {
+                //         visible: (data, item) => {
+                //             return item.isFile;
+                //         },
+                //         render: (data, dataFormFilterChange) => html`
+                //             <catalog-search-autocomplete
+                //                 .resource="${"FILE"}"
+                //                 .searchField="${"path"}"
+                //                 .config="${{
+                //                     multiple: false,
+                //                 }}"
+                //                 .opencgaSession="${this.opencgaSession}"
+                //                 @filterChange="${e => dataFormFilterChange(e.detail.value)}">
+                //             </catalog-search-autocomplete>
+                //         `,
+                //     },
+                // }
+            ],
+        });
     }
 
     #toggleEditItemOfObjectList(e, item, index, element) {
