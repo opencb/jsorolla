d0da9148fb0eb89972e412c64a4eca20e36e2570
Josemi <josemijuanes@gmail.com>
2025-11-05 17:23:59 +0100

    wc: center pipeline information in preprocessing toolbar #TASK-7977 #TASK-7645

--- src/webcomponents/clinical/preprocessing/clinical-preprocessing.js
+++ src/webcomponents/clinical/preprocessing/clinical-preprocessing.js
@@ -301,9 +301,9 @@ export default class ClinicalPreprocessing extends LitElement {
         const pipelineName = this._stepsParams?.pipeline?.name || "Untitled Pipeline";
 
         return html`
-            <div class="d-flex flex-column align-items-end w-full" style="max-width:320px;">
+            <div class="d-flex flex-column align-items-center w-full" style="max-width:320px;">
                 ${this._stepsParams?.pipeline ? html`
-                    <div class="fw-bold fs-3 w-full text-truncate" title="${pipelineName}">
+                    <div class="fw-bold fs-3 w-full text-truncate text-center" title="${pipelineName}">
                         <span>${pipelineName}</span>
                     </div>
                     <div class="d-flex align-items-center gap-2">
