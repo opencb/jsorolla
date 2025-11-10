08185ad4904031e5ebb02a88cf37f23a9c67e5c1
Josemi <josemijuanes@gmail.com>
2025-11-10 14:10:34 +0100

    wc: remove panels field from rd-tiering analysis form #TASK-7645

--- src/webcomponents/clinical/analysis/rd-tiering-analysis.js
+++ src/webcomponents/clinical/analysis/rd-tiering-analysis.js
@@ -53,18 +53,7 @@ export default class RdTieringAnalysis extends LitElement {
         this.DEFAULT_TOOLPARAMS = {};
 
         this._toolParams = UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS);
-
-        this.clinicalAnalysis = "";
-        this.diseasePanelIds = "";
         this._config = this.getDefaultConfig();
-    }
-
-    firstUpdated(changedProperties) {
-        if (changedProperties.has("toolParams")) {
-            // Save the initial clinicalAnalysis. Needed for onClear() method
-            this.clinicalAnalysis = this.toolParams.clinicalAnalysis || "";
-            this.diseasePanelIds = this.toolParams.panels || "";
-        }
     }
 
     update(changedProperties) {
@@ -94,8 +83,8 @@ export default class RdTieringAnalysis extends LitElement {
     onSubmit() {
         const toolParams = {
             clinicalAnalysis: this._toolParams.clinicalAnalysis || "",
-            panels: (this._toolParams.panels || "").split(","),
         };
+
         AnalysisUtils.submit(
             this.ANALYSIS_TITLE,
             this.opencgaSession.opencgaClient.clinical()
@@ -110,9 +99,7 @@ export default class RdTieringAnalysis extends LitElement {
     onClear() {
         this._toolParams = {
             ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-            // If a clinical analysis ID was passed (probably because we are in the interpreter) then we need to keep it
-            clinicalAnalysis: this.clinicalAnalysis,
-            panels: this.diseasePanelIds,
+            ...this._toolParams,
         };
         this._config = this.getDefaultConfig();
     }
@@ -144,50 +131,14 @@ export default class RdTieringAnalysis extends LitElement {
                                     .value="${clinicalAnalysis}"
                                     .resource="${"CLINICAL_ANALYSIS"}"
                                     .opencgaSession="${this.opencgaSession}"
-                                    .config="${{multiple: false, disabled: !!clinicalAnalysis}}"
-                                    @filterChange="${e => dataFormFilterChange(e.detail.value)}">
+                                    .config="${{
+                                        multiple: false,
+                                        disabled: !!this.toolParams?.clinicalAnalysis,
+                                    }}"
+                                    @filterChange="${event => dataFormFilterChange(event.detail.value)}">
                                 </catalog-search-autocomplete>
                             `,
                         },
-                    },
-                    {
-                        // QUESTION: not sure how panels need to be retrieved or how it works.
-                        //   - Once the clinical analysis id is selected, query its panels?
-                        //   - All the studies have panels?
-                        title: "Disease Panels",
-                        field: "panels",
-                        type: "custom",
-                        display: {
-                            render: (panels, dataFormFilterChange) => {
-                                // Get whether disease panels can be modified or are fixed
-                                const casePanelLock = !!this.clinicalAnalysis?.panelLocked;
-                                // Get the list of disease panels for the dropdown
-                                let diseasePanels = [];
-                                if (casePanelLock) {
-                                    for (const panelId of (panels || "").split(",")) {
-                                        const diseasePanel = this.opencgaSession.study?.panels?.find(p => p.id === panelId);
-                                        if (diseasePanel) {
-                                            diseasePanels.push(diseasePanel);
-                                        }
-                                    }
-                                } else {
-                                    diseasePanels = this.opencgaSession.study?.panels;
-                                }
-                                return html`
-                                    <select-field-filter
-                                        .data="${diseasePanels}"
-                                        .value=${panels || ""}
-                                        .config="${{
-                                            multiple: true,
-                                            liveSearch: diseasePanels?.length > 5,
-                                            disabled: casePanelLock,
-                                            separator: "\n"
-                                        }}"
-                                        @filterChange="${e => dataFormFilterChange(e.detail.value)}">
-                                    </select-field-filter>
-                                `;
-                            },
-                        }
                     },
                 ],
             }
