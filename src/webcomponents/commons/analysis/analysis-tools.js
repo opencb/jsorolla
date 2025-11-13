6e0748218281c1693d3315c010f8e8038a432aa8
Josemi <josemijuanes@gmail.com>
2025-11-13 10:46:47 +0100

    wc: fix imports of tool-analysis component #TASK-7689 #TASK-7610

--- src/webcomponents/commons/analysis/analysis-tools.js
+++ src/webcomponents/commons/analysis/analysis-tools.js
@@ -3,7 +3,7 @@ import "../tool-header.js";
 import "../view/vertical-menu.js";
 import "../../clinical/analysis/mutational-signature-analysis.js";
 import "../../clinical/analysis/rd-tiering-analysis.js";
-import "../../job/analysis/tool-analysis.js";
+import "../../workflow/analysis/tool-analysis.js";
 import "../../job/analysis/custom-tool-builder.js";
 import "../../variant/analysis/gwas-analysis.js";
 import "../../variant/analysis/sample-variant-stats-analysis.js";
