0e20f879ed26914b1d70852f4f47a778fc9c08d4
Josemi <josemijuanes@gmail.com>
2025-11-18 16:27:02 +0100

    wc: include the cellbase sources when creating an interpretation #TASK-7806 #TASK-7645

--- src/webcomponents/clinical/interpretation/clinical-interpretation-create.js
+++ src/webcomponents/clinical/interpretation/clinical-interpretation-create.js
@@ -122,6 +122,13 @@ export default class ClinicalInterpretationCreate extends LitElement {
     }
 
     onSubmit() {
+        // 1. prepare cellbase sources
+        const cellbaseSources = {};
+        (this.opencgaSession?.project?.cellbase?.sources || []).map(source => {
+            cellbaseSources[source.name] = source.version || source.date || "-";
+        });
+        
+        // 2. prepare method object with dependencies
         const data = {
             ...this.interpretation,
             method: {
@@ -135,19 +142,19 @@ export default class ClinicalInterpretationCreate extends LitElement {
                     },
                     {
                         name: "CellBase",
-                        version: this.opencgaSession.project?.cellbase?.version || "-",
+                        version: `${this.opencgaSession.project?.cellbase?.version} - DR ${this.opencgaSession.project?.cellbase?.dataRelease}`,
+                        params: cellbaseSources,
                     },
                 ],
             },
         };
 
+        // 2. create the clinical interpretation
         this.opencgaSession.opencgaClient.clinical().createInterpretation(this.clinicalAnalysis.id, data, {
             study: this.opencgaSession.study.fqn
         })
             .then(() => {
                 NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_SUCCESS, {
-                    title: "Clinical Interpretation created",
-                    // message: `The clinical interpretation ${response.responses[0].results[0].id} has been created successfully`,
                     message: "The new clinical interpretation has been created successfully",
                 });
                 this.notifyClinicalAnalysisWrite();
