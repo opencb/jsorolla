1416f9b645ed5a43e75433ced352cabc653d7125
Josemi <josemijuanes@gmail.com>
2025-11-25 17:39:07 +0100

    wc: add loading notification type #TASK-7645

--- src/webcomponents/commons/utils/notification-utils.js
+++ src/webcomponents/commons/utils/notification-utils.js
@@ -1,14 +1,17 @@
 // Notification utils class
 export default class NotificationUtils {
 
-    // Notification types
+    // notification types
     static NOTIFY = "notify";
     static NOTIFY_INFO = "notifyInfo";
     static NOTIFY_SUCCESS = "notifySuccess";
     static NOTIFY_WARNING = "notifyWarning";
     static NOTIFY_ERROR = "notifyError";
     static NOTIFY_RESPONSE = "notifyResponse";
+
+    // other notification types
     static NOTIFY_CONFIRMATION = "notifyConfirmation";
+    static NOTIFY_LOADING = "notify:loading";
 
     // Dispatch a notification event
     static dispatch(self, type, value) {
