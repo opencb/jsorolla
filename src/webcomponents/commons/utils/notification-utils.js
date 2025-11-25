89d667e711823581e7f8a17bf60a93d6911ae7d8
Josemi <josemijuanes@gmail.com>
2025-11-25 17:55:32 +0100

    wc: add utility to clear notifications and return the notification id #TASK-7645

--- src/webcomponents/commons/utils/notification-utils.js
+++ src/webcomponents/commons/utils/notification-utils.js
@@ -13,10 +13,39 @@ export default class NotificationUtils {
     static NOTIFY_CONFIRMATION = "notifyConfirmation";
     static NOTIFY_LOADING = "notify:loading";
 
+    // notification actions
+    static NOTIFY_CLEAR = "notify:clear";
+
+    // internal method to generate a unique id for each notification
+    static _generateId() {
+        return `notification:${Date.now()}:${Math.floor(Math.random() * 1000)}`;
+    }
+
     // Dispatch a notification event
-    static dispatch(self, type, value) {
+    static dispatch(self, type, value = {}) {
+        // 1. generate an unique identifier for the notification
+        const notificationId = NotificationUtils._generateId();
+
+        // 2. dispatch the notification event
         self.dispatchEvent(new CustomEvent(type, {
-            detail: value,
+            detail: {
+                id: notificationId,
+                ...value,
+            },
+            bubbles: true,
+            composed: true,
+        }));
+
+        // 3. return the notification id
+        return notificationId;
+    }
+
+    // clear notifications
+    static clear(self, notificationId = null) {
+        self.dispatchEvent(new CustomEvent(this.NOTIFY_CLEAR, {
+            detail: {
+                id: notificationId,
+            },
             bubbles: true,
             composed: true,
         }));
