73e60407babb1e5cbc69909dbb1900d5b1303b7d
Josemi <josemijuanes@gmail.com>
2025-12-03 18:19:26 +0100

    wc: display number of evidences reviewed in clinical variant review #TASK-7645

--- src/webcomponents/clinical/variant/clinical-variant-review.js
+++ src/webcomponents/clinical/variant/clinical-variant-review.js
@@ -235,6 +235,7 @@ export default class ClinicalVariantReview extends LitElement {
     renderVariantInfo() {
         const displayConsequenceType = this.variant?.annotation?.displayConsequenceType;
         const genes = VariantUtils.getGenes(this.variant);
+        const selectedEvidencesCount = (this._variant?.evidences?.filter(evidence => evidence?.review?.select) || []).length;
 
         return html`
             <div class="alert alert-light bg-white flex-grow-1 d-flex justify-content-center flex-column">
@@ -247,11 +248,12 @@ export default class ClinicalVariantReview extends LitElement {
                         </span>
                     ` : nothing}
                 </div>
-                ${displayConsequenceType ? html`
-                    <div class="mt-1 d-flex align-items-center flex-wrap column-gap-2" style="max-width:900px;">
+                <div class="mt-1 d-flex align-items-center flex-wrap gap-3">
+                    ${displayConsequenceType ? html`
                         <span style="color:${CONSEQUENCE_TYPES.style[CONSEQUENCE_TYPES.impact[displayConsequenceType]] || "black"}">${displayConsequenceType}</span>
-                    </div>
-                ` : nothing}
+                    ` : nothing}
+                    <span class="fw-bold">${selectedEvidencesCount} Evidence${selectedEvidencesCount > 1 ? "s" : ""} Reviewed</span>
+                </div>
             </div>
         `;
     }
