1547470efb401fedf502c31eab4e2069920e01cf
Josemi <josemijuanes@gmail.com>
2025-12-09 18:23:33 +0100

    wc: refactor tertiary-tools to support seleting a tool from a select component #TASK-8076

--- src/webcomponents/clinical/tertiary/clinical-tertiary-tools.js
+++ src/webcomponents/clinical/tertiary/clinical-tertiary-tools.js
@@ -1,8 +1,11 @@
 import {LitElement, html, nothing} from "lit";
 import UtilsNew from "../../../core/utils-new.js";
 import "../../commons/forms/data-form.js";
+import "../../workflow/analysis/tool-executor.js";
 
 export default class ClinicalTertiaryTools extends LitElement {
+
+
 
     constructor() {
         super();
@@ -29,15 +32,18 @@ export default class ClinicalTertiaryTools extends LitElement {
 
     #init() {
         this.DEFAULT_TOOLPARAMS = {
-            tools: [],
-            newToolName: "",
-            newToolVersion: "",
+            toolId: "",
         };
+        this._tools = [];
         this._toolParams = UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS);
         this._config = this.getDefaultConfig();
     }
 
     update(changedProperties) {
+        if (changedProperties.has("opencgaSession")) {
+            this.fetchTools();
+        }
+
         if (changedProperties.has("toolParams")) {
             this.toolParamsObserver();
         }
@@ -51,23 +57,41 @@ export default class ClinicalTertiaryTools extends LitElement {
 
     toolParamsObserver() {
         if (this.toolParams) {
-            this._toolParams = {...this.DEFAULT_TOOLPARAMS, ...this.toolParams};
-        } else {
-            this._toolParams = UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS);
+            this._toolParams = {
+                ...this.DEFAULT_TOOLPARAMS,
+                ...this.toolParams,
+            };
+        }
+    }
+
+    fetchTools() {
+        if (this.opencgaSession) {
+            this.opencgaSession.opencgaClient.userTool()
+                .search({
+                    scope: "CLINICAL_INTERPRETATION_ANALYSIS",
+                    study: this.opencgaSession.study.fqn,
+                })
+                .then(response => {
+                    this._tools = response.responses[0].results || [];
+                    this._config = this.getDefaultConfig();
+                    this.requestUpdate();
+                })
+                .catch(response => {
+                    console.error("An error occurred fetching tools: ", response);
+                });
         }
     }
 
     onFieldChange(event) {
-        const {field, value} = event.detail;
+        this._toolParams = {...this._toolParams};
+        this.requestUpdate();
+    }
+
+    onToolExecutorChange(event) {
         this._toolParams = {
             ...this._toolParams,
-            [field]: value,
+            ...event.detail,
         };
-        this.dispatchEvent(new CustomEvent("paramsChange", {
-            detail: this._toolParams,
-            bubbles: true,
-            composed: true,
-        }));
     }
 
     onClear() {
@@ -99,100 +123,37 @@ export default class ClinicalTertiaryTools extends LitElement {
             },
             sections: [
                 {
-                    title: "Tools Configuration",
                     elements: [
                         {
-                            title: "Select Tools",
-                            field: "tools",
-                            type: "table",
+                            name: "Select Clinical Analysis Tool",
+                            field: "toolId",
+                            type: "select",
+                            allowedValues: this._tools.map(t => t.id),
                             display: {
-                                getData: data => data?.tools || [],
-                                className: "table-borderless table-grid mb-0",
-                                defaultValue: "No tools selected.",
-                                columns: [
-                                    {
-                                        title: "Tool Name",
-                                        field: "name",
-                                    },
-                                    {
-                                        title: "Version",
-                                        field: "version",
-                                    },
-                                    {
-                                        title: "Enabled",
-                                        field: "enabled",
-                                        type: "custom",
-                                        display: {
-                                            render: (enabled, dataFormFieldChange, updateParams, data, row) => html`
-                                                <input
-                                                    type="checkbox"
-                                                    class="form-check-input"
-                                                    ?checked="${enabled || false}"
-                                                    @change="${event => {
-                                                        const updatedTools = (data?.tools || []).map(tool => 
-                                                            tool.name === row.name ? {...tool, enabled: event.target.checked} : tool
-                                                        );
-                                                        dataFormFieldChange(updatedTools);
-                                                    }}">
-                                            `,
-                                        },
-                                    },
-                                ],
+                                placeholder: "Select a tool...",
                             },
                         },
                         {
-                            title: "Add Tool",
+                            name: "Tool Configuration",
                             type: "custom",
                             display: {
-                                render: () => {
-                                    return html`
-                                        <div class="d-flex gap-2">
-                                            <input
-                                                type="text"
-                                                class="form-control"
-                                                placeholder="Tool name"
-                                                .value="${this._toolParams.newToolName || ""}"
-                                                @input="${e => {
-                                                    this._toolParams.newToolName = e.target.value;
-                                                    this.requestUpdate();
-                                                }}">
-                                            <input
-                                                type="text"
-                                                class="form-control"
-                                                placeholder="Version"
-                                                .value="${this._toolParams.newToolVersion || ""}"
-                                                @input="${e => {
-                                                    this._toolParams.newToolVersion = e.target.value;
-                                                    this.requestUpdate();
-                                                }}">
-                                            <button
-                                                class="btn btn-primary"
-                                                @click="${() => {
-                                                    if (this._toolParams.newToolName) {
-                                                        const updatedTools = [...(this._toolParams.tools || []), {
-                                                            name: this._toolParams.newToolName,
-                                                            version: this._toolParams.newToolVersion || "",
-                                                            enabled: true,
-                                                        }];
-                                                        this._toolParams = {
-                                                            ...this._toolParams,
-                                                            tools: updatedTools,
-                                                            newToolName: "",
-                                                            newToolVersion: "",
-                                                        };
-                                                        this.dispatchEvent(new CustomEvent("paramsChange", {
-                                                            detail: this._toolParams,
-                                                            bubbles: true,
-                                                            composed: true,
-                                                        }));
-                                                        this.requestUpdate();
-                                                    }
-                                                }}">
-                                                Add
-                                            </button>
-                                        </div>
-                                    `;
-                                },
+                                visible: data => !!data.toolId,
+                                render: (fieldValue, dataFormChange, updateParams, data) => html`
+                                    <tool-executor
+                                        .opencgaSession="${this.opencgaSession}"
+                                        .toolId="${data.toolId}"
+                                        .toolParams="${{
+                                            variables: {
+                                                clinicalAnalysisId: "",
+                                            },
+                                        }}"
+                                        .displayConfig="${{
+                                            titleVisible: false,
+                                            buttonsVisible: false,
+                                        }}"
+                                        @toolParamsChange="${event => this.onToolExecutorChange(event)}">
+                                    </tool-executor>
+                                `,
                             },
                         },
                     ],
