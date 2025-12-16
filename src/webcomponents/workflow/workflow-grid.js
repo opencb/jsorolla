e834bd4e51767a87eb76b416119cc25ce5198e0a
Josemi <josemijuanes@gmail.com>
2025-12-16 14:21:49 +0100

    wc: fix disabling execute button in workflow grid #TASK-7610

--- src/webcomponents/workflow/workflow-grid.js
+++ src/webcomponents/workflow/workflow-grid.js
@@ -543,8 +543,9 @@ export default class WorkflowGrid extends LitElement {
                 title: "Execute",
                 field: "execute",
                 formatter: _ => {
+                    const hasExecutePermission = this.gridCommons.hasPermission("EXECUTE", "JOB");
                     return `
-                        <a class="btn btn-primary cursor-pointer d-inline-flex align-items-center gap-1" data-action="execute">
+                        <a class="btn btn-primary cursor-pointer d-inline-flex align-items-center gap-1 ${!hasExecutePermission ? "disabled" : ""}" data-action="execute">
                             <i class="fas fa-play me-1"></i>
                             <span>Execute</span>
                         </a>
