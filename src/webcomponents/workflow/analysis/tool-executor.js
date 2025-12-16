8113c92f875dfb09be89d145eb3aff689f5fce83
Josemi <josemijuanes@gmail.com>
2025-12-16 12:14:32 +0100

    wc: fix clearing tool executor form #TASK-7610

--- src/webcomponents/workflow/analysis/tool-executor.js
+++ src/webcomponents/workflow/analysis/tool-executor.js
@@ -81,7 +81,7 @@ export default class UserToolExecutor extends LitElement {
     toolParamsObserver() {
         this._toolParams = {
             ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-            ...UtilsNew.objectClone(this.toolParams),
+            ...UtilsNew.objectClone(this.toolParams || {}),
         };
         this.addToolVariablesToParams();
     }
@@ -219,8 +219,9 @@ export default class UserToolExecutor extends LitElement {
     onClear() {
         this._toolParams = {
             ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-            ...UtilsNew.objectClone(this.toolParams),
+            ...UtilsNew.objectClone(this.toolParams || {}),
         };
+        debugger;
         this.addToolVariablesToParams();
         this._config = this.getDefaultConfig();
         this.requestUpdate();
