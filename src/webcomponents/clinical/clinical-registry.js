e3b3806235d2632868ba651d39383efe8c0df458
Josemi <josemijuanes@gmail.com>
2026-01-12 18:35:14 +0100

    wc: fix typo checking the family to create the clinical analysis #TASK-8104 #TASK-8076

--- src/webcomponents/clinical/clinical-registry.js
+++ src/webcomponents/clinical/clinical-registry.js
@@ -454,7 +454,7 @@ export default class ClinicalRegistry extends LitElement {
                         };
 
                         // 6.3. check if we have to create a family case or is just a single case
-                        const family = mapping.some(m => m.case === clinicalAnalysisId && !!m.family)?.family;
+                        const family = mapping.find(m => m.case === clinicalAnalysisId && !!m.family)?.family;
                         if (family) {
                             const familyMembers = mapping.filter(m => m.family === family && m.individual);
                             const proband = familyMembers.find(member => {
