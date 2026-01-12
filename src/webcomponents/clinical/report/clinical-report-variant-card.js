b6ac00b985c3c294e2730c04931d9cf6ff434fdc
Josemi <josemijuanes@gmail.com>
2026-01-12 12:44:00 +0100

    wc: fix text overflow in variant cards of reported variants #TASK-7645

--- src/webcomponents/clinical/report/clinical-report-variant-card.js
+++ src/webcomponents/clinical/report/clinical-report-variant-card.js
@@ -108,12 +108,12 @@ export default class ClinicalReportVariantCard extends LitElement {
                                 render: data => html`
                                     <div class="d-flex justify-content-between align-items-center">
                                         <div class="d-flex align-items-center">
-                                            <a class="link fw-bold" @click="${event => this.onVariantInfo(event)}">
+                                            <a class="link fw-bold text-truncate d-block" style="max-width:240px;" @click="${event => this.onVariantInfo(event)}">
                                                 <span class="fs-5">${data.id}</span>
                                             </a>
                                         </div>
                                         <div class="d-flex align-items-center gap-2">
-                                            <button class="btn btn-sm btn-light" @click="${event => this.onVariantReviewUpdate(event)}">
+                                            <button class="btn btn-sm btn-light text-nowrap" @click="${event => this.onVariantReviewUpdate(event)}">
                                                 <i class="fa fa-edit pe-1"></i>
                                                 <span>Update Review</span>
                                             </button>
