103623e24b0b71594aa40a8320252ca2c9881206
Josemi <josemijuanes@gmail.com>
2026-01-14 13:37:45 +0100

    wc: remove commented code in clinical-tertiary-select #TASK-8104 #TASK-8076

--- src/webcomponents/clinical/tertiary/clinical-tertiary-select.js
+++ src/webcomponents/clinical/tertiary/clinical-tertiary-select.js
@@ -1,13 +1,11 @@
 import {html, LitElement, nothing} from "lit";
 import UtilsNew from "../../../core/utils-new.js";
 import LitUtils from "../../commons/utils/lit-utils.js";
-import DataFormElements from "../../commons/forms/data-form-elements.js";
 import "../../commons/forms/data-form.js";
 import "../../commons/filters/catalog-search-autocomplete.js";
 import "../../commons/filters/disease-panel-filter.js";
 import "../filters/clinical-flag-filter.js";
 import CatalogGridFormatter from "../../commons/catalog-grid-formatter.js";
-import NotificationUtils from "../../commons/utils/notification-utils.js";
 import GridCommons from "../../commons/grid-commons";
 import WebUtils from "../../commons/utils/web-utils";
 
@@ -24,8 +22,8 @@ export default class ClinicalTertiarySelect extends LitElement {
 
     static get properties() {
         return {
-            toolParams: {
-                type: Object,
+            selectedClinicalAnalyses: {
+                type: Array,
             },
             opencgaSession: {
                 type: Object,
@@ -39,35 +37,16 @@ export default class ClinicalTertiarySelect extends LitElement {
     #init() {
         this._prefix = UtilsNew.randomString(8);
         this.gridId = this._prefix + "ClinicalTertiarySelectGrid";
-
-        this.DEFAULT_TOOLPARAMS = {
-            // mode: "Single", // Single, Batch
-            // selectionType: "Sample",
-            // sample: null,
-            // samples: [],
-            // caseId: "",
-            // panels: [],
-            // flags: "",
-            // type: "SINGLE",
-            // disorders: [],
-            // mappingFile: "",
-
-            selectedClinicalAnalyses: []
-        };
-
-        // this.clinicalAnalyses = [];
-
-        this._toolParams = UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS);
         this._config = this.getDefaultConfig();
     }
 
     update(changedProperties) {
-        if (changedProperties.has("toolParams")) {
-            this.toolParamsObserver();
-        }
-        if (changedProperties.has("displayConfig")) {
-            this._config = this.getDefaultConfig();
-        }
+        // if (changedProperties.has("toolParams")) {
+        //     this.toolParamsObserver();
+        // }
+        // if (changedProperties.has("displayConfig")) {
+        //     this._config = this.getDefaultConfig();
+        // }
         if (changedProperties.has("opencgaSession")) {
             this.propertyObserver();
         }
@@ -75,52 +54,14 @@ export default class ClinicalTertiarySelect extends LitElement {
     }
 
     propertyObserver() {
-        // With each property change we must update config and create the columns again. No extra checks are needed.
-        this._config = {
-            ...this.getDefaultConfig(),
-            ...this.config
-        };
+        this._config = this.getDefaultConfig();
         this.gridCommons = new GridCommons(this.gridId, this, this._config);
     }
 
     updated(changedProperties) {
-        if (changedProperties.size > 0 && this.active) {
+        if (changedProperties.size > 0) {
             this.renderRemoteTable();
         }
-        this.renderRemoteTable();
-    }
-
-    toolParamsObserver() {
-        this._toolParams = {
-            ...UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS),
-            ...this.toolParams,
-        };
-    }
-
-    dispatchChange() {
-        LitUtils.dispatchCustomEvent(this, "paramsChange", null, {
-            ...this._toolParams,
-        });
-    }
-
-    notifySelectClinicalAnalyses() {
-        LitUtils.dispatchCustomEvent(this, "clinicalAnalysesChange", {
-            clinicalAnalyses: this._toolParams.selectedClinicalAnalyses,
-        });
-    }
-
-    onFieldChange(event) {
-        this._toolParams = {
-            ...this._toolParams,
-        };
-
-        // this.requestUpdate();
-        // this.dispatchChange();
-    }
-
-    onClear() {
-        this._toolParams = UtilsNew.objectClone(this.DEFAULT_TOOLPARAMS);
-        this.requestUpdate();
     }
 
     fetchData(query) {
@@ -129,10 +70,10 @@ export default class ClinicalTertiarySelect extends LitElement {
 
     renderRemoteTable() {
         if (this.opencgaSession?.opencgaClient && this.opencgaSession?.study?.fqn) {
-            if (this.lastFilters && JSON.stringify(this.lastFilters) === JSON.stringify(this.query)) {
-                // Abort destroying and creating again the grid. The filters have not changed
-                return;
-            }
+            // if (this.lastFilters && JSON.stringify(this.lastFilters) === JSON.stringify(this.query)) {
+            //     // Abort destroying and creating again the grid. The filters have not changed
+            //     return;
+            // }
             this._columns = this._getDefaultColumns();
             this.table = $("#" + this.gridId);
             this.table.bootstrapTable("destroy");
@@ -228,18 +169,6 @@ export default class ClinicalTertiarySelect extends LitElement {
             });
         }
     }
-
-    // removeRowTable(clinicalAnalysisId) {
-    //     const data = this.table.bootstrapTable("getData");
-    //     this.table.bootstrapTable("remove", {
-    //         field: "id",
-    //         values: [clinicalAnalysisId]
-    //     });
-    //     if (data?.length === 0) {
-    //         this.table.bootstrapTable("prevPage");
-    //         this.table.bootstrapTable("refresh");
-    //     }
-    // }
 
     _getDefaultColumns() {
         this._columns = [
@@ -536,302 +465,11 @@ export default class ClinicalTertiarySelect extends LitElement {
 
     getDefaultConfig() {
         return {
-            readOnlyMode: false, // it hides priority and status selectors even if the user has permissions
             pagination: true,
             pageSize: 10,
             pageList: [5, 10, 25],
-
-            showToolbar: true,
-            showActions: true,
-
-            showExport: true,
-            showSettings: true,
-            exportTabs: ["download", "link", "code"],
         };
     }
-
-
-
-
-
-
-
-
-    // async onSelectSamples(resource, value) {
-    //     let resourcePromise = null;
-    //
-    //     // If not value, reset samples
-    //     if (!value) {
-    //         this._toolParams = {
-    //             ...this._toolParams,
-    //             samples: [],
-    //         };
-    //         this.requestUpdate();
-    //         return;
-    //     }
-    //
-    //     switch (resource) {
-    //         case "Sample":
-    //             resourcePromise = await this.opencgaSession.opencgaClient.samples()
-    //                 .search({
-    //                     id: value,
-    //                     study: this.opencgaSession.study.fqn,
-    //                     include: "id,individualId,somatic,internal.status.id",
-    //                     includeIndividual: true,
-    //                 });
-    //             // .then(response => {
-    //             //     return response?.responses?.[0]?.results || [];
-    //             // });
-    //             break;
-    //         case "Cohort":
-    //             resourcePromise = await this.opencgaSession.opencgaClient.samples()
-    //                 .search({
-    //                     cohortIds: value,
-    //                     study: this.opencgaSession.study.fqn,
-    //                     include: "id,individualId,somatic,internal.status.id",
-    //                     includeIndividual: true,
-    //                 });
-    //             // .then(response => {
-    //             //     return response?.responses?.[0]?.results?.[0]?.samples || [];
-    //             // });
-    //             break;
-    //     }
-    //
-    //
-    //     const samples = resourcePromise.getResults() || [];
-    //
-    //     // Now we need to read samples.individualId and fetch clinical analysis case if any
-    //     const individualIds = samples
-    //         .map(sample => sample.individualId)
-    //         .filter((value, index, self) => self.indexOf(value) === index); // unique values
-    //
-    //     if (individualIds.length > 0) {
-    //         // 1. fetch clinical analysis for the individuals
-    //         const clinicalResponse = await this.opencgaSession.opencgaClient.clinical()
-    //             .search({
-    //                 proband: individualIds.join(","),
-    //                 study: this.opencgaSession.study.fqn,
-    //                 include: "id,type,proband.id",
-    //             });
-    //         const clinicalCases = clinicalResponse?.responses?.[0]?.results || [];
-    //
-    //         // 2. fetch families for the individuals
-    //         const familiesResponse = await this.opencgaSession.opencgaClient.families()
-    //             .search({
-    //                 members: individualIds.join(","),
-    //                 study: this.opencgaSession.study.fqn,
-    //                 include: "id,members.id",
-    //             });
-    //         const families = familiesResponse?.responses?.[0]?.results || [];
-    //
-    //         // 3. add clinical analysis and family info to each sample
-    //         samples.forEach(sample => {
-    //             const caseForSample = clinicalCases.find(ca => ca.proband?.id === sample.individualId);
-    //             const familyForSample = families.find(family =>
-    //                 family.members?.some(member => member.id === sample.individualId)
-    //             );
-    //             sample.familyId = familyForSample?.id || null;
-    //             sample.clinicalAnalysisId = caseForSample?.id || null;
-    //         });
-    //     }
-    //
-    //     this._toolParams = {
-    //         ...this._toolParams,
-    //         samples: samples || [],
-    //     };
-    //
-    //     this.requestUpdate();
-    //     this.dispatchChange();
-    // }
-
-    // renderSelection(selectionType, allowedSelectionTypes) {
-    //     return html`
-    //         <div class="input-group flex-nowrap">
-    //             <catalog-search-autocomplete
-    //                 class="flex-grow-1"
-    //                 .resource="${selectionType.toUpperCase()}"
-    //                 .opencgaSession="${this.opencgaSession}"
-    //                 .config="${{
-    //                     multiple: false,
-    //                 }}"
-    //                 @filterChange="${event => this.onSelectSamples(selectionType, event.detail.value)}">
-    //             </catalog-search-autocomplete>
-    //             <button class="btn btn-outline-secondary dropdown-toggle mb-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    //                 <span>${selectionType}</span>
-    //             </button>
-    //             <div class="dropdown-menu dropdown-menu-end">
-    //                 ${allowedSelectionTypes.map(type => html`
-    //                     <div class="dropdown-item ${selectionType === type ? "active" : "cursor-pointer"}">
-    //                         ${type}
-    //                     </div>
-    //                 `)}
-    //             </div>
-    //         </div>
-    //     `;
-    // }
-
-    // getClinicalAnalysis() {
-    //     this.opencgaSession.opencgaClient.clinical()
-    //         .search({
-    //             // proband: individualIds.join(","),
-    //             study: this.opencgaSession.study.fqn,
-    //             include: "id,type,proband.id",
-    //         })
-    //         .then(response => {
-    //             this.clinicalAnalyses = response?.responses?.[0]?.results || [];
-    //         })
-    //         .catch(() =>
-    //             NotificationUtils.dispatch(this, NotificationUtils.NOTIFY_ERROR, {
-    //                 message: "Error fetching clinical analyses",
-    //             }))
-    //         .finally(() => {
-    //             this._toolParams = {
-    //                 ...this._toolParams,
-    //             };
-    //             this.requestUpdate();
-    //         });
-    // }
-    //
-    // render() {
-    //     if (!this.opencgaSession) {
-    //         return nothing;
-    //     }
-    //
-    //     return html`
-    //         <data-form
-    //             .data="${this._toolParams}"
-    //             .config="${this._config}"
-    //             @fieldChange="${event => this.onFieldChange(event)}"
-    //             @clear="${event => this.onClear(event)}">
-    //         </data-form>
-    //     `;
-    // }
-    //
-    // getDefaultConfig() {
-    //     return {
-    //         title: "Analysis Selection",
-    //         display: {
-    //             titleClassName: "mb-4",
-    //             ...this.displayConfig,
-    //         },
-    //         sections: [
-    //             // {
-    //             //     // title: "Select Analysis Mode",
-    //             //     elements: [
-    //             //         DataFormElements.tabsElement({
-    //             //             field: "mode",
-    //             //             tabs: [
-    //             //                 {id: "Single", text: "Single"},
-    //             //                 {id: "Batch", text: "Batch"},
-    //             //             ],
-    //             //         }),
-    //             //     ],
-    //             // },
-    //             {
-    //                 title: "Batch Analysis Configuration",
-    //                 display: {
-    //                 },
-    //                 elements: [
-    //                     {
-    //                         title: "Select Samples",
-    //                         field: "selectionType",
-    //                         type: "custom",
-    //                         display: {
-    //                             render: (selectionType) => {
-    //                                 return this.renderSelection(selectionType, ["Cohort"]);
-    //                             },
-    //                             helpMessage: "Select samples by Cohort.",
-    //                         },
-    //                     },
-    //                     {
-    //                         type: "table",
-    //                         // field: "samples",
-    //                         display: {
-    //                             getData: () => this.clinicalAnalyses || [],
-    //                             // visible: data => data?.samples?.length > 0,
-    //                             columns: [
-    //                                 {
-    //                                     title: "Sample",
-    //                                     field: "id",
-    //                                     type: "custom",
-    //                                     display: {
-    //                                         bodyCellClassName: "align-middle",
-    //                                         render: (sampleId, updateField, updateParams, data, row) => {
-    //                                             return html`
-    //                                                 <div class="fw-bold my-1">${sampleId}</div>
-    //                                                 <div class="text-secondary small my-1">${row?.somatic ? "Somatic" : "Germline"}</div>
-    //                                             `;
-    //                                         },
-    //                                     },
-    //                                 },
-    //                                 {
-    //                                     title: "Individual",
-    //                                     field: "individualId",
-    //                                     type: "custom",
-    //                                     display: {
-    //                                         bodyCellClassName: "align-middle",
-    //                                         render: (individualId, updateField, updateParams, data, row) => {
-    //                                             const individual = row?.attributes?.OPENCGA_INDIVIDUAL;
-    //                                             // const sexHtml = CatalogGridFormatter.sexFormatter(individual.sex, individual);
-    //                                             return html`
-    //                                                 <div class="fw-bold my-1">${individualId || "-"}</div>
-    //                                             `;
-    //                                         }
-    //                                     },
-    //                                 },
-    //                                 {
-    //                                     title: "Family",
-    //                                     field: "familyId",
-    //                                     type: "custom",
-    //                                     display: {
-    //                                         bodyCellClassName: "align-middle",
-    //                                         render: (familyId) => {
-    //                                             return html`
-    //                                                 <span class="fw-bold">${familyId || "-"}</span>
-    //                                             `;
-    //                                         }
-    //                                     },
-    //                                 },
-    //                                 {
-    //                                     title: "Clinical Analysis",
-    //                                     field: "clinicalAnalysisId",
-    //                                     type: "custom",
-    //                                     display: {
-    //                                         headerCellClassName: "text-center",
-    //                                         bodyCellClassName: "align-middle",
-    //                                         render: (clinicalAnalysisId) => {
-    //                                             return html`
-    //                                                 <div class="w-full d-flex justify-content-center">
-    //                                                     <i class="fas fs-5 ${!!clinicalAnalysisId ? "fa-check text-success" : "fa-times"}"></i>
-    //                                                 </div>
-    //                                             `;
-    //                                         },
-    //                                     },
-    //                                 },
-    //                                 {
-    //                                     title: "Select",
-    //                                     type: "custom",
-    //                                     display: {
-    //                                         headerCellClassName: "text-center",
-    //                                         bodyCellClassName: "align-middle",
-    //                                         render: (sample, onFieldChange) => {
-    //                                             return html`
-    //                                                 <div class="w-full d-flex justify-content-center">
-    //                                                     <input type="checkbox" class="form-check-input" ?checked="${true}">
-    //                                                 </div>
-    //                                             `;
-    //                                         },
-    //                                     },
-    //                                 },
-    //                             ],
-    //                         },
-    //                     },
-    //                 ],
-    //             },
-    //         ],
-    //     };
-    // }
-
 }
 
 customElements.define("clinical-tertiary-select", ClinicalTertiarySelect);
