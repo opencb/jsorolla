94b4fb7ab0a2203a3fa33c5adde3d19193042067
Josemi <josemijuanes@gmail.com>
2026-02-03 11:17:10 +0100

    wc: fix usage of hgvs formatter and link generator in some components #TASK-8140

--- src/webcomponents/variant/variant-browser-grid.js
+++ src/webcomponents/variant/variant-browser-grid.js
@@ -548,7 +548,11 @@ export default class VariantBrowserGrid extends LitElement {
                     title: "HGVS",
                     rowspan: 2,
                     colspan: 1,
-                    formatter: (value, row) => VariantGridFormatter.hgvsFormatter(row, this._config),
+                    formatter: (value, row) => {
+                        const species = this.opencgaSession?.project?.organism?.scientificName;
+                        const assembly = this.opencgaSession.project.organism.assembly;
+                        return VariantGridFormatter.hgvsFormatter(row, this._config, species, assembly);
+                    },
                     visible: this.gridCommons.isColumnVisible("hgvs"),
                 },
                 {
