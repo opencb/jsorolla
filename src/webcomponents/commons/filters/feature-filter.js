134cc12ae98c60e296f82bfd7bfa37a4bb811058
Josemi <josemijuanes@gmail.com>
2026-02-25 12:04:24 +0100

    wc: migrate feature-filter to new token-dropdown

--- src/webcomponents/commons/filters/feature-filter.js
+++ src/webcomponents/commons/filters/feature-filter.js
@@ -15,7 +15,8 @@
  */
 
 import {LitElement, html} from "lit";
-import "../../commons/forms/select-token-filter.js";
+import LitUtils from "../utils/lit-utils.js";
+import "../../commons/forms/token-dropdown.js";
 import "../../commons/forms/text-field-filter.js";
 
 export default class FeatureFilter extends LitElement {
@@ -47,131 +48,121 @@ export default class FeatureFilter extends LitElement {
 
     _init() {
         this.separator = ",";
+        this._value = null;
+        this._config = this.getDefaultConfig();
     }
 
-    connectedCallback() {
-        super.connectedCallback();
-        this._config = {...this.getDefaultConfig(), ...this.config};
-
-    }
-
-    updated(changedProperties) {
-        // TODO temp solution to handle CB being not available https://github.com/opencb/jsorolla/issues/426
-        if (changedProperties.has("cellbaseClient")) {
-            this.cellbaseClient.getMeta("about").then(() => {
-                this.cellbaseAvailable = true;
-            }).catch(()=> {
-                this.cellbaseAvailable = false;
-            }).finally(()=> {
-                this.requestUpdate();
-            });
-        }
-        // XRefs, Gene and Variant Ids
+    update(changedProperties) {
         if (changedProperties.has("query")) {
             if (this.query.xref) {
-                this.value = this.query.xref;
+                this._value = this.query.xref;
             } else if (this.query.ids) {
-                this.value = this.query.ids;
+                this._value = this.query.ids;
             } else if (this.query.gene) {
-                this.value = this.query.gene;
+                this._value = this.query.gene;
             } else if (this.query.geneName) {
-                this.value = this.query.geneName;
+                this._value = this.query.geneName;
             } else {
-                this.value = null;
+                this._value = null;
             }
-            this.requestUpdate();
+        }
+        super.update(changedProperties);
+    }
+
+    onFilterChange(event) {
+        event.stopPropagation();
+        LitUtils.dispatchCustomEvent(this, "filterChange", event.detail.value);
+    }
+
+    async onFetch(params, success, failure) {
+        try {
+            const queryTerm = params?.query?.toUpperCase();
+            let restResponse;
+
+            if (this.cellbaseClient) {
+                if (queryTerm) {
+                    restResponse = await this.cellbaseClient.get("feature", "gene", queryTerm, "startsWith", {
+                        limit: this._config.limit,
+                    });
+                } else {
+                    restResponse = await this.cellbaseClient.get("feature", "gene", "search", "", {
+                        limit: this._config.limit,
+                    });
+                }
+
+                const results = restResponse.responses[0].results.map(feature => ({
+                    id: feature.id,
+                    name: feature.name,
+                }));
+                success(results);
+            } else {
+                // Genes belong to Disease Panels
+                const query = {
+                    study: this.opencgaSession.study.fqn,
+                    include: "genes",
+                    limit: this._config.limit,
+                    skip: 0
+                };
+
+                if (queryTerm) {
+                    restResponse = await this.opencgaSession.opencgaClient.panels().search({
+                        ...query,
+                        genes: `~/^${queryTerm}/i`,
+                    });
+                } else {
+                    restResponse = await this.opencgaSession.opencgaClient.panels().search(query);
+                }
+
+                const results = restResponse.getResults().flatMap(panel => (panel.genes || []).map(gene => ({
+                    id: gene.id,
+                    name: gene.name,
+                })));
+                success(results);
+            }
+        } catch (e) {
+            failure(e);
         }
     }
 
-    onFilterChange(value) {
-        const event = new CustomEvent("filterChange", {
-            detail: {
-                value: value
-            }
-        });
-        this.dispatchEvent(event);
+    renderItem(item) {
+        return html`
+            <div class="d-flex flex-column my-1">
+                <span class="fw-bold">${item.name}</span>
+                <span class="small text-secondary">${item.id}</span>
+            </div>
+        `;
+    }
+
+    render() {
+        if (this.cellbaseClient) {
+            return html`
+                <token-dropdown
+                    .value="${this._value}"
+                    .placeholder="${this._config.placeholder}"
+                    .fetch="${(params, success, failure) => this.onFetch(params, success, failure)}"
+                    .renderItem="${item => this.renderItem(item)}"
+                    ?disabled="${this._config.disabled}"
+                    ?editable="${this._config.freeTag}"
+                    @filterChange="${event => this.onFilterChange(event)}">
+                </token-dropdown>
+            `;
+        } else {
+            return html`
+                <text-field-filter
+                    .value="${this._value}"
+                    .config=${this._config}
+                    @filterChange="${e => this.onFilterChange(e.detail.value)}">
+                </text-field-filter>
+            `;
+        }
     }
 
     getDefaultConfig() {
         return {
             limit: 10,
             freeTag: true,
-            fields: item => {
-                // item is the object from the list from `preprocessResults()`
-                return {
-                    id: item._id,
-                    name: item.name,
-                };
-            },
-            source: async (params, success, failure) => {
-                try {
-                    let restResponse;
-                    if (this.cellbaseClient) {
-                        if (params?.data?.term) {
-                            restResponse = await this.cellbaseClient.get("feature", "gene", params?.data?.term?.toUpperCase(), "startsWith", {
-                                limit: this._config.limit,
-                            }, {});
-                        } else {
-                            restResponse = await this.cellbaseClient.get("feature", "gene", "search", "", {limit: this._config.limit}, {});
-                        }
-                    } else {
-                    // Genes belong to Disease Panels
-                        const query = {
-                            study: this.opencgaSession.study.fqn,
-                            include: "genes",
-                            limit: this._config.limit,
-                            skip: 0
-                        };
-                        if (params?.data?.term) {
-                            restResponse = await this.opencgaSession.opencgaClient.panels().search({...query, genes: `~/^${params?.data?.term}/i`});
-                        } else {
-                            restResponse = await this.opencgaSession.opencgaClient.panels().search(query);
-                        }
-                    }
-                    success(restResponse);
-                } catch (e) {
-                    failure(e);
-                }
-            },
-            /* remap results coming from opencga. config.fields fn works for the dropdown, at a different stage. */
-            preprocessResults: results => {
-                if (this.cellbaseClient) {
-                    return results.map(s => ({
-                        id: s.name, // force selected gene (token) to be the name not the id.
-                        name: s.name,
-                        _id: s.id,
-                    }));
-                }
-                return results.flatMap(s => s.genes.map(gen => {
-                    return {
-                        id: gen.name,
-                        name: gen.name,
-                        _id: gen.id,
-                    };
-                })
-                );
-            }
+            placeholder: "Start typing",
         };
-    }
-
-    render() {
-        if (this.cellbaseAvailable) {
-            return html`
-                <select-token-filter
-                    .config=${this._config}
-                    .value="${this.value}"
-                    @filterChange="${e => this.onFilterChange(e.detail.value)}">
-                </select-token-filter>
-            `;
-        } else {
-            return html`
-                <text-field-filter
-                        .value="${this.value}"
-                        .config=${this._config}
-                        @filterChange="${e => this.onFilterChange(e.detail.value)}">
-                </text-field-filter>
-            `;
-        }
     }
 
 }
