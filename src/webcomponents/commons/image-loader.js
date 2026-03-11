9769c905b8b70a1e402e37cda2063ab61a21e24d
Josemi <josemijuanes@gmail.com>
2026-03-11 10:56:49 +0100

    wc: increment file size upload to 2mb in image-loader #TASK-8076

--- src/webcomponents/commons/image-loader.js
+++ src/webcomponents/commons/image-loader.js
@@ -130,8 +130,8 @@ export default class ImageLoader extends LitElement {
     getDefaultConfig() {
         return {
             title: "Drag and Drop or Click to upload an image",
-            description: "Select JPEG or PNG images to upload, up to 1MB.",
-            maxFileSize: 1 * 1024 * 1024, // 1MB
+            description: "Select JPEG or PNG images to upload, up to 2MB.",
+            maxFileSize: 2 * 1024 * 1024, // 2MB
         };
     }
 
